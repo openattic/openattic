a3f9333e275d0951ef1b7775d4e261c048813366
root <root@srvknecht01>
2011-02-03 11:57:44 +0100

    use absolute paths and sudo if required

--- lvm/procutils.py
+++ lvm/procutils.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # kate: space-indent on; indent-width 4; replace-tabs on;
 
-import sys
+import os, sys
 import subprocess
 from signal import signal, SIGTERM, SIGINT, SIG_DFL
 
@@ -26,7 +26,14 @@ def invoke(args):
 
 
 def lvm_command(cmd):
-    proc = subprocess.Popen([cmd, "--noheadings", "--nameprefixes", "--unquoted"],
+    if os.getuid() == 0:
+        args = []
+    else:
+        args = ["sudo"]
+
+    args.extend([cmd, "--noheadings", "--nameprefixes", "--unquoted"])
+
+    proc = subprocess.Popen(args,
         stdin = None, stdout = subprocess.PIPE, stderr = subprocess.PIPE, close_fds = True
         )
     out, err = proc.communicate()
@@ -40,8 +47,8 @@ def lvm_command(cmd):
         ]
 
 def lvm_vgs():
-    return dict( [ (lv["LVM2_VG_NAME"], lv) for lv in lvm_command("vgs") ] )
+    return dict( [ (lv["LVM2_VG_NAME"], lv) for lv in lvm_command("/sbin/vgs") ] )
 
 def lvm_lvs():
-    return dict( [ (lv["LVM2_LV_NAME"], lv) for lv in lvm_command("lvs") ] )
+    return dict( [ (lv["LVM2_LV_NAME"], lv) for lv in lvm_command("/sbin/lvs") ] )
 
