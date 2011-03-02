622b7961ca5b22aa999b3498686e4c83b8f2a762
Michael Ziegler <michael.ziegler@it-novum.com>
2011-03-02 17:03:34 +0100

    add DrbdDeviceHandler for the API

---
+++ drbd/api.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# kate: space-indent on; indent-width 4; replace-tabs on;
+
+from piston.handler import BaseHandler
+from drbd.models import DrbdDevice
+
+class DrbdDeviceHandler(BaseHandler):
+    allowed_methods = ('GET',)
+    model = DrbdDevice
+    exclude = ()
+
+    @staticmethod
+    def resource_uri():
+        return ('api_drbd_device_handler', ['id'])
+
+api_handlers = [
+    ( (r'drbd/devs/(?P<id>\w+)/', r'drbd/devs/'), DrbdDeviceHandler, 'api_drbd_device_handler' ),
+    ]
