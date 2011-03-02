e045354756e92f9e95722a89ee125bae6fb8ea3e
Michael Ziegler <michael.ziegler@it-novum.com>
2011-03-02 17:03:02 +0100

    allow stuff like domain and workgroup to be configured and find out hostname automatically

--- samba/installer.py
+++ samba/installer.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 # kate: space-indent on; indent-width 4; replace-tabs on;
 
-from django.db.models import Q
+import socket
 
+from django.db.models import Q
 from django.template.loader import render_to_string
 
 from lvm.procutils import invoke
@@ -12,7 +13,10 @@ from samba.conf    import settings as samba_settings
 def writeconf():
     fd = open( samba_settings.SMB_CONF, "w" )
     fd.write( render_to_string( "samba/smb.conf", {
-        'Shares': Share.objects.filter(state__in=("new", "update", "active")).exclude(volume__state="update")
+        'Hostname':  socket.gethostname(),
+        'Domain':    samba_settings.DOMAIN,
+        'Workgroup': samba_settings.WORKGROUP,
+        'Shares':    Share.objects.filter(state__in=("new", "update", "active")).exclude(volume__state="update")
         } ) )
     fd.close()
 
