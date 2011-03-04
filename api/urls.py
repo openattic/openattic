41bda873914da308c55557bc6b31c77d7525d441
Michael Ziegler <michael.ziegler@it-novum.com>
2011-03-04 15:25:37 +0100

    implement oauth

--- api/urls.py
+++ api/urls.py
@@ -6,10 +6,10 @@ from django.conf import settings
 
 from piston.resource import Resource
 from piston.handler import BaseHandler, AnonymousBaseHandler
-from piston.authentication import HttpBasicAuthentication
+from piston.authentication import OAuthAuthentication
 from piston.doc import documentation_view
 
-auth = HttpBasicAuthentication(realm="pyFiler API")
+auth = OAuthAuthentication(realm="openATTIC API")
 
 urlpatterns = []
 
@@ -26,9 +26,9 @@ for app in settings.INSTALLED_APPS:
             else:
                 pattern, instance = handler
                 urlname = None
-            
+
             resource = Resource(handler=instance, authentication=auth)
-            
+
             if isinstance( pattern, (tuple, list) ):
                 for pat in pattern:
                     urlpatterns.append(url(pat, resource, name=urlname))
@@ -36,4 +36,9 @@ for app in settings.INSTALLED_APPS:
             else:
                 urlpatterns.append(url(pattern, resource, name=urlname))
 
-urlpatterns.append(url(r'$', documentation_view))
+urlpatterns.extend([
+    url(r'^oauth/request_token/$', 'piston.authentication.oauth_request_token'),
+    url(r'^oauth/access_token/$',  'piston.authentication.oauth_access_token'),
+    url(r'^oauth/authorize/$',     'piston.authentication.oauth_user_auth'),
+    url(r'$', documentation_view)
+    ])
