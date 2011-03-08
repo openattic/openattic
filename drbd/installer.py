8d97e9f874c8bd773d12db7cb9b89699a98e0696
Michael Ziegler <michael.ziegler@it-novum.com>
2011-03-08 16:29:22 +0100

    allow basic updating of DRBDs

--- drbd/installer.py
+++ drbd/installer.py
@@ -7,19 +7,21 @@ from django.db.models import Q
 from django.template.loader import render_to_string
 
 from lvm.procutils import invoke
-#from drbd.models   import DrbdDevice
+from drbd.models   import DrbdDevice
 #from drbd.conf     import settings as nfs_settings
 
-
-def install_resource(dev):
-    dev.set_pending()
-
+def writeconf(dev):
     fd = open("/etc/drbd.d/%s_%s_%d.res" % (dev.volume.vg.name, dev.volume.name, dev.id), "w")
     fd.write( render_to_string( "drbd/device.res", {
         'Hostname':  socket.gethostname(),
         'Device':    dev
         } ) )
     fd.close()
+
+
+def install_resource(dev):
+    dev.set_pending()
+    writeconf(dev)
 
     invoke(["drbdadm", "create-md", dev.res])
     invoke(["drbdadm", "attach",    dev.res])
@@ -29,4 +31,12 @@ def install_resource(dev):
         invoke(["drbdsetup", dev.path, "primary", "--overwrite-data-of-peer"])
 
     dev.set_active()
+
+
+def postinst(options, args):
+    for dev in DrbdDevice.objects.filter(state="update"):
+        dev.set_pending()
+        writeconf(dev)
+        invoke(["drbdadm", "adjust", dev.res])
+        dev.set_active()
 
