52e85b43257a086f3b646505094bf2eebac92063
Michael Ziegler <michael.ziegler@it-novum.com>
2011-07-05 11:21:46 +0200

    set FORCE_SCRIPT_NAME for gunicorn/nginx combination

--- settings.py
+++ settings.py
@@ -3,6 +3,9 @@
 
 PROJECT_ROOT = None
 PROJECT_URL  = '/filer'
+# das folgende wird gebraucht weil gunicorn SCRIPT_URL und PATH_INFO nicht setzt
+# fyi: SCRIPT_URL=/filer/lvm/ PATH_INFO=/lvm/ würde klären
+FORCE_SCRIPT_NAME = "/filer"
 
 from os.path import join, dirname, abspath, exists
 if not PROJECT_ROOT or not exists( PROJECT_ROOT ):
