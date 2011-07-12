c3fb3ee2be04757cc2ce8a011cf8412c129bb2dc
Michael Ziegler <michael.ziegler@it-novum.com>
2011-07-12 11:13:33 +0200

    add certfile and keyfile options and the possibility to run as www-data

--- initscript_rpcd.sh
+++ initscript_rpcd.sh
@@ -15,11 +15,14 @@ set -e
 set -u
 
 PIDFILE="/var/run/openattic_rpcd.pid"
+CHUID="www-data:www-data"
 LOGFILE="/var/log/openattic_rpcd"
 LOGLEVEL="DEBUG"
 PYTHON="/usr/bin/python"
 OADIR="/srv/pyfiler"
 OPTIONS="$OADIR/manage.py runrpcd"
+CERTFILE="$OADIR/certs/ssl-cert-snakeoil.pem"
+KEYFILE="$OADIR/certs/ssl-cert-snakeoil.key"
 
 if [ $# -lt 1 ]
 then
@@ -32,8 +35,11 @@ fi
 case $1 in
 	start)
 		log_daemon_msg "Starting" "openATTIC rpcd"
+		if [ ! -z "$CERTFILE" -a ! -z "$KEYFILE" ]; then
+			OPTIONS="$OPTIONS -c $CERTFILE -k $KEYFILE"
+		fi
 		start-stop-daemon --pidfile=$PIDFILE --make-pidfile --background --oknodo --start \
-			--exec $PYTHON --chdir $OADIR -- $OPTIONS -l $LOGFILE -L $LOGLEVEL -q
+			--exec $PYTHON --chdir $OADIR --chuid $CHUID -- $OPTIONS -l $LOGFILE -L $LOGLEVEL -q
 		log_end_msg 0
 		;;
 	
