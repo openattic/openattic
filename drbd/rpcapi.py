c40027a7f16ae54e9989bdec159cb10b9ac22257
Michael Ziegler <michael.ziegler@it-novum.com>
2011-07-12 15:34:18 +0200

    export path and basedev variables

--- drbd/rpcapi.py
+++ drbd/rpcapi.py
@@ -8,4 +8,9 @@ from drbd.models import DrbdDevice
 class DrbdDeviceHandler(BaseHandler):
     model = DrbdDevice
 
+    def _override_get(self, obj, data):
+        data['path']    = obj.path
+        data['basedev'] = obj.basedev
+        return data
+
 RPCD_HANDLERS = [DrbdDeviceHandler]
