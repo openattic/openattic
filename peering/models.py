6696e621a4b13f2dbc6c8becd0591e0a69cabc04
Michael Ziegler <michael.ziegler@it-novum.com>
2011-07-12 14:01:12 +0200

    use xmlrpc instead of the old api

--- peering/models.py
+++ peering/models.py
@@ -1,12 +1,7 @@
 # -*- coding: utf-8 -*-
 # kate: space-indent on; indent-width 4; replace-tabs on;
 
-import base64
-import httplib
-import json
-import socket
-from urlparse import urlparse
-
+from xmlrpclib import ServerProxy
 from django.db import models
 
 class PeerHost(models.Model):
@@ -25,32 +20,8 @@ class PeerHost(models.Model):
     @property
     def connection(self):
         if self._connection is None:
-            urlinfo = urlparse(self.base_url)
-            server = urlinfo.hostname
-            port   = urlinfo.port or socket.getservbyname(urlinfo.scheme)
-            if urlinfo.scheme == "http":
-                self._connection = httplib.HTTPConnection("%s:%d" % (server, port))
-            else:
-                self._connection = httplib.HTTPSConnection("%s:%d" % (server, port))
+            self._connection = ServerProxy(self.base_url, allow_none=True)
         return self._connection
 
-    def get(self, url, body=None, headers={}):
-        if not url.lower().startswith("http"):
-            url = self.base_url + url
-        urlinfo = urlparse(url)
-        headers.update({ 'Authorization': "Basic " + base64.encodestring(
-            "%s:%s" % (self.username, self.password)
-            ) })
-        try:
-            self.connection.request("GET", urlinfo.path, body=body, headers=headers)
-        except httplib.CannotSendRequest:
-            # retry once with a new connection, in case the old one died in the meantime
-            self._connection = None
-            self.connection.request("GET", urlinfo.path, body=body, headers=headers)
-        response = self.connection.getresponse()
-        res = response.read()
-        #print "RESPONSE", res
-        return res
-
-    def getjson(self, url, body=None, headers={}):
-        return json.loads( self.get( url, body, headers ) )
+    def __getattr__(self, attr):
+        return getattr( self.connection, attr)
