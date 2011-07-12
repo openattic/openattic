0fcebda19debe47783ec4098e37df993089c2d1a
Michael Ziegler <michael.ziegler@it-novum.com>
2011-07-12 11:42:13 +0200

    set console loglevel according to -v

--- lvm/management/commands/systemd.py
+++ lvm/management/commands/systemd.py
@@ -91,7 +91,7 @@ class Command( BaseCommand ):
 
         if not options['quiet']:
             logch = logging.StreamHandler()
-            logch.setLevel(logging.DEBUG)
+            logch.setLevel({2: logging.DEBUG, 1: logging.INFO, 0: logging.WARNING}[options['verbosity']])
             logch.setFormatter( logging.Formatter('%(asctime)s - %(levelname)s - %(message)s') )
             rootlogger.addHandler(logch)
 
