0147afa10524468d533f75028400cce1e7413812
Michael Ziegler <michael.ziegler@it-novum.com>
2011-07-13 20:52:02 +0200

    add mount/unmount/is_mounted methods

--- lvm/rpcapi.py
+++ lvm/rpcapi.py
@@ -23,4 +23,45 @@ class LvHandler(BaseHandler):
         data['lvm_info'] = obj.lvm_info
         return data
 
+    def mount_all(self):
+        """ Mount all volumes which are not currently mounted. """
+        ret = []
+        for lv in LogicalVolume.objects.all():
+            if lv.filesystem and not lv.fs.mounted:
+                succ = lv.fs.mount()
+                lvid = self._idobj(lv)
+                lvid["success"] = succ
+                ret.append(lvid)
+        return ret
+
+    def mount(self, id):
+        """ Mount the given volume if it is not currently mounted. """
+        lv = LogicalVolume.objects.get(id=id)
+        if lv.filesystem and not lv.fs.mounted:
+            return lv.fs.mount()
+        return False
+
+    def unmount_all(self):
+        """ Unmount all volumes which are currently mounted. """
+        ret = []
+        for lv in LogicalVolume.objects.all():
+            if lv.filesystem and lv.fs.mounted:
+                succ = lv.fs.unmount()
+                lvid = self._idobj(lv)
+                lvid["success"] = succ
+                ret.append(lvid)
+        return ret
+
+    def unmount(self, id):
+        """ Unmount the given volume if it is currently mounted. """
+        lv = LogicalVolume.objects.get(id=id)
+        if lv.filesystem and lv.fs.mounted:
+            return lv.fs.unmount()
+        return False
+
+    def is_mounted(self, id):
+        """ Check if the given volume is currently mounted. """
+        lv = LogicalVolume.objects.get(id=id)
+        return lv.filesystem and lv.fs.mounted
+
 RPCD_HANDLERS = [VgHandler, LvHandler]
