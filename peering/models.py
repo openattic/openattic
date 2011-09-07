b2433e56c75689e377f3d41d0e116ed5508c4cb1
Michael Ziegler <michael.ziegler@it-novum.com>
2011-09-07 11:22:06 +0200

    add clusterpeer field, remove unused username/password fields, and add a few urlparsing properties

--- peering/models.py
+++ peering/models.py
@@ -3,21 +3,53 @@
 
 import socket
 
+from urlparse  import urlparse
 from xmlrpclib import ServerProxy
 from django.db import models
 
 class PeerHost(models.Model):
     name         = models.CharField(max_length=250)
     base_url     = models.CharField(max_length=250)
-    username     = models.CharField(max_length=50)
-    password     = models.CharField(max_length=50)
+    clusterpeer  = models.BooleanField(default=False,
+                       help_text="Set to true if I am in a Pacemaker cluster with this peer.")
 
     def __init__( self, *args, **kwargs ):
         models.Model.__init__( self, *args, **kwargs )
         self._connection = None
+        self._parsed_url = None
 
     def __unicode__(self):
         return self.name
+
+    @property
+    def parsed_url(self):
+        if self._parsed_url is None and self.base_url:
+            self._parsed_url = urlparse(self.base_url)
+        return self._parsed_url
+
+    @property
+    def username(self):
+        if not self.base_url:
+            return None
+        return self.parsed_url.username
+
+    @property
+    def password(self):
+        if not self.base_url:
+            return None
+        return self.parsed_url.password
+
+    @property
+    def hostname(self):
+        if not self.base_url:
+            return None
+        return self.parsed_url.hostname
+
+    @property
+    def port(self):
+        if not self.base_url:
+            return None
+        return self.parsed_url.port
 
     @property
     def connection(self):
