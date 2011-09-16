480467a84a97586fb85a146e93cb760d30505de9
Michael Ziegler <michael.ziegler@it-novum.com>
2011-09-16 14:13:50 +0200

    add RPC api to query general host statistics

---
+++ hoststats/rpcapi.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# kate: space-indent on; indent-width 4; replace-tabs on;
+
+import statgrab
+
+class HostStatsHandler(object):
+    class model:
+        class _meta:
+            app_label = "hoststats"
+            object_name = "HostStats"
+
+    def get_host_info(self):
+        """ Return some general information about this host. """
+        return statgrab.sg_get_host_info().attrs
+
+    def get_load(self):
+        """ Return system load statistics. """
+        return statgrab.sg_get_load_stats().attrs
+
+    def get_uptime(self):
+        """ Return the uptime in seconds. """
+        return statgrab.sg_get_host_info().uptime
+
+    def get_cpu(self):
+        return statgrab.sg_get_cpu_percents().attrs
+
+    def get_mem(self):
+        stat = statgrab.sg_get_mem_stats()
+        return { 'used': (stat.used - stat.cache), 'cache': stat.cache, 'free': stat.free}
+
+RPCD_HANDLERS = [HostStatsHandler]
