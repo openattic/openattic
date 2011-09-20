6bc834b4d92f880078a4530c7b0657e970db2185
Michael Ziegler <michael.ziegler@it-novum.com>
2011-09-20 17:18:27 +0200

    only show a range of commands, order properly, display end date, use cool new icon

--- htdocs/js/cmdlog__logentry.js
+++ htdocs/js/cmdlog__logentry.js
@@ -3,36 +3,56 @@ Ext.namespace("Ext.oa");
 Ext.oa.Cmdlog__LogEntry_Panel = Ext.extend(Ext.grid.GridPanel, {
   initComponent: function(){
     var currentChartId = null;
+    var fields = ['command', 'exitcode', 'endtime'];
+    var store = new Ext.data.DirectStore({
+      autoLoad: true,
+      remoteSort: true,
+      fields: fields,
+      directFn: cmdlog__LogEntry.range_values,
+      baseParams: {
+        'fields': fields,
+        'start': 0, 'limit': 100,
+        'sort': 'endtime', 'dir': 'DESC'
+      },
+      reader: new Ext.data.JsonReader({
+        root:           'objects',
+        totalProperty:  'total',
+        fields:         fields,
+      }),
+      paramOrder: ['start', 'limit', 'sort', 'dir', 'fields']
+    });
     Ext.apply(this, Ext.apply(this.initialConfig, {
       viewConfig: { forceFit: true },
       title: "Log Entries",
-      store: new Ext.data.DirectStore({
-          autoLoad: true,
-          fields: ['command', 'exitcode', 'text'],
-          directFn: cmdlog__LogEntry.all_values,
-          baseParams: {'fields': ['command', 'exitcode']}
-      }),
+      store: store,
       colModel: new Ext.grid.ColumnModel({
         defaults: {
           sortable: true,
         },
         columns: [{
+            header: 'Zeitpunkt (Ende)',
+            dataIndex: 'endtime'
+          }, {
             header: 'Befehl',
             dataIndex: 'command'
           }, {
             header: "Exit-Status",
             dataIndex: 'exitcode'
         }]
+      }),
+      bbar: new Ext.PagingToolbar({
+        pageSize: 100,
+        store:    store,
       })
     }));
     Ext.oa.Cmdlog__LogEntry_Panel.superclass.initComponent.apply(this, arguments);
   },
 
   prepareMenuTree: function(tree){
-    tree.root.attributes.children[5].children.push({
+    tree.root.attributes.children[0].children.push({
       text: 'Command Log',
       leaf: true,
-      icon: '/filer/static/icons2/22x22/mimetypes/x-office-spreadsheet.png',
+      icon: '/filer/static/icons2/22x22/actions/bookmark-new.png',
       panel: this,
       href: '#',
     });
