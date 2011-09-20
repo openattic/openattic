10df5b93a77da1495ccaf0c9b870cabec1a509d2
Michael Ziegler <michael.ziegler@it-novum.com>
2011-09-20 12:39:39 +0200

    move master panel to its own object

--- htdocs/js/mainviewmanager.js
+++ htdocs/js/mainviewmanager.js
@@ -2,11 +2,29 @@ Ext.namespace("Ext.oa");
 
 Ext.oa.MainViewManager = Ext.extend(Ext.Panel, {
   initComponent: function(){
-    var currentChartId = null;
+    var modules = [
+      new Ext.oa.Lvm__LogicalVolume_Panel(),
+      new Ext.oa.Nfs__Export_Panel(),
+      new Ext.oa.Samba__Share_Panel(),
+      new Ext.oa.Http__Export_Panel(),
+      new Ext.oa.Cmdlog__LogEntry_Panel(),
+      new Ext.oa.Munin__MuninNode_Panel()
+    ];
+
     Ext.apply(this, Ext.apply(this.initialConfig, {
-      
+      layout: 'border',
+      items: [ new Ext.oa.MenuTree({
+        region: 'west',
+        collapsible: true,
+      }), {
+        xtype: "tabpanel",
+        region: "center",
+        activeTab: 0,
+        items: modules
+      }],
+      modules: modules
     }));
-    Ext.oa.Lvm__LogicalVolume_Panel.superclass.initComponent.apply(this, arguments);
+    Ext.oa.MainViewManager.superclass.initComponent.apply(this, arguments);
   }
 });
 
