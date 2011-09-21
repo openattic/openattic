c4894f5028f771f0c93968d70b91e20ac6b73ca7
Michael Ziegler <michael.ziegler@it-novum.com>
2011-09-21 17:30:52 +0200

    copy-paste the ftp buttons

--- htdocs/js/http__export.js
+++ htdocs/js/http__export.js
@@ -3,8 +3,104 @@ Ext.namespace("Ext.oa");
 Ext.oa.Http__Export_Panel = Ext.extend(Ext.grid.GridPanel, {
   initComponent: function(){
     var currentChartId = null;
+    var httpGrid = this;
     Ext.apply(this, Ext.apply(this.initialConfig, {
       title: "http",
+      buttons: [{
+        text: "Add Export",
+        icon: "/filer/static/icons2/16x16/actions/add.png",
+        handler: function(){
+          var addwin = new Ext.Window({
+            title: "Add Export",
+            layout: "fit",
+            height: 300,
+            width: 500,
+            items: [{
+              xtype: "form",
+              defaults: {
+                xtype: "textfield",
+                anchor: '-20px',
+              },
+              items: [{
+                  xtype:      'combo',
+                  fieldLabel: 'Volume',
+                  name:       'volume',
+                  hiddenName: 'volume_id',
+                  store: new Ext.data.DirectStore({
+                    fields: ["app", "obj", "id", "name"],
+                    directFn: lvm__LogicalVolume.ids
+                  }),
+                  typeAhead:     true,
+                  triggerAction: 'all',
+                  emptyText:     'Select...',
+                  selectOnFocus: true,
+                  displayField:  'name',
+                  valueField:    'id',
+                  ref: 'volfield',
+                  listeners: {
+                    select: function(self, record, index){
+                      lvm__LogicalVolume.get( record.data.id, function( provider, response ){
+                        if( !response.result.filesystem ){
+                          alert( "This volume does not have a file system, so it cannot be used for HTTP." );
+                          self.ownerCt.dirfield.setValue("");
+                          self.ownerCt.dirfield.disable();
+                          self.expand();
+                        }
+                        else{
+                          self.ownerCt.dirfield.setValue( response.result.fs.mountpoints[0] );
+                          self.ownerCt.dirfield.enable();
+                        }
+                      } );
+                    }
+                  }
+                }, {
+                  fieldLabel: "Directory",
+                  name: "path",
+                  disabled: true,
+                  ref: 'dirfield'
+              }],
+              buttons: [{
+                text: 'Create Export',
+                icon: "/filer/static/icons/accept.png",
+                handler: function(self){
+                  http__Export.new({
+                    'volume': {
+                      'app': 'lvm',
+                      'obj': 'LogicalVolume',
+                      'id': self.ownerCt.ownerCt.volfield.getValue()
+                    },
+                    'path':  self.ownerCt.ownerCt.dirfield.getValue(),
+                  }, function(provider, response){
+                    if( response.result ){
+                      httpGrid.store.reload();
+                      addwin.hide();
+                    }
+                  });
+                }
+              }, {
+                text: 'Cancel',
+                icon: "/filer/static/icons2/16x16/actions/gtk-cancel.png",
+                handler: function(self){
+                  addwin.hide();
+                }
+              }]
+            }]
+          });
+          addwin.show();
+        }
+      }, {
+        text: "Delete Export",
+        icon: "/filer/static/icons2/16x16/actions/remove.png",
+        handler: function(self){
+          var sm = httpGrid.getSelectionModel();
+          if( sm.hasSelection() ){
+            var sel = sm.selections.items[0];
+            http__Export.delete( sel.data.id, function(provider, response){
+              httpGrid.store.reload();
+            } );
+          }
+        }
+      }],
       store: new Ext.data.DirectStore({
         autoLoad: true,
         fields: ['path', 'state', 'id', 'volume', {
