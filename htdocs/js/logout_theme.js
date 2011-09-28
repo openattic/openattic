d3381fcf186d2248e72f040a4740472d67b66a12
Michael Ziegler <michael.ziegler@it-novum.com>
2011-09-28 12:40:09 +0200

    add icon, switch logout/theme places

--- htdocs/js/logout_theme.js
+++ htdocs/js/logout_theme.js
@@ -1,33 +1,9 @@
 window.MainViewModules.push({
   prepareMenuTree: function(tree){
     tree.root.attributes.children[5].children.push({
-      text: 'Logout',
-      leaf: true,
-      icon: MEDIA_URL + '/oxygen/22x22/actions/system-log-out.png',
-      listeners: {
-        click: function(self, ev){
-          var conn = new Ext.data.Connection();
-          conn.request({
-            url: LOGOUT_URL,
-            success: function( response, options ){
-              var redirect = function(){
-                location.href = INDEX_URL;
-              };
-              window.setTimeout( redirect, 20000 );
-              Ext.Msg.show({
-                title:   'Logout',
-                msg:     'Sie haben sich erfolgreich ausgeloggt!',
-                buttons: Ext.MessageBox.OK,
-                fn:      redirect
-              });
-            }
-          });
-        }
-      }
-    });
-    tree.root.attributes.children[5].children.push({
       text: 'Theme',
       leaf: true,
+      icon: MEDIA_URL + '/oxygen/22x22/apps/preferences-desktop-theme.png',
       listeners: {
         click: function(self, ev){
           Ext.Msg.show({
@@ -47,6 +23,31 @@ window.MainViewModules.push({
                 Ext.state.Manager.clear( "theme" );
                 window.location.reload.defer(200);
               }
+            }
+          });
+        }
+      }
+    });
+    tree.root.attributes.children[5].children.push({
+      text: 'Logout',
+      leaf: true,
+      icon: MEDIA_URL + '/oxygen/22x22/actions/system-log-out.png',
+      listeners: {
+        click: function(self, ev){
+          var conn = new Ext.data.Connection();
+          conn.request({
+            url: LOGOUT_URL,
+            success: function( response, options ){
+              var redirect = function(){
+                location.href = INDEX_URL;
+              };
+              window.setTimeout( redirect, 20000 );
+              Ext.Msg.show({
+                title:   'Logout',
+                msg:     'Sie haben sich erfolgreich ausgeloggt!',
+                buttons: Ext.MessageBox.OK,
+                fn:      redirect
+              });
             }
           });
         }
