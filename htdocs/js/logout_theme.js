25c6f4dd208218eca4c084126304da7e8f313a3e
Kai Wagner <kai.wagner@it-novum.com>
2011-10-10 14:31:36 +0200

    shutdown redbox and logout message box

--- htdocs/js/logout_theme.js
+++ htdocs/js/logout_theme.js
@@ -34,22 +34,30 @@ window.MainViewModules.push({
       icon: MEDIA_URL + '/oxygen/22x22/actions/system-log-out.png',
       listeners: {
         click: function(self, ev){
-          var conn = new Ext.data.Connection();
-          conn.request({
-            url: LOGOUT_URL,
-            success: function( response, options ){
-              var redirect = function(){
-                location.href = INDEX_URL;
-              };
-              window.setTimeout( redirect, 20000 );
-              Ext.Msg.show({
-                title:   'Logout',
-                msg:     'Sie haben sich erfolgreich ausgeloggt!',
-                buttons: Ext.MessageBox.OK,
-                fn:      redirect
-              });
+            Ext.Msg.confirm(
+                'Logout',
+                'Do you really want to logout?',
+                function(btn, text){
+                    if(btn == 'yes'){
+                        var conn = new Ext.data.Connection();
+                        conn.request({
+                        url: LOGOUT_URL,
+                        success: function( response, options ){
+                        var redirect = function(){
+                            location.href = INDEX_URL;
+                        };
+                        window.setTimeout( redirect, 20000 );
+                        Ext.Msg.show({
+                            title:   'Logout',
+                            msg:     'Successfully logged out',
+                            buttons: Ext.MessageBox.OK,
+                        fn:      redirect
+                        });
+                    }
+                });
+              }
             }
-          });
+          );
         }
       }
     });
--- sysutils/templates/sysutils/sysutils.js
+++ sysutils/templates/sysutils/sysutils.js
@@ -2,22 +2,26 @@
 Ext.namespace("Ext.oa");
 
 
-Ext.oa.DangerousMessage = function(){
+Ext.oa.DangerousMessage = function(color){
   // http://www.sencha.com/forum/showthread.php?7613-Ext.MessageBox-extend-class&p=46792&viewfull=1#post46792
   var f = function(){};
   f.prototype = Ext.MessageBox;
   var o = Ext.extend(f, {
     getDialog: function() {
       var d = o.superclass.getDialog.apply(this, arguments);
-      d.mask.addClass("redmask");
+      d.mask.addClass(color);
       d.on("hide", function(){
-        d.mask.removeClass("redmask");
+        d.mask.removeClass(color);
       });
       return d;
     }
   });
   return new o();
-}();
+};
+Ext.oa.RedDangerousMessage = Ext.oa.DangerousMessage("redmask");
+
+Ext.oa.YellowDangerousMessage = Ext.oa.DangerousMessage("yellowmask");
+
 
 
 Ext.oa.SysUtils_Panel = Ext.extend(Ext.grid.GridPanel, {
@@ -73,13 +77,13 @@ Ext.oa.SysUtils_Panel = Ext.extend(Ext.grid.GridPanel, {
       icon: '{{ MEDIA_URL }}/oxygen/22x22/actions/system-reboot.png',
       listeners: {
         click: function(self, ev){
-          Ext.oa.DangerousMessage.confirm(
+          Ext.oa.YellowDangerousMessage.confirm(
             "Reboot",
             "Do you really want to reboot openATTIC?",
             function(btn, text){
               if( btn == 'yes' ){
                 sysutils__System.reboot( function(provider, response){
-                  Ext.oa.DangerousMessage.alert("Rebooting", "The system is rebooting.");
+                  Ext.oa.YellowDangerousMessage.alert("Rebooting", "The system is rebooting.");
                 } );
               }
             } );
@@ -93,13 +97,13 @@ Ext.oa.SysUtils_Panel = Ext.extend(Ext.grid.GridPanel, {
       icon: '{{ MEDIA_URL }}/oxygen/22x22/actions/system-shutdown.png',
       listeners: {
         click: function(self, ev){
-          Ext.oa.DangerousMessage.confirm(
+          Ext.oa.RedDangerousMessage.confirm(
             "Shutdown",
             "Do you really want to shutdown openATTIC?",
             function(btn, text){
               if( btn == 'yes' ){
                 sysutils__System.shutdown( function(provider, response){
-                  Ext.oa.DangerousMessage.alert("Shutting down", "The system is shutting down.");
+                  Ext.oa.RedDangerousMessage.alert("Shutting down", "The system is shutting down.");
                 } );
               }
             });
