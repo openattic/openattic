da1eab247f56760b1460a6abb83eb0bed0ffd8e0
Michael Ziegler <michael.ziegler@it-novum.com>
2011-10-12 15:38:47 +0200

    add set_time method

--- sysutils/systemapi.py
+++ sysutils/systemapi.py
@@ -2,6 +2,7 @@
 # kate: space-indent on; indent-width 4; replace-tabs on;
 
 import os.path
+import ctypes
 
 from systemd import invoke, logged, BasePlugin, method
 
@@ -23,3 +24,16 @@ class SystemD(BasePlugin):
         if not os.path.exists(spath):
             raise ValueError("No such file or directory: '%s'" % spath)
         return invoke([spath, command], log=( command != "status" ))
+
+    @method(in_signature="i", out_signature="i")
+    def set_time(self, seconds):
+        librt = ctypes.cdll.LoadLibrary("librt.so")
+
+        class timespec(ctypes.Structure):
+            _fields_ = [
+                ('tv_sec', ctypes.c_long),
+                ('tv_nsec', ctypes.c_long)]
+
+        timeobj = timespec(int(seconds), 0)
+        return librt.clock_settime( 0, ctypes.pointer(timeobj) )
+
