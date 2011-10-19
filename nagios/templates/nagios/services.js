3ff136d25c1d9c4ab901122c50549639b2d015ca
Michael Ziegler <michael.ziegler@it-novum.com>
2011-10-19 17:48:20 +0200

    properly implement the nagios status page

--- nagios/templates/nagios/services.js
+++ nagios/templates/nagios/services.js
@@ -2,7 +2,55 @@
 
 Ext.namespace("Ext.oa");
 
-Ext.oa.Nagios__Service_Panel = Ext.extend(Ext.grid.GridPanel, {
+
+Ext.oa.Nagios__Graph_ImagePanel = Ext.extend(Ext.Panel, {
+  initComponent: function(){
+    Ext.apply(this, Ext.apply(this.initialConfig, {
+      layout: "vbox",
+      layoutConfig: { "align": "center" },
+      items: new Ext.BoxComponent({
+        autoEl: {
+          tag: "img",
+          src: MEDIA_URL + "/extjs/resources/images/default/s.gif"
+        },
+        listeners: {
+          render: function(self){
+            self.el.on("load", function(ev, target, options){
+              this.ownerCt.doLayout();
+              this.ownerCt.el.unmask();
+            }, self);
+          }
+        }
+      }),
+    }));
+    Ext.oa.Nagios__Graph_ImagePanel.superclass.initComponent.apply(this, arguments);
+    this.on( "afterrender", function(){
+      // Wait until the <img> element has actually been created, then reload the record
+      this.items.items[0].on( "afterrender", function(){
+        if( this.currentRecord ){
+          this.loadRecord( this.currentRecord, this.currentId );
+        }
+      }, this );
+    }, this );
+  },
+
+  loadRecord: function(record, id){
+    this.currentRecord = record;
+    this.currentId = id;
+    if( this.el ){
+      this.el.mask("Loading...");
+      this.items.items[0].el.dom.src = String.format(
+        PROJECT_URL + "/nagios/{0}/{1}.png?start={2}",
+        record.data.id, id, (new Date().format("U") - this.timespan)
+      );
+    }
+  }
+});
+
+Ext.reg("naggraphimage", Ext.oa.Nagios__Graph_ImagePanel);
+
+
+Ext.oa.Nagios__Service_Panel = Ext.extend(Ext.Panel, {
   initComponent: function(){
     var nagiosGrid = this;
     var renderDate = function(val, x, store){
@@ -19,7 +67,7 @@ Ext.oa.Nagios__Service_Panel = Ext.extend(Ext.grid.GridPanel, {
 
     Ext.apply(this, Ext.apply(this.initialConfig, {
       title: "{% trans 'Nagios Services' %}",
-      viewConfig: { forceFit: true },
+      layout: "border",
       buttons: [
         {
           text: "",
@@ -30,80 +78,130 @@ Ext.oa.Nagios__Service_Panel = Ext.extend(Ext.grid.GridPanel, {
           }
         }
       ],
-      store: new Ext.data.DirectStore({
-        autoLoad: true,
-        fields: ['id', 'description', {
-          name: "volumename",    mapping: "volume", convert: function(val, row){ return val.name; }
+      items: [ {
+        xtype: 'grid',
+        region: "center",
+        viewConfig: { forceFit: true },
+        store: new Ext.data.DirectStore({
+          autoLoad: true,
+          fields: ['id', 'description', {
+            name: "volumename",    mapping: "volume", convert: function(val, row){ return val.name; }
+          }, {
+            name: "plugin_output", mapping: "state",  convert: function(val, row){ return val.plugin_output; }
+          }, {
+            name: "current_state", mapping: "state",  convert: function(val, row){ return val.current_state; }
+          }, {
+            name: "last_check",    mapping: "state",  convert: function(val, row){ return val.last_check; }
+          }, {
+            name: "next_check",    mapping: "state",  convert: function(val, row){ return val.next_check; }
+          }],
+          directFn: nagios__Service.all
+        }),
+        colModel: new Ext.grid.ColumnModel({
+          defaults: {
+            sortable: true
+          },
+          columns: [{
+            header: "{% trans 'Service Description' %}",
+            width: 200,
+            dataIndex: "description",
+            renderer: renderDesc
+          }, {
+            header: "{% trans 'Volume' %}",
+            width: 100,
+            dataIndex: "volumename"
+          }, {
+            header: "{% trans 'Last Check' %}",
+            width: 120,
+            dataIndex: "last_check",
+            renderer: renderDate
+          }, {
+            header: "{% trans 'Next Check' %}",
+            width: 120,
+            dataIndex: "next_check",
+            renderer: renderDate
+          }]
+        }),
+        listeners: {
+          cellmousedown: function( self, rowIndex, colIndex, evt ){
+            var record = self.getStore().getAt(rowIndex);
+            if( record.json.graphs.length === 0 )
+              return;
+            self.ownerCt.items.items[1].loadRecord(record);
+          }
+        }
+      }, {
+        region: "south",
+        layout: "border",
+        height: 300,
+        items: [{
+          xtype: "tabpanel",
+          region: "center",
+          activeTab: 0,
+          items: [{
+            xtype: "naggraphimage",
+            title: "4 hours",
+            timespan: 4*60*60
+          }, {
+            xtype: "naggraphimage",
+            title: "1 day",
+            timespan: 24*60*60
+          }, {
+            xtype: "naggraphimage",
+            title: "1 week",
+            timespan: 7*24*60*60
+          }, {
+            xtype: "naggraphimage",
+            title: "1 month",
+            timespan: 30*24*60*60
+          }, {
+            xtype: "naggraphimage",
+            title: "1 year",
+            timespan: 365*24*60*60
+          }, {
+            html:  "coming soon",
+            title: "Plugin output",
+            loadRecord: function( record ){
+            }
+          }],
+          loadRecord: function( record, id ){
+            this.items.each( function(item){
+              item.loadRecord( record, id );
+            } );
+          }
         }, {
-          name: "plugin_output", mapping: "state",  convert: function(val, row){ return val.plugin_output; }
-        }, {
-          name: "current_state", mapping: "state",  convert: function(val, row){ return val.current_state; }
-        }, {
-          name: "last_check",    mapping: "state",  convert: function(val, row){ return val.last_check; }
-        }, {
-          name: "next_check",    mapping: "state",  convert: function(val, row){ return val.next_check; }
+          region: "west",
+          title: "Graphs",
+          xtype:  'grid',
+          width: 160,
+          viewConfig: { forceFit: true },
+          store: new Ext.data.JsonStore({
+            fields: ["id", "title"],
+          }),
+          colModel: new Ext.grid.ColumnModel({
+            columns: [{
+              header: "{% trans 'Graph' %}",
+              dataIndex: "title"
+            } ]
+          }),
+          listeners: {
+            cellclick: function( self, rowIndex, colIndex, evt ){
+              var record = self.getStore().getAt(rowIndex);
+              self.ownerCt.items.items[0].loadRecord(self.currentRecord, record.data.id);
+            }
+          },
+          loadRecord: function( record ){
+            this.currentRecord = record;
+            this.store.loadData( record.json.graphs );
+            this.ownerCt.items.items[0].loadRecord(record, record.json.graphs[0].id);
+          }
         }],
-        directFn: nagios__Service.all
-      }),
-      colModel: new Ext.grid.ColumnModel({
-        defaults: {
-          sortable: true
-        },
-        columns: [{
-          header: "{% trans 'Service Description' %}",
-          width: 200,
-          dataIndex: "description",
-          renderer: renderDesc
-        }, {
-          header: "{% trans 'Volume' %}",
-          width: 100,
-          dataIndex: "volumename"
-        }, {
-          header: "{% trans 'Last Check' %}",
-          width: 120,
-          dataIndex: "last_check",
-          renderer: renderDate
-        }, {
-          header: "{% trans 'Next Check' %}",
-          width: 120,
-          dataIndex: "next_check",
-          renderer: renderDate
-        }]
-      })
+        loadRecord: function( record ){
+          this.items.items[1].loadRecord( record );
+        }
+      }]
     }));
     Ext.oa.Nagios__Service_Panel.superclass.initComponent.apply(this, arguments);
-
-    this.on("cellmousedown", function( self, rowIndex, colIndex, evt ){
-        var record = self.getStore().getAt(rowIndex);
-        if( record.json.perfdata.length === 0 )
-          return;
-        var graphwin = new Ext.Window({
-          title: record.data.description,
-          height: 230,
-          width:  610,
-          items: new Ext.BoxComponent({
-            autoEl: {
-              tag: "img",
-              src: String.format(
-                PROJECT_URL + "/nagios/{0}/{1}.png",
-                record.data.id, 0
-              )
-            },
-            listeners: {
-              render: function(self){
-                self.el.on("load", function(ev, target, options){
-                  var offsetH = this.ownerCt.getHeight() - this.ownerCt.getInnerHeight()
-                  this.ownerCt.setHeight( target.height + offsetH );
-                  var offsetW = this.ownerCt.getWidth() - this.ownerCt.getInnerWidth()
-                  this.ownerCt.setWidth( target.width + offsetW );
-                }, self);
-              }
-            }
-          }),
-        });
-        graphwin.show();
-      }, this);
-
   },
 
   prepareMenuTree: function(tree){
