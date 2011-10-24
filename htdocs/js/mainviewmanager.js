480de64c61237bcc04158ea7067d182f4b9b8ba2
Michael Ziegler <michael.ziegler@it-novum.com>
2011-10-24 18:01:04 +0200

    implement deferred rendering (in a backwards compatible fashion even)

--- htdocs/js/mainviewmanager.js
+++ htdocs/js/mainviewmanager.js
@@ -7,14 +7,14 @@ Ext.oa.MainViewManager = Ext.extend(Ext.Panel, {
       tbar: new Ext.Toolbar({
         items: [
           new Ext.BoxComponent({
-            autoEl: { tag: "img", src: MEDIA_URL + '/openattic.png' }, region: "north", height: 50}), 
+            autoEl: { tag: "img", src: MEDIA_URL + '/openattic.png' }, region: "north", height: 50}),
         ],
         region: 'center',
         plugins : [
           new Ext.ux.ToolbarDroppable({
             createItem: function(data) {
               var record = data.draggedRecord;
-              
+
               return new Ext.Button({
                 text   : "test",
                 iconCls: '/srv/openattic/htdocs/openattic.png',
@@ -25,7 +25,6 @@ Ext.oa.MainViewManager = Ext.extend(Ext.Panel, {
           new Ext.ux.ToolbarReorderer({defaultReorderable: false})
         ],
       }),
-            
       items: [ new Ext.oa.MenuTree({
         title: 'Menu',
         region: "west",
@@ -35,20 +34,33 @@ Ext.oa.MainViewManager = Ext.extend(Ext.Panel, {
         maxSize: 400,
         collapsible: true,
         enableDD: true
-        
       }), {
         region: "center",
         activeItem: 0,
         border: false,
         hideBorders: true,
         layout: "card",
-        items: window.MainViewModules
+        layoutConfig: { deferredRender: true },
+        items: (function(){
+          var it = [];
+          window.MainViewModules.forEach(function(mod){
+            if( typeof mod.panel === "string" ){
+              console.log( "Pushing xtype "+mod.panel );
+              it.push({ xtype: mod.panel });
+            }
+            else if( typeof mod !== "undefined" ){
+              console.log( "Pushing panel "+mod.title );
+              it.push( mod );
+            }
+          });
+          return it;
+        }())
       }
     ],
       modules: window.MainViewModules
     }));
     Ext.oa.MainViewManager.superclass.initComponent.apply(this, arguments);
-      
+
     this.menutree = this.items.items[0];
     this.modcontainer = this.items.items[1];
     this.currentComponent = window.MainViewModules[0];
@@ -59,16 +71,19 @@ Ext.oa.MainViewManager = Ext.extend(Ext.Panel, {
 
     this.menutree.on( 'beforeclick', this.treenodeClicked, this );
   },
-  
+
   treenodeClicked: function( node, event ){
     if( node.leaf && typeof node.attributes.panel != "undefined" )
       this.switchComponent( node.attributes.panel );
   },
 
   switchComponent: function( toComponent ){
-    this.modcontainer.layout.setActiveItem( toComponent.id );
+    if( typeof toComponent === "string" )
+      this.modcontainer.layout.setActiveItem( toComponent );
+    else
+      this.modcontainer.layout.setActiveItem( toComponent.id );
   },
-  
+
 });
 
 
