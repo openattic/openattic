4a82f22ab00768cb8132c6a6c53131965dad520e
Kai Wagner <kai.wagner@it-novum.com>
2011-10-24 17:57:18 +0200

    zfs delete options

--- lvm/templates/lvm/zfssnapshot.js
+++ lvm/templates/lvm/zfssnapshot.js
@@ -5,20 +5,47 @@ Ext.namespace("Ext.oa");
 Ext.oa.Zfs__Snapshot_Panel = Ext.extend(Ext.Panel, {
   initComponent: function(){
     var zfsSnapPanel = this;
+    var snapshotid = null;
+      
     Ext.apply(this, Ext.apply(this.initialConfig, {
       title: "{% trans "Zfs Snapshots" %}",
+      layout: "border",
        buttons: [
         {
           text: "",
           icon: MEDIA_URL + "/icons2/16x16/actions/reload.png",
           tooltip: "{% trans 'Reload' %}",
           handler: function(self){
-          zfsSnapPanel.store.reload();
+          zfsSnapPanel.snapGrid.store.reload();
           }
-        }],
+        },{
+            text: "{% trans "Delete Snapshot" %}",
+            icon: MEDIA_URL + "/icons2/16x16/actions/remove.png",
+            handler: function(self){
+              var sm = zfsSnapPanel.snapGrid.getSelectionModel();
+              if( sm.hasSelection() ){
+                var sel = sm.selections.items[0];
+                Ext.Msg.confirm(
+                  "{% trans 'Confirm delete' %}",
+                   interpolate(
+                     "{% trans 'Really delete snapshot %s ?<br /><b>There is no undo.</b>' %}",
+                     [sel.data.snapname] ),
+                  function(btn, text){
+                    if( btn == 'yes' ) {
+                       lvm__ZfsSnapshot.remove( sel.data.id, function (provider, response){
+                       zfsSnapPanel.store.reload();
+                       })
+                    }
+                  }  
+                )
+              }
+            }
+        }
+      ],
       items: [{
         xtype: "grid",
         region: "center",
+        autoScroll: true,
         viewConfig: { forceFit: true },
         store: new Ext.data.DirectStore({
           autoLoad: true,
@@ -32,11 +59,37 @@ Ext.oa.Zfs__Snapshot_Panel = Ext.extend(Ext.Panel, {
           },
           columns: [{
             header: "{% trans 'Volume' %}",
-            width: 100,
             dataIndex: "name"
           }]
-        })
-      }]
+        }),
+        listeners: {
+            cellclick: function( self, rowIndex, colIndex, evt ){
+            var record = self.getStore().getAt(rowIndex);
+            zfsSnapPanel.snapGrid.store.load({ params: {"volume__name":record.data.name}});
+          }
+        },
+      },{
+        xtype: "grid",
+        region: "south",
+        autoScroll: true,
+        height: 300,
+        viewConfig: { forceFit: true },
+        ref: "snapGrid",
+        store: new Ext.data.DirectStore({
+          fields: ['snapname', 'id'],
+          directFn: lvm__ZfsSnapshot.filter
+        }),
+        colModel: new Ext.grid.ColumnModel({
+          defaults: {
+            sortable: true
+          },
+          columns: [{
+            header: "{% trans 'Snapshots' %}",
+            dataIndex: "snapname"
+          }]
+        }),      
+      }
+      ]
     }));
     Ext.oa.Zfs__Snapshot_Panel.superclass.initComponent.apply(this, arguments);
   },
