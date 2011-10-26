c79a9d4bbd78cbed74a3494694588150eb371494
Michael Ziegler <michael.ziegler@it-novum.com>
2011-10-26 15:22:50 +0200

    outsource DangerousMessage into its own file for re-use by other modules

--- sysutils/templates/sysutils/sysutils.js
+++ sysutils/templates/sysutils/sysutils.js
@@ -1,29 +1,6 @@
 {% load i18n %}
 Ext.namespace("Ext.oa");
 
-
-Ext.oa.DangerousMessage = function(color){
-  // http://www.sencha.com/forum/showthread.php?7613-Ext.MessageBox-extend-class&p=46792&viewfull=1#post46792
-  var f = function(){};
-  f.prototype = Ext.MessageBox;
-  var o = Ext.extend(f, {
-    getDialog: function() {
-      var d = o.superclass.getDialog.apply(this, arguments);
-      d.mask.addClass(color);
-      d.on("hide", function(){
-        d.mask.removeClass(color);
-      });
-      return d;
-    }
-  });
-  return new o();
-};
-Ext.oa.RedDangerousMessage = Ext.oa.DangerousMessage("redmask");
-
-Ext.oa.YellowDangerousMessage = Ext.oa.DangerousMessage("yellowmask");
-
-
-
 Ext.oa.SysUtils__Service_Panel = Ext.extend(Ext.grid.GridPanel, {
   initComponent: function(){
     var sysUtilsGrid = this;
