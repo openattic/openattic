b5f730e1d50294b3babc82a2708dc5210df8d7a0
Michael Ziegler <michael.ziegler@it-novum.com>
2011-11-02 11:16:38 +0100

    replace auth.models.User's {check,set}_password methods to allow checking by PAM

--- sysutils/models.py
+++ sysutils/models.py
@@ -2,7 +2,6 @@
 # kate: space-indent on; indent-width 4; replace-tabs on;
 
 import dbus
-import socket
 
 from django.db import models
 from django.conf import settings
@@ -35,17 +34,46 @@ class InitScript(models.Model):
     def stopped(self):
         return self.state == 3
 
+
 class NTP(models.Model):
     server = models.CharField(max_length=50)
-    
+
     def save( self, *args, **kwargs ):
         ret = models.Model.save(self, *args, **kwargs)
         ntp = dbus.SystemBus().get_object(settings.DBUS_IFACE_SYSTEMD, "/sysutils")
         ntp.write_ntp()
         return ret
-  
+
     def delete( self ):
         ret = models.Model.delete(self)
         ntp = dbus.SystemBus().get_object(settings.DBUS_IFACE_SYSTEMD, "/sysutils")
         ntp.write_ntp()
         return ret
+
+
+import new
+import hashlib
+from django.db.models import signals
+from django.contrib.auth import models as auth_models
+from django.utils.encoding import smart_str
+
+def set_password_crypt(self, raw_password):
+    """ Set the password as unsalted SHA1 so it can be checked using pam_*sql. """
+    self.password = str(hashlib.sha1(smart_str(raw_password)).hexdigest())
+
+def check_password_crypt(self, raw_password):
+    """ See if the current password was stored in Django format, and if not,
+        auth using the PAM compatible SHA1 hash.
+    """
+    if '$' in self.password:
+        return auth_models.User.check_password(self, raw_password)
+    return str(hashlib.sha1( smart_str(raw_password) ).hexdigest()) == self.password
+
+def replace_set_password(instance=None, **kwargs):
+    """ Replace the standard *_password functions in the auth model. """
+    instance.set_password = new.instancemethod(
+        set_password_crypt, instance, instance.__class__)
+    instance.check_password = new.instancemethod(
+        check_password_crypt, instance, instance.__class__)
+
+signals.post_init.connect(replace_set_password, sender=auth_models.User)
