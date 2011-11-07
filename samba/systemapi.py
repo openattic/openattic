8a509e960a75dc68a689751ddcbdea0a24385a5b
Michael Ziegler <michael.ziegler@it-novum.com>
2011-11-07 14:11:42 +0100

    implement samba password update

--- samba/systemapi.py
+++ samba/systemapi.py
@@ -6,6 +6,7 @@ import socket
 from django.template.loader import render_to_string
 
 from systemd       import logged, invoke, LockingPlugin, method
+from pamauth       import PamBackend
 from samba.models  import Share
 from samba.conf    import settings as samba_settings
 
@@ -33,3 +34,9 @@ class SystemD(LockingPlugin):
     @method(in_signature="", out_signature="i")
     def reload(self):
         return invoke([samba_settings.INITSCRIPT, "reload"])
+
+    @method(in_signature="ss", out_signature="i")
+    def setpasswd(self, username, passwd):
+        return invoke(["smbpasswd", "-as", username], log=False, stdin=("%s\n%s\n" % (passwd, passwd)))
+
+    setpasswd._enable_logging = False
