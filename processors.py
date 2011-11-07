a4002a5fea636c5575138c44d34a15c816eefae7
Michael Ziegler <michael.ziegler@it-novum.com>
2011-11-07 13:51:46 +0100

    add profile processor

--- processors.py
+++ processors.py
@@ -4,3 +4,16 @@
 def project_url(request):
     from django.conf import settings
     return {'PROJECT_URL': settings.PROJECT_URL}
+
+def profile(request):
+    if not request.user.is_authenticated():
+        return {}
+    else:
+        try:
+            profile = request.user.get_profile()
+        except UserProfile.DoesNotExist:
+            profile = UserProfile(user=request.user)
+            profile.save()
+
+        return {"PROFILE": profile}
+
