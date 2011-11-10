77b4f74f893061e9d6262f2b7988efd553126e8f
Michael Ziegler <michael.ziegler@it-novum.com>
2011-11-10 12:47:20 +0100

    cache pvs/vgs/lvs to respond to sequential requests more quickly

--- lvm/conf/settings.py
+++ lvm/conf/settings.py
@@ -6,3 +6,4 @@ from django.conf import settings
 MOUNT_PREFIX     = getattr( settings, "LVM_MOUNT_PREFIX",   "/media"       )
 CHOWN_GROUP      = getattr( settings, "LVM_CHOWN_GROUP",    "domain users" )
 LOG_COMMANDS     = getattr( settings, "LVM_LOG_COMMANDS",   False )
+SYSD_INFO_TTL    = 5
