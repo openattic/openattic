1a17818089c6e3bf4f393d7202ddc9c1550b317e
Michael Ziegler <michael.ziegler@it-novum.com>
2011-11-11 12:30:21 +0100

    re-add the renderer that was lost somewhere

--- lvm/templates/lvm/diskmgmt.js
+++ lvm/templates/lvm/diskmgmt.js
@@ -184,16 +184,18 @@ Ext.oa.volumeGroup_Panel = Ext.extend(Ext.grid.GridPanel, {
           dataIndex: "name"
         },{
           header: "Size",
-          dataIndex: "LVM_VG_SIZE"
+          dataIndex: "LVM_VG_SIZE",
+          renderer: function(val){ if( val ) return val; return '♻' }
         },{
           header: "Free",
-          dataIndex: "LVM_VG_FREE"
+          dataIndex: "LVM_VG_FREE",
+          renderer: function(val){ if( val ) return val; return '♻' }
         },{
           header: "Attributes",
-          dataIndex: "LVM_VG_ATTR"
+          dataIndex: "LVM_VG_ATTR",
+          renderer: function(val){ if( val ) return val; return '♻' }
         }]
       })
-          
     }));
     Ext.oa.volumeGroup_Panel.superclass.initComponent.apply(this, arguments);
   },
@@ -201,7 +203,6 @@ Ext.oa.volumeGroup_Panel = Ext.extend(Ext.grid.GridPanel, {
     Ext.oa.volumeGroup_Panel.superclass.onRender.apply(this, arguments);
     volumeGroups.load();
   }
-  
 });
 
 Ext.reg("volumeGroup_Panel", Ext.oa.volumeGroup_Panel);
