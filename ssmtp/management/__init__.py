f9d23c180f619ebda68087aae91a8e7f31d0197c
Michael Ziegler <michael.ziegler@it-novum.com>
2011-11-14 10:22:31 +0100

    add syncdb hooks for SSMTP and NTP modules

---
+++ ssmtp/management/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# kate: space-indent on; indent-width 4; replace-tabs on;
+
+import ssmtp.models
+from ssmtp.models     import SSMTP
+from django.db.models import signals
+
+def create_ssmtp(app, created_models, verbosity, **kwargs):
+    try:
+        SSMTP.objects.get(id=1)
+    except SSMTP.DoesNotExist:
+        if not kwargs.get('interactive', True):
+            print "Skipping initialization of the SSMTP module."
+    else:
+        print "The SSMTP module has already been initialized before."
+        return
+
+    if kwargs.get('interactive', True):
+        mailhub       = raw_input("Please enter the mail hub that should be used to deliver email: ")
+        root          = raw_input("Please enter the email address where mail for root should be sent to: ")
+        rewriteDomain = raw_input("Please enter the domain from which mail should appear to be from: ")
+        s = SSMTP(id=1, mailhub=mailhub, root=root, rewriteDomain=rewriteDomain)
+        s.save()
+        print "The SSMTP module has been initialized successfully."
+
+signals.post_syncdb.connect(create_ssmtp, sender=ssmtp.models)
---
+++ sysutils/management/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# kate: space-indent on; indent-width 4; replace-tabs on;
+
+import sysutils.models
+from sysutils.models  import NTP
+from django.db.models import signals
+
+def create_ssmtp(app, created_models, verbosity, **kwargs):
+    try:
+        NTP.objects.get(id=1)
+    except NTP.DoesNotExist:
+        if not kwargs.get('interactive', True):
+            print "Skipping initialization of the NTP module."
+    else:
+        print "The NTP module has already been initialized before."
+        return
+
+    if kwargs.get('interactive', True):
+        server = raw_input("Please enter the address of the NTP server: ")
+        s = NTP(id=1, server=server)
+        s.save()
+        print "The NTP module has been initialized successfully."
+
+signals.post_syncdb.connect(create_ssmtp, sender=sysutils.models)
