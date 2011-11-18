b27fa79c7c098a72d3d8960d03be1ad58dccc68e
Michael Ziegler <michael.ziegler@it-novum.com>
2011-11-18 10:30:00 +0100

    move logout thing to templates, indent, add translation markers, language update

---
+++ templates/logout.js
@@ -0,0 +1,39 @@
+window.MainViewModules.push({
+  prepareMenuTree: function(tree){
+    tree.appendToRootNodeById("menu_shutdown", {
+      text: "{% trans 'Logout' %}",
+      leaf: true,
+      icon: MEDIA_URL + '/oxygen/22x22/actions/system-log-out.png',
+      listeners: {
+        click: function(self, ev){
+          Ext.Msg.confirm(
+            "{% trans 'Logout' %}",
+            "{% trans 'Do you really want to logout?' %}",
+            function(btn, text){
+              if(btn == 'yes'){
+                var conn = new Ext.data.Connection();
+                conn.request({
+                  url: LOGOUT_URL,
+                  success: function( response, options ){
+                    var redirect = function(){
+                      location.href = INDEX_URL;
+                    };
+                    window.setTimeout( redirect, 20000 );
+                    Ext.Msg.show({
+                      title:   "{% trans 'Logout' %}",
+                      msg:     "{% trans 'Successfully logged out' %}",
+                      buttons: Ext.MessageBox.OK,
+                      fn:      redirect
+                    });
+                  }
+                });
+              }
+            }
+          );
+        }
+      }
+    });
+  }
+});
+
+// kate: space-indent on; indent-width 2; replace-tabs on;
