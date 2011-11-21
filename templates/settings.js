d614fae56ca2fff4f37efaf9b50dcc0f6f719c4d
Laura Paduano <laura.paduano@it-novum.com>
2011-11-21 18:07:29 +0100

    tipify, auto-expand root nodes, update package lists, theme, threshold

--- templates/settings.js
+++ templates/settings.js
@@ -1,3 +1,4 @@
+
 Ext.namespace("Ext.oa");
 Ext.oa.Settings_Panel = Ext.extend(Ext.Panel,{
   initComponent: function(){
@@ -9,6 +10,7 @@ Ext.oa.Settings_Panel = Ext.extend(Ext.Panel,{
       labelWidth: 250,
       bodyStyle: 'padding:10px;',     // lazy inline style
       layout: 'form',
+      reader: new Ext.data.JsonReader({fields: ['LV_test', 'id']}),
       items: [ {
         xtype: "checkbox",
         fieldLabel: "Auto-expand root nodes",
@@ -30,18 +32,46 @@ Ext.oa.Settings_Panel = Ext.extend(Ext.Panel,{
         }
       },{
         xtype:"numberfield",
-        fieldLabel: "ab welchem Schwellwert LV rot:",
+        fieldLabel: "threshold for LV",
+        name: "LV_test",
+        ref: 'thresholdfield',
+        value: Ext.state.Manager.get("lv_red_threshold",90),
         listeners: {
-          check: function(self, checked){
-            Ext.state.Manager.set("lv_red_threshold", parseFloat(text));
+          change: function( self, newValue, oldValue ){
+            Ext.state.Manager.set("lv_red_threshold", parseFloat(newValue));
           }
         }
-      } ],
+      },{
+        xtype: "checkbox",
+        fieldLabel: "Erlaube Installation/Löschen von Paketen",
+        listeners: {
+          check: function(self, checked){
+            Ext.state.Manager.set("pkgapt_distupgrade", checked);
+          }
+        }
+      },{
+        xtype: "checkbox",
+        fieldLabel: "Graph with Gradient",
+        listeners:{
+          check: function(self, checked){
+            Ext.state.Manager.set("nagios_graph_grad", checked);
+          }
+        }
+      },
+      ],
       buttons: [{
         text: 'Save',
+        handler: function(self){
+          Ext.Msg.show({
+            title: 'Settings',
+            msg: 'Successfully changed threshold',
+            buttons: Ext.MessageBox.OK
+          });
+        }
       },{
         text: 'Change THEME',
-        icon: MEDIA_URL + '/oxygen/16x16/apps/preferences-desktop-theme.png',
+        icon: MEDIA_URL +
+'/oxygen/16x16/apps/preferences-desktop-theme.png',
         listeners: {
           click: function(self, ev){
             var addwin = new Ext.Window({
@@ -57,19 +87,19 @@ Ext.oa.Settings_Panel = Ext.extend(Ext.Panel,{
                 handler: function(btn){
                   Ext.state.Manager.set( "theme", "access" );
                   // For some reason, window.location.reload.defer() does not work in chrome.
-                  setTimeout( function(){window.location.reload()}, 200 );
+                  setTimeout( function(){window.location.reload()}, 200);
                 }
               },{
                 text:  'gray',
                 handler: function(btn){
                   Ext.state.Manager.set( "theme", "gray" );
-                  setTimeout( function(){window.location.reload()}, 200 );
+                  setTimeout( function(){window.location.reload()}, 200);
                 }
               },{
                 text:  'default',
                 handler: function(btn){
                   Ext.state.Manager.clear( "theme" );
-                  setTimeout( function(){window.location.reload()}, 200 );
+                  setTimeout( function(){window.location.reload()}, 200);
                 }
               }]
             });
