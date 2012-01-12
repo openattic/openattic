bcb2063b9e937682e0628099ab4f4b831417c7b9
Michael Ziegler <michael.ziegler@it-novum.com>
2012-01-12 10:09:17 +0100

    export OACONFIG envvar and display oaconfig as %prog in ./manage.py help if appropriate

--- manage.py
+++ manage.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# kate: space-indent on; indent-width 4; replace-tabs on;
+
 from django.core.management import execute_manager
+
 try:
     import settings # Assumed to be in the same directory.
 except ImportError:
@@ -7,5 +11,10 @@ except ImportError:
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
     sys.exit(1)
 
+
 if __name__ == "__main__":
+    import os, sys
+    if "OACONFIG" in os.environ and os.environ["OACONFIG"] == "True":
+        sys.argv[0] = "oaconfig"
+
     execute_manager(settings)
