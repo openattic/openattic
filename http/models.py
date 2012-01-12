5d675ed751de7830c65bcbe37f8c84d18679d87d
Michael Ziegler <michael.ziegler@it-novum.com>
2012-01-12 11:47:50 +0100

    remove statefulmodel from http app

--- http/models.py
+++ http/models.py
@@ -7,9 +7,9 @@ from os import unlink, symlink
 from http.conf import settings as http_settings
 from django.db import models
 
-from lvm.models import StatefulModel, LogicalVolume
+from lvm.models import LogicalVolume
 
-class Export(StatefulModel):
+class Export(models.Model):
     volume      = models.ForeignKey(LogicalVolume, related_name="http_export_set")
     path        = models.CharField(max_length=255)
 
@@ -24,16 +24,14 @@ class Export(StatefulModel):
             raise ValidationError('This share type can only be used on volumes with a file system.')
 
     def save( self, *args, **kwargs ):
-        self.state = "active"
-        ret = StatefulModel.save(self, ignore_state=True, *args, **kwargs)
+        ret = models.Model.save(self, *args, **kwargs)
         linkname = join(http_settings.VOLUMESDIR, self.volume.name)
         if not exists( linkname ):
             symlink( self.path, linkname )
         return ret
 
     def delete( self ):
-        self.state = "done"
-        ret = StatefulModel.delete(self)
+        ret = models.Model.delete(self)
         linkname = join(http_settings.VOLUMESDIR, self.volume.name)
         if islink( linkname ):
             unlink( linkname )
