e6c6d24fd4dd23f3af00f62921cf670d19986a77
Michael Ziegler <michael.ziegler@it-novum.com>
2012-01-18 16:17:01 +0100

    remove it-n domain names from samba configuration

--- samba/conf/settings.py
+++ samba/conf/settings.py
@@ -5,5 +5,5 @@ from django.conf import settings
 
 INITSCRIPT      = getattr( settings, "SAMBA_INITSCRIPT", "/etc/init.d/smbd" )
 SMB_CONF        = getattr( settings, "SAMBA_SMB_CONF",   "/etc/samba/smb.conf" )
-DOMAIN          = getattr( settings, "SAMBA_DOMAIN",     "master.dns" )
-WORKGROUP       = getattr( settings, "SAMBA_WORKGROUP",  "SAPDOM" )
+DOMAIN          = getattr( settings, "SAMBA_DOMAIN",     "" )
+WORKGROUP       = getattr( settings, "SAMBA_WORKGROUP",  "" )
