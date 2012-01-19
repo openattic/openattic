78527e2b6c9be11a10f512a64dc3481fe8afe6de
Michael Ziegler <michael.ziegler@it-novum.com>
2012-01-19 12:52:39 +0100

    remove empty or obsolete urls.pys

--- drbd/urls.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-# kate: space-indent on; indent-width 4; replace-tabs on;
-
--- samba/urls.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# kate: space-indent on; indent-width 4; replace-tabs on;
-
-from django.conf.urls.defaults import url, patterns, include
