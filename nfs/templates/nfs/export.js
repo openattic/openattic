836cf8159fd4f6865e09c74ac6257ae75d861201
Laura Paduano <laura.paduano@it-novum.com>
2012-02-10 17:07:40 +0100

    refactored nfs user-form-panel

--- nfs/templates/nfs/export.js
+++ nfs/templates/nfs/export.js
@@ -3,58 +3,45 @@
 Ext.namespace("Ext.oa");
 
 Ext.oa.Nfs__Export_Panel = Ext.extend(Ext.grid.GridPanel, {
-  initComponent: function(){
+  showEditWindow: function(config, record){
     var nfsGrid = this;
-    var deleteFunction = function(self){
-      var sm = nfsGrid.getSelectionModel();
-      if( sm.hasSelection() ){
-        var sel = sm.selections.items[0];
-        Ext.Msg.confirm(
-          "{% trans 'Delete Export' %}",
-          interpolate(
-            "{% trans 'Do you really want to delete %s?' %}",[sel.data.path]),
-          function(btn){
-            if(btn == 'yes'){
-              nfs__Export.remove( sel.data.id, function(provider, response){
-                nfsGrid.store.reload();
-              } );
-            }
+    var addwin = new Ext.Window(Ext.apply(config, {
+      layout: "fit",
+      defaults: {autoScroll: true},
+        height: 200,
+        width: 500,
+      items:[{
+        xtype: "form",
+        bodyStyle: 'padding 5px 5px;',
+        api: {
+          load: nfs__Export.get_ext,
+          submit: nfs__Export.set_ext
+        },
+        baseParams: {
+          id: (record ? record.id: -1)
+        },
+        paramOrder: ["id"],
+        listeners: {
+          afterrender: function(self){
+            self.getForm().load();
           }
-        );
-      }
-    }
-    Ext.apply(this, Ext.apply(this.initialConfig, {
-      id: 'nfs__export_panel_inst',
-      title: "{% trans 'NFS' %}",
-      viewConfig: { forceFit: true },
-      buttons: [
-        {
-          text: "",
-          icon: MEDIA_URL + "/icons2/16x16/actions/reload.png",
-          tooltip: "{% trans 'Reload' %}",
-          handler: function(self){
-            nfsGrid.store.reload();
+        },
+        defaults: {
+          xtype: "textfield",
+          anchor: '-20px',
+          defaults : {
+            anchor: "0px"
           }
-        }, {
-        text: "{% trans 'Add Export' %}",
-        icon: MEDIA_URL + "/icons2/16x16/actions/add.png",
-        handler: function(){
-          var addwin = new Ext.Window({
-            title: "{% trans 'Add Export' %}",
-            layout: "fit",
-            height: 200,
-            width: 500,
-            items: [{
-              xtype: "form",
-              autoScroll: true,
-              bodyStyle: 'padding:5px 5px;',
-              defaults: {
-                xtype: "textfield",
-                anchor: '-20px'
-              },
-              items: [
+        },
+        items: [{
+          xtype: 'fieldset',
+          title: '',
+          layout: 'form',
+          items: [
                 tipify({
                   xtype: 'volumefield',
+                  name: "volume_",
+                  hiddenName: "volume",
                   listeners: {
                     select: function(self, record, index){
                       lvm__LogicalVolume.get( record.data.id, function( provider, response ){
@@ -65,39 +52,49 @@ Ext.oa.Nfs__Export_Panel = Ext.extend(Ext.grid.GridPanel, {
                   }
                 }, "{% trans 'Please select the volume to share.' %}"),
               tipify({
+                  xtype: 'textfield',
                   fieldLabel: "{% trans 'Directory' %}",
                   name: "path",
                   disabled: true,
                   ref: 'dirfield'
                 }, "{% trans 'If you wish to share only a subpath of the volume, enter the path here.' %}" ),
               {
+                xtype: 'textfield',
                 fieldLabel: "{% trans 'Address' %}",
                 allowBlank: false,
                 name: "address",
                 ref: 'addrfield'
               }, tipify({
+                xtype: 'textfield',
                 fieldLabel: "{% trans 'Options' %}",
                 name: "options",
                 ref: 'optfield',
                 value: "rw,no_subtree_check,no_root_squash"
-              },"{% trans 'this is default. rw: read/write rights are given,<br> no_subtree_check means that every file request is going to be checked to make sure that this file is in an exported subdirectory,<br> no_root_squash means share the folder (public), every IP-Adress has access, root can connect as root' %}")],
+              },"{% trans 'this is default. rw: read/write rights are given,<br> no_subtree_check means that every file request is going to be checked to make sure that this file is in an exported subdirectory,<br> no_root_squash means share the folder (public), every IP-Adress has access, root can connect as root' %}")]
+        }],
+         
+    /*Ext.apply(this, Ext.apply(this.initialConfig, {
+      id: 'nfs__export_panel_inst',
+      title: "{% trans 'NFS' %}",
+      viewConfig: { forceFit: true },
+    */  
+      
+      
+      
+          
+        
+              
               buttons: [{
-                text: "{% trans 'Create Export' %}",
+                text: config.submitButtonText,
                 icon: MEDIA_URL + "/oxygen/16x16/actions/dialog-ok-apply.png",
                 handler: function(self){
-                  nfs__Export.create({
-                    'volume': {
-                      'app': 'lvm',
-                      'obj': 'LogicalVolume',
-                      'id': self.ownerCt.ownerCt.volfield.getValue()
-                    },
-                    'path':    self.ownerCt.ownerCt.dirfield.getValue(),
-                    'options': self.ownerCt.ownerCt.optfield.getValue(),
-                    'address': self.ownerCt.ownerCt.addrfield.getValue()
-                  }, function(provider, response){
-                    if( response.result ){
-                      nfsGrid.store.reload();
-                      addwin.hide();
+                  self.ownerCt.ownerCt.getForm().submit({
+                    params: {id: -1, init_master: true, ordering: 0},
+                    success: function(provider, response){
+                      if(response.result){
+                        nfsGrid.store.reload();
+                        addwin.hide();
+                      }
                     }
                   });
                 }
@@ -109,90 +106,56 @@ Ext.oa.Nfs__Export_Panel = Ext.extend(Ext.grid.GridPanel, {
                 }
               }]
             }]
-          });
+          }));
           addwin.show();
+        },
+      initComponent: function(){
+        var nfsGrid = this;
+        Ext.apply(this, Ext.apply(this.initialConfig,{
+          id: "nfs__export_panel_inst",
+          title: "nfs",
+          viewConfig: {forceFit: true},
+          buttons: [{
+            text: "",
+            icon: MEDIA_URL + "/icons2/16x16/actions/reload.png",
+            tooltip: "{% trans 'Reload' %}",
+            handler: function(self){
+              nfsGrid.store.reload();
+            }
+        },{
+        text: "{% trans 'Add Export' %}",
+        icon: MEDIA_URL + "/icons2/16x16/actions/add.png",
+        handler: function(){
+          nfsGrid.showEditWindow({
+            title: "{% trans 'Add Export' %}",
+            submitButtonText: "{% trans 'Create Export' %}",
+          });
         }
-      },{
+       },{
         text:  "{% trans 'Edit' %}",
         icon: MEDIA_URL + "/icons2/16x16/actions/edit-redo.png",
         handler: function(self){
           var sm = nfsGrid.getSelectionModel();
           if( sm.hasSelection() ){
             var sel = sm.selections.items[0];
-            var addwin = new Ext.Window({
-              title: "Edit",
-              layout: "fit",
-              height: 200,
-              width: 500,
-              items: [{
-                xtype: "form",
-                autoScroll: true,
-                bodyStyle: 'padding:5px 5px;',
-                defaults: {
-                  xtype: "textfield",
-                  anchor: '-20px'
-                },
-                items: [{
-                  fieldLabel: "Volume",
-                  name: "volume",
-                  readOnly: true,
-                  disabled: true,
-                  ref: 'volumefield',
-                  
-                  value: sel.json.volume.name
-                },{
-                  fieldLabel: "Directory",
-                  name: "directory",
-                  ref: 'directoryfield',
-                  value: sel.data.path
-                },{
-                  fieldLabel: "Address",
-                  name: "address",
-                  allowBlank: false,
-                  ref: 'addressfield',
-                  value: sel.data.address
-                },{
-                  fieldLabel: "Options",
-                  name: "options",
-                  ref: 'optionsfield',
-                  value: sel.data.options
-                }],
-                buttons: [{
-                  text: 'Save',
-                  handler: function(self){
-                    var sm = nfsGrid.getSelectionModel();
-                    if( sm.hasSelection() ){
-                      var sel = sm.selections.items[0];
-                      nfs__Export.set(sel.data.id, {
-                        'path':    self.ownerCt.ownerCt.directoryfield.getValue(),
-                        'address':  self.ownerCt.ownerCt.addressfield.getValue(),
-                        'options':   self.ownerCt.ownerCt.optionsfield.getValue()
-                      }, function(provider, response){
-                        if( response.result ){
-                          nfsGrid.store.reload();
-                          addwin.hide();
-                        }
-                      });
-                    }
-                  }
-                }]
-              }]
-            });
-            addwin.show();
+            nfsGrid.showEditWindow({
+              title: "{% trans 'Edit' %}",
+              submitButtonText: "{% trans 'Edit' %}"
+            }, sel.data);
           }
         }
-      },{
+       },{
         text: "{% trans 'Delete Export' %}",
         icon: MEDIA_URL + "/icons2/16x16/actions/remove.png",
-        handler: deleteFunction
-        }
-      ],
-       keys: [{ key: [Ext.EventObject.DELETE], handler: deleteFunction}],
-      store: {
-        xtype: 'directstore',
+        handler: this.deleteFunction,
+        scope: nfsGrid
+        }],
+       keys: [{ scope: nfsGrid, key: [Ext.EventObject.DELETE], handler: this.deleteFunction}],
+       store: new Ext.data.DirectStore({
         fields: ['id', 'address', 'path', 'options'],
         directFn: nfs__Export.all
-      },
+      }),
+      viewConfig: {forceFit: true},
       colModel: new Ext.grid.ColumnModel({
         defaults: {
           sortable: true
@@ -214,9 +177,49 @@ Ext.oa.Nfs__Export_Panel = Ext.extend(Ext.grid.GridPanel, {
     }));
     Ext.oa.Nfs__Export_Panel.superclass.initComponent.apply(this, arguments);
   },
+    deleteFunction: function(self){
+      var sm = this.getSelectionModel();
+      if( sm.hasSelection() ){
+        var sel = sm.selections.items[0];
+        Ext.Msg.confirm(
+          "{% trans 'Delete Export' %}",
+          interpolate(
+            "{% trans 'Do you really want to delete %s?' %}",[sel.data.path]),
+          function(btn){
+            if(btn == 'yes'){
+              nfs__Export.remove( sel.data.id, function(provider, response){
+                sel.store.reload();
+              } );
+            }
+          });
+      }
+    },
+
   onRender: function(){
     Ext.oa.Nfs__Export_Panel.superclass.onRender.apply(this, arguments);
     this.store.reload();
+         var self = this;
+    var menu = new Ext.menu.Menu({
+    items: [{
+            id: 'delete',
+            text: 'delete',
+            icon: MEDIA_URL + "/icons2/16x16/actions/remove.png",
+        }],
+        listeners: {
+          itemclick: function(item) {
+                    self.deleteFunction()
+          }
+        }
+   });
+    this.on({
+      'contextmenu': function(event) {
+        if( this.getSelectionModel().hasSelection() ){
+          event.stopEvent();
+          this.getSelectionModel
+          menu.showAt(event.xy);
+        }
+      }
+    });
   }
 });
 
