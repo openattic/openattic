c31d71048e51d66a1c2721c118f4f7830876d1f3
Michael Ziegler <michael.ziegler@it-novum.com>
2012-03-12 14:55:13 +0100

    indent

--- tftp/templates/tftp/instance.js
+++ tftp/templates/tftp/instance.js
@@ -21,123 +21,124 @@ Ext.oa.Tftp__Instance_Panel = Ext.extend(Ext.grid.GridPanel, {
     var addwin = new Ext.Window(Ext.apply(config,{
       layout: "fit",
       defaults: {autoScroll: true},
-        height: 205,
-        width: 500,
-          items:[{
-          xtype: "form",
-          bodyStyle: 'padding: 5px 5px;',
-          api: {
-            load: tftp__Instance.get_ext,
-            submit: tftp__Instance.set_ext
-          },
-          baseParams: {
-            id: (record ? record.id: -1)
-          },
-          paramOrder: ["id"],
-          listeners: {
-            afterrender: function(self){
-              self.getForm().load();
-            }
-          },
-          defaults: {
-            xtype: "textfield",
-            anchor: '-20px',
-            defaults: {
-              anchor: "0px"
-            }
-          },
-          items: [{
-            xtype: 'fieldset',
-            layout: 'form',
-            items: [
-              tipify({
-                xtype: 'volumefield',
-                listeners: {
-                  select: function(self, record, index){
-                    lvm__LogicalVolume.get( record.data.id, function( provider, response ){
-                      self.ownerCt.dirfield.setValue( response.result.fs.mountpoints[0] );
-                      self.ownerCt.dirfield.enable();
-                    } );
-                  }
-                }
-              }, "{% trans 'Please select the volume to share.' %}"),
-              tipify({
-                  xtype: 'textfield',
-                  fieldLabel: "{% trans 'Directory' %}",
-                  name: "path",
-                  disabled: true,
-                  ref: 'dirfield'
-                }, "{% trans 'If you wish to share only a subpath of the volume, enter the path here.' %}" ),
-              {
-                xtype:      'combo',
-                fieldLabel: "{% trans 'Address' %}",
-                name:       "address",
-                ref:        'addrfield',
-                allowBlank: false,
-                hiddenName: 'address',
-                store: new Ext.data.DirectStore({
-                  fields: ["app", "obj", "id", "address"],
-                  baseParams: {fields: ["app", "obj", "id", "address"] },
-                  directFn: ifconfig__IPAddress.ids
-                }),
-                typeAhead:     true,
-                triggerAction: 'all',
-                emptyText:     "{% trans 'Select...' %}",
-                selectOnFocus: true,
-                displayField:  'address',
-                valueField:    'id'
-              }]
-            }],
-            buttons: [{
-            text: config.submitButtonText,
-            icon: MEDIA_URL + "/oxygen/16x16/actions/dialog-ok-apply.png",
-            handler: function(self){
-              self.ownerCt.ownerCt.getForm().submit({
-                params: {id: -1, init_master: true, ordering: 0},
-                success: function(provider, response){
-                  if(response.result){
-                    tftpGrid.store.reload();
-                    addwin.hide();
-                  }
-                }
-              });
-            }
-            },{
-              text: "{% trans 'Cancel' %}",
-              icon: MEDIA_URL + "/icons2/16x16/actions/gtk-cancel.png",
-              handler: function(self){
-                    addwin.hide();
-              }
-            }]
-          }]
-      }));
-         addwin.show();
+      height: 205,
+      width: 500,
+      items: [{
+        xtype: "form",
+        bodyStyle: 'padding: 5px 5px;',
+        api: {
+          load: tftp__Instance.get_ext,
+          submit: tftp__Instance.set_ext
         },
+        baseParams: {
+          id: (record ? record.id: -1)
+        },
+        paramOrder: ["id"],
+        listeners: {
+          afterrender: function(self){
+            self.getForm().load();
+          }
+        },
+        defaults: {
+          xtype: "textfield",
+          anchor: '-20px',
+          defaults: {
+            anchor: "0px"
+          }
+        },
+        items: [{
+          xtype: 'fieldset',
+          layout: 'form',
+          items: [
+            tipify({
+              xtype: 'volumefield',
+              listeners: {
+                select: function(self, record, index){
+                  lvm__LogicalVolume.get( record.data.id, function( provider, response ){
+                    self.ownerCt.dirfield.setValue( response.result.fs.mountpoints[0] );
+                    self.ownerCt.dirfield.enable();
+                  } );
+                }
+              }
+            }, "{% trans 'Please select the volume to share.' %}"),
+            tipify({
+              xtype: 'textfield',
+              fieldLabel: "{% trans 'Directory' %}",
+              name: "path",
+              disabled: true,
+              ref: 'dirfield'
+            }, "{% trans 'If you wish to share only a subpath of the volume, enter the path here.' %}" ),
+            {
+              xtype:      'combo',
+              fieldLabel: "{% trans 'Address' %}",
+              name:       "address",
+              ref:        'addrfield',
+              allowBlank: false,
+              hiddenName: 'address',
+              store: new Ext.data.DirectStore({
+                fields: ["app", "obj", "id", "address"],
+                baseParams: {fields: ["app", "obj", "id", "address"] },
+                directFn: ifconfig__IPAddress.ids
+              }),
+              typeAhead:     true,
+              triggerAction: 'all',
+              emptyText:     "{% trans 'Select...' %}",
+              selectOnFocus: true,
+              displayField:  'address',
+              valueField:    'id'
+            }
+          ]
+        }],
+        buttons: [{
+          text: config.submitButtonText,
+          icon: MEDIA_URL + "/oxygen/16x16/actions/dialog-ok-apply.png",
+          handler: function(self){
+            self.ownerCt.ownerCt.getForm().submit({
+              params: {id: -1, init_master: true, ordering: 0},
+              success: function(provider, response){
+                if(response.result){
+                  tftpGrid.store.reload();
+                  addwin.hide();
+                }
+              }
+            });
+          }
+        },{
+          text: "{% trans 'Cancel' %}",
+          icon: MEDIA_URL + "/icons2/16x16/actions/gtk-cancel.png",
+          handler: function(self){
+            addwin.hide();
+          }
+        }]
+      }]
+    }));
+    addwin.show();
+  },
 
-   initComponent: function(){
-   var tftpGrid = this;
-   Ext.apply(this, Ext.apply(this.initialConfig, {
-   id: "tftp__instance_panel_inst",
-   title: "tftp",
-   viewConfig: {forceFit: true},
-    buttons: [{
-    text: "",
-    icon: MEDIA_URL + "/icons2/16x16/actions/reload.png",
-    tooltip: "{% trans 'Reload' %}",
-      handler: function(self){
-        tftpGrid.store.reload();
-      }
-     },{
-      text: "{% trans 'Add Instance' %}",
-      icon: MEDIA_URL + "/icons2/16x16/actions/add.png",
+  initComponent: function(){
+    var tftpGrid = this;
+    Ext.apply(this, Ext.apply(this.initialConfig, {
+      id: "tftp__instance_panel_inst",
+      title: "tftp",
+      viewConfig: {forceFit: true},
+      buttons: [{
+        text: "",
+        icon: MEDIA_URL + "/icons2/16x16/actions/reload.png",
+        tooltip: "{% trans 'Reload' %}",
+        handler: function(self){
+          tftpGrid.store.reload();
+        }
+      },{
+        text: "{% trans 'Add Instance' %}",
+        icon: MEDIA_URL + "/icons2/16x16/actions/add.png",
         handler: function(){
           tftpGrid.showEditWindow({
             title: "{% trans 'Add Instance' %}",
             submitButtonText: "{% trans 'Add Instance' %}" 
           });
-          }
-     },{
-     text:  "{% trans 'Edit' %}",
+        }
+      },{
+        text:  "{% trans 'Edit' %}",
         icon: MEDIA_URL + "/icons2/16x16/actions/edit-redo.png",
         handler: function(self){
           var sm = tftpGrid.getSelectionModel();
@@ -149,13 +150,12 @@ Ext.oa.Tftp__Instance_Panel = Ext.extend(Ext.grid.GridPanel, {
             }, sel.data);
           }
         }
-       },{
+      },{
         text: "{% trans 'Delete Instance' %}",
         icon: MEDIA_URL + "/icons2/16x16/actions/remove.png",
         handler: this.deleteFunction,
         scope: tftpGrid
-        }
-      ],
+      } ],
       keys: [{scope: tftpGrid, key: [Ext.EventObject.DELETE], handler: this.deleteFunction}],
       store: new Ext.data.DirectStore({
         fields: ['id', 'address', 'path', {
@@ -181,7 +181,7 @@ Ext.oa.Tftp__Instance_Panel = Ext.extend(Ext.grid.GridPanel, {
     }));
     Ext.oa.Tftp__Instance_Panel.superclass.initComponent.apply(this, arguments);
   },
-    deleteFunction: function(self){
+  deleteFunction: function(self){
     var sm = this.getSelectionModel();
     if( sm.hasSelection() ){
       var sel = sm.selections.items[0];
@@ -195,24 +195,25 @@ Ext.oa.Tftp__Instance_Panel = Ext.extend(Ext.grid.GridPanel, {
               sel.store.reload();
             } );
           }
-        });
-      }
-    },
+        }
+      );
+    }
+  },
   onRender: function(){
     Ext.oa.Tftp__Instance_Panel.superclass.onRender.apply(this, arguments);
     this.store.reload();
-     var self = this;
+    var self = this;
     var menu = new Ext.menu.Menu({
-    items: [{
-            id: 'delete',
-            text: 'delete',
-            icon: MEDIA_URL + "/icons2/16x16/actions/remove.png"
-        }],
-        listeners: {
-          itemclick: function(item) {
-                    self.deleteFunction()
-          }
+      items: [{
+        id: 'delete',
+        text: 'delete',
+        icon: MEDIA_URL + "/icons2/16x16/actions/remove.png"
+      }],
+      listeners: {
+        itemclick: function(item) {
+          self.deleteFunction()
         }
+      }
     });
     this.on({
       'contextmenu': function(event) {
