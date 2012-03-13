c77bf14629b608c1d6f354f64ed2df1be6a90a3d
Laura Paduano <laura.paduano@it-novum.com>
2012-03-13 17:36:24 +0100

    fix indentation

--- http/templates/http/export.js
+++ http/templates/http/export.js
@@ -20,94 +20,94 @@ Ext.oa.Http__Export_Panel = Ext.extend(Ext.grid.GridPanel, {
     var httpGrid = this;
     var addwin = new Ext.Window(Ext.apply(config,{
 // Ext.apply(this, Ext.apply(this.initialConfig, {
-   layout: "fit",
-   defaults: { autoScroll: true },
-     height: 200,
-     width: 500,
-     items: [{
-       xtype: "form",
-       bodyStyle: 'padding:5px ;',
-       api: {
-         load: http__Export.get_ext,
-         submit: http__Export.set_ext
-      },
-      baseParams: {
-        id: (record ? record.id: -1)
-      },
-      paramOrder: ["id"],
-      listeners: {
-        afterrender: function(self){
-          self.getForm().load();
-        }
-      },
-      defaults: {
-        xtype: "textfield",
-        anchor: '-20',
-        defaults: {
-          anchor: "0px"
-        }
-      },
+      layout: "fit",
+      defaults: { autoScroll: true },
+      height: 200,
+      width: 500,
       items: [{
-        xtype: 'fieldset',
-        title: 'HTTP Export',
-        layout: 'form',
-        items: [{
-          xtype: 'volumefield',
-          listeners: {
-            select: function(self, record, index){
-              lvm__LogicalVolume.get( record.data.id, function( provider, response ){
-                self.ownerCt.dirfield.setValue( response.result.fs.mountpoints[0] );
-                self.ownerCt.dirfield.enable();
-              } );
-            }
+        xtype: "form",
+        bodyStyle: 'padding:5px ;',
+        api: {
+          load: http__Export.get_ext,
+          submit: http__Export.set_ext
+        },
+        baseParams: {
+          id: (record ? record.id: -1)
+        },
+        paramOrder: ["id"],
+        listeners: {
+          afterrender: function(self){
+            self.getForm().load();
           }
-        }, {
+        },
+        defaults: {
+          xtype: "textfield",
+          anchor: '-20',
+          defaults: {
+            anchor: "0px"
+          }
+        },
+        items: [{
+          xtype: 'fieldset',
+          title: 'HTTP Export',
+          layout: 'form',
+          items: [{
+            xtype: 'volumefield',
+            listeners: {
+              select: function(self, record, index){
+                lvm__LogicalVolume.get( record.data.id, function( provider, response ){
+                  self.ownerCt.dirfield.setValue( response.result.fs.mountpoints[0] );
+                  self.ownerCt.dirfield.enable();
+                } );
+              }
+            }
+          }, {
           xtype: 'textfield',
           fieldLabel: "{% trans 'Directory' %}",
           name: "path",
           disabled: (!record),
           ref: 'dirfield'
-        }]
-      }],
-      buttons: [{
-        text: config.submitButtonText,
-        icon: MEDIA_URL + "/oxygen/16x16/actions/dialog-ok-apply.png",
-        handler: function(self){
-          self.ownerCt.ownerCt.getForm().submit({
-          params: {id: -1, init_master: true, ordering: 0},
-          success: function(provider,response){
-            if(response.result){
-              httpGrid.store.reload();
-              addwin.hide();
-            }
+          }]
+        }],
+        buttons: [{
+          text: config.submitButtonText,
+          icon: MEDIA_URL + "/oxygen/16x16/actions/dialog-ok-apply.png",
+          handler: function(self){
+            self.ownerCt.ownerCt.getForm().submit({
+            params: {id: -1, init_master: true, ordering: 0},
+              success: function(provider,response){
+                if(response.result){
+                  httpGrid.store.reload();
+                  addwin.hide();
+                }
+              }
+            });
           }
-        });
-      }
-    },{
-        text: "{% trans 'Cancel' %}",
-        icon: MEDIA_URL + "/icons2/16x16/actions/gtk-cancel.png",
-        handler: function(self){
-                 addwin.hide();
-        }
+        },{
+          text: "{% trans 'Cancel' %}",
+          icon: MEDIA_URL + "/icons2/16x16/actions/gtk-cancel.png",
+          handler: function(self){
+           addwin.hide();
+          }
+        }]
       }]
-    }]
- }));
- addwin.show();
-    },
-     initComponent: function(){
-       var httpGrid = this;
-       Ext.apply(this, Ext.apply(this.initialConfig, {
-         id: "http__export_panel_inst",
-         title: "http",
-         viewConfig: {forceFit: true},
-         buttons: [{
-        text: "",
-        icon: MEDIA_URL + "/icons2/16x16/actions/reload.png",
-        tooltip: "{% trans 'Reload' %}",
-        handler: function(self){
-          httpGrid.store.reload();
-        }
-      }, {
+    }));
+    addwin.show();
+  },
+   initComponent: function(){
+     var httpGrid = this;
+     Ext.apply(this, Ext.apply(this.initialConfig, {
+       id: "http__export_panel_inst",
+       title: "http",
+       viewConfig: {forceFit: true},
+       buttons: [{
+          text: "",
+          icon: MEDIA_URL + "/icons2/16x16/actions/reload.png",
+          tooltip: "{% trans 'Reload' %}",
+          handler: function(self){
+            httpGrid.store.reload();
+          }
+       }, {
         text: "{% trans 'Add Export' %}",
         icon: MEDIA_URL + "/icons2/16x16/actions/add.png",
         handler: function(){
@@ -121,7 +121,7 @@ Ext.oa.Http__Export_Panel = Ext.extend(Ext.grid.GridPanel, {
         icon: MEDIA_URL + "/icons2/16x16/actions/edit-redo.png",
         handler: this.editFunction,
         scope: httpGrid
-      },{
+    },{
         text: "{% trans 'Delete Export' %}",
         icon: MEDIA_URL + "/icons2/16x16/actions/remove.png",
         handler: this.deleteFunction,
@@ -134,7 +134,7 @@ Ext.oa.Http__Export_Panel = Ext.extend(Ext.grid.GridPanel, {
         }],
         directFn: http__Export.all
       }),
-      viewConfig: {forceFit: true },
+  viewConfig: {forceFit: true },
       colModel: new Ext.grid.ColumnModel({
         defaults: {
           sortable: true
@@ -154,63 +154,62 @@ Ext.oa.Http__Export_Panel = Ext.extend(Ext.grid.GridPanel, {
               '</a>',
               val );
           }
-        } ]
+        }]
       })
     }));
     Ext.oa.Http__Export_Panel.superclass.initComponent.apply(this, arguments);
   },
-    editFunction: function(self){
-      var sm = this.getSelectionModel();
-      if( sm.hasSelection() ){
-        var sel = sm.selections.items[0];
-        this.showEditWindow({
-          title:  "{% trans 'Edit Export' %}",
-          submitButtonText:  "{% trans 'Edit Export' %}"
-        },sel.data);
-      }
-    },
-    deleteFunction: function(self){
+  editFunction: function(self){
     var sm = this.getSelectionModel();
-      if( sm.hasSelection() ){
-        var sel = sm.selections.items[0];
-        Ext.Msg.confirm(
-            "{% trans 'Delete Export' %}",
-            interpolate(
-              "{% trans 'Do you really want to delete %s?' %}",[sel.data.path]),
-              function(btn){
-                if(btn == 'yes'){
-                  http__Export.remove( sel.data.id, function(provider, response){
-                  sel.store.reload();
-                  });
-                }
-          });
-      }
-    },
-    
+    if( sm.hasSelection() ){
+      var sel = sm.selections.items[0];
+      this.showEditWindow({
+        title:  "{% trans 'Edit Export' %}",
+        submitButtonText:  "{% trans 'Edit Export' %}"
+      },sel.data);
+    }
+  },
+  deleteFunction: function(self){
+    var sm = this.getSelectionModel();
+    if( sm.hasSelection() ){
+      var sel = sm.selections.items[0];
+      Ext.Msg.confirm(
+        "{% trans 'Delete Export' %}",
+        interpolate(
+          "{% trans 'Do you really want to delete %s?' %}",[sel.data.path]),
+        function(btn){
+          if(btn == 'yes'){
+            http__Export.remove( sel.data.id, function(provider, response){
+            sel.store.reload();
+            });
+          }
+        }
+      );
+    }
+  },
   onRender: function(){
     Ext.oa.Http__Export_Panel.superclass.onRender.apply(this, arguments);
     this.store.reload();
     var self = this;
     var menu = new Ext.menu.Menu({
-    items: [{
-            text: 'delete',
-            icon: MEDIA_URL + "/icons2/16x16/actions/remove.png"
-        }],
-        listeners: {
-          itemclick: function(item) {
-                    self.deleteFunction()
-          }
+      items: [{
+        text: 'delete',
+        icon: MEDIA_URL + "/icons2/16x16/actions/remove.png"
+      }],
+      listeners: {
+        itemclick: function(item) {
+          self.deleteFunction()
         }
-   });
+      }
+    });
     this.on({
       'rowcontextmenu': function(igrid, row, event) {
         this.selectedNode = this.store.getAt(row);
-	if((row) !== false) {
-	  this.getSelectionModel().selectRow(row);
-	}
-          event.stopEvent();
-          menu.showAt(event.xy);
-        
+        if((row) !== false) {
+          this.getSelectionModel().selectRow(row);
+        }
+        event.stopEvent();
+        menu.showAt(event.xy);
       },
       'rowdblclick': function(eventGrid, rowIndex, e) {
         if( this.getSelectionModel().hasSelection() ){
