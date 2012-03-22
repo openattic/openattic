35ff0d87d0864737e04b8a25d4f0295455e0c3e9
Michael Ziegler <michael.ziegler@it-novum.com>
2012-03-22 13:11:17 +0100

    outsource commonly used functionality into ShareGridPanel, language update

--- nfs/templates/nfs/export.js
+++ nfs/templates/nfs/export.js
@@ -15,212 +15,65 @@
 
 Ext.namespace("Ext.oa");
 
-Ext.oa.Nfs__Export_Panel = Ext.extend(Ext.grid.GridPanel, {
-  showEditWindow: function(config, record){
-    var nfsGrid = this;
-    var addwin = new Ext.Window(Ext.apply(config, {
-      layout: "fit",
-      defaults: {autoScroll: true},
-      height: 240,
-      width: 500,
-      items:[{
-        xtype: "form",
-        bodyStyle: 'padding: 5px 5px;',
-        api: {
-          load: nfs__Export.get_ext,
-          submit: nfs__Export.set_ext
+Ext.oa.Nfs__Export_Panel = Ext.extend(Ext.oa.ShareGridPanel, {
+  api: nfs__Export,
+  id: "nfs__export_panel_inst",
+  title: "NFS",
+  fields: [{
+    fieldLabel: "{% trans 'Address' %}",
+    width: 100,
+    name: "address"
+  }, {
+    fieldLabel: "{% trans 'Path' %}",
+    width: 200,
+    name: "path"
+  }, {
+    fieldLabel: "{% trans 'Options' %}",
+    width: 200,
+    name: "options"
+  }],
+  form: {
+    items: [{
+      xtype: 'fieldset',
+      title: 'NFS Export',
+      layout: 'form',
+      items: [
+        tipify({
+          xtype: 'volumefield',
+          listeners: {
+            select: function(self, record, index){
+              lvm__LogicalVolume.get( record.data.id, function( provider, response ){
+                self.ownerCt.dirfield.setValue( response.result.fs.mountpoints[0] );
+                self.ownerCt.dirfield.enable();
+              } );
+            }
+          }
+        }, "{% trans 'Please select the volume to share.' %}"),
+        tipify({
+          xtype: 'textfield',
+          fieldLabel: "{% trans 'Directory' %}",
+          name: "path",
+          ref: 'dirfield'
+        }, "{% trans 'If you wish to share only a subpath of the volume, enter the path here.' %}" ),
+        {
+          xtype: 'textfield',
+          fieldLabel: "{% trans 'Address' %}",
+          allowBlank: false,
+          name: "address",
+          ref: 'addrfield'
         },
-        baseParams: {
-          id: (record ? record.id: -1)
-        },
-        paramOrder: ["id"],
-        listeners: {
-          afterrender: function(self){
-            self.getForm().load();
-          }
-        },
-        defaults: {
-          xtype: "textfield",
-          anchor: '-20px',
-          defaults : {
-            anchor: "0px"
-          }
-        },
-        items: [{
-          xtype: 'fieldset',
-          title: 'NFS Export',
-          layout: 'form',
-          items: [
-            tipify({
-              xtype: 'volumefield',
-              listeners: {
-                select: function(self, record, index){
-                  lvm__LogicalVolume.get( record.data.id, function( provider, response ){
-                    self.ownerCt.dirfield.setValue( response.result.fs.mountpoints[0] );
-                    self.ownerCt.dirfield.enable();
-                  } );
-                }
-              }
-            }, "{% trans 'Please select the volume to share.' %}"),
-            tipify({
-              xtype: 'textfield',
-              fieldLabel: "{% trans 'Directory' %}",
-              name: "path",
-              disabled: (!record),
-              ref: 'dirfield'
-            }, "{% trans 'If you wish to share only a subpath of the volume, enter the path here.' %}" ),
-            {
-              xtype: 'textfield',
-              fieldLabel: "{% trans 'Address' %}",
-              allowBlank: false,
-              name: "address",
-              ref: 'addrfield'
-            },
-            tipify({
-              xtype: 'textfield',
-              fieldLabel: "{% trans 'Options' %}",
-              name: "options",
-              ref: 'optfield',
-              value: "rw,no_subtree_check,no_root_squash"
-            },"{% trans 'this is default. rw: read/write rights are given,<br> no_subtree_check means that every file request is going to be checked to make sure that this file is in an exported subdirectory,<br> no_root_squash means share the folder (public), every IP-Adress has access, root can connect as root' %}")
-          ]
-        }],
-        buttons: [{
-          text: config.submitButtonText,
-          icon: MEDIA_URL + "/oxygen/16x16/actions/dialog-ok-apply.png",
-          handler: function(self){
-            self.ownerCt.ownerCt.getForm().submit({
-              success: function(provider, response){
-                if(response.result){
-                  nfsGrid.store.reload();
-                  addwin.hide();
-                }
-              }
-            });
-          }
-        },{
-          text: "{% trans 'Cancel' %}",
-          icon: MEDIA_URL + "/icons2/16x16/actions/gtk-cancel.png",
-          handler: function(self){
-            addwin.hide();
-          }
-        }]
-      }]
-    }));
-    addwin.show();
-  },
-  initComponent: function(){
-    var nfsGrid = this;
-    Ext.apply(this, Ext.apply(this.initialConfig,{
-      id: "nfs__export_panel_inst",
-      title: "nfs",
-      viewConfig: {forceFit: true},
-      buttons: [{
-        text: "",
-        icon: MEDIA_URL + "/icons2/16x16/actions/reload.png",
-        tooltip: "{% trans 'Reload' %}",
-        handler: function(self){
-          nfsGrid.store.reload();
-        }
-      },{
-        text: "{% trans 'Add Export' %}",
-        icon: MEDIA_URL + "/icons2/16x16/actions/add.png",
-        handler: function(){
-          nfsGrid.showEditWindow({
-            title: "{% trans 'Add Export' %}",
-            submitButtonText: "{% trans 'Create Export' %}"
-          });
-        }
-      },{
-        text:  "{% trans 'Edit' %}",
-        icon: MEDIA_URL + "/icons2/16x16/actions/edit-redo.png",
-        handler: function(self){
-          var sm = nfsGrid.getSelectionModel();
-          if( sm.hasSelection() ){
-            var sel = sm.selections.items[0];
-            nfsGrid.showEditWindow({
-              title: "{% trans 'Edit' %}",
-              submitButtonText: "{% trans 'Edit' %}"
-            }, sel.data);
-          }
-        }
-      },{
-        text: "{% trans 'Delete Export' %}",
-        icon: MEDIA_URL + "/icons2/16x16/actions/remove.png",
-        handler: this.deleteFunction,
-        scope: nfsGrid
-      }],
-      keys: [{ scope: nfsGrid, key: [Ext.EventObject.DELETE], handler: this.deleteFunction}],
-      store: new Ext.data.DirectStore({
-        fields: ['id', 'address', 'path', 'options'],
-        directFn: nfs__Export.all
-      }),
-      viewConfig: {forceFit: true},
-      colModel: new Ext.grid.ColumnModel({
-        defaults: {
-          sortable: true
-        },
-        columns: [{
-          header: "{% trans 'Address' %}",
-          width: 100,
-          dataIndex: "address"
-        }, {
-          header: "{% trans 'Path' %}",
-          width: 200,
-          dataIndex: "path"
-        }, {
-          header: "{% trans 'Options' %}",
-          width: 200,
-          dataIndex: "options"
-        }]
-      })
-    }));
-    Ext.oa.Nfs__Export_Panel.superclass.initComponent.apply(this, arguments);
-  },
-  deleteFunction: function(self){
-    var sm = this.getSelectionModel();
-    if( sm.hasSelection() ){
-      var sel = sm.selections.items[0];
-      Ext.Msg.confirm(
-        "{% trans 'Delete Export' %}",
-        interpolate(
-          "{% trans 'Do you really want to delete %s?' %}",[sel.data.path]),
-        function(btn){
-          if(btn == 'yes'){
-            nfs__Export.remove( sel.data.id, function(provider, response){
-              sel.store.reload();
-            } );
-          }
-        }
-      );
-    }
-  },
-
-  onRender: function(){
-    Ext.oa.Nfs__Export_Panel.superclass.onRender.apply(this, arguments);
-    this.store.reload();
-    var self = this;
-    var menu = new Ext.menu.Menu({
-      items: [{
-        text: 'delete',
-        icon: MEDIA_URL + "/icons2/16x16/actions/remove.png"
-      }],
-      listeners: {
-        itemclick: function(item) {
-                  self.deleteFunction()
-        }
-      }
-    });
-    this.on({
-      'contextmenu': function(event) {
-        if( this.getSelectionModel().hasSelection() ){
-          event.stopEvent();
-          menu.showAt(event.xy);
-        }
-      }
-    });
+        tipify({
+          xtype: 'textfield',
+          fieldLabel: "{% trans 'Options' %}",
+          name: "options",
+          ref: 'optfield',
+          value: "rw,no_subtree_check,no_root_squash"
+        },"{% trans 'this is default. rw: read/write rights are given,<br> no_subtree_check means that every file request is going to be checked to make sure that this file is in an exported subdirectory,<br> no_root_squash means share the folder (public), every IP-Adress has access, root can connect as root' %}")
+      ]
+    }],
   }
 });
+
 
 Ext.reg("nfs__export_panel", Ext.oa.Nfs__Export_Panel);
 
---
+++ templates/sharegridpanel.js
@@ -0,0 +1,229 @@
+{% load i18n %}
+
+/*
+ Copyright (C) 2011-2012, it-novum GmbH <community@open-attic.org>
+
+ openATTIC is free software; you can redistribute it and/or modify it
+ under the terms of the GNU General Public License as published by
+ the Free Software Foundation; version 2.
+
+ This package is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+*/
+
+Ext.namespace("Ext.oa");
+
+Ext.oa.ShareGridPanel = Ext.extend(Ext.grid.GridPanel, {
+  api: null,
+  form: {},
+  texts: {
+    reload:  "{% trans 'Reload' %}",
+    add:     "{% trans 'Add Export' %}",
+    edit:    "{% trans 'Edit Export' %}",
+    remove:  "{% trans 'Delete Export' %}",
+    submit:  "{% trans 'Submit' %}",
+    cancel:  "{% trans 'Cancel' %}",
+    confirm: "{% trans 'Do you really want to delete export %s?' %}"
+  },
+
+  initComponent: function(){
+    var self = this;
+    Ext.apply(this, Ext.applyIf(this.initialConfig, {
+      viewConfig: {forceFit: true},
+      buttons: [{
+        text: "",
+        icon: MEDIA_URL + "/icons2/16x16/actions/reload.png",
+        tooltip: self.texts.reload,
+        handler: function(){
+          self.store.reload();
+        }
+      },{
+        text: self.texts.add,
+        icon: MEDIA_URL + "/icons2/16x16/actions/add.png",
+        handler: function(){
+          self.showEditWindow({
+            title: self.texts.add,
+            submitButtonText: self.texts.add
+          });
+        }
+      },{
+        text:  self.texts.edit,
+        icon: MEDIA_URL + "/icons2/16x16/actions/edit-redo.png",
+        handler: function(){
+          var sm = self.getSelectionModel();
+          if( sm.hasSelection() ){
+            var sel = sm.selections.items[0];
+            self.showEditWindow({
+              title: self.texts.edit,
+              submitButtonText: self.texts.edit
+            }, sel.data);
+          }
+        }
+      },{
+        text: self.texts.remove,
+        icon: MEDIA_URL + "/icons2/16x16/actions/remove.png",
+        handler: this.deleteFunction,
+        scope: self
+      }],
+      keys: [{
+        scope: self,
+        key: [Ext.EventObject.DELETE],
+        handler: this.deleteFunction
+        }],
+      store: new Ext.data.DirectStore({
+        fields: (function(){
+          var cols = ["id"];
+          for( var i = 0; i < self.fields.length; i++ ){
+            cols.push(self.fields[i].name);
+          }
+          return cols;
+        }()),
+        directFn: self.api.all
+      }),
+      viewConfig: {
+        forceFit: true
+      },
+      colModel: new Ext.grid.ColumnModel({
+        defaults: {
+          sortable: true
+        },
+        columns: (function(){
+          var cols = [];
+          for( var i = 0; i < self.fields.length; i++ ){
+            cols.push({
+              dataIndex: self.fields[i].name,
+              width:     self.fields[i].width,
+              header:    self.fields[i].fieldLabel
+            });
+          }
+          return cols;
+        }())
+      })
+    }));
+    Ext.oa.ShareGridPanel.superclass.initComponent.apply(this, arguments);
+  },
+
+  editFunction: function(self){
+    var sm = this.getSelectionModel();
+    if( sm.hasSelection() ){
+      var sel = sm.selections.items[0];
+      this.showEditWindow({
+        title: self.texts.edit,
+        submitButtonText: self.texts.edit
+      },sel.data);
+    }
+  },
+
+  deleteConfirm: function(sel){
+    return interpolate(this.texts.confirm, [sel.data.path]);
+  },
+
+  deleteFunction: function(){
+    var sm = this.getSelectionModel();
+    var self = this;
+    if( sm.hasSelection() ){
+      var sel = sm.selections.items[0];
+      Ext.Msg.confirm(
+        self.texts.remove,
+        self.deleteConfirm(sel),
+        function(btn){
+          if(btn == 'yes'){
+            self.api.remove( sel.data.id, function(provider, response){
+              sel.store.reload();
+            } );
+          }
+        }
+      );
+    }
+  },
+
+  showEditWindow: function(config, record){
+    var self = this;
+    var addwin = new Ext.Window(Ext.apply(config, {
+      layout: "fit",
+      defaults: {autoScroll: true},
+      height: 240,
+      width: 500,
+      items: (function(){
+        var form = {
+          xtype: "form",
+          bodyStyle: 'padding: 5px 5px;',
+          api: {
+            load:   self.api.get_ext,
+            submit: self.api.set_ext
+          },
+          baseParams: {
+            id: (record ? record.id: -1)
+          },
+          paramOrder: ["id"],
+          listeners: {
+            afterrender: function(form){
+              form.getForm().load();
+            }
+          },
+          defaults: {
+            xtype: "textfield",
+            anchor: '-20px',
+            defaults : {
+              anchor: "0px"
+            }
+          },
+          buttons: [{
+            text: config.submitButtonText,
+            icon: MEDIA_URL + "/oxygen/16x16/actions/dialog-ok-apply.png",
+            handler: function(btn){
+              btn.ownerCt.ownerCt.getForm().submit({
+                success: function(provider, response){
+                  if(response.result){
+                    self.store.reload();
+                    addwin.hide();
+                  }
+                }
+              });
+            }
+          },{
+            text: self.texts.cancel,
+            icon: MEDIA_URL + "/icons2/16x16/actions/gtk-cancel.png",
+            handler: function(){
+              addwin.hide();
+            }
+          }]
+        };
+        return Ext.apply(form, self.form);
+      }())
+    }));
+    addwin.show();
+  },
+
+  onRender: function(){
+    Ext.oa.ShareGridPanel.superclass.onRender.apply(this, arguments);
+    this.store.reload();
+    var self = this;
+    var menu = new Ext.menu.Menu({
+      items: [{
+        text: 'delete',
+        icon: MEDIA_URL + "/icons2/16x16/actions/remove.png"
+      }],
+      listeners: {
+        itemclick: function(item) {
+          self.deleteFunction();
+        }
+      }
+    });
+    this.on({
+      'contextmenu': function(event) {
+        if( this.getSelectionModel().hasSelection() ){
+          event.stopEvent();
+          menu.showAt(event.xy);
+        }
+      }
+    });
+  }
+});
+
+Ext.reg("volumefield", Ext.oa.ShareGridPanel);
+
+// kate: space-indent on; indent-width 2; replace-tabs on;
+
