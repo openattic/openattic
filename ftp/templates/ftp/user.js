b218ed002256b9af8a1f0a142e4d3456c8137d75
Michael Ziegler <michael.ziegler@it-novum.com>
2012-03-22 13:51:55 +0100

    refactor ftp module to use ShareGridPanel

--- ftp/templates/ftp/user.js
+++ ftp/templates/ftp/user.js
@@ -15,204 +15,62 @@
 
 Ext.namespace("Ext.oa");
 
-Ext.oa.Ftp__User_Panel = Ext.extend(Ext.grid.GridPanel, {
-  showEditWindow: function(config, record){
-    var ftpGrid = this;
-    var addwin = new Ext.Window(Ext.apply(config,{
-      layout: "fit",
-      defaults: {autoScroll: true},
-      height: 230,
-      width: 500,
-      //title: "{% trans 'Add FTP User' %}",
-      items: [{
-        xtype: "form",
-        bodyStyle: 'padding:5px 5px;',
-        api: {
-          load: ftp__User.get_ext,
-          submit: ftp__User.set_ext
-        },
-        baseParams: {
-          id: (record ? record.id : -1)
-        },
-        paramOrder: ["id"],
-        listeners: {
-          afterrender: function(self){
-            self.getForm().load();
-          }
-        },
-        defaults: {
-          xtype: "textfield",
-          anchor: '-20px',
-          defaults: {
-            anchor: "0px"
-          }
-        },
-        items: [{
-          xtype: 'fieldset',
-          layout: 'form',
-          items:[{
-            xtype: 'textfield',
-            name: "username",
-            fieldLabel: "Username"
-        }, {
-          xtype: 'textfield',
-          fieldLabel: "{% trans 'Password' %}",
-           name: "passwd",
-          inputType: 'password'
-        }, {
-          xtype: 'volumefield',
-          listeners: {
-            select: function(self, record, index){
-              lvm__LogicalVolume.get( record.data.id, function( provider, response ){
-                self.ownerCt.dirfield.setValue( response.result.fs.mountpoints[0] );
-                self.ownerCt.dirfield.enable();
-              } );
-            }
-          }
-        }, {
-         xtype: 'textfield',
-          fieldLabel: "{% trans 'Directory' %}",
-          name: "homedir",
-          disabled: (!record),
-          ref: 'dirfield'
-        },{
-            xtype: 'hidden',
-            name: "shell",
-            value: '/bin/true'
-        }]
-        }],
-        buttons: [{
-          text: config.submitButtonText,
-          icon: MEDIA_URL + "/oxygen/16x16/actions/dialog-ok-apply.png",
-          handler: function(self){
-            self.ownerCt.ownerCt.getForm().submit({
-              success: function(provider,response){
-                if(response.result){
-                  ftpGrid.store.reload();
-                  addwin.hide();
-                }
-              }
-            });
-          }
-        }, {
-          text: "{% trans 'Cancel' %}",
-          icon: MEDIA_URL + "/icons2/16x16/actions/gtk-cancel.png",
-          handler: function(self){
-            addwin.hide();
-          }
-        }]
-      }]
-    }));
-    addwin.show();
+Ext.oa.Ftp__User_Panel = Ext.extend(Ext.oa.ShareGridPanel, {
+  api: ftp__User,
+  id: "ftp__user_panel_inst",
+  title: "FTP",
+  texts: {
+    add:     "{% trans 'Add User' %}",
+    edit:    "{% trans 'Edit User' %}",
+    remove:  "{% trans 'Delete User' %}",
   },
-
-  initComponent: function(){
-    var ftpGrid = this;
-    Ext.apply(this, Ext.apply(this.initialConfig, {
-      id: "ftp__user_panel_inst",
-      title: "ftp",
-      viewConfig: {forceFit: true},
-      buttons: [{
-          text: "",
-          icon: MEDIA_URL + "/icons2/16x16/actions/reload.png",
-          tooltip: "{% trans 'Reload' %}",
-          handler: function(self){
-            ftpGrid.store.reload();
-          }
-      }, {
-        text: "{% trans 'Add FTP User' %}",
-        icon: MEDIA_URL + "/icons2/16x16/actions/add.png",
-        handler: function(){
-          ftpGrid.showEditWindow({
-            title: "{% trans 'Add User' %}",
-            submitButtonText: "{% trans 'Create User' %}"
-          });
-        }
-      }, {
-        text: "{%trans 'Edit' %}",
-        icon: MEDIA_URL + "/icons2/16x16/actions/edit-redo.png",
-        handler: function(self){
-          var sm = ftpGrid.getSelectionModel();
-          if( sm.hasSelection() ){
-            var sel = sm.selections.items[0];
-            ftpGrid.showEditWindow({
-            title: "{% trans 'Edit FTP User'%}",
-            submitButtonText: "{% trans 'Edit User'%}"
-          }, sel.data);
+  columns: [{
+    header: "{% trans 'Path' %}",
+    dataIndex: "homedir"
+  }, {
+    header: "{% trans 'User name' %}",
+    dataIndex: "username"
+  }],
+  storefields: [{
+    name: 'volumename',mapping: 'volume',convert: function( val, row ){ return val.name }
+  }],
+  form: {
+    items:[{
+      xtype: 'textfield',
+      name: "username",
+      fieldLabel: "Username"
+    }, {
+      xtype: 'textfield',
+      fieldLabel: "{% trans 'Password' %}",
+      name: "passwd",
+      inputType: 'password'
+    }, {
+      xtype: 'volumefield',
+      listeners: {
+        select: function(self, record, index){
+          lvm__LogicalVolume.get( record.data.id, function( provider, response ){
+            self.ownerCt.dirfield.setValue( response.result.fs.mountpoints[0] );
+            self.ownerCt.dirfield.enable();
+          } );
         }
       }
+    }, {
+      xtype: 'textfield',
+      fieldLabel: "{% trans 'Directory' %}",
+      name: "homedir",
+      disabled: true,
+      ref: 'dirfield'
     },{
-        text: "{% trans 'Delete User' %}",
-        icon: MEDIA_URL + "/icons2/16x16/actions/remove.png",
-        handler: this.deleteFunction,
-        scope: ftpGrid
-      }],
-      keys: [{scope: ftpGrid, key: [Ext.EventObject.DELETE], handler: this.deleteFunction}],
-      store: new Ext.data.DirectStore ({
-        fields: ['id', 'username', 'passwd','shell', 'homedir', 'volume', {
-          name: 'volumename',mapping: 'volume',convert: function( val, row ){ return val.name }
-        }],
-        directFn: ftp__User.all
-      }),
-  viewConfig: { forceFit: true },
-      colModel: new Ext.grid.ColumnModel({
-        defaults: {
-          sortable: true
-        },
-        columns: [ {
-          header: "{% trans 'Path' %}",
-          dataIndex: "homedir"
-        }, {
-          header: "{% trans 'User name' %}",
-          dataIndex: "username"
-        }]
-      })
-    }));
-    Ext.oa.Ftp__User_Panel.superclass.initComponent.apply(this, arguments);
+      xtype: 'hidden',
+      name:  "shell",
+      value: '/bin/true'
+    }]
   },
-   deleteFunction: function(self){
-  var sm = this.getSelectionModel();
-    if( sm.hasSelection() ){
-      var sel = sm.selections.items[0];
-        Ext.Msg.confirm(
-          "{% trans 'Delete Share' %}",
-          interpolate(
-            "{% trans 'Do you really want to delete %s?' %}",[sel.data.homedir]),
-            function(btn){
-              if(btn == 'yes'){
-                ftp__User.remove( sel.data.id, function(provider, response){
-                sel.store.reload();
-                });
-              } 
-           });
-    }
- },
-
-  onRender: function(){
-    Ext.oa.Ftp__User_Panel.superclass.onRender.apply(this, arguments);
-    this.store.reload();
-        var self = this;
-    var menu = new Ext.menu.Menu({
-    items: [{
-            text: 'delete',
-            icon: MEDIA_URL + "/icons2/16x16/actions/remove.png"
-        }],
-        listeners: {
-          itemclick: function(item) {
-                    self.deleteFunction()
-          }
-        }
-   });
-    this.on({
-      'contextmenu': function(event) {
-        if( this.getSelectionModel().hasSelection() ){
-          event.stopEvent();
-          menu.showAt(event.xy);
-        }
-      }
-    });
+  deleteConfirm: function(sel){
+    return interpolate("{% trans 'Do you really want to delete user %s?' %}", [sel.data.username]);
   }
 });
+
 
 Ext.reg("ftp__user_panel", Ext.oa.Ftp__User_Panel);
 
