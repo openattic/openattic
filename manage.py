e7c5e5885051b7baab24b1e81e8b07de83614042
Michael Ziegler <michael.ziegler@it-novum.com>
2012-03-23 11:29:50 +0100

    wrap long lines, remove duplicate import

--- manage.py
+++ manage.py
@@ -15,18 +15,20 @@
  *  GNU General Public License for more details.
 """
 
+import os, sys
 from django.core.management import execute_manager
 
 try:
     import settings # Assumed to be in the same directory.
 except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. "
+        "It appears you've customized things.\nYou'll have to run django-admin.py, passing it your "
+        "settings module.\n(If the file settings.py does indeed exist, it's causing an "
+        "ImportError somehow.)\n" % __file__)
     sys.exit(1)
 
 
 if __name__ == "__main__":
-    import os, sys
     if "OACONFIG" in os.environ and os.environ["OACONFIG"] == "True":
         sys.argv[0] = "oaconfig"
 
