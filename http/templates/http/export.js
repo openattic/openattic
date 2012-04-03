58fbcd1875cbeae8719b09d01c2bf65ef2966032
Michael Ziegler <michael.ziegler@it-novum.com>
2012-04-03 10:48:03 +0200

    transition http module to jsi18n

--- http/templates/http/export.js
+++ http/templates/http/export.js
@@ -1,6 +1,4 @@
-{% load i18n %}
-
-{% comment %}
+/*
  Copyright (C) 2011-2012, it-novum GmbH <community@open-attic.org>
 
  openATTIC is free software; you can redistribute it and/or modify it
@@ -11,14 +9,14 @@
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
-{% endcomment %}
+*/
 
 Ext.namespace("Ext.oa");
 
 Ext.oa.Http__Export_Panel = Ext.extend(Ext.oa.ShareGridPanel, {
   api: http__Export,
   id: "http__export_panel_inst",
-  title: "HTTP",
+  title: gettext("HTTP"),
   window: {
     height: 150
   },
@@ -34,17 +32,17 @@ Ext.oa.Http__Export_Panel = Ext.extend(Ext.oa.ShareGridPanel, {
     }
   }],
   columns: [{
-    header: "{% trans 'Path' %}",
+    header: gettext('Path'),
     width: 350,
     dataIndex: "path"
   },{
-    header: "{% trans 'Browse' %}",
+    header: gettext('Browse'),
     width: 100,
     dataIndex: "volumename",
     renderer: function(val, x, store){
       "use strict";
       return String.format(
-        '<a href="/volumes/{0}" target="_blank" title="{% trans "Browse in new window" %}">' +
+        '<a href="/volumes/{0}" target="_blank" title=gettext("Browse in new window")>' +
         '<img alt="Browser" src="{{ MEDIA_URL }}/oxygen/16x16/places/folder-remote.png">' +
         '</a>',
         val );
@@ -64,7 +62,7 @@ Ext.oa.Http__Export_Panel = Ext.extend(Ext.oa.ShareGridPanel, {
       }
     }, {
       xtype: 'textfield',
-      fieldLabel: "{% trans 'Directory' %}",
+      fieldLabel: gettext('Directory'),
       name: "path",
       disabled: true,
       ref: 'dirfield'
@@ -79,7 +77,7 @@ Ext.oa.Http__Export_Module = Ext.extend(Object, {
   prepareMenuTree: function(tree){
     "use strict";
     tree.appendToRootNodeById("menu_shares", {
-      text: "{% trans 'Web (HTTP)' %}",
+      text: gettext('Web (HTTP)'),
       leaf: true,
       panel: "http__export_panel_inst",
       icon: MEDIA_URL + '/icons2/22x22/mimetypes/www.png',
