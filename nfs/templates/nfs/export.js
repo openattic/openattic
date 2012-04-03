806b42ea7d118faf30151f01d5bc2415e95c89bc
Michael Ziegler <michael.ziegler@it-novum.com>
2012-04-03 10:59:13 +0200

    transition nfs module to jsi18n

--- nfs/templates/nfs/export.js
+++ nfs/templates/nfs/export.js
@@ -1,6 +1,4 @@
-{% load i18n %}
-
-{% comment %}
+/*
  Copyright (C) 2011-2012, it-novum GmbH <community@open-attic.org>
 
  openATTIC is free software; you can redistribute it and/or modify it
@@ -11,7 +9,7 @@
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
-{% endcomment %}
+*/
 
 Ext.namespace("Ext.oa");
 
@@ -20,15 +18,15 @@ Ext.oa.Nfs__Export_Panel = Ext.extend(Ext.oa.ShareGridPanel, {
   id: "nfs__export_panel_inst",
   title: "NFS",
   columns: [{
-    header: "{% trans 'Address' %}",
+    header: gettext('Address'),
     width: 100,
     dataIndex: "address"
   }, {
-    header: "{% trans 'Path' %}",
+    header: gettext('Path'),
     width: 200,
     dataIndex: "path"
   }, {
-    header: "{% trans 'Options' %}",
+    header: gettext('Options'),
     width: 200,
     dataIndex: "options"
   }],
@@ -49,28 +47,28 @@ Ext.oa.Nfs__Export_Panel = Ext.extend(Ext.oa.ShareGridPanel, {
               } );
             }
           }
-        }, "{% trans 'Please select the volume to share.' %}"),
+        }, gettext('Please select the volume to share.')),
         tipify({
           xtype: 'textfield',
-          fieldLabel: "{% trans 'Directory' %}",
+          fieldLabel: gettext('Directory'),
           name: "path",
           ref: 'dirfield',
           disabled: true
-        }, "{% trans 'If you wish to share only a subpath of the volume, enter the path here.' %}" ),
+        }, gettext('If you wish to share only a subpath of the volume, enter the path here.') ),
         {
           xtype: 'textfield',
-          fieldLabel: "{% trans 'Address' %}",
+          fieldLabel: gettext('Address'),
           allowBlank: false,
           name: "address",
           ref: 'addrfield'
         },
         tipify({
           xtype: 'textfield',
-          fieldLabel: "{% trans 'Options' %}",
+          fieldLabel: gettext('Options'),
           name: "options",
           ref: 'optfield',
           value: "rw,no_subtree_check,no_root_squash"
-        },"{% trans 'this is default. rw: read/write rights are given,<br> no_subtree_check means that every file request is going to be checked to make sure that this file is in an exported subdirectory,<br> no_root_squash means share the folder (public), every IP-Adress has access, root can connect as root' %}")
+        },gettext('this is default. rw: read/write rights are given,<br> no_subtree_check means that every file request is going to be checked to make sure that this file is in an exported subdirectory,<br> no_root_squash means share the folder (public), every IP-Adress has access, root can connect as root'))
       ]
     }]
   }
@@ -84,7 +82,7 @@ Ext.oa.Nfs__Export_Module = Ext.extend(Object, {
   prepareMenuTree: function(tree){
     "use strict";
     tree.appendToRootNodeById("menu_shares", {
-      text: "{% trans 'Linux (NFS)' %}",
+      text: gettext('Linux (NFS)'),
       leaf: true,
       icon: MEDIA_URL + '/icons2/22x22/apps/nfs.png',
       panel: 'nfs__export_panel_inst',
