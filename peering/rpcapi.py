987af045b7dd26594356c5f3b7dfd7aad3c0fcc1
Michael Ziegler <michael.ziegler@it-novum.com>
2012-04-18 13:48:24 +0200

    add the name field to peerhost idobjs

--- peering/rpcapi.py
+++ peering/rpcapi.py
@@ -22,6 +22,10 @@ class PeerHostHandler(ModelHandler):
     model  = PeerHost
     fields = ["id", "name"]
 
+    def _idobj(self, obj):
+        """ Return an ID for the given object, including the app label and object name. """
+        return {'id': obj.id, 'app': obj._meta.app_label, 'obj': obj._meta.object_name, 'name': obj.name}
+
     def _override_get(self, obj, data):
         if obj.base_url:
             data['base_url'] = unicode(obj.base_url)
