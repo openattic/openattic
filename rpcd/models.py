e8dd31dcb0c14117a6f11430be81f0dd43892d0b
Michael Ziegler <michael.ziegler@it-novum.com>
2012-04-23 16:21:00 +0200

    move sha1 user authentication to systemd (rest of user stuff is done there too)

--- rpcd/models.py
+++ rpcd/models.py
@@ -29,3 +29,35 @@ class APIKey(models.Model):
         if not self.apikey:
             self.apikey = unicode(uuid.uuid4())
         return models.Model.full_clean(self)
+
+
+import new
+import hashlib
+from django.db.models import signals
+from django.contrib.auth import models as auth_models
+from django.utils.encoding import smart_str
+# http://djangosnippets.org/snippets/389/
+
+def replace_set_password(instance=None, **kwargs):
+    """ Replace the standard *_password functions in the auth model. """
+
+    oldcheck = instance.check_password
+
+    def set_password_crypt(self, raw_password):
+        """ Set the password as unsalted SHA1 so it can be checked using pam_*sql. """
+        self.password = str(hashlib.sha1(smart_str(raw_password)).hexdigest())
+
+    def check_password_crypt(self, raw_password):
+        """ See if the current password was stored in Django format, and if not,
+            auth using the PAM compatible SHA1 hash.
+        """
+        if '$' in self.password:
+            return oldcheck(raw_password)
+        return str(hashlib.sha1( smart_str(raw_password) ).hexdigest()) == self.password
+
+    instance.set_password = new.instancemethod(
+        set_password_crypt, instance, instance.__class__)
+    instance.check_password = new.instancemethod(
+        check_password_crypt, instance, instance.__class__)
+
+signals.post_init.connect(replace_set_password, sender=auth_models.User)
