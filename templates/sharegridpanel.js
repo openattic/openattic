53fb6aead13cd29aa8956879d5bfe0d1273aec30
Michael Ziegler <michael.ziegler@it-novum.com>
2012-06-11 14:08:01 +0200

    deep-copy the button objects too, not only the array that contains references to them

--- templates/sharegridpanel.js
+++ templates/sharegridpanel.js
@@ -45,12 +45,12 @@ Ext.oa.ShareGridPanel = Ext.extend(Ext.grid.GridPanel, {
       // Without the next line, further operations on this.buttons would alter
       // the buttons of the *prototype* instead of the object, thereby breaking
       // class inheritance. Solution: Copy the prototype's buttons to the object.
-      this.buttons = this.buttons.slice();
+      var mybuttons = [];
       for( i = 0; i < this.buttons.length; i++ ){
-        if( typeof this.buttons[i].scope === "undefined" ){
-          this.buttons[i].scope = this;
-        }
+        mybuttons[i] = Ext.apply({}, this.buttons[i]);
+        Ext.applyIf(mybuttons[i], { scope: this });
       }
+      this.buttons = mybuttons;
     }
     Ext.apply(this, Ext.applyIf(this.initialConfig, {
       keys: [{
