c6c1cef2a4a88d52d41019e7dd51c4a44c9ca336
Michael Ziegler <michael.ziegler@it-novum.com>
2012-06-15 16:42:35 +0200

    add draft of an IPMI module

---
+++ ipmi/rpcapi.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# kate: space-indent on; indent-width 4; replace-tabs on;
+
+"""
+ *  Copyright (C) 2011-2012, it-novum GmbH <community@open-attic.org>
+ *
+ *  openATTIC is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2.
+ *
+ *  This package is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+"""
+
+import dbus
+
+from django.conf import settings
+
+from rpcd.handlers import BaseHandler
+
+from systemd.helpers import dbus_to_python
+
+class IpmiHandler(BaseHandler):
+    @classmethod
+    def _get_handler_name(cls):
+        return "ipmi.Sensor"
+
+    def get_all_sensors(self):
+        return dbus_to_python(dbus.SystemBus().get_object(settings.DBUS_IFACE_SYSTEMD, "/ipmi").get_all_sensors())
+
+    def get_most_sensors(self):
+        return dbus_to_python(dbus.SystemBus().get_object(settings.DBUS_IFACE_SYSTEMD, "/ipmi").get_most_sensors())
+
+    def get_sensors_by_type(self, stype):
+        return dbus_to_python(dbus.SystemBus().get_object(settings.DBUS_IFACE_SYSTEMD, "/ipmi").get_sensors_by_type(stype))
+
+    def get_sensor_types(self):
+        return dbus_to_python(dbus.SystemBus().get_object(settings.DBUS_IFACE_SYSTEMD, "/ipmi").get_sensor_types())
+
+RPCD_HANDLERS = [IpmiHandler]
---
+++ ipmi/systemapi.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# kate: space-indent on; indent-width 4; replace-tabs on;
+
+"""
+ *  Copyright (C) 2011-2012, it-novum GmbH <community@open-attic.org>
+ *
+ *  openATTIC is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2.
+ *
+ *  This package is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+"""
+
+import re
+from django.template.loader import render_to_string
+
+from django.contrib.auth.models import User
+
+from systemd       import invoke, logged, BasePlugin, method
+from nagios.models import Command, Service
+
+@logged
+class SystemD(BasePlugin):
+    dbus_path = "/ipmi"
+
+    @method(in_signature="", out_signature="aas")
+    def get_all_sensors(self):
+        ret, out, err = invoke(["ipmitool", "sdr", "list", "all"], return_out_err=True)
+        return [
+            [ part.strip() for part in line.split('|') ]
+            for line in out.split("\n") if line ]
+
+    @method(in_signature="", out_signature="aas")
+    def get_most_sensors(self):
+        ret, out, err = invoke(["ipmitool", "sdr", "list", "full"], return_out_err=True)
+        return [
+            [ part.strip() for part in line.split('|') ]
+            for line in out.split("\n") if line ]
+
+    @method(in_signature="s", out_signature="aas")
+    def get_sensors_by_type(self, stype):
+        ret, out, err = invoke(["ipmitool", "sdr", "type", stype], return_out_err=True)
+        return [
+            [ part.strip() for part in line.split('|') ]
+            for line in out.split("\n") if line ]
+
+    @method(in_signature="", out_signature="as")
+    def get_sensor_types(self):
+        # ipmitool returns lines such as:
+        # "\tSlot / Connector            System ACPI Power State".
+        ret, out, err = invoke(["ipmitool", "sdr", "type", "list"], return_out_err=True)
+        return reduce( lambda x, y: x + y,
+            [ re.split( r"\s{2,}", line.strip() ) for line in out.split("\n")[1:] if line ] )
