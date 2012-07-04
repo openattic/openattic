10fa818dc51399e5571b79dec4ebe5096943cade
Michael Ziegler <michael.ziegler@it-novum.com>
2012-07-04 16:35:49 +0200

    update smb.conf during syncdb

---
+++ samba/management/__init__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# kate: space-indent on; indent-width 4; replace-tabs on;
+
+"""
+ *  Copyright (C) 2011-2012, it-novum GmbH <community@open-attic.org>
+ *
+ *  openATTIC is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2.
+ *
+ *  This package is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+"""
+
+import dbus
+
+from django.conf import settings
+from django.db.models import signals
+
+import samba.models
+from samba.models import Share
+
+def writeconf(app, created_models, verbosity, **kwargs):
+    try:
+        samba = dbus.SystemBus().get_object(settings.DBUS_IFACE_SYSTEMD, "/samba")
+    except dbus.DBusException:
+        pass
+    else:
+        samba.writeconf()
+
+signals.post_syncdb.connect(writeconf, sender=samba.models)
