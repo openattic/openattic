320fec1df792e8aca0788fee13939f70e64b017f
Michael Ziegler <michael.ziegler@it-novum.com>
2012-07-13 15:09:19 +0200

    add add-host command

---
+++ ifconfig/management/commands/add-host.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# kate: space-indent on; indent-width 4; replace-tabs on;
+
+"""
+ *  Copyright (C) 2011-2012, it-novum GmbH <community@open-attic.org>
+ *
+ *  openATTIC is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2.
+ *
+ *  This package is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+"""
+
+import os
+
+from optparse import make_option
+
+from django.core.management.base import BaseCommand
+
+from ifconfig.models import Host
+
+class Command( BaseCommand ):
+    help = "Make sure a Host entry for this host exists."
+
+    def handle(self, **options):
+        try:
+            host = Host.objects.get_current()
+        except Host.DoesNotExist:
+            host = Host(name=socket.gethostname())
+            host.save()
