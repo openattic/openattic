bbb47fae48d68a8a3e189adc29b4f06011c7bfaa
Michael Ziegler <michael.ziegler@it-novum.com>
2012-07-17 14:18:29 +0200

    add xmlrpclib.Fault to exception translator

---
+++ rpcd/exceptionhelper.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# kate: space-indent on; indent-width 4; replace-tabs on;
+
+"""
+ *  Copyright (C) 2011-2012, it-novum GmbH <community@open-attic.org>
+ *
+ *  openATTIC is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2.
+ *
+ *  This package is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+"""
+
+import re
+import exceptions
+
+from django.core.exceptions import ValidationError
+from xmlrpclib import Fault
+
+def translate_exception(rpcfault):
+    """ Examines an XMLRPClib `Fault' instance and returns the "real" exception wrapped in it.
+
+        In case this translation fails or this is not an xmlrpclib.Fault instance,
+        the original exception will be returned as-is.
+    """
+    if not isinstance(rpcfault, Fault):
+        return rpcfault
+
+    # faultString is always:
+    # <class|type '$class'>:reason
+
+    m = re.match( r"""<(class|type) '(?P<exctype>[a-zA-Z.]+)'>:(?P<reason>.*)""", rpcfault.faultString )
+    if not m:
+        return rpcfault
+
+    if m.group('exctype') == "django.core.exceptions.ValidationError":
+        err = ValidationError("")
+        # Reason is a repr() of a dict containing lists of messages.
+        # That is, it should be, which is why we harden eval() a bit.
+        err.message_dict = eval( m.group("reason"), {"__builtins__": None}, {} )
+        return err
+
+    module, classname = m.group('exctype').rsplit(".", 1)
+    if module == "exceptions":
+        # reason is a "string", so we need to strip away the "".
+        return getattr(exceptions, classname)(m.group("reason")[1:-1])
+
+    return rpcfault
