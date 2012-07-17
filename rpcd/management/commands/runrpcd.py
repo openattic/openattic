29619534a0df4b0f54ef1d44be4f6f84d9a9c3e7
Michael Ziegler <michael.ziegler@it-novum.com>
2012-07-17 15:38:41 +0200

    Backed out changeset 5d7d6706513c: it returns stuff from other hosts that cannot be retrieved via our api

--- rpcd/management/commands/runrpcd.py
+++ rpcd/management/commands/runrpcd.py
@@ -255,20 +255,12 @@ class RPCd(object):
 
     def get_related(self, idobj):
         """ Return objects that reference the object given by the ID dictionary. """
-        model = models.get_model(idobj["app"], idobj["obj"])
-        if hasattr(model, "all_objects"):
-            obj = model.all_objects.get(id=idobj["id"])
-        else:
-            obj = model.objects.get(id=idobj["id"])
+        obj = ModelHandler._get_object_by_id_dict(idobj)
         relids = []
         for relobj in ( obj._meta.get_all_related_objects() + obj._meta.get_all_related_many_to_many_objects() ):
-            if hasattr(relobj.model, "all_objects"):
-                relmanager = relobj.model.all_objects
-            else:
-                relmanager = relobj.model.objects
             relids.extend([
                 ModelHandler._get_handler_for_model(relobj.model)(None)._idobj(relmdl)
-                for relmdl in relmanager.filter( **{ relobj.field.name: obj } )
+                for relmdl in relobj.model.objects.filter( **{ relobj.field.name: obj } )
                 ])
         return relids
 
