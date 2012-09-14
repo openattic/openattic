36a166bacb1fa9c083d79b631378f05fc3d32bba
Michael Ziegler <michael.ziegler@it-novum.com>
2012-09-14 12:57:21 +0200

    remove _idobj methods from iscsi rpcapi

--- iscsi/rpcapi.py
+++ iscsi/rpcapi.py
@@ -21,18 +21,10 @@ from iscsi.models import Target, Lun, Initiator
 class IscsiTargetHandler(ModelHandler):
     model = Target
 
-    def _idobj(self, obj):
-        """ Return an ID for the given object, including the app label and object name. """
-        return {'id': obj.id, 'app': obj._meta.app_label, 'obj': obj._meta.object_name, 'name': obj.name}
-
 class IscsiLunHandler(ModelHandler):
     model = Lun
 
 class IscsiInitiatorHandler(ModelHandler):
     model = Initiator
-
-    def _idobj(self, obj):
-        """ Return an ID for the given object, including the app label and object name. """
-        return {'id': obj.id, 'app': obj._meta.app_label, 'obj': obj._meta.object_name, 'name': obj.name}
 
 RPCD_HANDLERS = [IscsiTargetHandler, IscsiLunHandler, IscsiInitiatorHandler]
