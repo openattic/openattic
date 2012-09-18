634067f3392a902646bc658b39383656304c7d57
Michael Ziegler <michael.ziegler@it-novum.com>
2012-09-18 15:56:38 +0200

    add /etc/openattic/templates and /usr/local/share/openattic/templates to TEMPLATE_DIRS

--- settings.py
+++ settings.py
@@ -201,6 +201,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/etc/openattic/templates',
+    '/usr/local/share/openattic/templates',
     join(PROJECT_ROOT, 'templates'),
 )
 
