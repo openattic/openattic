1b1b30486a920c7ed0982967761f9a93b6e9329b
Michael Ziegler <michael.ziegler@it-novum.com>
2012-12-19 13:10:38 +0100

    add rpcapi#

---
+++ twraid/rpcapi.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# kate: space-indent on; indent-width 4; replace-tabs on;
+
+"""
+ *  Copyright (C) 2011-2012, it-novum GmbH <community@open-attic.org>
+ *
+ *  openATTIC is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2.
+ *
+ *  This package is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+"""
+
+from rpcd.handlers import ModelHandler
+from rpcd.handlers import ProxyModelHandler
+
+from twraid.models import Controller, Enclosure, Unit, Disk
+
+class ControllerHandler(ModelHandler):
+    model = Controller
+
+class EnclosureHandler(ModelHandler):
+    model = Enclosure
+
+class UnitHandler(ModelHandler):
+    model = Unit
+
+class DiskHandler(ModelHandler):
+    model = Disk
+
+#class NfsExportProxy(ProxyModelHandler, NfsExportHandler):
+    #pass
+
+RPCD_HANDLERS = [ControllerHandler, EnclosureHandler, UnitHandler, DiskHandler]
