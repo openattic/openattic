4c300ac420c48c3433cc172ffa8abe6e4a5dc422
Michael Ziegler <michael.ziegler@it-novum.com>
2013-04-11 17:32:57 +0200

    remove SEG_PE_RANGES from lvs output because it messes up the results

--- lvm/models.py
+++ lvm/models.py
@@ -299,7 +299,6 @@ class LogicalVolume(models.Model):
         """ LV information from LVM. """
         if self._lvm_info is None:
             self._lvm_info = dbus_to_python(self.lvm.lvs())[self.name]
-            self._lvm_info['LVM2_SEG_PE_RANGES'] = self._lvm_info['LVM2_SEG_PE_RANGES'].split(' ')
         return self._lvm_info
 
     @property
