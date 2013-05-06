fb37e63f5662ace47c3837d8445f07ff5a924595
Michael Ziegler <michael.ziegler@it-novum.com>
2013-05-06 14:28:37 +0200

    create a default admin user as part of the installation

---
+++ rpcd/management/commands/makedefaultadmin.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# kate: space-indent on; indent-width 4; replace-tabs on;
+
+"""
+ *  Copyright (C) 2011-2012, it-novum GmbH <community@open-attic.org>
+ *
+ *  openATTIC is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2.
+ *
+ *  This package is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+"""
+
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models  import User
+
+class Command( BaseCommand ):
+    help = "If there is no other SuperUser, creates an openattic/openattic default user."
+
+    def handle(self, **options):
+        if User.objects.filter( is_superuser=True ).count() == 0:
+            admin = User(username="openattic")
+            admin.save()
+            admin.set_password("openattic")
+            admin.save()
+            print 'Created default user "openattic" with password "openattic".'
+        else:
+            print "We have an admin already, not creating default user."
