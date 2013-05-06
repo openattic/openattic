e2db8fbb8a09045eefa49cf66bc1a409193e682a
Michael Ziegler <michael.ziegler@it-novum.com>
2013-05-06 14:57:10 +0200

    turn install-cli command into a python script that queries the database for its default user

---
+++ rpcd/management/commands/install-cli.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# kate: space-indent on; indent-width 4; replace-tabs on;
+
+"""
+ *  Copyright (C) 2011-2012, it-novum GmbH <community@open-attic.org>
+ *
+ *  openATTIC is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2.
+ *
+ *  This package is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+"""
+
+import os
+
+from ConfigParser import ConfigParser
+
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models  import User
+
+class Command( BaseCommand ):
+    help = "Create a default oacli config file if none exists."
+
+    def handle(self, **options):
+        if os.path.exists("/etc/oopenattic/cli.conf"):
+            print "/etc/openattic/cli.conf already exists, quitting"
+            return
+
+        try:
+            admin = User.objects.get(username="openattic", is_superuser=True)
+        except User.DoesNotExist:
+            admin = User.objects.filter( is_superuser=True )[0]
+
+        key = APIKey( owner=admin, description="oacli access", active=True )
+        key.full_clean()
+        key.save()
+
+        conf = ConfigParser()
+        conf.add_section("options")
+        conf.set("options", "connect", "http://__:%s@localhost:31234/" % key.apikey)
+        conf.set("uidcheck", True)
+        conf.write( open( "/etc/oopenattic/cli.conf", "wb" ) )
+
