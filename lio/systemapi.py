0c474ecc3700b4cca5d8e5a0006194263fc9cf9f
Michael Ziegler <michael.ziegler@it-novum.com>
2013-06-20 10:41:19 +0200

    add code to create backstores, targets, tpgs, luns and portals

--- lio/systemapi.py
+++ lio/systemapi.py
@@ -15,18 +15,65 @@
 """
 
 from rtslib.root   import RTSRoot
-from rtslib.target import FabricModule, Target, TPG
+from rtslib        import target
 from rtslib.tcm    import IBlockBackstore, FileIOBackstore
 
 from systemd       import invoke, logged, LockingPlugin, method
 
-from lio.models    import Backstore, Target, Portal, TPG, LUN, ACL
+from lio           import models
 
 
 @logged
 class SystemD(LockingPlugin):
     dbus_path = "/lio"
 
-    @method(in_signature="", out_signature="")
-    def writeconf(self):
-        invoke(["echo", "ohai"])
+    @method(in_signature="i", out_signature="")
+    def backstore_create(self, id):
+        mdl_bs = models.Backstore.objects.get(id=id)
+        if bdl_bs.type == "iblock":
+            lio_bs = target.IBlockBackstore(0)
+        else:
+            lio_bs = target.FileIOBackstore(0)
+        storage = None
+        for sobj in lio_bs.storage_objects:
+            if sobj.name == mdl_bs.volume.name:
+                storage = sobj
+                break
+        if storage is None:
+            storage = lio_bs.storage_object(mdl_bs.volume.name, mdl_bs.volume.path)
+        mdl_bs.wwn = storage.wwn
+        mdl_bs.save()
+
+    @method(in_signature="i", out_signature="")
+    def target_create(self, id):
+        mdl_tgt = models.Target.objects.get(id=id)
+        fabric  = target.FabricModule(mdl_tgt.type)
+        if not fabric.exists:
+            for mod in fabric.load(): pass
+        if not fabric.exists:
+            raise SystemError("failed to load fabric module for '%s'" % mdl_tgt.type)
+        lio_tgt = target.Target(fabric, mdl_tgt.wwn)
+
+    @method(in_signature="i", out_signature="")
+    def tpg_create(self, id):
+        mdl_tpg = models.TPG.objects.get(id=id)
+        lio_tgt = mdl_tpg.target.lio_object
+        lio_tpg = target.TPG(lio_tgt, mdl_tpg.tag)
+        tpg.set_attribute("authentication",             str(int(mdl_tpg.chapauth)))
+        tpg.set_attribute("generate_node_acls",         "0")
+        tpg.set_attribute("demo_mode_write_protect",    "0")
+        tpg.set_parameter("InitialR2T",                 "No")
+        tpg.enable = True
+
+    @method(in_signature="i", out_signature="")
+    def lun_create(self, id):
+        mdl_lun = models.LUN.objects.get(id=id)
+        lio_tpg = mdl_lun.tpg.lio_object
+        tpg.lun(mdl_lun.lun_id, mdl_lun.storageobj.lio_object, "%s at %s" % (self.storageobj.volume.name, Host.objects.get_current().name))
+
+    @method(in_signature="i", out_signature="")
+    def portal_create(self, id, tpg_id):
+        mdl_ptl = models.Portal.objects.get(id=id)
+        mdl_tpg = models.TPG.objects.get(id=tpg_id)
+        lio_tpg = mdl_tpg.lio_object
+        lio_tpg.portal(mdl_ptl.ipaddress.host_part, mdl_ptl.port)
