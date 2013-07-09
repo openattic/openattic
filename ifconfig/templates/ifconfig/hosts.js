2f4545d248be96546045c6bf7701ca67ddf40015
Michael Ziegler <michael.ziegler@it-novum.com>
2013-07-09 10:04:08 +0200

    remove hostgroups from 'Hosts and Groups', turning it into 'Hosts'

--- ifconfig/templates/ifconfig/hosts.js
+++ ifconfig/templates/ifconfig/hosts.js
@@ -145,11 +145,6 @@ Ext.oa.Ifconfig__Host_Groups_Panel = Ext.extend(Ext.Panel, {
   initComponent: function(){
     "use strict";
 
-    var hostgroupstore = new Ext.data.JsonStore({
-      id: "hostgroupstore",
-      fields: ["app", "obj", "id", "__unicode__"]
-    });
-
     Ext.apply(this, Ext.apply(this.initialConfig, {
       id: "ifconfig__host_group_panel_inst",
       title: gettext('Hosts and Groups'),
@@ -161,141 +156,17 @@ Ext.oa.Ifconfig__Host_Groups_Panel = Ext.extend(Ext.Panel, {
         listeners: {
           cellclick: function( self, rowIndex, colIndex, evt ){
             var record = self.getStore().getAt(rowIndex);
-            hostgroupstore.host_id = record.data.id;
-            hostgroupstore.loadData(record.json.hostgroup_set);
             Ext.getCmp("ifconfig__host_attributes_panel_inst").setHost(record.data);
           }
         },
       }, {
         layout: "border",
-        region: "south",
-        height: (Ext.lib.Dom.getViewHeight() - 100) / 2,
-        id:    "ifconfig__host_south_panel_inst",
+        region: "east",
+        width: (Ext.lib.Dom.getViewWidth() - 200) / 2,
         split: true,
-        items: [{
-          region: "center",
-          title: "Host Attributes",
-          id:    "ifconfig__host_attributes_panel_inst",
-          xtype: 'ifconfig__host_attributes_panel'
-        }, {
-          region: "east",
-          width: (Ext.lib.Dom.getViewWidth() - 200) / 2,
-          title: "Host Groups",
-          id:    "ifconfig__host_hostgroups_panel_inst",
-          xtype: 'grid',
-          split: true,
-          viewConfig: { forceFit: true },
-          store: hostgroupstore,
-          colModel: new Ext.grid.ColumnModel({
-            defaults: {
-              sortable: true
-            },
-            columns: [{
-              header: "Host Group",
-              dataIndex: "__unicode__"
-            }]
-          }),
-          enableDragDrop: true,
-          ddGroup: "ifconfig__host_hostgroup",
-          buttons: [{
-            text: gettext('Host Groups'),
-            icon: MEDIA_URL + "/icons2/16x16/actions/gtk-execute.png",
-            handler: function(){
-              var addwin = new Ext.Window({
-                x: Ext.lib.Dom.getViewWidth() - 550,
-                y: Ext.lib.Dom.getViewHeight() - 350,
-                height: 300,
-                width: 500,
-                frame: true,
-                layout: 'fit',
-                title: 'Host Groups',
-                items: {
-                  xtype: 'sharegridpanel',
-                  api: ifconfig__HostGroup,
-                  ddGroup: "ifconfig__host_hostgroup",
-                  enableDrag: true,
-                  viewConfig: { forceFit: true },
-                  columns: [{
-                    header: "Name",
-                    dataIndex: "__unicode__"
-                  }],
-                  texts: {
-                    add:     gettext("Add Host Group"),
-                    edit:    gettext("Edit Host Group"),
-                    remove:  gettext("Delete Host Group"),
-                    confirm: gettext('Do you really want to delete host group %s?')
-                  },
-                  form: {
-                    items: [{
-                      xtype: 'fieldset',
-                      title: 'Host Group',
-                      layout: 'form',
-                      items: [{
-                        xtype: 'textfield',
-                        fieldLabel: gettext('Name'),
-                        allowBlank: false,
-                        name: "name"
-                      }]
-                    }]
-                  }
-                }
-              });
-              addwin.show();
-            }
-          }, {
-            text: gettext("Remove Host group"),
-            handler: function(){
-              Ext.getCmp("ifconfig__host_hostgroups_panel_inst").removeSelected();
-            }
-          }],
-          keys: [{
-            key: [Ext.EventObject.DELETE],
-            handler: function(self){
-              Ext.getCmp("ifconfig__host_hostgroups_panel_inst").removeSelected();
-            }
-          }],
-          removeSelected: function(){
-            var self = this;
-            var sm = self.getSelectionModel();
-            var sel;
-            if( sm.hasSelection() ){
-              sel = sm.getSelected();
-              ifconfig__Host.set(self.store.host_id, {"hostgroup_set__remove": [sel.data]}, function(provider, response){
-                self.store.remove(sel);
-              });
-            }
-          },
-          listeners: {
-            afterrender: function(self){
-              var droptarget_el =  self.getView().scroller.dom;
-              var droptarget = new Ext.dd.DropTarget(droptarget_el, {
-                ddGroup    : 'ifconfig__host_hostgroup',
-                notifyDrop : function(ddSource, e, data){
-                  var records =  ddSource.dragData.selections;
-                  var addrecords = [];
-                  for( var i = 0; i < records.length; i++ ){
-                    if( self.store.findExact("id", records[i].data.id) === -1 ){
-                      addrecords.push({
-                        'app': 'ifconfig',
-                        'obj': 'HostGroup',
-                        'id':  records[i].data.id,
-                        '__unicode__': records[i].data.__unicode__
-                      });
-                    }
-                  }
-                  if(addrecords.length > 0){
-                    ifconfig__Host.set(self.store.host_id, {"hostgroup_set__add": addrecords}, function(provider, response){
-                      for( var i = 0; i < addrecords.length; i++ ){
-                        self.store.add([new Ext.data.Record(addrecords[i])]);
-                      }
-                    });
-                  }
-                  return true;
-                }
-              });
-            }
-          }
-        }]
+        title: gettext("Host Attributes"),
+        id:    "ifconfig__host_attributes_panel_inst",
+        xtype: 'ifconfig__host_attributes_panel'
       }]
     }));
     Ext.oa.Ifconfig__Host_Groups_Panel.superclass.initComponent.apply(this, arguments);
@@ -315,7 +186,7 @@ Ext.oa.Ifconfig__Host_Module = Ext.extend(Object, {
   prepareMenuTree: function(tree){
     "use strict";
     tree.appendToRootNodeById("menu_system", {
-      text: gettext('Hosts and Groups'),
+      text: gettext('Hosts'),
       leaf: true,
       icon: MEDIA_URL + '/icons2/22x22/apps/nfs.png',
       panel: 'ifconfig__host_group_panel_inst',
