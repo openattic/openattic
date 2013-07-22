cf50ea410b60769aefde215b706bbaa1ded28248
Tatjana Dehler <tatjana.dehler@it-novum.com>
2013-07-22 12:02:40 +0200

    load datastore by datacenter- and datastore-namen

--- lvm/templates/lvm/snapcore.js
+++ lvm/templates/lvm/snapcore.js
@@ -665,7 +665,8 @@ Ext.oa.LVM__Snapcore_Panel = Ext.extend(Ext.Panel, {
                             {
                               for(var ds in config['plugin_data'][plugin][host_id]['children'])
                               {
-                                plugin_func.getVolume(host_id, ds, function(result, response){
+                                var dc = config['plugin_data'][plugin][host_id]['children'][ds]['dc_id'];
+                                plugin_func.getVolume(host_id, dc, ds, function(result, response){
                                   if(response.type !== 'exception'){
                                     volumes.push(result.volume);
                                   }
