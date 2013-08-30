f0e09d39e65eab93b41207ce6e73e308acbe330d
Tatjana Dehler <tatjana.dehler@it-novum.com>
2013-08-30 16:14:22 +0200

    extended create_items and find_relation function

--- lvm/snapcore.py
+++ lvm/snapcore.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # kate: space-indent on; indent-width 4; replace-tabs on;
 
+from ifconfig.models import Host
+
 """ 
 """
 
@@ -53,24 +55,42 @@ class Plugin(object):
       #    _save_items(hostconf, models)
 
     def create_items(self, conf_dict):
-        def _create_subitem(confobj, parent):
+        def _create_subitem(confobj, obj_id, parent_model_inst, parent, modelstack):
+            print "_create_subitem"
             merged_data = parent.data.copy()
             merged_data.update(confobj["data"])
-            if "children" in confobj and confobj["children"]:
-                obj = Container(merged_data)
-                parent.children.append(obj)
-                for childconf in confobj["children"].values():
-                    _create_subitem(childconf, obj)
+
+            print "model_instance = self.find_relation(%r, %r, %r)" % (parent_model_inst, modelstack[0], obj_id)
+            if isinstance(modelstack[0], tuple):
+                childmodel, childconfmodel = modelstack[0]
             else:
-                obj = Target(merged_data)
+                childmodel = modelstack[0]
+            model_instance = self.find_relation(parent_model_inst, childmodel, obj_id)
+            print "model", model_instance
+
+            if "children" in confobj and confobj["children"]:
+                obj = Container(merged_data, model_instance)
                 parent.children.append(obj)
-        plugindata = Container({})
-        for hostconf in conf_dict[self.plugin_name].values():
-            _create_subitem(hostconf, plugindata)
+                for child_id, child_conf in confobj["children"].items():
+                  _create_subitem(child_conf, child_id, model_instance, obj, modelstack[1:])
+            else:
+                obj = Target(merged_data, model_instance)
+                parent.children.append(obj)
+
+        plugindata = Container({}, None)
+
+        for host_id, host_conf in conf_dict[self.plugin_name].items():
+            host_model = Host.objects.get(name=host_id)
+            _create_subitem(host_conf, None, host_model, plugindata, self.models)
+
         return plugindata
 
-    def find_relation(obj, rel_model, rel_obj_name):
+    def find_relation(self, obj, rel_model, rel_obj_name=None):
+        print obj
         for related in obj._meta.get_all_related_objects():
             if related.model == rel_model:
                 rel_obj = getattr(obj, related.get_accessor_name())
-                return rel_obj.get_or_create(name=rel_obj_name)
+                if rel_obj_name is not None:
+                    return rel_obj.get_or_create(name=rel_obj_name)[0]
+                else:
+                    return rel_obj.all()[0]
