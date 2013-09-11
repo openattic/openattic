1622be652a0d2f1063ae29c6e69b0b1349b82408
Michael Ziegler <michael.ziegler@it-novum.com>
2013-09-11 14:33:26 +0200

    update the initial migration

--- volumes/migrations/0001_initial.py
+++ volumes/migrations/0001_initial.py
@@ -13,22 +13,20 @@ class Migration(SchemaMigration):
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('content_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'])),
             ('object_id', self.gf('django.db.models.fields.PositiveIntegerField')()),
+            ('capflags', self.gf('django.db.models.fields.BigIntegerField')()),
         ))
         db.send_create_signal('volumes', ['BlockVolume'])
-
-        # Adding model 'FileSystem'
-        db.create_table('volumes_filesystem', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('fstype', self.gf('django.db.models.fields.CharField')(max_length=250)),
-        ))
-        db.send_create_signal('volumes', ['FileSystem'])
 
         # Adding model 'FileSystemVolume'
         db.create_table('volumes_filesystemvolume', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('content_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'])),
             ('object_id', self.gf('django.db.models.fields.PositiveIntegerField')()),
-            ('filesystem', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['volumes.FileSystem'])),
+            ('capflags', self.gf('django.db.models.fields.BigIntegerField')()),
+            ('filesystem', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('owner', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], blank=True)),
+            ('fswarning', self.gf('django.db.models.fields.IntegerField')(default=75)),
+            ('fscritical', self.gf('django.db.models.fields.IntegerField')(default=85)),
         ))
         db.send_create_signal('volumes', ['FileSystemVolume'])
 
@@ -37,14 +35,40 @@ class Migration(SchemaMigration):
         # Deleting model 'BlockVolume'
         db.delete_table('volumes_blockvolume')
 
-        # Deleting model 'FileSystem'
-        db.delete_table('volumes_filesystem')
-
         # Deleting model 'FileSystemVolume'
         db.delete_table('volumes_filesystemvolume')
 
 
     models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
         'contenttypes.contenttype': {
             'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
             'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
@@ -54,21 +78,21 @@ class Migration(SchemaMigration):
         },
         'volumes.blockvolume': {
             'Meta': {'object_name': 'BlockVolume'},
+            'capflags': ('django.db.models.fields.BigIntegerField', [], {}),
             'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'object_id': ('django.db.models.fields.PositiveIntegerField', [], {})
-        },
-        'volumes.filesystem': {
-            'Meta': {'object_name': 'FileSystem'},
-            'fstype': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
         },
         'volumes.filesystemvolume': {
             'Meta': {'object_name': 'FileSystemVolume'},
+            'capflags': ('django.db.models.fields.BigIntegerField', [], {}),
             'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'filesystem': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['volumes.FileSystem']"}),
+            'filesystem': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'fscritical': ('django.db.models.fields.IntegerField', [], {'default': '85'}),
+            'fswarning': ('django.db.models.fields.IntegerField', [], {'default': '75'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {})
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'blank': 'True'})
         }
     }
 
