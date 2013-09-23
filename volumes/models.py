ed6978c19b7ae361c38ea525a635d61526a734be
Michael Ziegler <michael.ziegler@it-novum.com>
2013-09-23 10:43:33 +0200

    add VolumePool model and decorator

--- volumes/models.py
+++ volumes/models.py
@@ -27,6 +27,18 @@ class CapabilitiesAwareManager(models.Manager):
         return self.extra(where=[self.model._meta.db_table + '.capflags & %s = %s'], params=[capability.flag, capability.flag])
 
 
+class VolumePool(models.Model):
+    content_type= models.ForeignKey(ContentType)
+    object_id   = models.PositiveIntegerField()
+    volumepool  = generic.GenericForeignKey()
+    capflags    = models.BigIntegerField()
+
+    objects     = CapabilitiesAwareManager()
+
+    class Meta:
+        unique_together = (('content_type', 'object_id'),)
+
+
 class AbstractVolume(models.Model):
     content_type= models.ForeignKey(ContentType)
     object_id   = models.PositiveIntegerField()
