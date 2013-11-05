a8b822e859c8c910226f84ce2d0aa08e6e833a7e
Michael Ziegler <michael.ziegler@it-novum.com>
2013-11-05 12:34:17 +0100

    add dependency to lvm/0001_initial

--- ftp/migrations/0001_initial.py
+++ ftp/migrations/0001_initial.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("lvm", "0001_initial"),
+    )
+
     needed_by = (
         ("lvm", "0002_volumesapp"),
     )
