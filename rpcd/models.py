b33979dcfc0d1748a14233a3dafed92ecb34ca9f
Michael Ziegler <michael.ziegler@it-novum.com>
2014-01-14 16:35:34 +0100

    remove the password check for unsalted sha1

--- rpcd/models.py
+++ rpcd/models.py
@@ -29,29 +29,3 @@ class APIKey(models.Model):
         if not self.apikey:
             self.apikey = unicode(uuid.uuid4())
         return models.Model.full_clean(self)
-
-
-import new
-import hashlib
-from django.db.models import signals
-from django.contrib.auth import models as auth_models
-from django.utils.encoding import smart_str
-# http://djangosnippets.org/snippets/389/
-
-def replace_set_password(instance=None, **kwargs):
-    """ Replace the standard *_password functions in the auth model. """
-
-    oldcheck = instance.check_password
-
-    def check_password_crypt(self, raw_password):
-        """ See if the current password was stored in Django format, and if not,
-            auth using the PAM compatible SHA1 hash.
-        """
-        if '$' in self.password:
-            return oldcheck(raw_password)
-        return str(hashlib.sha1( smart_str(raw_password) ).hexdigest()) == self.password
-
-    instance.check_password = new.instancemethod(
-        check_password_crypt, instance, instance.__class__)
-
-signals.post_init.connect(replace_set_password, sender=auth_models.User)
