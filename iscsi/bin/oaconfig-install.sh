27767b4eb5967e8f3e109d080637751882a9c667
Michael Ziegler <michael.ziegler@it-novum.com>
2014-02-14 16:18:37 +0100

    remove IET iscsi module

--- iscsi/bin/oaconfig-install.sh
+++
@@ -1,5 +0,0 @@
-if grep -q 'ISCSITARGET_ENABLE=false' /etc/default/iscsitarget; then
-	# Enable IETd
-	sed -i -e 's/ISCSITARGET_ENABLE=false/ISCSITARGET_ENABLE=true/' /etc/default/iscsitarget
-	invoke-rc.d iscsitarget start
-fi
