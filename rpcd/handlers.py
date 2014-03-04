be2f22a790de922ed5c9af5e74fe5fe311ee6a9a
Michael Ziegler <michael.ziegler@it-novum.com>
2014-03-04 17:34:56 +0100

    properly log connection errors instead of printing to stderr

--- rpcd/handlers.py
+++ rpcd/handlers.py
@@ -519,7 +519,8 @@ class ProxyModelHandler(ProxyModelBaseHandler):
                 except socket.error, err:
                     if err.errno in (errno.ECONNREFUSED, errno.ECONNABORTED, errno.ECONNRESET,
                             errno.EHOSTUNREACH, errno.ENETUNREACH, errno.ETIMEDOUT) or isinstance(err, socket.timeout):
-                        print >> sys.stderr, "Connection to peer %s failed: %s" % (peer.host, err)
+                        import logging
+                        logging.error("Connection to peer %s failed: %s" % (peer.host, err))
                         continue
                     else:
                         raise
