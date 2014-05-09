13c5ac3e4b56ea01888cf4d22dafbb829f402c00
Michael Ziegler <michael.ziegler@it-novum.com>
2014-05-09 14:17:26 +0000

    [DJANGO 1.6] update manage.py

--- manage.py
+++ manage.py
@@ -15,21 +15,12 @@
  *  GNU General Public License for more details.
 """
 
-import os, sys
-from django.core.management import execute_manager
-
-try:
-    import settings # Assumed to be in the same directory.
-except ImportError:
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. "
-        "It appears you've customized things.\nYou'll have to run django-admin.py, passing it your "
-        "settings module.\n(If the file settings.py does indeed exist, it's causing an "
-        "ImportError somehow.)\n" % __file__)
-    sys.exit(1)
-
+import os
+import sys
 
 if __name__ == "__main__":
-    if "OACONFIG" in os.environ and os.environ["OACONFIG"] == "True":
-        sys.argv[0] = "oaconfig"
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
-    execute_manager(settings)
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
