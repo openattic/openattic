7ca834c6163475cf2a49d487d4f68d9ee745a80f
Michael Ziegler <michael.ziegler@it-novum.com>
2014-05-28 10:16:47 +0200

    add descriptive failure messages

--- systemd/tests.py
+++ systemd/tests.py
@@ -31,51 +31,51 @@ class LockutilsTestCase(TestCase):
             os.unlink("/tmp/testlock")
 
     def test_acquire_nonexistant_lock(self):
-        self.assertFalse(os.path.exists("/tmp/testlock"))
+        self.assertFalse(os.path.exists("/tmp/testlock"), msg="Lock exists beforehand")
 
         lock = acquire_lock("/tmp/testlock")
-        self.assertTrue(os.path.exists("/tmp/testlock"))
+        self.assertTrue(os.path.exists("/tmp/testlock"),  msg="Lock has not been created")
         self.assertTrue(isinstance(lock, tuple))
 
         release_lock(lock)
-        self.assertFalse(os.path.exists("/tmp/testlock"))
+        self.assertFalse(os.path.exists("/tmp/testlock"), msg="Lock has not been removed")
 
     def test_acquire_existing_lock(self):
         if not os.path.exists("/tmp/testlock"):
             # create an empty testlock file
             open("/tmp/testlock", "a").close()
 
-        self.assertTrue(os.path.exists("/tmp/testlock"))
+        self.assertTrue(os.path.exists("/tmp/testlock"),  msg="Lock has not been created (we need it to exist for this test)")
 
         lock = acquire_lock("/tmp/testlock")
-        self.assertTrue(os.path.exists("/tmp/testlock"))
+        self.assertTrue(os.path.exists("/tmp/testlock"),  msg="Lock has been removed while acquiring")
         self.assertIsInstance(lock, tuple)
         release_lock(lock)
         # Existing locks are not supposed to get unlink()ed
-        self.assertTrue(os.path.exists("/tmp/testlock"))
+        self.assertTrue(os.path.exists("/tmp/testlock"),  msg="Pre-existing lock has been removed")
 
     def test_acquire_lock_fails_when_acquired(self):
-        self.assertFalse(os.path.exists("/tmp/testlock"))
+        self.assertFalse(os.path.exists("/tmp/testlock"), msg="Lock exists beforehand")
         lock = acquire_lock("/tmp/testlock")
 
         start = time()
         with mock.patch("systemd.lockutils.logging") as mock_logging:
             with self.assertRaises(AlreadyLocked):
                 acquire_lock("/tmp/testlock", 0)
-            self.assertTrue(mock_logging.error.called)
+            self.assertTrue(mock_logging.error.called,    msg="Error has not been logged")
 
-        self.assertTrue( time() - start < 0.1 )
+        self.assertTrue( time() - start < 0.1,            msg="Acquire timeout has not been honoured" )
 
         release_lock(lock)
-        self.assertFalse(os.path.exists("/tmp/testlock"))
+        self.assertFalse(os.path.exists("/tmp/testlock"), msg="Lock has not been removed")
 
     def test_lockfile_noexception(self):
         with mock.patch("systemd.lockutils.acquire_lock") as mock_acquire_lock, \
              mock.patch("systemd.lockutils.release_lock") as mock_release_lock:
             with Lockfile("/tmp/testlock"):
                 pass
-            self.assertTrue(mock_acquire_lock.called)
-            self.assertTrue(mock_release_lock.called)
+            self.assertTrue(mock_acquire_lock.called,     msg="Lock has not been acquired")
+            self.assertTrue(mock_release_lock.called,     msg="Lock has not been released")
 
     def test_lockfile_exception(self):
         with mock.patch("systemd.lockutils.acquire_lock") as mock_acquire_lock, \
@@ -83,5 +83,5 @@ class LockutilsTestCase(TestCase):
             with self.assertRaises(Exception):
                 with Lockfile("/tmp/testlock"):
                     raise Exception()
-            self.assertTrue(mock_acquire_lock.called)
-            self.assertTrue(mock_release_lock.called)
+            self.assertTrue(mock_acquire_lock.called,     msg="Lock has not been acquired")
+            self.assertTrue(mock_release_lock.called,     msg="Lock has not been released")
