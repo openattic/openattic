6a4ca6c8b76f7e2f23b4687784a2a03115c827af
Michael Ziegler <michael.ziegler@it-novum.com>
2014-10-29 16:00:23 +0100

    add a little more detail to the /volumes part

--- volumes/restapi.py
+++ volumes/restapi.py
@@ -22,17 +22,58 @@ from rest_framework.response import Response
 from volumes import models
 
 
+class AbstractFileSystemVolumeSerializer(serializers.Serializer):
+    host        = serializers.Field()
+    path        = serializers.CharField()
+    #mounted     = serializers.BooleanField()
+    #usedmegs    = serializers.Field()
+    status      = serializers.Field()
+
 class FileSystemVolumeSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.FileSystemVolume
+
+    volume = serializers.SerializerMethodField("serialize_volume")
+
+    def serialize_volume(self, obj):
+        ser = AbstractFileSystemVolumeSerializer(obj.volume)
+        return ser.data
+
+
+
+class AbstractBlockVolumeSerializer(serializers.Serializer):
+    host        = serializers.Field()
+    path        = serializers.CharField()
+    status      = serializers.Field()
 
 class BlockVolumeSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.BlockVolume
 
+    volume = serializers.SerializerMethodField("serialize_volume")
+
+    def serialize_volume(self, obj):
+        ser = AbstractBlockVolumeSerializer(obj.volume)
+        return ser.data
+
+
+
+class AbstractVolumePoolSerializer(serializers.Serializer):
+    host        = serializers.Field()
+    usedmegs    = serializers.Field()
+    status      = serializers.Field()
+
 class VolumePoolSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.VolumePool
+
+    volumepool = serializers.SerializerMethodField("serialize_volumepool")
+
+    def serialize_volumepool(self, obj):
+        ser = AbstractVolumePoolSerializer(obj.volumepool)
+        return ser.data
+
+
 
 class StorageObjectSerializer(serializers.ModelSerializer):
     volumepool          = VolumePoolSerializer()
