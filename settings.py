852b6835dcc2b13f7596fa39ede40894e44a17c5
Michael Ziegler <michael.ziegler@it-novum.com>
2014-11-05 12:22:47 +0100

    add DEFAULT_AUTHENTICATION_CLASSES and DEFAULT_PERMISSION_CLASSES settings

--- settings.py
+++ settings.py
@@ -40,6 +40,13 @@ LVM_CHOWN_GROUP = "users"
 MANAGERS = ADMINS
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
     'DEFAULT_FILTER_BACKENDS': (
         'rest_framework.filters.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',
