e22a9257c819e9897a227c8191081bb4bbd14462
Michael Ziegler <michael.ziegler@it-novum.com>
2015-01-19 11:36:12 +0100

    move module loading code into a generic loader module

---
+++ rest/loader.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# kate: space-indent on; indent-width 4; replace-tabs on;
+
+"""
+ *  Copyright (C) 2011-2014, it-novum GmbH <community@open-attic.org>
+ *
+ *  openATTIC is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2.
+ *
+ *  This package is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+"""
+
+import logging
+
+from django.conf import settings
+from django.utils.importlib import import_module
+
+def load_viewsets(module_param):
+    logging.info("Detecting modules...")
+    rpcdplugins = []
+    for app in settings.INSTALLED_APPS:
+        try:
+            module = import_module( app+".restapi" )
+        except ImportError, err:
+            if unicode(err) != "No module named restapi":
+                logging.error("Got error when checking app %s: %s", app, unicode(err))
+        else:
+            rpcdplugins.append(module)
+    logging.info( "Loaded modules: %s", ', '.join([module.__name__ for module in rpcdplugins]) )
+
+    viewsets = []
+
+    for plugin in rpcdplugins:
+        viewsets.extend(getattr(plugin, module_param, []))
+
+    return viewsets
--- rest/router.py
+++ rest/router.py
@@ -19,24 +19,13 @@ import logging
 from django.conf import settings
 from rest_framework.routers import DefaultRouter
 
-def get_router():
-    logging.info("Detecting modules...")
-    rpcdplugins = []
-    for app in settings.INSTALLED_APPS:
-        try:
-            module = __import__( app+".restapi" )
-        except ImportError, err:
-            if unicode(err) != "No module named restapi":
-                logging.error("Got error when checking app %s: %s", app, unicode(err))
-        else:
-            rpcdplugins.append(module)
-    logging.info( "Loaded modules: %s", ', '.join([module.__name__ for module in rpcdplugins]) )
+from rest.loader import load_viewsets
 
+def get_router():
     router = DefaultRouter(trailing_slash=False)
 
-    for plugin in rpcdplugins:
-        for args in getattr(getattr(plugin, "restapi"), "RESTAPI_VIEWSETS", []):
-            router.register(*args)
+    for args in load_viewsets("RESTAPI_VIEWSETS"):
+        router.register(*args)
 
     return router
 
