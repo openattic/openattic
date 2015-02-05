5eb1ece2274c7e6235df141218e8da4e4ec5ff56
Michael Ziegler <michael.ziegler@it-novum.com>
2015-02-05 21:32:25 +0100

    rebuild the volumes in the same way we built the pools [WIP]

--- app/scripts/config/routeConfig.js
+++ app/scripts/config/routeConfig.js
@@ -38,12 +38,65 @@ angular.module('openattic').config(function ($stateProvider, $urlRouterProvider)
             }
         }
       })
-      .state("volumes", {
+      .state('volumes', {
         url: "/volumes",
+        controller: 'VolumeCtrl',
         views: {
-            "main": {
-                templateUrl: "templates/volumes.html"
-            }
+          "main": {templateUrl: "templates/volumes.html"}
+        }
+      })
+      .state('volumes.detail', {
+        url: "/:volume",
+        views: {
+          "tab": {templateUrl: "templates/volumes/tab.html"}
+        }
+      })
+      .state('volumes.detail.status', {
+        url: "/status",
+        views: {
+          "tab-content": {templateUrl: "templates/volumes/status.html"}
+        }
+      })
+      .state('volumes.detail.statistics', {
+        url: "/statistics",
+        views: {
+          "tab-content": {templateUrl: "templates/volumes/statistics.html"}
+        }
+      })
+      .state('volumes.detail.cifs', {
+        url: "/cifs",
+        views: {
+          "tab-content": {templateUrl: "templates/volumes/cifs.html"}
+        }
+      })
+      .state('volumes.detail.nfs', {
+        url: "/nfs",
+        views: {
+          "tab-content": {templateUrl: "templates/volumes/nfs.html"}
+        }
+      })
+      .state('volumes.detail.luns', {
+        url: "/luns",
+        views: {
+          "tab-content": {templateUrl: "templates/volumes/luns.html"}
+        }
+      })
+      .state('volumes.detail.http', {
+        url: "/http",
+        views: {
+          "tab-content": {templateUrl: "templates/volumes/http.html"}
+        }
+      })
+      .state('volumes.detail.storage', {
+        url: "/storage",
+        views: {
+          "tab-content": {templateUrl: "templates/volumes/storage.html"}
+        }
+      })
+      .state('volumes.detail.snapshots', {
+        url: "/snapshots",
+        views: {
+          "tab-content": {templateUrl: "templates/volumes/snapshots.html"}
         }
       })
       .state("hosts", {
---
+++ app/scripts/controller/VolumesCtrl.js
@@ -0,0 +1,44 @@
+angular.module('openattic')
+  .controller('VolumeCtrl', function ($scope, $stateParams, $state, VolumeService) {
+    $scope.stateParams = $stateParams;
+
+    $scope.data = {};
+
+    $scope.filterConfig = {
+      page: 0,
+      entries: 10,
+      search: '',
+      sortfield: 'name',
+      sortorder: 'ASC'
+    };
+
+    $scope.selection = {
+    };
+
+    $scope.$watch("filterConfig", function(){
+      VolumeService.filter({
+        page:      $scope.filterConfig.page + 1,
+        page_size: $scope.filterConfig.entries,
+        search:    $scope.filterConfig.search,
+        ordering:  $scope.filterConfig.ordering
+      })
+      .$promise
+      .then(function (res) {
+        $scope.data = res;
+      })
+      .catch(function (error) {
+        console.log('An error occurred', error);
+      });
+    });
+
+    $scope.$watch("selection.item", function(selitem){
+      if (selitem) {
+        $state.go('volumes.detail.status', {volume: selitem.id})
+      }
+      else {
+        $state.go('volumes');
+      }
+    });
+  });
+
+// kate: space-indent on; indent-width 2; replace-tabs on;
