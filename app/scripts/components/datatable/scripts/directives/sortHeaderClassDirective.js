742c7a4638c16aec862e0ae1f63d0c9dce180f12
Michael Ziegler <michael.ziegler@it-novum.com>
2015-02-05 23:14:39 +0100

    refactor sortheaderclass

--- app/scripts/components/datatable/scripts/directives/sortHeaderClassDirective.js
+++ app/scripts/components/datatable/scripts/directives/sortHeaderClassDirective.js
@@ -1,17 +1,15 @@
 angular.module('openattic.datatable')
   .directive('sortheaderclass', function ($compile) {
     return {
-      // TODO: Default remove
-      restrict: 'A',
       terminal: true,
       priority: 1000,
       link: function link(scope, element, attrs) {
-        var replace = scope.sortfields[$(element).text()];
-        element.attr("ng-click", "sortByField('" + replace + "')");
+        var fieldName = scope.sortfields[$(element).text()];
+        element.attr("ng-click", "sortByField('" + fieldName + "')");
         element.attr("ng-class",
-          "{ sorting_asc: sortfield == '" + replace + "' && sortorder == 'ASC'," +
-          "sorting: sortfield != '" + replace + "'," +
-          "sorting_desc: sortfield == '" + replace + "' && sortorder == 'DESC'" +
+          "{ sorting_asc: sortfield == '" + fieldName + "' && sortorder == 'ASC'," +
+            "sorting: sortfield != '" + fieldName + "'," +
+            "sorting_desc: sortfield == '" + fieldName + "' && sortorder == 'DESC'" +
           "}");
         element.removeAttr('sortheaderclass');
         $compile(element)(scope);
