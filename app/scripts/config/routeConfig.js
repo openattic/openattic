439e8cf68613bd42470bce2777d5479df441a286
Tatjana Dehler <tatjana.dehler@it-novum.com>
2015-02-05 12:55:41 +0100

    added route states

--- app/scripts/config/routeConfig.js
+++ app/scripts/config/routeConfig.js
@@ -24,4 +24,52 @@ angular.module('openattic').config(function ($stateProvider, $urlRouterProvider)
           "tab-content": {template: "<h1>Storage</h1>"}
         }
       })
+       .state("disks", {
+        url: "/disks",
+        views: {
+            "main": {
+                templateUrl: "templates/disks.html"
+            }
+        }
+      })
+      .state("pools", {
+        url: "/pools",
+        views: {
+            "main": {
+                templateUrl: "templates/pools.html"
+            }
+        }
+      })
+      .state("volumes", {
+        url: "/volumes",
+        views: {
+            "main": {
+                templateUrl: "templates/volumes.html"
+            }
+        }
+      })
+      .state("hosts", {
+        url: "/hosts",
+        views: {
+            "main": {
+                templateUrl: "templates/hosts.html"
+            }
+        }
+      })
+      .state("users", {
+        url: "/users",
+        views: {
+            "main": {
+                templateUrl: "templates/users.html"
+            }
+        }
+      })
+      .state("apikeys", {
+        url: "/apikeys",
+        views: {
+            "main": {
+                templateUrl: "templates/apikeys.html"
+            }
+        }
+      })
 });
