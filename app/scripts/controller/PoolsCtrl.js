6b88b13116f34043b64af9b210d348ed1f80ca1e
Robin Boehm <devnull@localhost>
2015-02-05 15:03:16 +0100

    Cleanup PoolsView

--- app/scripts/controller/PoolsCtrl.js
+++ app/scripts/controller/PoolsCtrl.js
@@ -1,19 +1,23 @@
 angular.module('openattic')
-    .controller('PoolCtrl', function ($scope, $stateParams, PoolService) {
-        $scope.stateParams = $stateParams;
+  .controller('PoolCtrl', function ($scope, $stateParams, $state, PoolService) {
+    $scope.stateParams = $stateParams;
 
 
-      $scope.onSelectionChange = function(oadatatable){
-          if( oadatatable.getSelection().length == 1 ){
-              $scope.active_pool = oadatatable.getSelection()[0];
-              $scope.active_pool_storage = null;
-              new PoolService($scope.active_pool).$storage().then(function(res){
-                  $scope.active_pool_storage = res;
-              });
-          }
-          else{
-              $scope.active_pool = null;
-              $scope.active_pool_storage = null;
-          }
+    $scope.onSelectionChange = function (oadatatable) {
+      var selection = oadatatable.getSelection();
+      if (selection.length == 1) {
+        $scope.active_pool = selection[0];
+        $scope.active_pool_storage = null;
+        $state.go('pools.detail.status', {pool: selection[0].id})
+        new PoolService($scope.active_pool).$storage().then(function (res) {
+          $scope.active_pool_storage = res;
+        });
+
       }
-    });
+      else {
+        $state.go('pools');
+        $scope.active_pool = null;
+        $scope.active_pool_storage = null;
+      }
+    }
+  });
