a3e3155b3355c0fd7d14ac87ff8467ca2a8a6589
Robin Boehm <devnull@localhost>
2015-02-06 14:19:04 +0100

    fix grunt for styles/css folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -58,7 +58,7 @@ module.exports = function (grunt) {
             },
             styles: {
               bundle: [
-                '<%= buildConfig.src %>styles/**/*.css'
+                '<%= buildConfig.src %>css/**/*.css'
               ]
             }
           }
