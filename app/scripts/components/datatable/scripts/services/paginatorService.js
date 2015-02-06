3e2a52f077d548ca9bd5ef10040f8e36a9ac77db
Robin Boehm <devnull@localhost>
2015-02-06 12:27:13 +0100

    add example test for paginatorService

---
+++ app/scripts/components/datatable/scripts/services/paginatorService.js
@@ -0,0 +1,53 @@
+angular.module('openattic.datatable')
+  .factory('paginatorService', function () {
+    // TODO: Extract this in a service
+    var _range = function () {
+      var start, end, i, nums = [];
+      if (arguments.length == 1) {
+        start = 0;
+        end = arguments[0];
+      }
+      else {
+        start = arguments[0];
+        end = arguments[1];
+      }
+      for (i = start; i < end; i++) {
+        nums.push(i);
+      }
+      return nums;
+    };
+
+    // TODO: Extract this in a service
+    var _numbers = function (page, pages, buttons) {
+      // Shamelessly stolen from DataTables:
+      // https://github.com/DataTables/DataTables/blob/master/media/js/jquery.dataTables.js#L13852
+      var numbers = [],
+        half = Math.floor(buttons / 2);
+      if (pages <= buttons) {
+        numbers = _range(0, pages);
+      }
+      else if (page <= half) {
+        numbers = _range(0, buttons - 2);
+        numbers.push('ellipsis');
+        numbers.push(pages - 1);
+      }
+      else if (page >= pages - 1 - half) {
+        numbers = _range(pages - (buttons - 2), pages);
+        numbers.splice(0, 0, 'ellipsis'); // no unshift in ie6
+        numbers.splice(0, 0, 0);
+      }
+      else {
+        numbers = _range(page - 1, page + 2);
+        numbers.push('ellipsis');
+        numbers.push(pages - 1);
+        numbers.splice(0, 0, 'ellipsis');
+        numbers.splice(0, 0, 0);
+      }
+      numbers.DT_el = 'span';
+      return numbers;
+    };
+
+    return {
+      getNumbers: _numbers
+    };
+  });
---
+++ test/unit/services/paginatorService.js
@@ -0,0 +1,72 @@
+'use strict';
+
+describe('Service: paginatorService', function () {
+
+    var paginatorService;
+
+    beforeEach(module('openattic.datatable'));
+
+    beforeEach(inject(function(_paginatorService_){
+        paginatorService = _paginatorService_;
+    }));
+
+    it('should provide a method getNumbers()', function(){
+        expect(typeof paginatorService.getNumbers).toBe('function');
+    });
+
+
+    describe('getNumbers()-Method', function(){
+
+        it('should return an array', function(){
+            expect(paginatorService.getNumbers(1,2,3) instanceof Array).toBe(true);
+        });
+
+        it('should throw an error when calling with less or more then three args', function(){
+            expect(function(){
+                paginatorService.getNumbers();
+            }).toThrow();
+
+            expect(function(){
+                paginatorService.getNumbers(1);
+            }).toThrow();
+
+            expect(function(){
+                paginatorService.getNumbers(1,2,3);
+            }).not.toThrow();
+        });
+
+        it('should return an array of correct numbers when 1,1,7', function(){
+            expect(paginatorService.getNumbers(0,1,7)).toEqual([0]);
+        });
+
+
+        it('should always return 0 as first', function(){
+            expect(paginatorService.getNumbers(4,26,7)[0]).toBe(0);
+            expect(paginatorService.getNumbers(13,26,3)[0]).toBe(0);
+            expect(paginatorService.getNumbers(9,26,7)[0]).toBe(0);
+        });
+
+        it('should always return the max number', function(){
+            expect(paginatorService.getNumbers(4,26,7)[6]).toBe(25);
+            expect(paginatorService.getNumbers(3,10,3)[2]).toBe(9);
+            expect(paginatorService.getNumbers(9,99,7)[6]).toBe(98);
+        });
+
+        it('should always return 0 as first', function(){
+            expect(paginatorService.getNumbers(4,26,10).length).toBe(10);
+            expect(paginatorService.getNumbers(4,260,100).length).toBe(100);
+            expect(paginatorService.getNumbers(4,260,99).length).toBe(99);
+        });
+
+
+        it('should return an array of correct numbers when 5,26,7', function(){
+            expect(paginatorService.getNumbers(0,4,4)).toEqual([0,1,2,3]);
+        });
+
+        it('should return an array of correct numbers when 5,26,7', function(){
+            expect(paginatorService.getNumbers(4,26,7)).toEqual([0,'ellipsis',3,4,5,'ellipsis',25]);
+        });
+
+    });
+
+});
