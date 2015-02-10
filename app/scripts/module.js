f886ee7bead152cea5fa7a75f07f4c39334ac419
Michael Ziegler <michael.ziegler@it-novum.com>
2015-02-10 14:15:06 +0100

    indent, set X-CSRFToken header for Django

--- app/scripts/module.js
+++ app/scripts/module.js
@@ -1,9 +1,16 @@
 'use strict';
 angular.module('openattic', [
-	'ngResource',
-	'ui.router',
-	'openattic.datatable',
-	'openattic.datatree',
-	'openattic.graph',
-	'smartadmin.smartmenu'
-	]);
+  'ngResource',
+  'ui.router',
+  'openattic.datatable',
+  'openattic.datatree',
+  'openattic.graph',
+  'smartadmin.smartmenu'
+]);
+
+angular.module('openattic').config(function($httpProvider){
+  $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+  $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+});
+
+// kate: space-indent on; indent-width 2; replace-tabs on;
