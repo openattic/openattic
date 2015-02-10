c948f7fad38e2dde2d47ef1c58113c5dfa2b4055
Michael Ziegler <michael.ziegler@it-novum.com>
2015-02-10 10:11:10 +0100

    add UserService and UserController

---
+++ app/scripts/controller/UsersCtrl.js
@@ -0,0 +1,47 @@
+angular.module('openattic')
+  .controller('UserCtrl', function ($scope, $state, UserService) {
+    'use strict';
+
+    $scope.data = {};
+
+    $scope.filterConfig = {
+      page: 0,
+      entries: 10,
+      search: '',
+      sortfield: null,
+      sortorder: null
+    };
+
+    $scope.selection = {
+    };
+
+    $scope.$watch('filterConfig', function(){
+      UserService.filter({
+        page:      $scope.filterConfig.page + 1,
+        page_size: $scope.filterConfig.entries,
+        search:    $scope.filterConfig.search,
+        ordering:  ($scope.filterConfig.sortorder === 'ASC' ? '' : '-') + $scope.filterConfig.sortfield
+      })
+      .$promise
+      .then(function (res) {
+        $scope.data = res;
+      })
+      .catch(function (error) {
+        console.log('An error occurred', error);
+      });
+    }, true);
+
+    $scope.$watchCollection("selection.item", function(item){
+      $scope.hasSelection = !!item;
+    });
+
+    $scope.addAction = function(){
+      console.log(["addAction", arguments]);
+    }
+
+    $scope.deleteAction = function(){
+      console.log(["deleteAction", arguments]);
+    }
+  });
+
+// kate: space-indent on; indent-width 2; replace-tabs on;
---
+++ app/scripts/services/userService.js
@@ -0,0 +1,19 @@
+angular.module('openattic')
+  .factory('UserService', function ($resource) {
+    'use strict';
+    return $resource('/openattic/api/users/:id', {
+      id: '@id'
+    }, {
+      update: {method: 'PUT'},
+      query: {
+        method: 'GET',
+        isArray: true,
+        transformResponse: function (data) {
+          return JSON.parse(data).results;
+        }
+      },
+      filter: {
+        method: 'GET'
+      }
+    });
+  });
