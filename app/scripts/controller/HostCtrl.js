bb39ce50e0711e8f64a9d28812d32bdd9d637f56
Michael Ziegler <michael.ziegler@it-novum.com>
2015-02-11 15:57:02 +0100

    add delete host

--- app/scripts/controller/HostCtrl.js
+++ app/scripts/controller/HostCtrl.js
@@ -44,6 +44,29 @@ angular.module('openattic')
     }
 
     $scope.deleteAction = function(){
-      console.log(["deleteAction", arguments]);
+      $.SmartMessageBox({
+        title: 'Delete Host',
+        content: 'Do you really want to delete the Host "' + $scope.selection.item.name + '"?',
+        buttons: '[No][Yes]'
+      }, function (ButtonPressed) {
+        if (ButtonPressed === 'Yes') {
+          HostService.delete({id: $scope.selection.item.id})
+            .$promise
+            .then(function() {
+              $scope.filterConfig.refresh = new Date();
+            }, function(error){
+              console.log('An error occured', error);
+            });
+        }
+        if (ButtonPressed === 'No') {
+          $.smallBox({
+            title: 'Delete Host',
+            content: '<i class="fa fa-clock-o"></i> <i>Cancelled</i>',
+            color: '#C46A69',
+            iconSmall: 'fa fa-times fa-2x fadeInRight animated',
+            timeout: 4000
+          });
+        }
+      });
     }
   });
