112edf79833277d12f5b19d937b4ecd6e22f4bb3
Michael Ziegler <michael.ziegler@it-novum.com>
2015-02-11 17:34:12 +0100

    load dem srs attributez

---
+++ app/scripts/services/initiatorService.js
@@ -0,0 +1,16 @@
+angular.module('openattic')
+  .factory('InitiatorService', function ($resource) {
+    'use strict';
+    return $resource('/openattic/api/initiators/:id', {
+      id: '@id'
+    }, {
+      update: {method: 'PUT'},
+      query: {
+        method: 'GET',
+        isArray: true,
+        transformResponse: function (data) {
+          return JSON.parse(data).results;
+        }
+      }
+    });
+  });
