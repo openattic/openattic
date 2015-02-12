34c4d56fdadcb1245ebe63e937aeeac9e7b370e7
Michael Ziegler <michael.ziegler@it-novum.com>
2015-02-12 16:00:52 +0100

    set X-Requested-With: XMLHttpRequest header for ajax requests

--- app/components/auth/module.js
+++ app/components/auth/module.js
@@ -12,6 +12,11 @@ angular.module('openattic.auth').config(function($httpProvider){
 angular.module('openattic.auth').config(function($provide, $httpProvider){
   $provide.factory('AuthHttpInterceptor', function($q){
     return {
+      request: function(config) {
+        // Give the backend a clue that we're using AJAX here...
+        config.headers["X-Requested-With"] = "XMLHttpRequest";
+        return config;
+      },
       responseError: function(rejection){
         if(rejection.status === 401){
           window.location.href = 'login.html';
