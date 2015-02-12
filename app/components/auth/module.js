de6f634d51347f65005dee338edbf1872bbf81b6
Michael Ziegler <michael.ziegler@it-novum.com>
2015-02-12 15:46:18 +0100

    redirect to the login if we catch an HTTP 401 UNAUTHORIZED error

--- app/components/auth/module.js
+++ app/components/auth/module.js
@@ -8,3 +8,18 @@ angular.module('openattic.auth').config(function($httpProvider){
   $httpProvider.defaults.xsrfCookieName = 'csrftoken';
   $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
 });
+
+angular.module('openattic.auth').config(function($provide, $httpProvider){
+  $provide.factory('AuthHttpInterceptor', function($q){
+    return {
+      responseError: function(rejection){
+        if(rejection.status === 401){
+          window.location.href = 'login.html';
+        }
+        return $q.reject(rejection);
+      }
+    };
+  });
+
+  $httpProvider.interceptors.push('AuthHttpInterceptor');
+});
