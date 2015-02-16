a2631239ed8cfed46e91278bd1240f7808024cde
Michael Ziegler <michael.ziegler@it-novum.com>
2015-02-16 15:24:40 +0100

    add restapi

---
+++ restapi.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# kate: space-indent on; indent-width 4; replace-tabs on;
+
+"""
+ *  Copyright (C) 2011-2014, it-novum GmbH <community@open-attic.org>
+ *
+ *  openATTIC is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2.
+ *
+ *  This package is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+"""
+
+import django_filters
+
+from rest_framework import serializers, viewsets, status
+from rest_framework.response import Response
+
+from rest import relations
+
+from ceph.models import Cluster
+
+class ClusterSerializer(serializers.HyperlinkedModelSerializer):
+    """ Serializer for a NFS Export. """
+    url         = serializers.HyperlinkedIdentityField(view_name="nfsshare-detail")
+
+    class Meta:
+        model = Cluster
+        fields = ('url', 'id', 'name', 'auth_cluster_required', 'auth_client_required', 'auth_service_required')
+
+
+class ClusterViewSet(viewsets.ModelViewSet):
+    queryset         = Cluster.objects.all()
+    serializer_class = ClusterSerializer
+    filter_fields    = ('name',)
+    search_fields    = ('name',)
+
+
+RESTAPI_VIEWSETS = [
+    ('cephclusters', ClusterViewSet, 'cephcluster')
+]
