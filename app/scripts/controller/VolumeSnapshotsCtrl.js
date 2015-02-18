87c0dcf4ed99aaf5cede29bddc7129b13803b3e2
Laura Paduano <laura.paduano@it-novum.com>
2015-02-18 18:01:05 +0100

    forgot ; deleteAction is WIP

--- app/scripts/controller/VolumeSnapshotsCtrl.js
+++ app/scripts/controller/VolumeSnapshotsCtrl.js
@@ -1,5 +1,5 @@
 angular.module('openattic')
-  .controller('VolumeSnapshotsCtrl', function ($scope, VolumeService) {
+  .controller('VolumeSnapshotsCtrl', function ($scope, $state, VolumeService) {
     'use strict';
 
     $scope.snapshotsData = {};
@@ -37,6 +37,37 @@ angular.module('openattic')
         console.log('An error occurred', error);
       });
     }, true);
-  });
+
+    $scope.addAction = function(){
+      $state.go('volumes.detail.snapshots-add');
+    }
+
+//     $scope.deleteAction = function(){
+//       $.SmartMessageBox({
+//         title: 'Delete snapshot',
+//         content: 'Do you really want to delete snapshot  "' + $scope.snapshotsSelection.item.snapshot.title + '"?',
+//         buttons: '[No][Yes]'
+//       }, function (ButtonPressed) {
+//         if (ButtonPressed === 'Yes') {
+//           .delete({id: $scope.snapshotsSelection.item.snapshot.id})
+//             .$promise
+//             .then(function() {
+//               $scope.snapshotsFilter.refresh = new Date();
+//             }, function(error){
+//               console.log('An error occured', error);
+//             });
+//         }
+//         if (ButtonPressed === 'No') {
+//           $.smallBox({
+//             title: 'Delete snapshot',
+//             content: '<i class="fa fa-clock-o"></i> <i>Cancelled</i>',
+//             color: '#C46A69',
+//             iconSmall: 'fa fa-times fa-2x fadeInRight animated',
+//             timeout: 4000
+//           });
+//         }
+//       });
+//     }
+   });
 
 // kate: space-indent on; indent-width 2; replace-tabs on;
