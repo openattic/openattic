c01425df4fdb2f8d2961c65cd644c17f708e306d
Tatjana Dehler <tatjana.dehler@it-novum.com>
2015-02-18 08:39:16 +0100

    added deleteByDateAction [WIP]

--- app/scripts/controller/CmdlogCtrl.js
+++ app/scripts/controller/CmdlogCtrl.js
@@ -1,5 +1,5 @@
 angular.module('openattic')
-  .controller('CmdlogCtrl', function ($scope, $state, $filter, CmdlogService) {
+  .controller('CmdlogCtrl', function ($scope, $state, $filter, CmdlogService, $modal) {
     'use strict';
 
     $scope.data = {};
@@ -77,5 +77,37 @@ angular.module('openattic')
           });
         }
       });
-    }
+    };
+
+    $scope.deleteByDateAction = function(){
+      var modalInstance = $modal.open({
+        windowTemplateUrl: 'templates/cmdlogs/delete-by-date-window.html',
+        templateUrl: 'templates/cmdlogs/delete-by-date.html',
+        controller: 'DeleteByDateCtrl'
+      });
+    };
+  })
+
+  .controller('DeleteByDateCtrl', function ($scope, $modalInstance) {
+    $scope.open = function($event){
+      $event.preventDefault();
+      $event.stopPropagation();
+      $scope.opened = true;
+    };
+
+    $scope.yes = function(){
+    };
+
+    $scope.no = function(){
+      $modalInstance.dismiss('cancel');
+
+      $.smallBox({
+        title: 'Delete log entry',
+        content: '<i class="fa fa-clock-o"></i> <i>Cancelled</i>',
+        color: '#C46A69',
+        iconSmall: 'fa fa-times fa-2x fadeInRight animated',
+        timeout: 4000
+      });
+    };
   });
+
