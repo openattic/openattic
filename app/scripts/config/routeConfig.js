fcf1b5358861ad275df982701a5153f2e8e84533
Michael Ziegler <michael.ziegler@it-novum.com>
2015-02-18 15:55:59 +0100

    add ceph pool editor dummy page

--- app/scripts/config/routeConfig.js
+++ app/scripts/config/routeConfig.js
@@ -30,6 +30,12 @@ angular.module('openattic').config(function ($stateProvider, $urlRouterProvider)
         'tab-content': {templateUrl: 'templates/pools/storage.html'}
       }
     })
+    .state('pools.detail.cephpool', {
+      url: '/cephpool',
+      views: {
+        'tab-content': {templateUrl: 'templates/pools/cephpool.html'}
+      }
+    })
     .state('disks', {
       url: '/disks',
       views: {
