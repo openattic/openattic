15881f05c59a0d8462b5bf80c706d37bef97df6b
Tatjana Dehler <tatjana.dehler@it-novum.com>
2015-02-20 10:00:19 +0100

    moved the CmdlogDeleteByDateCtrl in its own file

--- app/scripts/controller/CmdlogCtrl.js
+++ app/scripts/controller/CmdlogCtrl.js
@@ -90,48 +90,5 @@ angular.module('openattic')
         $scope.filterConfig.refresh = new Date();
       });
     };
-  })
-
-  .controller('CmdlogDeleteByDateCtrl', function ($scope, CmdlogService, $modalInstance) {
-    $scope.datePicker = {
-      opened    : false,
-      maxDate   : null,
-      dateTime  : null,
-      format    : 'dd/MM/yyyy',
-      showBtnBar: false
-    };
-
-    $scope.options = {
-      startingDay: 1
-    };
-
-    $scope.open = function($event){
-      $event.preventDefault();
-      $event.stopPropagation();
-      $scope.datePicker.maxDate = new Date();
-      $scope.datePicker.opened = true;
-    };
-
-    $scope.yes = function(){
-      CmdlogService.delete({'datetime': $scope.datePicker.dateTime})
-        .$promise
-        .then(function() {
-          $modalInstance.close('deleted');
-        }, function(error){
-          console.log('An error occured', error);
-        });
-    };
-
-    $scope.no = function(){
-      $modalInstance.dismiss('cancel');
-
-      $.smallBox({
-        title: 'Delete log entry',
-        content: '<i class="fa fa-clock-o"></i> <i>Cancelled</i>',
-        color: '#C46A69',
-        iconSmall: 'fa fa-times fa-2x fadeInRight animated',
-        timeout: 4000
-      });
-    };
   });
 
