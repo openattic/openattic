16ffa3d53c7a699be063b73fedb05f2dde93cf66
Laura Paduano <laura.paduano@it-novum.com>
2015-02-20 09:38:06 +0100

    we need another SnapshotService in order to delete a snapshot

---
+++ app/scripts/services/snapshotService.js
@@ -0,0 +1,19 @@
+angular.module('openattic')
+  .factory('SnapshotService', function ($resource) {
+    'use strict';
+    return $resource('/openattic/api/snapshots/:id', {
+      id: '@id'
+    }, {
+      update: {method: 'PUT'},
+      query: {
+        method: 'GET',
+        isArray: true,
+        transformResponse: function (data) {
+          return JSON.parse(data).results;
+        }
+      },
+      filter: {
+        method: 'GET'
+      }
+    });
+  });
