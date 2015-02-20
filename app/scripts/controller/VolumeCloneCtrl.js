93b55933d6f23a3d257c34162f95d3b95979b161
Tatjana Dehler <tatjana.dehler@it-novum.com>
2015-02-20 16:39:23 +0100

    call clone function of the volume service

--- app/scripts/controller/VolumeCloneCtrl.js
+++ app/scripts/controller/VolumeCloneCtrl.js
@@ -13,6 +13,13 @@ angular.module('openattic')
     }
 
     $scope.clone = function(){
+      VolumeService.clone({'id': $scope.volume.id, 'clone_obj': $scope.clone_obj})
+        .$promise
+        .then(function() {
+
+        }, function(error){
+          console.log('An error occured', error);
+        });
     };
 
     $scope.cancel = function(){
