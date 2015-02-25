5d9d76cf269ec35fc119a2f953da4a6ecb7f1b0f
Laura Paduano <laura.paduano@it-novum.com>
2015-02-25 18:01:03 +0100

    actionsDirective: fixed jshint single quote error

--- app/components/datatable/scripts/directives/actionsDirective.js
+++ app/components/datatable/scripts/directives/actionsDirective.js
@@ -8,11 +8,11 @@ angular.module('openattic.datatable')
         var actionsscope = scope.$parent.$new();
         transclude(actionsscope, function (clone, scope) {
           var i;
-          var liElems = clone.filter("li");
+          var liElems = clone.filter('li');
           for(i = 0; i < liElems.length; i++){
             element.find('.oadatatableactionsmenu').append(liElems[i]);
           };
-          var aElems = clone.filter(".btn-primary");
+          var aElems = clone.filter('.btn-primary');
           for(i = aElems.length - 1; i >= 0; --i){
             element.find('.btn-group').prepend(aElems[i]);
           };
