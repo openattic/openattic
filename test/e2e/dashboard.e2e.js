be5b4c86b024726e570e830f2038bbb9ce907633
Tatjana Dehler <tatjana.dehler@it-novum.com>
2015-02-26 16:43:57 +0100

    protrator has a problem with the .location.href redirect of the authController. here is the workaround.

--- test/e2e/dashboard.e2e.js
+++ test/e2e/dashboard.e2e.js
@@ -1,5 +1,4 @@
 describe('Dashboard', function() {
-
   it('should have a title', function() {
     browser.get('http://srvopenatticci01.master.dns/openattic/angular/login.html');
 
@@ -7,13 +6,10 @@ describe('Dashboard', function() {
     element.all(by.model('password')).sendKeys('openattic');
     element.all(by.css('input[type="submit"]')).click();
 
+    browser.sleep(1000);
+
     expect(browser.getTitle()).toEqual('openATTIC');
     expect(element.all(by.css('.breadcrumb li')).get(0).getText()).toEqual('Home');
     expect(element.all(by.css('.breadcrumb li')).get(1).getText()).toEqual('Dashboard');
-
-
   });
-
-
-
 });
