046d716d36d70865049d3a2bd56d7db2caffb391
Tatjana Dehler <tatjana.dehler@it-novum.com>
2015-03-02 11:54:15 +0100

    added delete protection files

---
+++ app/scripts/controller/VolumeProtectionCtrl.js
@@ -0,0 +1,30 @@
+angular.module('openattic')
+  .controller('VolumeProtectionCtrl', function($scope, VolumeService, $modalInstance, volume) {
+    'use strict';
+
+    $scope.volume = volume;
+
+    $scope.setProtection = function() {
+      new VolumeService({
+        id: volume.id,
+        is_protected: volume.is_protected
+      }).$update()
+        .then(function() {
+          $modalInstance.dismiss('protection set');
+        }, function(error) {
+          console.log('An error occured', error);
+        })
+    };
+
+    $scope.cancel = function(){
+      $modalInstance.dismiss('cancel');
+
+      $.smallBox({
+        title: 'Set volume protection',
+        content: '<i class="fa fa-clock-o"></i> <i>Cancelled</i>',
+        color: '#C46A69',
+        iconSmall: 'fa fa-times fa-2x fadeInRight animated',
+        timeout: 4000
+      });
+    };
+  });
