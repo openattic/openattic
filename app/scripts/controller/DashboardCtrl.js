7b182f0350913870147efdcdd18be2c857b936e0
Tatjana Dehler <tatjana.dehler@it-novum.com>
2015-03-05 16:36:11 +0100

    added new dashboard draft

--- app/scripts/controller/DashboardCtrl.js
+++ app/scripts/controller/DashboardCtrl.js
@@ -1,1 +1,21 @@
+'use strict';
+
+angular.module('openattic')
+  .value('widgetDefinitions', [{
+      name: 'volumes',
+      directive: 'volumewidget'
+    }
+  ])
+  .value('defaultWidgets', [
+    { name: 'volumes' }
+  ])
+  .controller('DashboardCtrl', function ($scope, $window, widgetDefinitions, defaultWidgets){
+    $scope.dashboardOptions = {
+      widgetButtons: true,
+      widgetDefinitions: widgetDefinitions,
+      defaultWidgets: defaultWidgets,
+      storage: $window.localStorage,
+      storageId: 'oa_widgets'
+    };
+  });
 // kate: space-indent on; indent-width 2; replace-tabs on;
