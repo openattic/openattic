eb17fa29c1b888ee8aa3d173b250de3eef72e9f6
Tatjana Dehler <tatjana.dehler@it-novum.com>
2015-03-09 16:13:36 +0100

    use ng-src instead of src

--- app/components/userinfo/scripts/directives/UserInfoDirective.js
+++ app/components/userinfo/scripts/directives/UserInfoDirective.js
@@ -8,17 +8,20 @@ angular.module('openattic.userinfo')
         '<div class="login-info">',
         '<span>',
         '<a ui-sref="users-edit({user:user.id})" id="show-shortcut" data-action="toggleShortcut">',
-        '<img src="http://www.gravatar.com/avatar/{{ user.email | gravatar }}.jpg?d=monsterid" alt="me" style="border-left: 0px;"/>',
+        '<img ng-src="{{image}}" style="border-left: 0px;"/>',
         '<span ng-bind="user.username"></span>',
         '</a>',
         '</span>',
         '</div>'
       ].join(''),
-      controller: function($scope, UserService){
+      controller: function($scope, $filter, UserService){
         UserService.current()
           .$promise
           .then(function(res) {
             $scope.user = res;
+
+            var gravatarId = $filter('gravatar')($scope.user.email);
+            $scope.image = 'http://www.gravatar.com/avatar/' + gravatarId + '.jpg?d=monsterid';
           })
           .catch(function(){
             console.log('an error occured');
