27b6835baad03fe9239680a747c78cfc232b490f
Tatjana Dehler <tatjana.dehler@it-novum.com>
2015-03-12 16:54:41 +0100

    removed wrong tests

--- test/e2e/dashboard.e2e.js
+++ test/e2e/dashboard.e2e.js
@@ -1,15 +1,10 @@
 describe('Dashboard', function() {
-  it('should have a title', function() {
-    browser.get('http://srvopenatticci01.master.dns/openattic/angular/login.html');
-
+  beforeEach(function() {
+    browser.get('http://172.16.13.135/openattic/angular/login.html');
     element.all(by.model('username')).sendKeys('openattic');
     element.all(by.model('password')).sendKeys('openattic');
     element.all(by.css('input[type="submit"]')).click();
 
-    browser.sleep(1000);
-
-    expect(browser.getTitle()).toEqual('openATTIC');
-    expect(element.all(by.css('.breadcrumb li')).get(0).getText()).toEqual('Home');
-    expect(element.all(by.css('.breadcrumb li')).get(1).getText()).toEqual('Dashboard');
+    browser.sleep(2000);
   });
 });
