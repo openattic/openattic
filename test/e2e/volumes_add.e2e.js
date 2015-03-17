c64424caaf4fbaddd78343b151dec4f07398ea9a
Tatjana Dehler <tatjana.dehler@it-novum.com>
2015-03-17 16:48:53 +0100

    added some more e2e tests

--- test/e2e/volumes_add.e2e.js
+++ test/e2e/volumes_add.e2e.js
@@ -17,4 +17,30 @@ describe('Volumes add', function() {
   it('should open an add volume form with "Create Volume:" header', function(){
     expect(element(by.css('h2')).getText()).toEqual('Create Volume:');
   });
+
+  it('should have a back button', function(){
+    expect(element(by.css('.tc_backButton')).isPresent()).toBe(true);
+  });
+
+  it('should have a back button to navigate back to the volume overview', function(){
+    var backButton = element(by.css('.tc_backButton'));
+    backButton.click();
+
+    expect(element(by.css('.tc_oadatatable_volumes')).isPresent()).toBe(true);
+  });
+
+  it('should show the typed in volume name in the header', function(){
+    var volumeNameInput = element(by.model('volume.name'));
+    volumeNameInput.sendKeys('protractor_test');
+
+    expect(element(by.css('h2')).getText()).toEqual('Create Volume: protractor_test');
+  });
+
+  it('should offer a list of volume pools', function(){
+    var volumepoolSelect = element(by.model('data.sourcePool'));
+    volumepoolSelect.click();
+
+    expect(volumepoolSelect.all(by.css('select .tc_volumePoolOption')).count()).toBeGreaterThan(0);
+  });
+
 });
