548806b71d1c26bd528c38ec6739d570c86a1394
Tatjana Dehler <tatjana.dehler@it-novum.com>
2015-03-17 09:24:55 +0100

    moved in a separate file

--- test/e2e/volumes_add.e2e.js
+++ test/e2e/volumes_add.e2e.js
@@ -1,4 +1,4 @@
-describe('Volumes', function() {
+describe('Volumes add', function() {
   beforeEach(function() {
     browser.get('http://172.16.13.135/openattic/angular/login.html');
     element.all(by.model('username')).sendKeys('openattic');
@@ -9,13 +9,12 @@ describe('Volumes', function() {
 
     var volumesItem = element.all(by.css('ul .tc_menuitem')).get(3);
     volumesItem.click();
+
+    var addBtn = element(by.css('oadatatable .tc_add_btn'));
+    addBtn.click();
   });
 
-  it('should have a oadatatable element', function(){
-    expect(element(by.css('oadatatable')).isPresent()).toBe(true);
-  });
-
-  it('should have an add button', function(){
-    expect(element(by.css('oadatatable .tc_add_btn')).isPresent()).toBe(true);
+  it('should open an add volume form with "Create Volume:" header', function(){
+    expect(element(by.css('h2')).getText()).toEqual('Create Volume:');
   });
 });
