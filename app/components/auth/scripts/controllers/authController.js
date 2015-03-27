0feafecb69b8ca91b6bf6f6565d7897abf2d16ef
Tatjana Dehler <tatjana.dehler@it-novum.com>
2015-03-27 14:04:54 +0100

    added login form validations

--- app/components/auth/scripts/controllers/authController.js
+++ app/components/auth/scripts/controllers/authController.js
@@ -2,16 +2,23 @@
 
 angular.module('openattic.auth')
   .controller('authController', function ($scope, $window, authService) {
-    $scope.login = function(){
-      var loginData = {'username': $scope.username, 'password': $scope.password};
-      authService.login(loginData)
-        .$promise
-        .then(function(res){
-          $scope.user = res.username;
-          $window.location.href = '/openattic/angular/#/dashboard';
-        })
-        .catch(function(){
-          $window.location.href = '/openattic/angular/login.html';
-        });
+    $scope.fieldRequired = "This field is required.";
+    $scope.correctInput = "The given login informations are not correct.";
+
+    $scope.login = function(loginForm){
+      $scope.submitted = true;
+      if(loginForm.$valid === true) {
+        var loginData = {'username': $scope.username, 'password': $scope.password};
+        authService.login(loginData)
+          .$promise
+          .then(function (res) {
+            $scope.user = res.username;
+            $window.location.href = '/openattic/angular/#/dashboard';
+          })
+          .catch(function () {
+            loginForm.username.$setValidity('correctInput', false);
+            loginForm.password.$setValidity('correctInput', false);
+          });
+      }
     };
   });
