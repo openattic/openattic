fb92a5b51c6a59bd309077682746e0b043c1bcf3
Tatjana Dehler <tatjana.dehler@it-novum.com>
2015-04-09 15:12:43 +0200

    moved configs into a separate file

--- test/e2e/common.js
+++ test/e2e/common.js
@@ -1,48 +1,9 @@
 'use strict';
 
-var configs = {
-  url     : 'http://172.16.13.135/openattic/angular/login.html',
-  username: 'openattic',
-  password: 'openattic',
-  sleep   : 2000,
-  pools   : {
-    vg: {
-      name: 'vg_lorlen',
-      size: 19.70,
-      unit: 'GB'
-    },
-    btrfs: {
-      name: 'testnfs',
-      size: 100.00,
-      unit: 'MB'
-    },
-    zfs: {
-      name: 'tank',
-      size: 19.60,
-      unit: 'GB'
-    }
-  }
-};
-
 (function() {
+  var configs = require('./configs.js');
   module.exports = {
     configs: configs,
-    volumeTypes: {
-      vg: [
-        'Create LUN',
-        'Create Virtualization Store -> XFS',
-        'Create ZFS Volume',
-        'Create File Store -> BTRFS'
-      ],
-      btrfs: [
-        'Create LUN',
-        'Create File Store -> BTRFS'
-      ],
-      zfs: [
-        'Create LUN',
-        'Create ZFS Volume'
-      ]
-    },
     login: function() {
       browser.get(configs.url);
       element.all(by.model('username')).sendKeys(configs.username);
