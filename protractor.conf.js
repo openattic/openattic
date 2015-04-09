58a1824b7511a98bbacafbe1871fdfc70932c6c4
Tatjana Dehler <tatjana.dehler@it-novum.com>
2015-04-09 12:33:15 +0200

    added testsuites

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,4 +1,8 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['test/e2e/**/*.e2e.js']
+  suites: {
+    dashboard: 'test/e2e/dashboard/**/*.e2e.js',
+    general: 'test/e2e/general/**/*.e2e.js',
+    volumes: 'test/e2e/volumes/**/*.e2e.js'
+  }
 };
