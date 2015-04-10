eff6076e10340ee319a1a39ce87edf6c3260f26b
Tatjana Dehler <tatjana.dehler@it-novum.com>
2015-04-10 11:40:18 +0200

    increased timeout limit

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,5 +1,8 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 360000
+  },
   suites: {
     dashboard: 'test/e2e/dashboard/**/*.e2e.js',
     general: 'test/e2e/general/**/*.e2e.js',
