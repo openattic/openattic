c5c9807889f244fe5ff9b80b0f88a75d70a0a380
Laura Paduano <laura.paduano@it-novum.com>
2015-04-14 16:43:53 +0200

    added todowidget-e2e-test to protractor.conf

--- protractor.conf.js
+++ protractor.conf.js
@@ -5,6 +5,7 @@ exports.config = {
   },
   suites: {
     dashboard  : 'test/e2e/dashboard/**/*.e2e.js',
+    todowidget : 'test/e2e/dashboard/todoWidget/*.e2e.js',
     general    : 'test/e2e/general/**/*.e2e.js',
     volumes    : 'test/e2e/volumes/**/*.e2e.js',
     volumes_add: 'test/e2e/volumes/add/**/*.e2e.js'
