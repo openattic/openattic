bf6c7453a53c2bda6794eea837786a247ed79e0f
Tatjana Dehler <tatjana.dehler@it-novum.com>
2015-04-16 10:54:02 +0200

    moved add protected volumes checks into a separate file

--- test/e2e/volumes/add/volumes_add.e2e.js
+++ test/e2e/volumes/add/volumes_add.e2e.js
@@ -44,13 +44,6 @@ describe('Volumes add', function() {
     expect(element(by.id('data.megs')).isDisplayed()).toBe(true);
   });
 
-  it('should have a deletion protection checkbox', function(){
-    expect(element(by.id('volume.is_protected')).isPresent()).toBe(true);
-
-    // Does not work because of the awesome bootstrap checkbox style
-    // expect(protectedCheck.isDisplayed()).toBe(true);
-  });
-
   it('should stay on the create volume form if the submit button is clicked without editing anything', function(){
     var submitButton = element(by.css('.tc_submitButton'));
     submitButton.click();
@@ -209,57 +202,6 @@ describe('Volumes add', function() {
       expect(element(by.css('.tc_noValidNumber')).isDisplayed()).toBe(true);
 
       break;
-    }
-  });
-
-  it('should create a volume with deletion protection', function(){
-    for(var key in helpers.configs.pools) {
-      // create a protected volume
-      var volumename = 'protractor_volume_protected';
-      element(by.id('volume.name')).sendKeys(volumename);
-
-      var pool = helpers.configs.pools[key];
-      var volumePoolSelect = element(by.id('data.sourcePool'));
-      volumePoolSelect.click();
-      volumePoolSelect.element(by.cssContainingText('option', pool.name)).click();
-
-      element(by.model('data.megs')).sendKeys('100mb');
-      element(by.model('volume.is_protected')).click();
-      element(by.css('.tc_submitButton')).click();
-
-      // is it displayed on the volume overview?
-      browser.sleep(helpers.configs.sleep);
-      var volume = element(by.cssContainingText('tr', volumename));
-      var protectedColumn = volume.element(by.id('is_protected'));
-      expect(protectedColumn.element(by.className('fa-check')).isDisplayed()).toBe(true);
-
-      // release the protection
-      volume.click();
-      browser.sleep(400);
-      element(by.css('.tc_menudropdown')).click();
-      browser.sleep(400);
-      element(by.css('.tc_setProtection')).click();
-      browser.sleep(400);
-
-      element(by.model('volume.is_protected')).click();
-
-      element(by.id('bot2-Msg1')).click();
-      browser.sleep(400);
-
-      // delete the volume
-      volume.click();
-      browser.sleep(400);
-      element(by.css('.tc_menudropdown')).click();
-      browser.sleep(400);
-      element(by.css('.tc_deleteItem')).click();
-      browser.sleep(400);
-
-      element(by.model('input.enteredName')).sendKeys(volumename);
-      element(by.id('bot2-Msg1')).click();
-
-      expect(volume.isPresent()).toBe(false);
-
-      element(by.css('oadatatable .tc_add_btn')).click();
     }
   });
 
