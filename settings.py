7aaff11a7cce0539b57187a51f7c269ea391cd7d
Michael Ziegler <michael.ziegler@it-novum.com>
2015-05-09 13:12:20 +0200

    if the rrdcached socket is found, use it

--- settings.py
+++ settings.py
@@ -127,6 +127,9 @@ else:
         SAMBA_SERVICE_NAME = "smbd"
         USE_SYSTEMD_IF_AVAIL = False # Ubuntu ships with broken systemd. *sigh*
 
+if exists('/var/run/rrdcached.sock'):
+    NAGIOS_RRDCACHED_SOCKET = '/var/run/rrdcached.sock'
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
