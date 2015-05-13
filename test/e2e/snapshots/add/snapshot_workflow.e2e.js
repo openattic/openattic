6bc28787df5e15d254df0b0ee6c26801cc95303d
Laura Paduano <laura.paduano@it-novum.com>
2015-05-13 13:17:40 +0200

    refactored snapshot_add_workflow test;

--- test/e2e/snapshots/add/snapshot_workflow.e2e.js
+++ test/e2e/snapshots/add/snapshot_workflow.e2e.js
@@ -6,15 +6,10 @@ describe('Should check the snapshot add workflow', function(){
   var snapshotname = 'protractor_test_snap';
   var snapshot = element(by.cssContainingText('tr', snapshotname));
   var submitButton = element(by.css('.tc_submitButton'));    
-
+  var volumesItem = element.all(by.css('ul .tc_menuitem')).get(3);
+  
   beforeEach(function() {
-    helpers.login(); 
-    var volumesItem = element.all(by.css('ul .tc_menuitem')).get(3);
     volumesItem.click();
-  });  
-
-//navigate to snapshot form
-  function goToSnapAdd() {
     expect(volume.isDisplayed()).toBe(true);
     volume.click();
     browser.sleep(400);
@@ -22,25 +17,21 @@ describe('Should check the snapshot add workflow', function(){
     browser.sleep(400);
     element(by.css('.tc_snapshotAdd')).click();
     browser.sleep(400);
-  }
+  });  
 
   it('should a "Create Snapshot" header', function(){
-    goToSnapAdd();
     expect(element(by.css('h2')).getText()).toEqual('Create Snapshot');
   });
 
   it('should have a back button', function(){
-    goToSnapAdd();
     expect(element(by.css('.tc_backButton')).isPresent()).toBe(true);
   });
   
   it('should have a submit button', function(){
-    goToSnapAdd();
     expect(element(by.css('.tc_submitButton')).isPresent()).toBe(true);
   });
 
   it('should have a back button which navigates back to the snapshot overview', function(){
-    goToSnapAdd();
     var backButton = element(by.css('.tc_backButton'));
     backButton.click();
 
@@ -48,12 +39,10 @@ describe('Should check the snapshot add workflow', function(){
   });
 
   it('should have a snapshot name input field', function(){
-    goToSnapAdd();
     expect(element(by.id('snap.name')).isDisplayed()).toBe(true);
   });
 
   it('should have a snapshot size input field', function(){
-    goToSnapAdd();
     expect(element(by.id('megs')).isDisplayed()).toBe(true);
   });
 
@@ -66,7 +55,6 @@ describe('Should check the snapshot add workflow', function(){
 
   //test if given megs value is origin vol size
   it('given megs should match to the origin volume size', function(){
-    goToSnapAdd();
     //we need the volume.megs here
     var volmegs = '100';
     var megs = element(by.id('megs'));
@@ -75,7 +63,6 @@ describe('Should check the snapshot add workflow', function(){
   });
 
   it('should show required field errors if the submit button is clicked without any input data', function(){
-    goToSnapAdd();
     element(by.id('snap.name')).clear();
     element(by.id('megs')).clear();
     submitButton.click();
@@ -85,7 +72,6 @@ describe('Should check the snapshot add workflow', function(){
   });
 
   it('should show an error message when the given snapshot size is bigger than the source pool', function(){
-    goToSnapAdd();
     var volumepool = element(by.model('data.sourcePool'));
     var snapSizeInput = element(by.model('megs'));
     for(var key in helpers.configs.pools) {
