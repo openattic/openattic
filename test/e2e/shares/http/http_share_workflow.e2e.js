1f493a99b89a3f226cac76c46d304164cf4934c1
Laura Paduano <laura.paduano@it-novum.com>
2015-05-13 12:45:14 +0200

    refactored http share workflow test; moved test to subfolder 'http';

--- test/e2e/shares/http/http_share_workflow.e2e.js
+++ test/e2e/shares/http/http_share_workflow.e2e.js
@@ -1,12 +1,15 @@
-var helpers = require('../common.js');  
+var helpers = require('../../common.js');  
 
 describe('HTTP Share workflow', function(){  
   
   var volumename = 'protractor_test_volume';
   var volume = element(by.cssContainingText('tr', volumename));
   var submitButton = element(by.css('.tc_submitButton'));
+  var volumesItem = element.all(by.css('ul .tc_menuitem')).get(3);
  
-  function goToHTTPShare(){
+  beforeEach(function(){
+    
+    volumesItem.click();
     expect(volume.isDisplayed()).toBe(true);
     volume.click();
     browser.sleep(400);
@@ -14,40 +17,34 @@ describe('HTTP Share workflow', function(){
     browser.sleep(400);
     element(by.css('.tc_httpShareAdd')).click();
     browser.sleep();
-  }
+  });
   
   it('should have a "Create HTTP Share" title', function(){
-    goToHTTPShare();
-    expect(element(by.css('h2')).getText()).toEqual('Create HTTP Share');    
+    expect(element(by.css('h2')).getText()).toEqual('Create HTTP Share');
   });
 
   it('should have one input field', function(){
-    goToHTTPShare();
     expect(element(by.id('sharePath')).isDisplayed()).toBe(true);
   });
   
   it('should should have a back button', function(){
-    goToHTTPShare();
     expect(element(by.css('.tc_backButton')).isPresent()).toBe(true);
   });
   
   it('should have a submit button', function(){
-    goToHTTPShare();
     expect(element(by.css('.tc_submitButton')).isPresent()).toBe(true);
   });
   
   it('should show an error message when hitting the submit button without any data', function(){
-    goToHTTPShare();
     element(by.id('sharePath')).clear();
     submitButton.click();
     expect(element(by.css('.tc_pathRequired')).isDisplayed()).toBe(true);
   });
   
   it('should navigate back to the HTTP-Shares overview when hitting the button', function(){
-    goToHTTPShare();
     var backButton = element(by.css('.tc_backButton'));
     backButton.click();
 
-    expect(element(by.css('.tc_oadatatable_http_shares')).isPresent()).toBe(true);    
+    expect(element(by.css('.tc_oadatatable_http_shares')).isPresent()).toBe(true);
   });
 });
