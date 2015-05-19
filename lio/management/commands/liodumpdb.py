5b21f5ad9fe4029a1b6547d478d74eba4c762f92
Michael Ziegler <michael.ziegler@it-novum.com>
2015-05-19 22:26:09 +0200

    update liodumpdb command

--- lio/management/commands/liodumpdb.py
+++ lio/management/commands/liodumpdb.py
@@ -14,34 +14,43 @@
  *  GNU General Public License for more details.
 """
 
-from rtslib.root   import RTSRoot
+import os
+import socket
 
 from django.core.management.base import BaseCommand
 
-from lio.models import Backstore, Target, TARGET_TYPE_CHOICES
+from lio.models import HostACL
+from volumes.models import BlockVolume
 
 class Command( BaseCommand ):
     help = "Dump the current LIO database object structure."
 
     def handle(self, **options):
-        r = RTSRoot()
+        localname = socket.gethostname().split(".")[0]
+        localarch = os.uname()[4].replace("_", "")
+        prefix = "iqn.2003-01.org.linux-iscsi.%s.%s" % (localname, localarch)
+        prefix = prefix.strip().lower()
 
-        for backstore in Backstore.objects.all():
-            print "Backstore: %s%d" % (backstore.type, backstore.store_id)
-            for storage_object in backstore.storageobject_set.all():
-                print "    -> %s (%s, WWN=%s)" % (storage_object.volume.storageobj.name, storage_object.volume.volume.path, storage_object.wwn)
+        print "Storage Objects:"
+        for blockvolume in BlockVolume.objects.filter(hostacl__isnull=False).distinct():
+            print "    -> %s (%s, WWN=%s)" % (blockvolume.storageobj.name, blockvolume.volume.path, blockvolume.storageobj.uuid)
 
-        for fabricname, fabricdesc in TARGET_TYPE_CHOICES:
-            print "Fabric:", fabricname
-            for target in Target.objects.filter(type=fabricname):
-                print "    -> Target %d: %s = %s" % (target.id, target.name, target.wwn)
-                for tpg in target.tpg_set.all():
-                    print "       -> TPG %d: %s" % (tpg.id, tpg.tag)
-                    for acl in tpg.acl_set.all():
-                        print "          -> ACL %d: %s" % (acl.id, acl.initiator.wwn)
-                        for mlun in acl.mapped_luns.all():
-                            print "              -> Mapped LUN %d: %s" % (mlun.lun_id, mlun.storageobj.volume.volume.path)
-                    for lun in tpg.lun_set.all():
-                        print "          -> LUN %d: %s (%d)" % (lun.id, lun.storageobj.volume.volume.path, lun.lun_id)
-                    for portal in tpg.portals.all():
-                        print "          -> Portal %d: %s" % (portal.id, portal)
+        print "Fabric: iscsi"
+        for hostacl in HostACL.objects.filter(host__initiator__type="iscsi"):
+            volume_name = hostacl.volume.storageobj.name
+            tgt_wwn = "%s:%s" % (prefix, volume_name.replace("_", "").replace(" ", ""))
+            print "    -> Target iqn=%s" % tgt_wwn
+            print "       -> TPG 1"
+            for initiator in hostacl.host.initiator_set.filter(type="iscsi"):
+                print "          -> ACL %s" % (initiator.wwn)
+                print "              -> Mapped LUN %d: %s" % (hostacl.lun_id, hostacl.volume.volume.path)
+            for portal in hostacl.portals.all():
+                print "          -> Portal %s" % portal
+
+        print "Fabric: qla2xxx"
+        for hostacl in HostACL.objects.filter(host__initiator__type="qla2xxx"):
+            print "    -> Target free=UNKNOWN"
+            print "       -> TPG 1"
+            for initiator in hostacl.host.initiator_set.filter(type="qla2xxx"):
+                print "          -> ACL %s" % (initiator.wwn)
+                print "              -> Mapped LUN %d: %s" % (hostacl.lun_id, hostacl.volume.volume.path)
