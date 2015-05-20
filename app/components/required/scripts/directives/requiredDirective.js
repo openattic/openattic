cbcd3fda268fcc06b6866f23aeb619e6b55624ff
Tatjana Dehler <tatjana.dehler@it-novum.com>
2015-05-20 15:17:25 +0200

    show a required red star for ng-required too

--- app/components/required/scripts/directives/requiredDirective.js
+++ app/components/required/scripts/directives/requiredDirective.js
@@ -12,4 +12,16 @@ angular.module('openattic.required')
         }
       }
     };
+  })
+  .directive('ngRequired', function($document){
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs){
+        var labelNode = $document[0].body.querySelector("label[for='" + attrs['id'] + "']");
+        if (labelNode) {
+          var labelElement = angular.element(labelNode);
+          labelElement.append("<span class='required'> *</span>");
+        }
+      }
+    };
   });
