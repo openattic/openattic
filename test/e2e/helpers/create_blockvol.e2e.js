db07ea6d0f13a74e5655a137a414f68c8cbecfbf
Laura Paduano <laura.paduano@it-novum.com>
2015-05-26 17:49:40 +0200

    updated block volume helper

--- test/e2e/helpers/create_blockvol.e2e.js
+++ test/e2e/helpers/create_blockvol.e2e.js
@@ -3,9 +3,11 @@ var helpers = require('../common.js');
 describe('should create a blockvolume', function(){
   var volumename = 'protractor_test_volume';  
   var volume = element(by.cssContainingText('tr', volumename));
+  var volumesItem = element.all(by.css('ul .tc_menuitem')).get(3);
+  
   
   it('should create a simple block volume', function(){
-
+    volumesItem.click();
     element(by.css('oadatatable .tc_add_btn')).click();
 
     for(var key in helpers.configs.pools) {
