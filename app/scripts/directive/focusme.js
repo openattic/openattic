cd818e171ec5a61a1af12584e83f36323af58a48
Michael Ziegler <michael.ziegler@it-novum.com>
2015-05-27 14:26:03 +0200

    add focusMe directive

---
+++ app/scripts/directive/focusme.js
@@ -0,0 +1,20 @@
+angular.module('openattic')
+  .directive('focusMe', function($timeout, $parse) {
+    return {
+      link: function(scope, element, attrs) {
+        var model = $parse(attrs.focusMe);
+        scope.$watch(model, function(value) {
+          if(value === true) {
+            $timeout(function() {
+              element[0].focus();
+              element[0].select();
+            });
+          }
+        });
+        element.bind('blur', function() {
+           console.log('blur');
+           scope.$apply(model.assign(scope, false));
+        });
+      }
+    };
+  });
