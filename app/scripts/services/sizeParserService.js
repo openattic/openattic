e350aec000321e76a9480876420647f71b197a78
Tatjana Dehler <tatjana.dehler@it-novum.com>
2015-06-03 14:48:36 +0200

    added parseFloat function

--- app/scripts/services/sizeParserService.js
+++ app/scripts/services/sizeParserService.js
@@ -28,13 +28,38 @@ angular.module('openattic.sizeparser', [])
       return null;
     };
 
+    var _parseFloat = function(value){
+      // If it's a plain number, just parseInt() it
+      if(!value){
+        return null;
+      }
+
+      if( /^[\d.]+$/.test(value) ){
+        return parseFloat(value);
+      }
+
+      value = value.toLowerCase().replace(/\s/g, '');
+      // If it's a valid size string, calc its int value
+      var facs = mult.join('');
+      var rgx  = new RegExp( '^([\\d.]+)([' + facs + ']?)(i?)(b?)$' );
+
+      if( rgx.test(value) ){
+        var matched = rgx.exec(value);
+        return parseFloat(matched[1], 10) * Math.pow(1024, mult.indexOf(matched[2]));
+      }
+
+      // It didn't parse...
+      return null;
+    };
+
     var _isValid = function(value){
       return _parseInt(value) !== null;
     };
 
     return {
-      parseInt: _parseInt,
-      isValid:  _isValid
+      parseInt:   _parseInt,
+      parseFloat: _parseFloat,
+      isValid:    _isValid
     };
   });
 
