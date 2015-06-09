191ab691cde4ed3eff8d51df125be4f4540ea690
Laura Paduano <laura.paduano@it-novum.com>
2015-06-09 15:47:16 +0200

    snapshot clone test: removed no longer needed variables; removed line breaks

--- test/e2e/snapshots/clone/filesystem_vol_snapshot_clone.e2e.js
+++ test/e2e/snapshots/clone/filesystem_vol_snapshot_clone.e2e.js
@@ -1,19 +1,11 @@
 var helpers = require('../../common.js');
 
 describe('should create a clone volume of a snapshot (base: filesystem volume)', function(){
-  
-  var volumename = 'protractor_test_volume';
-  var volume = element(by.cssContainingText('tr', volumename));
-  
-  var snapshotname = 'protractor_test_snap';
-  var snapshot = element(by.cssContainingText('tr', snapshotname));
-  
+
   var clonename ="protractor_test_clone";
   var clone = element(by.cssContainingText('tr', clonename));
-    
-  var snapMenuBtn = element.all(by.css('.tc_menudropdown')).get(1);
   var volumesItem = element.all(by.css('ul .tc_menuitem')).get(3);
-  
+ 
   beforeAll(function(){
     helpers.login();
     volumesItem.click();
@@ -22,13 +14,10 @@ describe('should create a clone volume of a snapshot (base: filesystem volume)',
     helpers.create_snap_clone();
   });
   
-  
   it('should display the clone in the volumes list', function(){
     browser.sleep(800);
     expect(clone.isDisplayed()).toBe(true);
   });
-  
-  
   
   afterAll(function(){
     helpers.delete_snapshot();
