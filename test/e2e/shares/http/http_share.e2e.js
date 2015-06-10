15d1bf3ab4db957ea40cd948cea2609b050eae9e
Laura Paduano <laura.paduano@it-novum.com>
2015-06-10 17:20:42 +0200

    http share tests: fixed protractor warning (more than one element found)

--- test/e2e/shares/http/http_share.e2e.js
+++ test/e2e/shares/http/http_share.e2e.js
@@ -1,21 +1,21 @@
 var helpers = require('../../common.js');
 
 describe('HTTP Share add', function(){
-  
+
   var volumename = 'protractor_test_volume';
-  var volume = element(by.cssContainingText('tr', volumename));
+  var volume = element.all(by.cssContainingText('tr', volumename)).get(0);
   var share = element(by.css('.tc_http_share'));
   var volumesItem = element.all(by.css('ul .tc_menuitem')).get(3);
-  
-  
+
+
   beforeAll(function(){
     helpers.login();
     volumesItem.click();
     helpers.create_volume("xfs");
   });
-  
+
   require('./http_share_workflow.e2e.js');
-  
+
   function goToHTTPShare(){
     expect(volume.isDisplayed()).toBe(true);
     volume.click();
@@ -25,12 +25,12 @@ describe('HTTP Share add', function(){
     element(by.css('.tc_httpShareAdd')).click();
     browser.sleep(400);
   }
-  
+
   it('should create the http-share', function(){
     goToHTTPShare();
     element(by.css('.tc_submitButton')).click();
   });
-  
+
   it('should display the created http share in the http shares overview panel', function(){
     expect(volume.isDisplayed()).toBe(true);
     volume.click();
@@ -40,7 +40,7 @@ describe('HTTP Share add', function(){
     browser.sleep(400);
     expect(share.isPresent()).toBe(true);
   });
-  
+
   it('should delete the http-share', function(){
     expect(volume.isDisplayed()).toBe(true);
     volume.click();
@@ -54,7 +54,7 @@ describe('HTTP Share add', function(){
     element(by.id('bot2-Msg1')).click();
     browser.sleep(400);
   });
-  
+
   it('should not display the share anymore', function(){
     expect(volume.isDisplayed()).toBe(true);
     volume.click();
@@ -62,8 +62,8 @@ describe('HTTP Share add', function(){
     element(by.css('.tc_httpShareTab')).click();
     expect(share.isPresent()).toBe(false);
   });
-  
+
   afterAll(function(){
-    helpers.delete_volume();  
+    helpers.delete_volume();
   });
 });
--- test/e2e/shares/http/http_share_workflow.e2e.js
+++ test/e2e/shares/http/http_share_workflow.e2e.js
@@ -1,11 +1,11 @@
-var helpers = require('../../common.js');  
+var helpers = require('../../common.js');
 
-describe('HTTP Share workflow', function(){  
-  
+describe('HTTP Share workflow', function(){
+
   var volumename = 'protractor_test_volume';
-  var volume = element(by.cssContainingText('tr', volumename));
+  var volume = element.all(by.cssContainingText('tr', volumename)).get(0);
   var volumesItem = element.all(by.css('ul .tc_menuitem')).get(3);
- 
+
   beforeEach(function(){
     volumesItem.click();
     expect(volume.isDisplayed()).toBe(true);
@@ -16,7 +16,7 @@ describe('HTTP Share workflow', function(){
     element(by.css('.tc_httpShareAdd')).click();
     browser.sleep();
   });
-  
+
   it('should have a "Create HTTP Share" title', function(){
     expect(element(by.css('h2')).getText()).toEqual('Create HTTP Share');
   });
@@ -24,21 +24,21 @@ describe('HTTP Share workflow', function(){
   it('should have one input field', function(){
     expect(element(by.id('sharePath')).isDisplayed()).toBe(true);
   });
-  
+
   it('should should have a back button', function(){
     expect(element(by.css('.tc_backButton')).isPresent()).toBe(true);
   });
-  
+
   it('should have a submit button', function(){
     expect(element(by.css('.tc_submitButton')).isPresent()).toBe(true);
   });
-  
+
   it('should show an error message when hitting the submit button without any data', function(){
     element(by.id('sharePath')).clear();
     element(by.css('.tc_submitButton')).click();
     expect(element(by.css('.tc_pathRequired')).isDisplayed()).toBe(true);
   });
-  
+
   it('should navigate back to the HTTP-Shares overview when hitting the button', function(){
     var backButton = element(by.css('.tc_backButton'));
     backButton.click();
