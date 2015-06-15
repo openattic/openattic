25824871db290a6288ccc79ec8ab523a6ef369fd
Michael Ziegler <michael.ziegler@it-novum.com>
2015-06-15 16:42:49 +0200

    add disk detection

--- volumes/management/__init__.py
+++ volumes/management/__init__.py
@@ -14,13 +14,76 @@
  *  GNU General Public License for more details.
 """
 
+import os
+import os.path
+import pyudev
 import sysutils.models
 
-from volumes.models import GenericDisk
+from ifconfig.models import Host
+from volumes.models import StorageObject, DiskDevice, GenericDisk
+from systemd.helpers import get_dbus_object
 
-def update_disksize(**kwargs):
-    for gdisk in GenericDisk.objects.all():
-        gdisk.megs = int(gdisk.udev_device.attributes["size"]) * 512. / 1024. / 1024.
-        gdisk.save()
+def update_disks(**kwargs):
+    ctx = pyudev.Context()
 
-sysutils.models.post_install.connect(update_disksize, sender=sysutils.models)
+    for dev in ctx.list_devices():
+        if dev.subsystem != "block":
+            continue
+        if "MAJOR" not in dev or int(dev["MAJOR"].strip("\0")) != 8:
+            continue
+
+        if "MINOR" not in dev or int(dev["MINOR"].strip("\0")) % 16 != 0:
+            continue
+
+        if "ID_TYPE" not in dev or dev["ID_TYPE"].strip("\0") != "disk":
+            continue
+
+        if "ID_VENDOR" in dev and dev["ID_VENDOR"].strip("\0") in ("LSI", "IBM", "AMI"):
+            # skip disks created by LSI hardware raid adapters or AMI IPMI (SuperMicro)
+            continue
+
+        print "checking disk", dev.device_node
+
+        # See if we have a serial
+        for attr in ("ID_SCSI_SERIAL", "ID_SERIAL_SHORT", "ID_SERIAL"):
+            if attr in dev:
+                serial = dev[attr].strip("\0")
+                break
+        else:
+            print "serial not found"
+            continue
+
+        try:
+            diskdev = DiskDevice.objects.get(serial=serial)
+        except DiskDevice.DoesNotExist:
+            rotational_path = os.path.join(dev.sys_path, "queue", "rotational")
+
+            with open(rotational_path, "rb") as fd:
+                rotational = fd.read().strip() == "1"
+
+            if not rotational:
+                dtype = "SSD"
+                drpm = 0
+            else:
+                if "/end_device-" in dev.sys_path:
+                    # see http://unix.stackexchange.com/questions/96041/can-you-identify-transport-via-sysfs
+                    dtype = "SAS"
+                else:
+                    dtype = "SATA"
+
+                drpm = int(get_dbus_object("/volumes").get_rotational_rate(dev.device_node))
+
+            megs = int(get_dbus_object("/volumes").get_disk_size(dev.device_node))
+
+            with StorageObject(name=dev.get("ID_MODEL", "unknown disk"), megs=megs) as so:
+                diskdev = DiskDevice(storageobj=so, host=Host.objects.get_current(), serial=serial,
+                                    model=dev.get("ID_MODEL", "unknown"), type=dtype, rpm=drpm)
+                diskdev.save()
+
+        try:
+            diskdev.genericdisk
+        except GenericDisk.DoesNotExist:
+            gd = GenericDisk(storageobj=diskdev.storageobj, disk_device=diskdev)
+            gd.save()
+
+sysutils.models.post_install.connect(update_disks, sender=sysutils.models)
