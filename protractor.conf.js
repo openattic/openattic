ad326ec5013656fef6e7df30ee274bdc3e9e7276
Laura Paduano <laura.paduano@it-novum.com>
2015-06-18 16:01:07 +0200

    updated protractor.conf

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,10 +1,11 @@
 exports.config = {
-    
+
   seleniumAddress: 'http://localhost:4444/wd/hub',
   jasmineNodeOpts: {
-    defaultTimeoutInterval: 360000
+    defaultTimeoutInterval: 360000,
   },
   framework: 'jasmine2',
+//   allScriptsTimeout: 50000,
   
   suites: {
     dashboard         : 'test/e2e/dashboard/**/*.e2e.js',
@@ -14,13 +15,17 @@ exports.config = {
     volumes_add       : 'test/e2e/volumes/add/**/*.e2e.js',
     volumes_protection: 'test/e2e/volumes/protection/**/*.e2e.js',
     volumes_resize    : 'test/e2e/volumes/resize/**/*.e2e.js',
-//     snapshot_add      : 'test/e2e/snapshots/add/**/*.e2e.js',
-//     snapshot_clone    : 'test/e2e/snapshots/clone/**/*.e2e.js',
-//     http_share_add    : 'test/e2e/shares/http/*.e2e.js',
-//     nfs_share_add     : 'test/e2e/shares/nfs/*.e2e.js',
-//     hosts             : 'test/e2e/hosts/**/*.e2e.js',
+    //snapshot_add      : 'test/e2e/snapshots/add/**/*.e2e.js',
+    //snapshot_clone    : 'test/e2e/snapshots/clone/**/*.e2e.js',
+    //http_share_add    : 'test/e2e/shares/http/*.e2e.js',
+    //nfs_share_add     : 'test/e2e/shares/nfs/*.e2e.js',
+    //cifs_share_add    : 'test/e2e/shares/cifs/*.e2e.js',
+    //lun_add           : 'test/e2e/shares/lun/*.e2e.js',
+    //hosts             : 'test/e2e/hosts/**/*.e2e.js',
+    //users             : 'test/e2e/users/**/*.e2e.js',
+    //auth              : 'test/e2e/auth/*.e2e.js',
   },
-  
+
   onPrepare: function(){
     browser.driver.manage().window().maximize();
   },
