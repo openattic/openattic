59e316afa55e5316e8ecbb7547b75b273f470efb
Laura Paduano <laura.paduano@it-novum.com>
2015-06-19 15:49:33 +0200

    updated authController

--- app/components/auth/scripts/controllers/authController.js
+++ app/components/auth/scripts/controllers/authController.js
@@ -4,17 +4,19 @@ angular.module('openattic.auth')
   .controller('authController', function ($scope, $rootScope, $state, authService) {
     $scope.fieldRequired = 'This field is required.';
     $scope.correctInput = 'The given credentials are not correct.';
-
-    $scope.login = function(loginForm){
+    
+    $scope.$watchGroup(['username', 'password'], function(){
+        $scope.submitted = false;
+    });
+    $scope.login = function(){
       $scope.submitted = true;
+      $scope.loginFailed = false;
       var loginData = {'username': $scope.username, 'password': $scope.password};
       authService.login(loginData, function(res){
         $rootScope.user = res;
         $state.go('dashboard');
       }, function(){
-        loginForm.username.$setValidity('correctInput', false);
-        loginForm.password.$setValidity('correctInput', false);
-        $scope.submitted = false;
-      });
+        $scope.loginFailed = true;
+    });
     };
   });
