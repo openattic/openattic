0bd016966ef6a0662eef373d24f46feae1ca736a
Laura Paduano <laura.paduano@it-novum.com>
2015-06-22 12:34:43 +0200

    added more e2e tests for login

--- test/e2e/auth/login_logout.e2e.js
+++ test/e2e/auth/login_logout.e2e.js
@@ -1,13 +1,19 @@
 var configs = require('../configs.js');
 
 describe('should test the login', function(){
-    
+  
+  var name = element(by.model('username'));
+  var passwd = element(by.model('password'));
+  
+  var submitBtn = element(by.css('input[type="submit"]'));
+  
   it('should login and get to the dashboard site', function(){
     browser.get(configs.url);
     element.all(by.model('username')).sendKeys(configs.username);
     element.all(by.model('password')).sendKeys(configs.password);
-    element.all(by.css('input[type="submit"]')).click();
-    expect(browser.getCurrentUrl()).toContain('#/dashboard'); 
+    submitBtn.click();
+    //if login was successful the url should contain /dashboard
+    expect(browser.getCurrentUrl()).toContain('#/dashboard');
   });
   
   it('should click any menu entry', function(){
@@ -20,5 +26,50 @@ describe('should test the login', function(){
      element(by.css('.tc_logout')).click();
      expect(browser.getCurrentUrl()).toContain('/#/login');
     
+  });
+  
+  //login workflow
+  
+  it('should have an user input field', function(){
+    expect(name.isDisplayed()).toBe(true);
+  });
+  
+  it('should have an password input field', function(){
+    expect(element(by.model('password')).isDisplayed()).toBe(true);
+  });
+
+  it('should show an error if user input field has no data', function(){
+      //make sure that input field username is empty
+      name.clear();
+      submitBtn.click();
+      expect(element(by.css('.tc_usernameRequired')).getText()).toBe('This field is required.');
+  });
+  
+  it('should show an error if password input field has no data', function(){
+      passwd.clear();
+      submitBtn.click();
+      expect(element(by.css('.tc_passwdRequired')).getText()).toBe('This field is required.');
+  });
+
+  it('should show an error if username and password input fields have no input data and submit button was clicked', function(){
+      //make sure that user and password fields are empty
+      name.clear();
+      passwd.clear();
+      submitBtn.click();
+      expect(element(by.css('.tc_usernameRequired')).getText()).toBe('This field is required.');
+      expect(element(by.css('.tc_passwdRequired')).getText()).toBe('This field is required.');
+  });
+  
+  it('should display an error message if given credentials are incorrect', function(){
+      name.click();
+      browser.sleep(400);
+      name.clear();
+      name.sendKeys('wer344fv     resfferwwxd');
+      passwd.click();
+      browser.sleep(400);
+      passwd.clear();
+      passwd.sendKeys('2943tr3befc vr');
+      submitBtn.click();
+      expect(element(by.binding('correctInput')).getText()).toBe('The given credentials are not correct.');
   });
 });     
