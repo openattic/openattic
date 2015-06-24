9f05dd7f9f26dd35169ef901b6477775063b66ab
Laura Paduano <laura.paduano@it-novum.com>
2015-06-24 17:21:01 +0200

    added basic draft for vmStorage and blockStorage tests [WIP]

---
+++ test/e2e/wizard/vmStorage.e2e.js
@@ -0,0 +1,35 @@
+var helpers = require('../common.js');
+var configs = require('../configs.js');
+
+describe('VM Storage Wizard', function(){
+    
+  var wizardOverviewBtn = element(by.css('.tc_wizardOverview'));
+  var previousBtn = element(by.css('.tc_previousBtn'));
+
+  beforeAll(function(){
+    helpers.login();  
+  });
+
+  it('should land on the dashboard site after login', function(){
+    expect(browser.getCurrentUrl()).toContain('#/dashboard');    
+  });
+  
+  //   <-- VM Storage Wizard -->
+   it('should have a button "VM Storage";navigate through the wizard', function(){
+    var wizards = element.all(by.repeater('wizard in wizards')).then(function(wizards){
+      var fs_wizard = wizards[1].element(by.className('btn-block'));
+      expect(fs_wizard.isDisplayed()).toBe(true);
+      fs_wizard.click();
+      
+      //first site
+      
+      //check available buttons
+      expect(wizardOverviewBtn.isDisplayed()).toBe(true);
+      expect(previousBtn.isDisplayed()).toBe(true);
+    });
+    //check if angular expression contains 'Next' or 'Done
+    var nextBtn = element(by.id('nextBtn')).evaluate('nextBtnText()');
+    expect(nextBtn.getText()).toEqual('Next');    
+  });    
+    
+});
