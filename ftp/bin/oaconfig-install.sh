7ce0409bc27a97d69d3e74e328339a7e66fafa6e
Michael Ziegler <michael.ziegler@it-novum.com>
2015-07-03 13:01:53 +0200

    remove ftp module

--- ftp/bin/oaconfig-install.sh
+++
@@ -1,7 +0,0 @@
-if [ -e "/etc/proftpd/proftpd.conf" -a -e "/etc/proftpd/conf.d" ]; then
-	if ! grep -q 'Include /etc/proftpd/conf.d/' /etc/proftpd/proftpd.conf; then
-		echo 'Include /etc/proftpd/conf.d/' >> /etc/proftpd/proftpd.conf
-		service proftpd restart
-	fi
-fi
-service proftpd status >/dev/null || service proftpd start
