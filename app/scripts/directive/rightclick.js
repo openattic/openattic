ca858f4668821780c6f5112977c06f5c327a3774
Sebastian Krah <sebastian.krah@it-novum.com>
2015-07-15 10:14:57 +0200

    added new directive to handle right-clicks

---
+++ app/scripts/directive/rightclick.js
@@ -0,0 +1,12 @@
+angular.module('openattic')
+    .directive('ngRightClick', function($parse) {
+        return function(scope, element, attrs) {
+            var fn = $parse(attrs.ngRightClick);
+            element.bind('contextmenu', function(event) {
+                scope.$apply(function() {
+                    event.preventDefault();
+                    fn(scope, {$event:event});
+                });
+            });
+        };
+    });
