aa80c8952905683f60d1c311521ebc013112c7a8
Tatjana Dehler <tatjana.dehler@it-novum.com>
2015-07-28 10:34:37 +0200

    removed test_ping because it does not work for rest apis

--- test_ping.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# kate: space-indent on; indent-width 4; replace-tabs on;
-
-from testcase import GatlingTestCase
-
-class TestPing(GatlingTestCase):
-    def test_ping(self):
-        self.assertEqual(self.api.ping(), "pong")
