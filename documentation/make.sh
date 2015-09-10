4d66c9a2857a897892492fee12461bc134e7c04a
Michael Ziegler <michael.ziegler@it-novum.com>
2015-09-10 15:32:17 +0200

    set PYTHONPATH to backend and remove rpcapi make command

--- documentation/make.sh
+++ documentation/make.sh
@@ -7,12 +7,10 @@ set -u
 MYPATH="$(dirname `readlink -f "$0"`)"
 cd "$MYPATH"
 
-export PYTHONPATH=/srv/openattic
+export PYTHONPATH=/srv/openattic/backend
 export DJANGO_SETTINGS_MODULE=settings
 export http_proxy="http://proxy.master.dns:8080/"
 export https_proxy="http://proxy.master.dns:8080/"
-
-./integration/api/rpcapi/make.sh
 
 make html
 #make singlehtml
