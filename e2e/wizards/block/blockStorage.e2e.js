70cafe402ad529a66f0591af50f10651688132a7
Laura Paduano <laura.paduano@it-novum.com>
2015-09-16 12:11:31 +0200

    updated blockstorage wizard e2e

--- e2e/wizards/block/blockStorage.e2e.js
+++ e2e/wizards/block/blockStorage.e2e.js
@@ -2,10 +2,10 @@ var helpers = require('../../common.js');
 var configs = require('../../configs.js');
 
 describe('Raw Block Storage Wizard', function(){
-    
+
   var wizardOverviewBtn = element(by.css('.tc_wizardOverview'));
   var previousBtn = element(by.css('.tc_previousBtn'));
-  
+
   var volume = element(by.id('volumename'));
   var pool = element(by.id('source_pool'));
   var size = element(by.id('volumemegs'));
@@ -13,12 +13,12 @@ describe('Raw Block Storage Wizard', function(){
   var hostname = "protractor_test_host";
   var host = element(by.cssContainingText('tr', hostname));
   var iqn = "iqn.1991-05.com.microsoft:protractor_test_host";
-  
+
   beforeAll(function(){
     helpers.login();
     helpers.create_host();
   });
-  
+
   it('should add an iqn to created host', function(){
     browser.sleep(400);
     expect(host.isPresent()).toBe(true);
@@ -26,12 +26,12 @@ describe('Raw Block Storage Wizard', function(){
     browser.sleep(400);
     element(by.model('data.iscsiInis')).click();
     element.all(by.model('newTag.text')).get(1).sendKeys(iqn);
-    browser.sleep(400);      
+    browser.sleep(400);
   });
 
   it('should navigate back to the dashboard after creating a host', function(){
     var dashboard = element.all(by.css('ul .tc_menuitem')).get(0);
-    dashboard.click();    
+    dashboard.click();
   });
   //<-- Raw Block Storage Wizard --->
   it ('should have a button "Raw Block Storage"; navigate through the wizard', function(){
@@ -39,9 +39,9 @@ describe('Raw Block Storage Wizard', function(){
       var fs_wizard = wizards[2].element(by.cssContainingText('span', 'Raw Block Storage'));
       expect(fs_wizard.isDisplayed()).toBe(true);
       fs_wizard.click();
-      
+
       //first site
-      
+
       //check available buttons
       expect(wizardOverviewBtn.isDisplayed()).toBe(true);
       expect(previousBtn.isDisplayed()).toBe(true);
@@ -53,10 +53,10 @@ describe('Raw Block Storage Wizard', function(){
     expect(volume.isDisplayed()).toBe(true);
     expect(size.isDisplayed()).toBe(true);
     //expect(is_protected.Present()).toBe(true);
-    
+
     //enter volume data
     volume.sendKeys('protractor_test_volume');
-    
+
     //in order to enter a size we need to choose a pool first
       for(var key in configs.pools) {
         var pool = configs.pools[key];
@@ -64,12 +64,12 @@ describe('Raw Block Storage Wizard', function(){
         volumePoolSelect.click();
         element.all(by.cssContainingText('option', '(volume group,')).get(0).click();
         break;
-      }    
-    
+      }
+
     //enter some data to get to the next site
     size.sendKeys('100MB');
     nextBtn.click();
-    
+
     //Step 2 - check at least the title then skip and available buttons
     expect(element(by.css('.tc_step2')).getText()).toEqual('Raw Block Storage Step 2 - Create Mirror - Coming Soon...');
     expect(wizardOverviewBtn.isDisplayed()).toBe(true);
@@ -77,32 +77,64 @@ describe('Raw Block Storage Wizard', function(){
     expect(nextBtn.getText()).toEqual('Next');
     browser.sleep(400);
     nextBtn.click();
-    
+
     //Step 3 - create LUN
-    
+
     expect(element(by.css('.tc_step3')).getText()).toEqual('Raw Block Storage Step 3 - Create a iSCSI/FC Share');
-    
+
     expect(wizardOverviewBtn.isDisplayed()).toBe(true);
     expect(previousBtn.isDisplayed()).toBe(true);
     expect(nextBtn.getText()).toEqual('Next');
     //select host
     var hostSelect = element(by.model('input.iscsi_fc.host'));
     hostSelect.element(by.cssContainingText('option', hostname)).click();
-    
+
     nextBtn.click();
-    
+
     //Finish
-    expect(element(by.css('.tc_wizardDone')).getText()).toEqual('Raw Block Storage Step 4 - Save configuration');    
-    expect(nextBtn.getText()).toEqual('Done');    
+    expect(element(by.css('.tc_wizardDone')).getText()).toEqual('Raw Block Storage Step 4 - Save configuration');
+    expect(nextBtn.getText()).toEqual('Done');
     nextBtn.click();
-    console.log('<----- raw block storage test ended ------>');    
+
+    element.all(by.css('ul .tc_menuitem')).get(3).click();
+
+		//check if lun exists
+    browser.sleep(400);
+    element.all(by.css('.tc_entries_dropdown')).get(0).click();
+    element.all(by.css('.tc_entries_100')).get(0).click();
+    browser.sleep(400);
+    expect(volume.isPresent()).toBe(true);
+    volume.click();
+    browser.sleep(400);
+    element(by.css('.tc_iscsi_fcTab')).click();
+    browser.sleep(400);
+    expect(element(by.cssContainingText('tr', hostname)).isDisplayed()).toBe(true);
+
+		//remove the lun map
+		element.all(by.css('ul .tc_menuitem')).get(3).click();
+    browser.sleep(400);
+    element.all(by.css('.tc_entries_dropdown')).get(0).click();
+    element.all(by.css('.tc_entries_100')).get(0).click();
+    browser.sleep(400);
+    expect(volume.isPresent()).toBe(true);
+    volume.click();
+    browser.sleep(400);
+    element(by.css('.tc_iscsi_fcTab')).click();
+    browser.sleep(400);
+    element(by.cssContainingText('tr', hostname)).click();
+    element(by.css('.tc_lunDelete')).click();
+    browser.sleep(400);
+    element(by.id('bot2-Msg1')).click();
+    browser.sleep(800);
+		expect(element(by.cssContainingText('tr', hostname)).isPresent()).toBe(false);
+    console.log('<----- raw block storage test ended ------>');
   });
-  
+
   afterAll(function(){
     helpers.delete_volume();
-    helpers.delete_host();
+		helpers.delete_host();
     console.log('<-----Raw Block Storage volume and host removed --->');
   });
-    
+
 });
 
