ddc50ad702b45ba5b9651d0fe50cab214939e3c5
Laura Paduano <laura.paduano@it-novum.com>
2015-09-18 10:34:50 +0200

    updated snapshot e2e tests

--- e2e/snapshots/add/snapshot_workflow.e2e.js
+++ e2e/snapshots/add/snapshot_workflow.e2e.js
@@ -1,17 +1,17 @@
 var helpers = require('../../common.js');
 
 describe('Should check the snapshot add workflow', function(){
-  var volumename = 'protractor_test_volume';
-  var volume = element.all(by.cssContainingText('tr', volumename)).get(0);
+  var volumename = 'protractor_testvol_snapworkflow';
+  var volume = element(by.cssContainingText('tr', volumename));
   var submitBtn = element(by.css('.tc_submitButton'));
   var snapshot = element(by.id('snap.name'));
   var snap_size = element(by.id('megs'));
   var backBtn = element(by.css('.tc_backButton'));
   var snapname = 'protractor_test_snap';
-  
+
   beforeAll(function(){
     helpers.login();
-    helpers.create_volume("lun");
+    helpers.create_volume(volumename, "lun");
     volume.click();
     element(by.css('.tc_snapshotTab')).click();
     element(by.css('.tc_snapshotAdd')).click();
@@ -24,7 +24,7 @@ describe('Should check the snapshot add workflow', function(){
   it('should have a back button', function(){
     expect(backBtn.isPresent()).toBe(true);
   });
-  
+
   it('should have a submit button', function(){
     expect(submitBtn.isPresent()).toBe(true);
   });
@@ -49,7 +49,7 @@ describe('Should check the snapshot add workflow', function(){
     //we need the volume.megs here
     var volmegs = '100';
     expect(snap_size.getAttribute('value')).toEqual(volmegs + ".00MB");
-    
+
   });
 
   it('should show required field errors if the submit button is clicked without any input data', function(){
@@ -60,16 +60,16 @@ describe('Should check the snapshot add workflow', function(){
     expect(element(by.css('.tc_nameRequired')).isDisplayed()).toBe(true);
     expect(element(by.css('.tc_sizeRequired')).isDisplayed()).toBe(true);
   });
-  
+
   it('should show an error message if snapshot name has no data', function(){
     snapshot.clear();
     snap_size.sendKeys('100MB');
     submitBtn.click();
-    
+
     expect(element(by.css('.tc_nameRequired')).isDisplayed()).toBe(true);
     expect(element(by.css('.tc_sizeRequired')).isDisplayed()).toBe(false);
   });
-  
+
   it('should show an error message if snapshot size has no data', function(){
     snapshot.sendKeys(snapname);
     snap_size.clear();
@@ -77,21 +77,21 @@ describe('Should check the snapshot add workflow', function(){
     expect(element(by.css('.tc_nameRequired')).isDisplayed()).toBe(false);
     expect(element(by.css('.tc_sizeRequired')).isDisplayed()).toBe(true);
   });
-  
+
 //TODO fix!
 /*  it('should allow a snapshot size that is as big as the free left space of the pool', function(){
     for(var key in helpers.configs.pools) {
       var pool = helpers.configs.pools[key];
       var pool_size = element(by.id('megs')).evaluate('pool.usage.max_new_fsv_text').then(function(psize){
-        snap_size.clear().sendKeys(psize); 
+        snap_size.clear().sendKeys(psize);
       });
       browser.sleep(400);
       expect(element(by.css('.tc_sizeExceeded')).isDisplayed()).toBe(false);
-      
+
       break;
     }
-  });*/   
-  
+  });*/
+
   it('should show an error message when the given snapshot size is bigger than the source pool', function(){
     for(var key in helpers.configs.pools) {
       var pool = helpers.configs.pools[key];
@@ -99,20 +99,21 @@ describe('Should check the snapshot add workflow', function(){
       snap_size.clear().sendKeys(snapSize + pool.unit);
       browser.sleep(400);
       expect(element(by.css('.tc_sizeExceeded')).isDisplayed()).toBe(true);
-      
+
       break;
-    }    
+    }
   });
 
   it('should have a back button which navigates back to the snapshot overview', function(){
     backBtn.click();
 
     expect(element(by.css('.tc_oadatatable_snapshots')).isPresent()).toBe(true);
-  });  
-  
+		browser.sleep(400);
+  });
+
   afterAll(function(){
     console.log('snapshot_workflow');
-    helpers.delete_volume();    
+    helpers.delete_volume(volume, volumename);
   });
-  
+
 });
