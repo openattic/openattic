c2a447afc4e81cf6dc0c2fc883cbcef46a41bc42
Tatjana Dehler <tatjana.dehler@it-novum.com>
2015-09-21 12:53:05 +0200

    get just the local vg by the _get_vg_by_name function. the remote vg will be handled by the RemoteLvTestScenario [WIP]

--- gatling/lvm/scenarios.py
+++ gatling/lvm/scenarios.py
@@ -11,30 +11,9 @@ class LvTestScenario(GatlingTestCase):
         super(LvTestScenario, cls).setUpClass()
         cls.require_config("options", "connect")
         cls.require_config("options", "auth_token")
-
-        if cls.__name__.startswith("Remote"):
-            cls.require_enabled("lvm:remote")
-            cls.require_config("lvm:remote", "vg")
-            lvm_conf_part = "lvm:remote"
-        else:
-            cls.require_enabled("lvm")
-            cls.require_config("lvm", "vg")
-            lvm_conf_part = "lvm"
-
-        vg_name = cls.conf.get(lvm_conf_part, "vg")
-
-        res = cls.send_request("GET", "pools", search_param=("name=%s" % vg_name))
-
-        if res["count"] != 1:
-            raise SkipTest("REST api returned no or more than one object(s). But only one is expected.")
-        cls.vg = res["response"][0]
-
-        if cls.vg["name"] != cls.conf.get(lvm_conf_part, "vg") or \
-                        cls.vg["type"]["app_label"] != "lvm" or \
-                        cls.vg["type"]["model"] != "volumegroup":
-            print "VG not found"
-            print cls.vg["name"]
-            raise SkipTest("VG not found")
+        cls.require_enabled("lvm")
+        cls.require_config("lvm", "vg")
+        cls.vg = cls._get_vg_by_name(cls.conf.get("lvm", "vg"))
 
     @classmethod
     def setUp(self):
