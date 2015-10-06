a214ae38fb2985cd6977720d0a4f77d05995ea73
Tatjana Dehler <tatjana.dehler@it-novum.com>
2015-10-06 10:20:51 +0200

    added helper function to create the volume that should be mirrored

--- gatling/drbd/test_drbd.py
+++ gatling/drbd/test_drbd.py
@@ -9,14 +9,10 @@ class DrbdTests(object):
     def test_create_get_delete(self):
         """ Create a Connection and check that its Endpoints are created correctly. """
         # Create a volume that should be mirrored
-        vol_data = {"megs"          : 1000,
-                    "name"          : "gatling_drbd_vol1",
-                    "source_pool"   : {"id": self._get_pool()["id"]}}
-        vol_res = self.send_request("POST", "volumes", data=vol_data)
-        time.sleep(self.sleeptime)
+        vol = self._get_mirror_volume(self._get_pool()["id"])
 
         # Create the drbd mirror
-        mirror_data = {"source_volume"  : vol_res["response"],
+        mirror_data = {"source_volume"  : vol,
                        "remote_pool"    : self._get_remote_pool(),
                        "protocol"       : "C",
                        "syncer_rate"    : "30M"}
@@ -32,14 +28,10 @@ class DrbdTests(object):
     def test_remote_create_get_delete(self):
         """ Create a Connection for a volume located on the remote host. """
         # Create a volume on the remote host
-        vol_data = {"megs"          : 1000,
-                    "name"          : "gatling_drbd_vol1",
-                    "source_pool"   : {"id": self._get_remote_pool()["id"]}}
-        vol_res = self.send_request("POST", "volumes", data=vol_data)
-        time.sleep(self.sleeptime)
+        vol = self._get_mirror_volume(self._get_remote_pool()["id"])
 
         # Create a drbd mirror
-        mirror_data = {"source_volume"  : vol_res["response"],
+        mirror_data = {"source_volume"  : vol,
                        "remote_pool"    : self._get_pool(),
                        "protocol"       : "C",
                        "syncer_rate"    : "30M"}
@@ -55,14 +47,10 @@ class DrbdTests(object):
     def test_create_protocol_f(self):
         """ Try to create a Connection with protocol F. """
         # Create a volume that should be mirrored
-        vol_data = {"megs": 1000,
-                    "name": "gatling_drbd_vol1",
-                    "source_pool": {"id": self._get_pool()["id"]}}
-        vol_res = self.send_request("POST", "volumes", data=vol_data)
-        time.sleep(self.sleeptime)
+        vol = self._get_mirror_volume(self._get_pool()["id"])
 
         # Try to create the drbd mirror with protocol F
-        mirror_data = {"source_volume"  : vol_res["response"],
+        mirror_data = {"source_volume"  : vol,
                        "remote_pool"    : self._get_remote_pool(),
                        "protocol"       : "F",
                        "syncer_rate"    : "30M"}
@@ -70,20 +58,16 @@ class DrbdTests(object):
             mirror_res = self.send_request("POST", "mirrors", data=mirror_data)
             time.sleep(self.sleeptime)
             self.send_request("DELETE", "mirrors", obj_id=mirror_res["response"]["id"])
-        self.send_request("DELETE", "volumes", obj_id=vol_res["response"]["id"])
+        self.send_request("DELETE", "volumes", obj_id=vol["id"])
         self.assertEqual(str(err.exception), "400 Client Error: Bad Request")
 
     def test_create_0m(self):
         """ Try to create a Connection with syncer rate set to 0M. """
         # Create a volume that should be mirrored
-        vol_data = {"megs": 1000,
-                    "name": "gatling_drbd_vol1",
-                    "source_pool": {"id": self._get_pool()["id"]}}
-        vol_res = self.send_request("POST", "volumes", data=vol_data)
-        time.sleep(self.sleeptime)
+        vol = self._get_mirror_volume(self._get_pool()["id"])
 
         # Try to create the drbd mirror with syncer rate 0M
-        mirror_data = {"source_volume"  : vol_res["response"],
+        mirror_data = {"source_volume"  : vol,
                        "remote_pool"    : self._get_remote_pool(),
                        "protocol"       : "C",
                        "syncer_rate"    : "0M"}
@@ -91,8 +75,16 @@ class DrbdTests(object):
             mirror_res = self.send_request("POST", "mirrors", data=mirror_data)
             time.sleep(self.sleeptime)
             self.send_request("DELETE", "mirrors", obj_id=mirror_res["response"]["id"])
-        self.send_request("DELETE", "volumes", obj_id=vol_res["response"]["id"])
+        self.send_request("DELETE", "volumes", obj_id=vol["id"])
         self.assertEqual(str(err.exception), "400 Client Error: Bad Request")
+
+    def _get_mirror_volume(self, source_pool_id):
+        vol_data = {"megs": 1000,
+                    "name": "gatling_drbd_vol1",
+                    "source_pool": {"id": source_pool_id}}
+        vol_res = self.send_request("POST", "volumes", data=vol_data)
+        time.sleep(self.sleeptime)
+        return vol_res["response"]
 
 
 class DrbdTestCase(DrbdTestScenario, DrbdTests):
