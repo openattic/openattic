4885610cb99e064ad91dc8883d48ec3c5e13189a
Flying Pasta <stephan.mueller@it-novum.com>
2015-10-28 12:24:42 +0000

    example_config.js edited online with Bitbucket

--- e2e/example_config.js
+++ e2e/example_config.js
@@ -1,13 +1,15 @@
+/*
 'use strict';
 
 (function() {
   module.exports = {
-    url     : '<server-url>/openattic/#/login',
+    url     : '<openattic-url>/openattic/#/login',
     username: 'openattic',
     password: 'openattic',
     sleep   : 2000,
     pools   : {
-      vg1: {
+// Please make sure that the first item in pools is a VG.
+      vg: {
         name        : '<name>',
         size        : <size>,
         unit        : 'GB',
@@ -30,4 +32,4 @@
     }
   };
 }());
-
+*/
