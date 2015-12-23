716e4a7dfe027e90ced5ae391a551972ee55a0fe
Sebastian Krah <sebastian@open-attic.org>
2015-12-23 15:04:34 +0100

    Refactored
    Adapted to new coding conventions
    
    Signed-off-by: Sebastian Krah <sebastian@open-attic.org>

--- webui/app/scripts/controller/DashboardCtrl.js
+++ webui/app/scripts/controller/DashboardCtrl.js
@@ -1,64 +1,71 @@
-'use strict';
+"use strict";
 
-angular.module('openattic')
-  .value('widgetDefinitions', [{
-    name: 'Cluster Status',
-    title: 'Cluster Status',
-    directive: 'clusterstatuswidget',
-    enableVerticalResize: false,
-    size: {
-      width: '100%'
-    },
-    settingsModalOptions: {
-      lockHorizontalResize: true
+var app = angular.module("openattic");
+
+app.value("widgetDefinitions", [
+    {
+        name: "Cluster Status",
+        title: "Cluster Status",
+        directive: "clusterstatuswidget",
+        enableVerticalResize: false,
+        size: {
+            width: "100%"
+        },
+        settingsModalOptions: {
+            lockHorizontalResize: true
+        }
+    }, {
+        name: "openATTIC Wizards",
+        title: "openATTIC Wizards",
+        directive: "wizardselector",
+        enableVerticalResize: false,
+        size: {
+            width: "50%"
+        },
+        style: {
+            minWidth: "270px"
+        }
+    }, {
+        name: "Volume Stats",
+        title: "Volume Stats",
+        directive: "volumestatwidget",
+        enableVerticalResize: false,
+        size: {
+            width: "100%"
+        }
+    }, {
+        name: "ToDos",
+        title: "ToDos",
+        directive: "todowidget",
+        size: {
+            width: "50%"
+        },
+        enableVerticalResize: false
     }
-  }, {
-    name: 'openATTIC Wizards',
-    title: 'openATTIC Wizards',
-    directive: 'wizardselector',
-    enableVerticalResize: false,
-    size: {
-      width: '50%'
-    },
-    style: {
-      minWidth: '270px'
+]);
+
+app.value("defaultWidgets", [
+    {
+        name: "Cluster Status"
+    }, {
+        name: "openATTIC Wizards"
     }
-  }, {
-    name: 'Volume Stats',
-    title: 'Volume Stats',
-    directive: 'volumestatwidget',
-    enableVerticalResize: false,
-    size: {
-      width: '100%'
-    }
-  }, {
-    name: 'ToDos',
-    title: 'ToDos',
-    directive: 'todowidget',
-    size: {
-      width: '50%'
-    },
-    enableVerticalResize: false
-  }])
-  .value('defaultWidgets', [
-    { name: 'Cluster Status' },
-    { name: 'openATTIC Wizards' }
-  ])
-  .controller('DashboardCtrl', function ($scope, $window, widgetDefinitions, defaultWidgets){
-    if(!window.EventSource){
-      defaultWidgets.splice( _.findIndex(defaultWidgets, {name: 'Cluster Status'}) ,1);
+]);
+
+app.controller("DashboardCtrl", function ($scope, $window, widgetDefinitions, defaultWidgets) {
+    if (!window.EventSource) {
+        defaultWidgets.splice(_.findIndex(defaultWidgets, {name: "Cluster Status"}), 1);
     }
 
     $scope.dashboardOptions = {
-      widgetButtons: false,
-      widgetDefinitions: widgetDefinitions,
-      defaultWidgets: defaultWidgets,
-      storage: $window.localStorage,
-      storageId: 'oa_widgets',
-      hideWidgetName: true,
-      hideToolbar: true,
-      hideWidgetSettings: true,
-      hideWidgetClose: true
+        widgetButtons: false,
+        widgetDefinitions: widgetDefinitions,
+        defaultWidgets: defaultWidgets,
+        storage: $window.localStorage,
+        storageId: "oa_widgets",
+        hideWidgetName: true,
+        hideToolbar: true,
+        hideWidgetSettings: true,
+        hideWidgetClose: true
     };
-  });
-// kate: space-indent on; indent-width 2; replace-tabs on;
+});
