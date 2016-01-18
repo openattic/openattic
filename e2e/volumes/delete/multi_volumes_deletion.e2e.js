8a34dd91ac583ed72458146b4fd55f29f6251b07
Stephan Müller <stephan.mueller@it-novum.com>
2016-01-18 13:42:23 +0100

    Test the deletion of multiple volumes of various types
    * Added new entry "volumes_deletion" in protractor.conf
    
    * Added new e2e-test file
    
    The first test creates on every pool a volume for each type it supports.
    In the second test all volumes will be deleted via a mass-deletion.
    It will also control if the volume list updates correctly.
    
    
    
    Signed-off-by: Stephan Müller <stephan@open-attic.org>

---
+++ e2e/volumes/delete/multi_volumes_deletion.e2e.js
@@ -0,0 +1,76 @@
+var helpers = require('../../common.js');
+
+describe('Volumes delete', function(){
+
+  var volumesItem = element.all(by.css('ul .tc_menuitem > a')).get(3);
+  var volumeNameInput = element(by.model('volume.name'));
+  var volumePoolSelect = element(by.model('data.sourcePool'));
+  var volumeSizeInput = element(by.model('data.megs'));
+  var volumename = 'protractor_test_volume';
+  var volume = element(by.cssContainingText('tr', volumename));
+  var submitButton = element(by.css('.tc_submitButton'));
+  var addBtn = element(by.css('.tc_add_btn'));
+
+  var usePool = function(pool, callback){
+    addBtn.click();
+    volumePoolSelect.sendKeys(pool.name);
+    callback(pool.name, pool);
+  };
+  var forEachPool = function(callback){
+    for(var key in helpers.configs.pools){
+      usePool(helpers.configs.pools[key], callback);
+    }
+  };
+  var withFirstPool = function(callback){
+    for(var key in helpers.configs.pools){
+      usePool(helpers.configs.pools[key], callback);
+      break;
+    }
+  };
+
+  beforeAll(function(){
+    helpers.login();
+  });
+
+  beforeEach(function(){
+    volumesItem.click();
+  });
+
+  it('should create a volume of each configured volume types in every configured pool', function(){
+    for(var key in helpers.configs.pools){
+      var pool = helpers.configs.pools[key];
+      for(var i=0; i < pool.volumeTypes.length; i++){
+        usePool(pool, function(exact_poolname, pool){
+          var volumeType = pool.volumeTypes[i];
+          var volumename = ('e2e_' + exact_poolname + '_' + volumeType).replace(/[^a-zA-Z0-9+_.-]/g, "_");
+          var volume = element(by.cssContainingText('tr', volumename));
+
+          //create a volume
+          volumeNameInput.sendKeys(volumename);
+          element(by.cssContainingText('label', volumeType)).click();
+          element(by.id('data.megs')).sendKeys('100mb');
+          submitButton.click();
+
+          //is it displayed on the volume overview?
+          browser.sleep(helpers.configs.sleep);
+          expect(volume.isDisplayed()).toBe(true);
+        });
+      }
+    }
+  });
+
+  it('delete all e2e volumes', function(){
+    // Select all e2e volumes.
+    element.all(by.cssContainingText('tr', 'e2e_')).all(by.css("input")).click();
+
+    // Delete all volumes.
+    element(by.css('.tc_menudropdown')).click();
+    element(by.css('.tc_deleteItem')).click();
+    element(by.model('input.enteredName')).sendKeys("yes");
+    element(by.id('bot2-Msg1')).click();
+
+    // Check if they are deleted.
+    browser.sleep(10000);
+    expect(element.all(by.cssContainingText('tr', 'e2e_')).isDisplayed()).toBe(false);
+  });
+});
