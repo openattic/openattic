3b9e73f5636def99e077940754c28689b98d8042
Stephan Müller <stephan.mueller@it-novum.com>
2016-01-25 13:34:44 +0100

    Display first and last name in the left panel
    The username in the left panel will be replaces by first and last name if they exist.
    
    
    Signed-off-by: Stephan Müller <stephan@open-attic.org>

--- webui/app/components/userinfo/scripts/directives/UserInfoDirective.js
+++ webui/app/components/userinfo/scripts/directives/UserInfoDirective.js
@@ -8,7 +8,10 @@ app.directive("userinfo", function () {
       "<div class=\"login-info\">",
       "<span>",
       "<a ui-sref=\"users-edit({user:user.id})\" id=\"show-shortcut\" data-action=\"toggleShortcut\">",
-      "<span class=\"tc_usernameinfo\" ng-bind=\"user.username\"></span>",
+      "<span class=\"tc_usernameinfo\" ng-if=\"user.first_name === '' && user.last_name === ''\" ",
+          "ng-bind=\"user.username\"></span>",
+      "<span class=\"tc_usernameinfo\" ng-if=\"user.first_name !== '' || user.last_name !== ''\" ",
+          "ng-bind=\"user.first_name + ' ' + user.last_name\"></span>",
       "</a>",
       "</span>",
       "</div>"
