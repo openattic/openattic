3b68bdb1a2d081d86262e374ea5f37229b861d0f
Sebastian Krah <sebastian@open-attic.org>
2016-01-26 15:01:13 +0100

    added copyright/license header
    Signed-off-by: Sebastian Krah <sebastian@open-attic.org>

--- gatling/samba/scenarios.py
+++ gatling/samba/scenarios.py
@@ -1,3 +1,15 @@
+"""
+ *  Copyright (C) 2011-2016, it-novum GmbH <community@openattic.org>
+ *
+ *  openATTIC is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2.
+ *
+ *  This package is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+"""
 
 from testcase import GatlingTestCase
 
--- gatling/volumes/volumetests.py
+++ gatling/volumes/volumetests.py
@@ -1,5 +1,16 @@
 # -*- coding: utf-8 -*-
-# kate: space-indent on; indent-width 4; replace-tabs on;
+"""
+ *  Copyright (C) 2011-2016, it-novum GmbH <community@openattic.org>
+ *
+ *  openATTIC is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2.
+ *
+ *  This package is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+"""
 
 import unittest
 import requests
