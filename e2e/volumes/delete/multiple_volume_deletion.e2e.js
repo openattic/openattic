e6a3382cb2bade62c2b65c32ff3ea3d94669f783
Laura Paduano <laura@open-attic.org>
2016-02-26 09:19:27 +0100

    adapted tests based on OP-884
    Signed-off-by: Laura Paduano <laura@open-attic.org>

--- e2e/volumes/delete/multiple_volume_deletion.e2e.js
+++ e2e/volumes/delete/multiple_volume_deletion.e2e.js
@@ -10,13 +10,17 @@ describe('Volumes delete', function(){
 
   var usePool = function(pool, callback){
     addBtn.click();
+    browser.sleep(400);
     volumePoolSelect.sendKeys(pool.name);
+    browser.sleep(400);
     callback(pool.name, pool);
   };
 
   beforeAll(function(){
     helpers.login();
+    browser.sleep(400);
     volumesItem.click();
+    browser.sleep(400);
     // should create a volume of each configured volume types in every configured pool
     for(var key in helpers.configs.pools){
       var pool = helpers.configs.pools[key];
@@ -28,9 +32,13 @@ describe('Volumes delete', function(){
 
           //create a volume
           volumeNameInput.sendKeys(volumename);
+          browser.sleep(400);
           element(by.cssContainingText('label', volumeType)).click();
+          browser.sleep(400);
           element(by.id('data.megs')).sendKeys('100mb');
+          browser.sleep(400);
           submitButton.click();
+          browser.sleep(400);
 
           //is it displayed on the volume overview?
           browser.sleep(helpers.configs.sleep);
@@ -42,31 +50,56 @@ describe('Volumes delete', function(){
 
   beforeEach(function(){
     volumesItem.click();
+    browser.sleep(400);
     element(by.css('.tc_entries_dropdown')).click();
+    browser.sleep(400);
+
     element(by.css('.tc_entries_100 > a')).click();
     // Select all e2e volumes.
+    browser.sleep(400);
     element.all(by.cssContainingText('tr', 'e2e_')).all(by.css("input")).click();
   });
 
   it('should not show any tab', function(){
     expect(element(by.css('.tabbable.tabs-left')).isDisplayed()).toBe(false);
+    browser.sleep(400);
+  });
+
+  it('should display the delete button when selecting several volumes', function(){
+    expect(element(by.css('.tc_delete_btn')).isDisplayed()).toBe(true);
+    browser.sleep(400);
   });
 
   it('should only have add and delete avaiable in the dropdown action menu', function(){
     element(by.css(".tc_menudropdown")).click();
+    browser.sleep(400);
     var list = element.all(by.css('.oa-dropdown-actions li:not(.disabled) a'));
     expect(list.count()).toBe(2);
+    browser.sleep(400);
     expect(list.first().getText()).toBe('Add');
+    browser.sleep(400);
     expect(list.last().getText()).toBe('Delete');
+
+    browser.sleep(400);
+    var disabledOptionList = element.all(by.css('.oa-dropdown-actions .disabled'));
+
+    disabledOptionList.getText().then(function(disabledOptionList){
+      expect(disabledOptionList.toString()).toEqual('Resize,Clone,Protection,More Options');
+    });
+    browser.sleep(400);
+    element(by.css(".tc_menudropdown")).click();
   });
 
   it('should delete all volumes that begin with "e2e_"', function(){
     // Delete all volumes.
     element(by.css('.tc_menudropdown')).click();
+    browser.sleep(400);
     element(by.css('.tc_deleteItem')).click();
+    browser.sleep(400);
     element(by.model('input.enteredName')).sendKeys("yes");
+    browser.sleep(600);
     element(by.id('bot2-Msg1')).click();
-
+    browser.sleep(helpers.configs.sleep);
     // Check if they are deleted.
     expect(element.all(by.cssContainingText('tr', 'e2e_'))).toEqual([]);
   });
