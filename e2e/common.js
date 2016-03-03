25c60ff0d9a14d10c7b95ce952bc82cbb51fd604
Stephan Müller <stephan.mueller@it-novum.com>
2016-03-03 15:07:08 +0100

    Refactored volumes add E2E tests (OP-921)
    common.js:
    
    * Changed to a longer browser sleep time the delete_volume function.
    
    volumes_add.e2e.js:
    
    * Replaced selectPool with the usePool function.
    
    * Refactored 'should create a volume of the configured volume types in the configured pools' test
    
    * Added after all function to output test name.
    
    volumes_add_protection.e2e.js:
    
    * Added after all function to output test name.
    
    For changelog:
    
    * WebUI/QA: Refactored volumes add E2E tests (OP-921)
    
    
    Signed-off-by: Stephan Müller <stephan@open-attic.org>

--- e2e/common.js
+++ e2e/common.js
@@ -90,7 +90,7 @@
       browser.sleep(400);
       element(by.model('input.enteredName')).sendKeys('yes');
       element(by.id('bot2-Msg1')).click();
-      browser.sleep(600);
+      browser.sleep(configs.sleep);
       volume = element(by.cssContainingText('tr', volumename));
       expect(volume.isPresent()).toBe(false);
     },
