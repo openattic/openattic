b324f832ff5ccff9217fff8b39cf38e1bbf73e10
Tatjana Dehler <tatjana@openattic.org>
2016-03-29 15:19:21 +0200

    Limit line length to 100 chars
    Signed-off-by: Tatjana Dehler <tatjana@openattic.org>

--- backend/ifconfig/migrations/0001_initial.py
+++ backend/ifconfig/migrations/0001_initial.py
@@ -13,7 +13,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Host',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True,
+                                        primary_key=True)),
                 ('name', models.CharField(unique=True, max_length=63)),
             ],
             options={
@@ -23,7 +24,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='HostGroup',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True,
+                                        primary_key=True)),
                 ('name', models.CharField(max_length=250)),
                 ('hosts', models.ManyToManyField(to='ifconfig.Host')),
             ],
@@ -34,7 +36,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='IPAddress',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True,
+                                        primary_key=True)),
                 ('address', models.CharField(max_length=250)),
                 ('gateway', models.CharField(max_length=50, blank=True)),
                 ('nameservers', models.CharField(max_length=50, null=True, blank=True)),
@@ -49,19 +52,40 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='NetDevice',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True,
+                                        primary_key=True)),
                 ('devname', models.CharField(max_length=10)),
                 ('dhcp', models.BooleanField(default=False)),
                 ('auto', models.BooleanField(default=True)),
                 ('jumbo', models.BooleanField(default=False)),
-                ('bond_mode', models.CharField(default=b'active-backup', max_length=50, choices=[(b'balance-rr', b'balance-rr: Balanced Round Robin'), (b'active-backup', b'active-backup: Failover'), (b'balance-xor', b'balance-xor'), (b'broadcast', b'broadcast'), (b'802.3ad', b'802.3ad'), (b'balance-tlb', b'balance-tlb'), (b'balance-alb', b'balance-alb')])),
+                ('bond_mode', models.CharField(default=b'active-backup', max_length=50,
+                                               choices=[(b'balance-rr',
+                                                         b'balance-rr: Balanced Round Robin'),
+                                                        (b'active-backup',
+                                                         b'active-backup: Failover'),
+                                                        (b'balance-xor', b'balance-xor'),
+                                                        (b'broadcast', b'broadcast'),
+                                                        (b'802.3ad', b'802.3ad'),
+                                                        (b'balance-tlb', b'balance-tlb'),
+                                                        (b'balance-alb', b'balance-alb')])),
                 ('bond_miimon', models.IntegerField(default=100)),
                 ('bond_downdelay', models.IntegerField(default=200)),
                 ('bond_updelay', models.IntegerField(default=200)),
-                ('brports', models.ManyToManyField(help_text=b'If this interface is a bridge, add the ports here.', related_name='bridge_dev_set', to='ifconfig.NetDevice', blank=True)),
+                ('brports', models.ManyToManyField(help_text=b'If this interface is a bridge, add '
+                                                             b'the ports here.',
+                                                   related_name='bridge_dev_set',
+                                                   to='ifconfig.NetDevice',
+                                                   blank=True)),
                 ('host', models.ForeignKey(to='ifconfig.Host')),
-                ('slaves', models.ManyToManyField(help_text=b'If this interface is a bonding device, add the slave devices here.', related_name='bond_dev_set', to='ifconfig.NetDevice', blank=True)),
-                ('vlanrawdev', models.ForeignKey(related_name='vlan_dev_set', blank=True, to='ifconfig.NetDevice', help_text=b'If this interface is VLAN device, name the raw device here.', null=True)),
+                ('slaves', models.ManyToManyField(help_text=b'If this interface is a bonding '
+                                                            b'device, add the slave devices here.',
+                                                  related_name='bond_dev_set',
+                                                  to='ifconfig.NetDevice', blank=True)),
+                ('vlanrawdev', models.ForeignKey(related_name='vlan_dev_set', blank=True,
+                                                 to='ifconfig.NetDevice',
+                                                 help_text=b'If this interface is VLAN device, '
+                                                           b'name the raw device here.', null=True
+                                                 )),
             ],
             options={
             },
