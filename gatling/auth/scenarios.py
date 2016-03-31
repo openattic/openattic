2e4340ce0adb1e340869e13e3a54cce60478111f
Tatjana Dehler <tatjana@openattic.org>
2016-03-31 14:12:59 +0200

    Removed unnecessary config option 'api_token_auth'
    Signed-off-by: Tatjana Dehler <tatjana@openattic.org>

--- gatling/auth/scenarios.py
+++ gatling/auth/scenarios.py
@@ -25,14 +25,14 @@ class TokenAuthTestScenario(GatlingTestCase):
         super(TokenAuthTestScenario, cls).setUpClass()
         cls.require_config("users", "admin")
         cls.require_config("users", "password")
-        cls.require_config("options", "api_token_auth")
+        cls.require_config("options", "connect")
         cls.require_config("options", "auth_token")
         cls.require_enabled("auth")
         cls.auth_token = cls.conf.get("options", "auth_token")
 
     @classmethod
     def send_api_token_auth_request(cls, *args, **kwargs):
-        request_url = cls.conf.get("options", "api_token_auth")
+        request_url = cls.conf.get("options", "connect") + "api-token-auth"
         username = kwargs.get("username", cls.conf.get("users", "admin"))
         password = kwargs.get("password", cls.conf.get("users", "password"))
 
