99d721dfdd9b622b3d0aa10f5dfe6d723e38da4a
Sebastian Krah <sebastian@open-attic.org>
2016-04-25 16:02:34 +0200

    add cluster selection support
    Signed-off-by: Sebastian Krah <sebastian@open-attic.org>

--- webui/app/components/ceph-pools/controllers/CephPoolsCtrl.js
+++ webui/app/components/ceph-pools/controllers/CephPoolsCtrl.js
@@ -32,7 +32,7 @@
 
 var app = angular.module("openattic.cephPools");
 
-app.controller("CephPoolsCtrl", function ($scope, $state, cephPoolsService, cephClustersService) {
+app.controller("CephPoolsCtrl", function ($scope, $state, cephPoolsService, cephClustersService, $timeout) {
   $scope.clusters = null;
   $scope.pools = {};
 
@@ -51,31 +51,18 @@ app.controller("CephPoolsCtrl", function ($scope, $state, cephPoolsService, ceph
     $scope.clusters = res.results;
   }).catch(function () {
     $scope.clusters = false;
+    console.log("No Ceph cluster available");
   });
 
-  //var updateResults = function (res, clusterId) {
-  //  res.results.forEach(function (pool, index) {
-  //    pool.used = pool.num_bytes / pool.max_avail * 100;
-  //    pool.unused = 100 - pool.used;
-  //    pool.free = pool.max_avail - pool.num_bytes;
-  //    res.results[index] = pool;
-  //  });
-  //  if ($scope.pools.hasOwnProperty("results")) {
-  //    // Does the pool contains information about antother cluster?
-  //    var otherClusterPools = $scope.pools.results.filter(function (pool) {
-  //      return pool.cluster.id !== clusterId;
-  //    });
-  //
-  //    if (otherClusterPools.length === $scope.pools.results.length) {
-  //      //do a merge
-  //      res.count += $scope.pools.count;
-  //      $scope.pools.results.forEach(function (pool) {
-  //        res.results.push(pool);
-  //      });
-  //    }
-  //  }
-  //  return res;
-  //};
+  var modifyResult = function (res) {
+    res.results.forEach(function (pool) {
+      pool.oaUsed = pool.num_bytes / pool.max_avail * 100;
+      pool.oaUnused = 100 - pool.oaUsed;
+      pool.oaFree = pool.max_avail - pool.num_bytes;
+    });
+
+    return res;
+  };
 
   $scope.$watch("selectedCluster", function () {
     if ($scope.selectedCluster) {
@@ -87,40 +74,29 @@ app.controller("CephPoolsCtrl", function ($scope, $state, cephPoolsService, ceph
           )
           .$promise
           .then(function (res) {
-            $scope.pools = res;
+            $scope.pools = modifyResult(res);
           });
     }
   });
 
   $scope.$watch("filterConfig", function () {
-    //Paginator
-    //  .clusters()
-    //  .$promise
-    //  .then(function (res) {
-    //    $scope.clusters = res.results;
-    //    $scope.clusters.forEach(function (cluster) {
-    //      Paginator
-    //        .pools({
-    //          id: cluster.fsid,
-    //          page: $scope.filterConfig.page + 1,
-    //          pageSize: $scope.filterConfig.entries,
-    //          search: $scope.filterConfig.search,
-    //          ordering: ($scope.filterConfig.sortorder === "ASC" ? "" : "-") + $scope.filterConfig.sortfield,
-    //          upper__isnull: "True"
-    //        })
-    //        .$promise
-    //        .then(function (res) {
-    //          $scope.pools = updateResults(res, cluster.fsid);
-    //        })
-    //        .catch(function (error) {
-    //          console.log("Ceph has no pools", error);
-    //        });
-    //    });
-    //  })
-    //  .catch(function () {
-    //    $scope.clusters = false;
-    //    console.log("Ceph not available.");
-    //  });
+    if ($scope.selectedCluster) {
+      cephPoolsService
+          .get({
+            id      : $scope.selectedCluster.fsid,
+            page    : $scope.filterConfig.page + 1,
+            pageSize: $scope.filterConfig.entries,
+            search  : $scope.filterConfig.search,
+            ordering: ($scope.filterConfig.sortorder === "ASC" ? "" : "-") + $scope.filterConfig.sortfield
+          })
+          .$promise
+          .then(function (res) {
+            $scope.pools = modifyResult(res);
+          })
+          .catch(function (error) {
+            console.log("No Ceph pools available.", error);
+          });
+    }
   }, true);
 
   $scope.$watchCollection("selection", function (selection) {
