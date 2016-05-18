3c9704d6852ce5c655025005a0bd08de9fe1cb8d
Laura Paduano <laura@open-attic.org>
2016-05-18 11:01:53 +0200

    added console.log to comment section; added another test case
    Signed-off-by: Laura Paduano <laura@open-attic.org>

--- e2e/ceph/ceph_osds.e2e.js
+++ e2e/ceph/ceph_osds.e2e.js
@@ -37,10 +37,22 @@ describe('should test the ceph osd panel', function(){
   it('should display the following table headers', function(){
     for(tableHeader in tableHeaders){
       expect(element(by.cssContainingText('th', tableHeaders[tableHeader])).isDisplayed()).toBe(true);
+      //check: console.log(tableHeaders[tableHeader]);
     }
   });
 
   it('should have at least one ceph osd table entry', function(){
+    expect(element.all(by.binding('row.name')).count()).toBeGreaterThan(0);
+  });
+
+  it('should still have the cluster selected and display OSDs when switching between panels', function(){
+    element(by.css('ul .tc_menuitem_pools > a')).click();
+    expect(browser.getCurrentUrl()).toContain('/#/pools');
+    cephMenu.click();
+    cephOSDs.click();
+    expect(browser.getCurrentUrl()).toContain('/ceph/osds');
+    expect(element(by.id('cluster-selection')).getText()).toContain('ceph');
+    console.log(element(by.id('cluster-selection')).getText());
     expect(element.all(by.binding('row.name')).count()).toBeGreaterThan(0);
   });
 
