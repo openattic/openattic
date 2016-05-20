853f455b57922cf3940298c5cb7d4b0a3fa6e92b
Laura Paduano <laura@open-attic.org>
2016-05-20 10:22:55 +0200

    added test cases which ceck the description text; fixed bug (clicking the label results in opening the link sometimes)
    Signed-off-by: Laura Paduano <laura@open-attic.org>

--- e2e/volumes/add/volumes_add.e2e.js
+++ e2e/volumes/add/volumes_add.e2e.js
@@ -9,6 +9,7 @@ describe('Volumes add', function(){
   var volume = element(by.cssContainingText('tr', volumename));
   var submitButton = element(by.css('.tc_submitButton'));
   var addBtn = element(by.css('.tc_add_btn'));
+  var volumePoolSelect = element(by.model('pool'));
 
 
   var usePool = function(pool, callback){
@@ -180,6 +181,66 @@ describe('Volumes add', function(){
     });
   });
 
+  it('should display a description when selecting a volume type (volume group)', function(){
+    volumePoolSelect.click();
+    element.all(by.cssContainingText('option', '(volume group,')).get(0).click();
+
+    var descriptions = element.all(by.repeater('(fs, text) in filesystems'))
+      .then(function(descriptions){
+        descriptions[0].element(by.css('.text-info')).evaluate('text.desc').then(function(txt){
+          expect(txt).toEqual('iSCSI, Fibre Channel shares and volume mirroring');
+        });
+
+        element(by.id('xfs')).click();
+        descriptions[1].element(by.css('.text-info')).evaluate('text.desc').then(function(txt){
+          expect(txt).toEqual('recommended for virtualization, optimized for parallel IO');
+        });
+
+        element(by.id('zfs')).click();
+        descriptions[2].element(by.css('.text-info')).evaluate('text.desc').then(function(txt){
+          expect(txt).toEqual('supports snapshots, deduplication and compression');
+        });
+
+        element(by.id('btrfs')).click();
+        descriptions[3].element(by.css('.text-info')).evaluate('text.desc').then(function(txt){
+          expect(txt).toEqual('supports snapshots, compression - Experimental');
+        });
+
+        element(by.id('ext4')).click();
+        descriptions[4].element(by.css('.text-info')).evaluate('text.desc').then(function(txt){
+          expect(txt).toEqual('max. 1 EiB - Linux default');
+        });
+
+        element(by.id('ext3')).click();
+        descriptions[5].element(by.css('.text-info')).evaluate('text.desc').then(function(txt){
+          expect(txt).toEqual('max. 32TiB - old Linux default since 2010');
+        });
+
+        element(by.id('ext2')).click();
+        descriptions[6].element(by.css('.text-info')).evaluate('text.desc').then(function(txt){
+          expect(txt).toEqual('deprecated');
+        });
+    });
+  });
+
+  it('should display a description when selecting a volume type (zpool)', function(){
+    volumePoolSelect.click();
+    element.all(by.cssContainingText('option', '(zpool,')).get(0).click();
+
+    var descriptions = element.all(by.repeater('(fs, text) in filesystems'))
+      .then(function(descriptions){
+        descriptions[0].element(by.css('.text-info')).evaluate('text.desc').then(function(txt){
+        expect(txt).toEqual('iSCSI, Fibre Channel shares and volume mirroring');
+      });
+
+      element(by.id('zfs')).click();
+      browser.sleep(400);
+      descriptions[2].element(by.css('.text-info')).evaluate('text.desc').then(function(txt){
+        expect(txt).toEqual('supports snapshots, deduplication and compression');
+      });
+   });
+  });
+
   it('should show a message if the chosen volume size is smaller than 100mb', function(){
     withFirstPool(function(exact_poolname, pool){
       volumeSizeInput.clear().sendKeys('99mb');
@@ -264,8 +325,9 @@ describe('Volumes add', function(){
         var volume = element(by.cssContainingText('tr', volumename));
 
         //create a volume
+        volumeType = volumeType.toLowerCase();
         volumeNameInput.sendKeys(volumename);
-        element(by.cssContainingText('label', volumeType)).click();
+        element(by.id(volumeType)).click();
         volumeSizeInput.sendKeys('100mb');
         submitButton.click();
 
