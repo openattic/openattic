415e06f16bddfbaf818a33220963c453ce668157
Tatjana Dehler <tatjana@openattic.org>
2016-06-15 12:36:40 +0200

    Code formatting -> PEP8
    Signed-off-by: Tatjana Dehler <tatjana@openattic.org>

--- backend/nagios/management/__init__.py
+++ backend/nagios/management/__init__.py
@@ -14,19 +14,18 @@
  *  GNU General Public License for more details.
 """
 
-from django.core.management import call_command
 from django.contrib.contenttypes.models import ContentType
 from django.db.models import signals
 
 from systemd import get_dbus_object
 from systemd.helpers import Transaction
 from nagios.conf import settings as nagios_settings
-from ifconfig.models import Host, IPAddress
+from ifconfig.models import Host
 from volumes.models import BlockVolume, FileSystemVolume
 
-import nagios.models
 import sysutils.models
-from nagios.models    import Service, Command, update_conf
+from nagios.models import Service, Command, update_conf
+
 
 def create_nagios(**kwargs):
     # Make sure the contacts config exists
@@ -38,11 +37,11 @@ def create_nagios(**kwargs):
         cmdargs = cmdargs[1:]
 
         try:
-            cmd = Command.objects.get( name=cmdname )
+            cmd = Command.objects.get(name=cmdname)
         except Command.DoesNotExist:
             # Commands that don't exist have not been configured by us, so query_only
             print "Adding Check Command %s" % cmdname
-            cmd = Command( name=cmdname, query_only=True )
+            cmd = Command(name=cmdname, query_only=True)
             cmd.save()
 
         if not cmd.query_only:
@@ -56,22 +55,15 @@ def create_nagios(**kwargs):
                 )
         except Service.DoesNotExist:
             print "Adding Service '%s'" % servstate["service_description"]
-            serv = Service(
-                host        = Host.objects.get_current(),
-                description = servstate["service_description"],
-                command     = cmd,
-                arguments   = ('!'.join(cmdargs))
-                )
+            serv = Service(host=Host.objects.get_current(),
+                           description=servstate["service_description"], command=cmd,
+                           arguments=('!'.join(cmdargs)))
             serv.save()
 
     cmd = Command.objects.get(name=nagios_settings.CPUTIME_CHECK_CMD)
     if Service.objects.filter(host=Host.objects.get_current(), command=cmd).count() == 0:
-        serv = Service(
-            host        = Host.objects.get_current(),
-            command     = cmd,
-            description = nagios_settings.CPUTIME_DESCRIPTION,
-            arguments   = ""
-            )
+        serv = Service(host=Host.objects.get_current(), command=cmd,
+                       description=nagios_settings.CPUTIME_DESCRIPTION, arguments="")
         serv.save()
 
     nagios = get_dbus_object("/nagios")
@@ -83,15 +75,12 @@ def create_nagios(**kwargs):
     for bv in BlockVolume.objects.all():
         instance = bv.volume
         ctype = ContentType.objects.get_for_model(instance.__class__)
-        if Service.objects.filter(command=cmd, target_type=ctype, target_id=instance.id).count() != 0:
+        if Service.objects.filter(command=cmd, target_type=ctype, target_id=instance.id).count() \
+                != 0:
             continue
-        srv = Service(
-            host        = instance.host,
-            target      = instance,
-            command     = cmd,
-            description = nagios_settings.LV_PERF_DESCRIPTION % unicode(instance),
-            arguments   = instance.path
-        )
+        srv = Service(host=instance.host, target=instance, command=cmd,
+                      description=nagios_settings.LV_PERF_DESCRIPTION % unicode(instance),
+                      arguments=instance.path)
         srv.save()
 
     cmd = Command.objects.get(name=nagios_settings.LV_UTIL_CHECK_CMD)
@@ -99,19 +88,15 @@ def create_nagios(**kwargs):
         instance = fsv.volume
         ctype = ContentType.objects.get_for_model(instance.__class__)
         print type(instance), instance, instance.id
-        if Service.objects.filter(command=cmd, target_type=ctype, target_id=instance.id).count() != 0:
+        if Service.objects.filter(command=cmd, target_type=ctype, target_id=instance.id).count() \
+                != 0:
             continue
-        srv = Service(
-            host        = instance.host,
-            target      = instance,
-            command     = cmd,
-            description = nagios_settings.LV_UTIL_DESCRIPTION % unicode(instance),
-            arguments   = instance.storageobj.uuid
-        )
+        srv = Service(host=instance.host, target=instance, command=cmd,
+                      description=nagios_settings.LV_UTIL_DESCRIPTION % unicode(instance),
+                      arguments=instance.storageobj.uuid)
         srv.save()
 
     update_conf()
     signals.post_save.connect(update_conf, sender=Service)
-
 
 sysutils.models.post_install.connect(create_nagios)
