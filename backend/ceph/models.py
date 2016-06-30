0cd80bc207c851a2d516fa5bf5d56ad2886162a2
Sebastian Wagner <sebastian.wagner@it-novum.com>
2016-06-30 14:26:59 +0200

    Some RBD improvements OP-1309, OP-1305:
    * Added order parameter to the create rbd image API call.
    * added `rbd du <image>` accessible as used_size to the REST API.
    * Rmoved order field of the CephRbd model
    * Tried to handle RBD creation that doesn't actually create RBDs.
    * Don't fail if the features don't match after creating an image.
    
    Signed-off-by: Sebastian Wagner <sebastian.wagner@it-novum.com>

--- backend/ceph/models.py
+++ backend/ceph/models.py
@@ -510,6 +510,14 @@ class CephPg(NodbModel):
         return ret
 
 
+def aggregate_dict(*args, **kwargs):
+    ret = {}
+    for arg in args:
+        ret.update(arg)
+    ret.update(**kwargs)
+    return ret
+
+
 class CephRbd(NodbModel):  # aka RADOS block device
     """
     See http://tracker.ceph.com/issues/15448
@@ -517,15 +525,15 @@ class CephRbd(NodbModel):  # aka RADOS block device
     name = models.CharField(max_length=100, primary_key=True)
     pool = models.ForeignKey(CephPool)
     size = models.IntegerField(help_text='Bytes', default=4 * 1024 ** 3)
-    obj_size = models.IntegerField(editable=False)
+    obj_size = models.IntegerField(null=True, blank=True)
     num_objs = models.IntegerField(editable=False)
-    order = models.IntegerField(editable=False)
     block_name_prefix = models.CharField(max_length=100, editable=False)
     features = JsonField(base_type=list, null=True, blank=True,
                          help_text='For example: [{}]'.format(', '.join(['"{}"'.format(v)
                                                                          for v
                                                                          in RbdApi.get_feature_mapping().values()])))
     old_format = models.BooleanField(default=False)
+    used_size = models.IntegerField(editable=False)
 
     @staticmethod
     def get_all_objects(context, query):
@@ -535,10 +543,12 @@ class CephRbd(NodbModel):  # aka RADOS block device
         pools = CephPool.objects.all()
         rbd_name_pools = (itertools.chain.from_iterable((((image, pool) for image in api.list(pool.name))
                                                          for pool in pools)))
-        rbds = ((dict(name=image_name,
-                      old_format=api.image_old_format(pool.name, image_name),
-                      features=api.image_features(pool.name, image_name),
-                      **api.image_stat(pool.name, image_name)), pool)
+        rbds = ((aggregate_dict(api.image_stat(pool.name, image_name),
+                                api.image_disk_usage(pool.name, image_name),  # This is really expensive. You should
+                                # first try to only call "rbd du" for rbds that will be serialized.
+                                old_format=api.image_old_format(pool.name, image_name),
+                                features=api.image_features(pool.name, image_name)),
+                 pool)
                 for (image_name, pool)
                 in rbd_name_pools)
 
@@ -557,10 +567,13 @@ class CephRbd(NodbModel):  # aka RADOS block device
         context = CephPool.objects.nodb_context
         insert = self._state.adding  # there seems to be no id field.
 
-        with undo_transaction(RbdApi(rados[context.fsid]), re_raise_exception=True) as api:
+        with undo_transaction(RbdApi(rados[context.fsid]), re_raise_exception=True, exception_type=CephRbd.DoesNotExist) as api:
 
             if insert:
-                api.create(self.pool.name, self.name, self.size, features=self.features, old_format=self.old_format)
+                order = None
+                if self.obj_size is not None and self.obj_size > 0:
+                    order = int(round(math.log(float(self.obj_size), 2)))
+                api.create(self.pool.name, self.name, self.size, features=self.features, old_format=self.old_format, order=order)
 
             diff, original = self.get_modified_fields()
             self.set_read_only_fields(original)
@@ -570,11 +583,13 @@ class CephRbd(NodbModel):  # aka RADOS block device
                     assert not insert
                     api.image_resize(self.pool.name, self.name, value)
                 if key == 'features':
-                    assert not insert
-                    for feature in set(original.features).difference(set(value)):
-                        api.image_set_feature(self.pool.name, self.name, feature, False)
-                    for feature in set(value).difference(set(original.features)):
-                        api.image_set_feature(self.pool.name, self.name, feature, True)
+                    if not insert:
+                        for feature in set(original.features).difference(set(value)):
+                            api.image_set_feature(self.pool.name, self.name, feature, False)
+                        for feature in set(value).difference(set(original.features)):
+                            api.image_set_feature(self.pool.name, self.name, feature, True)
+                    else:
+                        logger.warning('Tried to set features, but they should already match. {} != {}'.format(original.features, value))
                 else:
                     logger.warning('Tried to set "{}" to "{}" on rbd "{}", which is not '
                                    'supported'.format(key, value, self.name))
