31ce52c0dc6ff1df35fce643c7efa604bc720ad4
Tatjana Dehler <tatjana@openattic.org>
2016-07-07 16:48:56 +0200

    Add basic userprefs REST API
    Signed-off-by: Tatjana Dehler <tatjana@openattic.org>

---
+++ backend/userprefs/restapi.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# kate: space-indent on; indent-width 4; replace-tabs on;
+
+"""
+ *  Copyright (C) 2011-2016, it-novum GmbH <community@openattic.org>
+ *
+ *  openATTIC is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2.
+ *
+ *  This package is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+"""
+
+from rest_framework import serializers, viewsets
+
+from userprefs.models import UserProfile, UserPreference
+
+
+class UserPreferenceSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = UserPreference
+        fields = ("setting", "value")
+
+
+class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
+
+    preferences = serializers.SerializerMethodField("get_preferences")
+
+    class Meta:
+        model = UserProfile
+        fields = ("url", "id", "host", "user", "preferences")
+
+    def get_preferences(self, profile):
+        user_pref_ser = UserPreferenceSerializer(profile.userpreference_set.all(),
+                                                 context=self.context, many=True)
+        return user_pref_ser.data
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+
+
+RESTAPI_VIEWSETS = [
+    ("userprofiles", UserProfileViewSet, "userprofile"),
+]
