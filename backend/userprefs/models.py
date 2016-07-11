95cc6c0065d47f8cfa7ba1943cc747fbb951dbd7
Tatjana Dehler <tatjana@openattic.org>
2016-07-11 16:53:32 +0200

    Backed out changeset a286f985679e
    Signed-off-by: Tatjana Dehler <tatjana@openattic.org>

--- backend/userprefs/models.py
+++ backend/userprefs/models.py
@@ -14,30 +14,70 @@
  *  GNU General Public License for more details.
 """
 
+import json
+
 from django.db import models
 from django.contrib.auth.models import User
 
-from ifconfig.models import Host
+from ifconfig.models import Host, HostDependentManager
 
 
-def get_default_user():
-    user = User.objects.all()[0]
-    print user
-    return user.id
+class UserProfile(models.Model):
+    user = models.ForeignKey(User)
+    host = models.ForeignKey(Host)
 
+    objects = HostDependentManager()
 
-def get_default_host():
-    host = Host.objects.get_current()
-    print host
-    return host.id
+    class Meta:
+        unique_together = ("user", "host")
+
+    def __getitem__(self, item):
+        try:
+            pref = self.userpreference_set.get(setting=item)
+        except UserPreference.DoesNotExist:
+            raise KeyError(item)
+        else:
+            return json.loads(pref.value)
+
+    def __setitem__(self, item, value):
+        try:
+            pref = self.userpreference_set.get(setting=item)
+        except UserPreference.DoesNotExist:
+            pref = UserPreference(profile=self, setting=item)
+        pref.value = json.dumps(value)
+        pref.save()
+        return value
+
+    def __delitem__(self, item):
+        try:
+            pref = self.userpreference_set.get(setting=item)
+        except UserPreference.DoesNotExist:
+            raise KeyError(item)
+        else:
+            pref.delete()
+
+    def __contains__(self, item):
+        try:
+            self.userpreference_set.get(setting=item)
+        except UserPreference.DoesNotExist:
+            return False
+        else:
+            return True
+
+    def __len__(self):
+        return self.userpreference_set.count()
+
+    def __iter__(self):
+        return iter(self.userpreference_set.all())
 
 
 class UserPreference(models.Model):
-
-    user = models.ForeignKey(User, default=get_default_user)
-    host = models.ForeignKey(Host, default=get_default_host)
+    profile = models.ForeignKey(UserProfile)
     setting = models.CharField(max_length=50)
     value = models.TextField()
 
+    class Meta:
+        unique_together = ("profile", "setting")
+
     def __unicode__(self):
-        return self.setting
+        return "<UserPreference %s>" % self.setting
