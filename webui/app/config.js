1be78e64f55efde242cdc427b9c9270057c7cf65
Sebastian Krah <sebastian@openattic.org>
2016-07-27 14:47:54 +0200

    add config for default dashboard
    Signed-off-by: Sebastian Krah <sebastian@openattic.org>

--- webui/app/config.js
+++ webui/app/config.js
@@ -3,22 +3,34 @@ globalConfig = {
     "URL": "/openattic/api/"
   },
   "GUI": {
-    "activeModules": {
-      "apt": true,
-      "btrfs": true,
-      "ceph": false,
-      "cron": true,
-      "drbd": true,
-      "http": true,
-      "ipmi": true,
-      "lvm": true,
+    "activeModules"   : {
+      "apt"        : true,
+      "btrfs"      : true,
+      "ceph"       : false,
+      "cron"       : true,
+      "drbd"       : true,
+      "http"       : true,
+      "ipmi"       : true,
+      "lvm"        : true,
       "mailaliases": true,
-      "mdraid": true,
-      "nagios": true,
-      "nfs": true,
-      "samba": true,
-      "twraid": true,
-      "zfs": true
+      "mdraid"     : true,
+      "nagios"     : true,
+      "nfs"        : true,
+      "samba"      : true,
+      "twraid"     : true,
+      "zfs"        : true
+    },
+    "defaultDashboard": {
+      "boards"  : [{
+        "id"     : 0,
+        "name"   : "Default",
+        "widgets": []
+      }],
+      "settings": {
+        "activeBoard": 0,
+        "locked"     : false,
+        "orderBy"    : "name"
+      }
     }
   }
 };
