a6fa5af6bfe16587eb887a8b349acdec324c8817
Stephan Müller <stephan@openattic.org>
2016-08-04 12:57:22 +0200

    Takes a screenshots and write an error log if a e2e test fails
    * Added the functionallty to take screenshots and write the specific failure as reason for the screenshot.
    
    Signed-off-by: Stephan Müller <stephan@openattic.org>

--- webui/protractor.conf.js
+++ webui/protractor.conf.js
@@ -28,6 +28,8 @@
  * for the JavaScript code in this page.
  *
  */
+var config = require('../e2e/configs.js');
+
 exports.config = {
 
   seleniumAddress: 'http://localhost:4444/wd/hub',
@@ -75,5 +77,43 @@ exports.config = {
 
   onPrepare: function(){
     browser.driver.manage().window().maximize();
+    var fs = require('fs');
+    var path = require('path');
+    var errorCount = 0;
+    var savePath = path.join(config.outDir, new Date().toJSON().replace(/[-:.]/g, '_'));
+    var checkPath = function(callback){
+      fs.exists(savePath, function(exists){
+        if(!exists){
+          fs.mkdir(savePath);
+        }
+        callback();
+      });
+    };
+    console.log('If any errors appear they will be saved in "' + savePath + '".');
+    jasmine.getEnv().addReporter(new function(){
+      this.specDone = function(result){
+        if(result.failedExpectations.length > 0){
+          errorCount++;
+          checkPath(function(){
+            browser.takeScreenshot().then(function(png){
+              var stream = fs.createWriteStream(savePath + '/error' + errorCount + '.png');
+              stream.write(new Buffer(png, 'base64'));
+              stream.end();
+            });
+            var stream = fs.createWriteStream(savePath + '/error' + errorCount + '.log');
+            var out = new console.Console(stream);
+            out.log('Description of the suite:\n', result.description + '\n');
+            out.log('Description of the test:\n', result.fullName + '\n\n\n');
+            out.log('Failed expectations:' + '\n\n');
+            result.failedExpectations.forEach(function(fail){
+              out.log('Expected "' + fail.expected + '" but got "' + fail.actual + '" with Matcher "' +
+                fail.matcherName + '"\n');
+              out.log('Message:\n' + fail.message + '\n');
+              out.log('Call-Stack:\n' + fail.stack + '\n\n');
+            });
+          });
+        }
+      };
+    });
   }
 };
