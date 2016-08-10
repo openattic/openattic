b9a9762e6f86a45100f40aafa34475fc93e9ea90
Tatjana Dehler <tatjana@openattic.org>
2016-08-10 11:29:38 +0200

    Code formatting -> PEP8
    Signed-off-by: Tatjana Dehler <tatjana@openattic.org>

--- backend/samba/restapi.py
+++ backend/samba/restapi.py
@@ -16,7 +16,7 @@
 import socket
 import django_filters
 
-from rest_framework import serializers, viewsets, status
+from rest_framework import serializers, viewsets
 from rest_framework.decorators import list_route
 from rest_framework.response import Response
 
@@ -28,31 +28,37 @@ from samba.conf import settings as samba_settings
 
 from rest.multinode.handlers import RequestHandlers
 
+
 class SambaShareSerializer(serializers.HyperlinkedModelSerializer):
     """ Serializer for a Samba Share. """
-    url         = serializers.HyperlinkedIdentityField(view_name="sambashare-detail")
-    volume      = relations.HyperlinkedRelatedField(view_name="volume-detail", source="volume.storageobj", queryset=StorageObject.objects.all())
+    url = serializers.HyperlinkedIdentityField(view_name="sambashare-detail")
+    volume = relations.HyperlinkedRelatedField(view_name="volume-detail",
+                                               source="volume.storageobj",
+                                               queryset=StorageObject.objects.all())
 
     class Meta:
         model = Share
-        fields = ('url', 'id', 'name', 'path', 'available', 'browseable', 'guest_ok', 'writeable', 'comment', 'volume')
+        fields = ('url', 'id', 'name', 'path', 'available', 'browseable', 'guest_ok', 'writeable',
+                  'comment', 'volume')
 
     def restore_object(self, attrs, instance=None):
         attrs["volume"] = attrs["volume.storageobj"].filesystemvolume_or_none
         del attrs["volume.storageobj"]
         return super(SambaShareSerializer, self).restore_object(attrs, instance)
 
+
 class SambaShareFilter(django_filters.FilterSet):
     volume = django_filters.NumberFilter(name="volume__storageobj__id")
 
     class Meta:
-        model  = Share
+        model = Share
         fields = ['volume']
 
+
 class SambaShareViewSet(viewsets.ModelViewSet):
-    queryset         = Share.objects.all()
+    queryset = Share.objects.all()
     serializer_class = SambaShareSerializer
-    filter_class     = SambaShareFilter
+    filter_class = SambaShareFilter
 
     @list_route()
     def domainconfig(self, request):
@@ -65,10 +71,10 @@ class SambaShareViewSet(viewsets.ModelViewSet):
 
 
 class SambaShareProxyViewSet(RequestHandlers, SambaShareViewSet):
-    queryset    = Share.all_objects.all()
-    api_prefix  = 'sambashares'
+    queryset = Share.all_objects.all()
+    api_prefix = 'sambashares'
     host_filter = 'volume__storageobj__host'
-    model       = Share
+    model = Share
 
     @list_route()
     def domainconfig(self, request, *args, **kwargs):
