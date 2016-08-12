33378ede2ad53d21c700835bc6a0fb1f176e91a2
Sebastian Krah <sebastian@openattic.org>
2016-08-12 08:08:14 +0200

    id is now known as fsid
    Signed-off-by: Sebastian Krah <sebastian@openattic.org>

--- webui/app/components/ceph-cluster/services/cephClustersService.js
+++ webui/app/components/ceph-cluster/services/cephClustersService.js
@@ -32,7 +32,7 @@
 
 var app = angular.module("openattic.cephCluster");
 app.factory("cephClusterService", function ($resource) {
-  return $resource(globalConfig.API.URL + "ceph/:id", {
-    id: "@id"
+  return $resource(globalConfig.API.URL + "ceph/:fsid", {
+    fsid: "@fsid"
   });
 });
