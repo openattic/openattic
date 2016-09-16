f9ec46414e8f6fd05af8dd63f61b2af52f75d5b0
Sebastian Krah <sebastian@openattic.org>
2016-09-16 09:00:48 +0200

    add basic functionality
    Signed-off-by: Sebastian Krah <sebastian@openattic.org>

--- webui/app/components/ceph-pools/controller/CephPoolsStatisticsCtrl.js
+++ webui/app/components/ceph-pools/controller/CephPoolsStatisticsCtrl.js
@@ -31,6 +31,141 @@
 "use strict";
 
 var app = angular.module("openattic.cephPools");
-app.controller("CephPoolsStatisticsCtrl", function ($scope) {
-  $scope.hello = "world";
+app.controller("CephPoolsStatisticsCtrl", function ($scope, $interval, cephPoolsService) {
+  var promise;
+  $scope.isLoading = false;
+
+  $scope.data = [];
+  $scope.utilization = {};
+  $scope.noo = {};
+
+  // Options
+  $scope.graph = {
+    config: {
+      deepWatchDataDepth: 0
+    },
+    data: [],
+    options: {
+      chart: {
+        color: ["#288cea", "#6430ec", "#ffdb1b", "#ffa21b", "#19f13f", "#ff2b1b"],
+        forceY: [0],
+        interpolate: "linear",
+        legend: {
+          vers: "furious",
+          margin: {
+            top: 3,
+            right: 0,
+            bottom: 10,
+            left: -64
+          },
+          rightAlign: false
+        },
+        legendPosition: "top",
+        margin: {
+          top: 0,
+          right: 0,
+          bottom: 40,
+          left: 80
+        },
+        type: "lineChart",
+        useInteractiveGuideline: true,
+        x: function (d) {
+          return d[0];
+        },
+        xAxis: {
+          axisLabel: "Time",
+          fontSize: 11,
+          showMaxMin: false,
+          tickFormat: function (d) {
+            return d3.time.format("%d.%m %H:%M")(new Date(d * 1000));
+          }
+        },
+        y: function (d) {
+          if (d[1] === null) {
+            d[1] = 0;
+          }
+          return d[1];
+        },
+        yAxis: {
+          axisLabel: "placeholder",
+          axisLabelDistance: 20,
+          fontSize: 11,
+          tickFormat: function (d) {
+            return d;
+          }
+        }
+      }
+    }
+  };
+  $scope.utilization.config = $scope.graph.config;
+  $scope.utilization.options = $scope.graph.options;
+  $scope.utilization.data = [];
+  $scope.noo.config = $scope.graph.config;
+  $scope.noo.options = $scope.graph.options;
+  $scope.noo.data = [];
+
+  // Functions
+  var init = function () {
+    $scope.getData();
+    $scope.startInterval();
+  };
+
+  $scope.getData = function () {
+    cephPoolsService
+        .performancedata({
+          fsid: $scope.selection.item.cluster,
+          filter_pools: $scope.selection.item.name
+        })
+        .$promise
+        .then(function (res) {
+          angular.forEach(res[$scope.selection.item.name], function (value) {
+            // First call
+            if (angular.equals($scope.data, [])) {
+              $scope.data[value.key] = value;
+            }
+          });
+          $scope.utilization.data = [];
+          $scope.noo.data = [];
+          $scope.utilization.data.push($scope.data.max_avail);
+          $scope.utilization.data.push($scope.data.num_bytes);
+          $scope.noo.data.push($scope.data.num_objects);
+        })
+        .catch(function (err) {
+          throw err;
+        })
+        .finally(function () {
+          $interval(function () {
+            $scope.isLoading = false;
+          }, 1000, 1);
+
+          //console.log($scope.utilization);
+        });
+  };
+
+  $scope.startInterval = function () {
+    // stops any running interval to avoid two intervals running at the same time
+    $scope.stopInterval();
+
+    // store the interval promise
+    promise = $interval(function () {
+      $scope.getData();
+    }, 5000, false);
+  };
+
+  $scope.stopInterval = function () {
+    $interval.cancel(promise);
+  };
+
+  // Watcher
+  $scope.$watch("selection.item", function () {
+    $scope.getData();
+  });
+
+  // Event-Handler
+  $scope.$on("$destroy", function () {
+    $scope.stopInterval();
+  });
+
+  // init
+  init();
 });
