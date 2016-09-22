16892fd79dfac751802c18e4e8a13c50dbf887de
Stephan Müller <stephan@openattic.org>
2016-09-22 17:29:31 +0200

    Add tabs and selection to the task queue
    ## Modal dialog ##
    
    * Added different selection options into modal dialog. You can click on one task and with ctrl check another one or you can hold shift down to select all in between.
    
    * Check all functionality.
    
    * If only one task is check, details are shown.
    
    * Added tabs for all three states (pending, failed and finished).
    
    * All tabs have different selection scopes, this means you can select something complete different in each tab and if you go back your selection is still there.
    
    * Added close button at the bottom.
    
    * Tabs with table are generated through a config object in the controller.
    
    ## Test data ##
    
    * Generating test data with all the different states.
    
    Signed-off-by: Stephan Müller <stephan@openattic.org>

--- webui/app/components/runnersQ/controller/RunnersQModalCtrl.js
+++ webui/app/components/runnersQ/controller/RunnersQModalCtrl.js
@@ -31,47 +31,189 @@
 "use strict";
 
 var app = angular.module("openattic.runnersQ");
-app.controller("RunnersQModalCtrl", function ($scope, toasty, tasks) {
-  $scope.tableSort = {
-    attribute: 'done',
-    reverse: true
-  };
-
-  $scope.order = function (attribute) {
-    if ($scope.tableSort.attribute === attribute) {
-      $scope.tableSort.reverse = !$scope.tableSort.reverse;
+app.controller("RunnersQModalCtrl", function ($scope, $uibModalInstance, toasty, tasks, $state, $filter) {
+  $scope.order = function (attribute, tab) {
+    if (tab.tableSort.attribute === attribute) {
+      tab.tableSort.reverse = !tab.tableSort.reverse;
     }
-    $scope.tableSort.attribute = attribute;
+    tab.tableSort.attribute = attribute;
   };
 
-  $scope.orderClass = function (attribute) {
+  $scope.orderClass = function (attribute, tab) {
     var cssClass = "sorting";
-    if ($scope.tableSort.attribute === attribute) {
-      cssClass = $scope.tableSort.reverse ? "sorting_desc" : "sorting_asc";
+    if (tab.tableSort.attribute === attribute) {
+      cssClass = tab.tableSort.reverse ? "sorting_desc" : "sorting_asc";
     }
     return cssClass;
   };
 
-  $scope.tableColumns = [
-    {
-      name: "Name",
-      attribute: "name",
-      width: "30%"
-    },
-    {
-      name: "Created",
-      attribute: "started",
-      width: "15%"
-    },
-    {
-      name: "Complete",
-      attribute: "done",
-      width: "45%"
-    },
-    {
-      name: "Approx",
-      attribute: "approx",
-      width: "10%"
+  $scope.selectColumn = function (attr, data) {
+    var attribute = attr.displayAttr || attr.attribute;
+    return data[attribute];
+  };
+
+  $scope.updateTaskSelection = function (key, items) {
+    $scope.tabs[key].selection.item = items.length === 1 ? items[0] : null;
+    $scope.tabs[key].selection.items = items;
+  };
+
+  $scope.toggleTaskSelection = function (item, key, $event) {
+    var items = $scope.tabs[key].selection.items;
+    var exists = items.indexOf(item);
+    if (exists !== -1 && $event.target.tagName !== "INPUT" || $event.target.tagName === "INPUT" && !$event.target.checked){
+      items.splice(exists, 1);
+    } else {
+      if ($event.target.checked || $event.ctrlKey ){
+        items.push(item);
+      } else if ($event.shiftKey) {
+        var tab = $scope.tabs[key];
+        var sorted = $filter('orderBy')(tab.data, tab.tableSort.attribute);
+        if (tab.tableSort.reverse) {
+          sorted.reverse();
+        }
+        var iPrev = sorted.indexOf(items[items.length-1]);
+        var iNow = sorted.indexOf(item);
+        var newItems = iPrev < iNow ? sorted.slice(iPrev + 1, iNow + 1) : sorted.slice(iNow, iPrev);
+        newItems.forEach(function (item) {
+          var exists = items.indexOf(item);
+          if (exists !== -1){
+            items.splice(exists, 1);
+          } else {
+            items.push(item);
+          }
+        });
+      } else {
+        items = [item];
+      }
     }
-  ];
+    $scope.updateTaskSelection(key, items);
+  };
+
+  $scope.checkAllTasks = function (key) {
+    var tab = $scope.tabs[key];
+    $scope.tabs[key].selection.items = tab.selection.checkAll ? tab.data.slice() : [] ;
+  };
+
+  $scope.closeTasks = function () {
+    $uibModalInstance.dismiss("close");
+  };
+
+  $scope.tabs = {
+    pending: {
+      name: "Pending",
+      data: tasks.pending,
+      tableSort: {
+        attribute: 'done',
+        reverse: true
+      },
+      selection: {
+        item: null,
+        items: [],
+        checkAll: false
+      },
+      tableColumns: [
+        {
+          name: "Name",
+          attribute: "name",
+          type: "text",
+          width: "30%"
+        },
+        {
+          name: "Created",
+          attribute: "started",
+          type: "date",
+          width: "15%"
+        },
+        {
+          name: "Complete",
+          attribute: "done",
+          type: "percent",
+          width: "42%"
+        },
+        {
+          name: "Approx",
+          attribute: "approx",
+          displayAttr: "approxFormat",
+          type: "text",
+          width: "10%"
+        }
+      ]
+    },
+    failed: {
+      name: "Failed",
+      data: tasks.failed,
+      tableSort: {
+        attribute: 'ended',
+        reverse: true
+      },
+      selection: {
+        item: null,
+        items: [],
+        checkAll: false
+      },
+      tableColumns: [
+        {
+          name: "Name",
+          type: "text",
+          attribute: "name",
+          width: "22%"
+        },
+        {
+          name: "Created",
+          type: "date",
+          attribute: "started",
+          width: "15%"
+        },
+        {
+          name: "Completed to",
+          type: "percent",
+          attribute: "done",
+          css: "danger",
+          width: "35%"
+        },
+        {
+          name: "Failed",
+          type: "date",
+          attribute: "ended",
+          width: "15%"
+        },
+        {
+          name: "Error",
+          type: "text",
+          attribute: "errCode",
+          width: "10%"
+        }
+      ]
+    },
+    complete: {
+      name: "Done",
+      data: tasks.complete,
+      tableSort: {
+        attribute: 'ended',
+        reverse: true
+      },
+      selection: {
+        item: null,
+        items: [],
+        checkAll: false
+      },
+      tableColumns: [
+        {
+          name: "Name",
+          type: "text",
+          attribute: "name"
+        },
+        {
+          name: "Created",
+          type: "date",
+          attribute: "started"
+        },
+        {
+          name: "Finished",
+          type: "date",
+          attribute: "ended"
+        }
+      ]
+    }
+  };
 });
--- webui/app/components/runnersQ/directives/RunnersQDirective.js
+++ webui/app/components/runnersQ/directives/RunnersQDirective.js
@@ -35,6 +35,9 @@ app.directive("runnersQ", function () {
   return {
     restrict: "A",
     controller: function ($scope, toasty, $uibModal) {
+      var today = new Date();
+      var diff = new Date(today.getFullYear(), today.getMonth());
+      var word = "eeeeeennniiissrraattddhhulcgmobwf";
       //example
       $scope.randomList = function () {
         var sum = Math.floor(Math.random() * 80);
@@ -52,37 +55,53 @@ app.directive("runnersQ", function () {
           avr: avr,
           icon: icon
         };
+        $scope.tasks.item = {id: 0};
         $scope.tasks.pending = [];
-        var today = new Date();
-        var diff = new Date(today.getFullYear(), today.getMonth());
-        var word = "eeeeeennniiissrraattddhhulcgmobwf";
-        for (var i = 0; i < run; i++) {
-          var runtime = Math.floor(Math.random() * (today.getTime() - diff.getTime()));
-          var started = new Date( runtime + diff.getTime());
-          var done = Math.floor(Math.random() * 100);
-          if (done === 0) {
-            done = 1;
-          }
-          var approx = new Date(Math.floor((100 / done) * (runtime / 1000)));
-          var days = approx.getDate() - 1;
-          var h = approx.getHours() - 1;
-          var m = approx.getMinutes();
-          var approxFormat = (days > 0) ? days + "d " : "";
-          approxFormat += (h > 0) ? h + "h " : "";
-          approxFormat += (approxFormat !== "" || m > 0) ? m + "m" : "< 1m";
-          var name = "";
-          for (var j = 0; j < Math.floor(Math.random() * 6) + 3; j++) {
-            name += word[Math.floor(Math.random() * word.length)];
-          }
-          $scope.tasks.pending.push({
-            name: "Create ceph pool '" + name + "'",
-            started: started,
-            done: done,
-            approx: approx,
-            approxFormat: approxFormat
-          });
+        $scope.tasks.failed = [];
+        $scope.tasks.complete = [];
+        var i = 0;
+        for (i = 0; i < run; i++) {
+          $scope.tasks.pending.push($scope.genTask());
+        }
+        for (i = 0; i < fail; i++) {
+          $scope.tasks.failed.push($scope.genTask());
+        }
+        for (i = 0; i < fail; i++) {
+          $scope.tasks.complete.push($scope.genTask());
         }
         console.log($scope.tasks);
+      };
+
+      $scope.genTask = function () {
+        var name = "";
+        for (var j = 0; j < Math.floor(Math.random() * 6) + 3; j++) {
+          name += word[Math.floor(Math.random() * word.length)];
+        }
+        var runtime = Math.floor(Math.random() * (today.getTime() - diff.getTime()));
+        var started = new Date( runtime + diff.getTime());
+        var endTime = Math.floor(Math.random() * (today.getTime() - started.getTime()));
+        var ended = new Date( endTime + started.getTime());
+        var done = Math.floor(Math.random() * 100);
+        if (done === 0) {
+          done = 1;
+        }
+        var approx = new Date(Math.floor((100 / done) * (runtime / 1000)));
+        var days = approx.getDate() - 1;
+        var h = approx.getHours() - 1;
+        var m = approx.getMinutes();
+        var approxFormat = (days > 0) ? days + "d " : "";
+        approxFormat += (h > 0) ? h + "h " : "";
+        approxFormat += (approxFormat !== "" || m > 0) ? m + "m" : "< 1m";
+
+        return {
+          name: "Create ceph pool '" + name + "'",
+          errCode: Math.floor(Math.random() * 300 + 300),
+          started: started,
+          ended: ended,
+          done: done,
+          approx: approx,
+          approxFormat: approxFormat
+        };
       };
 
       $scope.randomList();
