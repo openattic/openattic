34d454dfcd7f352a515124cdee1a1ae56cf6351f
Sebastian Krah <sebastian@openattic.org>
2016-09-22 15:03:52 +0200

    add zooming
    Signed-off-by: Sebastian Krah <sebastian@openattic.org>

--- webui/app/components/graph/services/graphOptionsService.js
+++ webui/app/components/graph/services/graphOptionsService.js
@@ -35,8 +35,10 @@ app.factory("graphOptionsService", function () {
   return {
     chart: {
       color: ["#288cea", "#6430ec", "#ffdb1b", "#ffa21b", "#19f13f", "#ff2b1b"],
-      forceY: [0],
-      interpolate: "linear",
+      type: "lineChart",
+      useInteractiveGuideline: true,
+      height: 250,
+      duration: 350,
       legend: {
         vers: "furious",
         margin: {
@@ -54,8 +56,6 @@ app.factory("graphOptionsService", function () {
         bottom: 40,
         left: 80
       },
-      type: "lineChart",
-      useInteractiveGuideline: true,
       x: function (d) {
         return d[0];
       },
@@ -74,12 +74,32 @@ app.factory("graphOptionsService", function () {
         return d[1];
       },
       yAxis: {
-        axisLabel: "placeholder",
+        axisLabel: "",
         axisLabelDistance: 20,
         fontSize: 11,
+        showMaxMin: false,
         tickFormat: function (d) {
-          return d;
+          var value = 0;
+          var unit = "B";
+          if (d !== null && d !== 0) {
+            var units = ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
+            var i = Math.floor(Math.log(d) / Math.log(1024));
+            value = parseFloat((d / Math.pow(1024, i)));
+            unit = units[i];
+          }
+
+          return d3.format(".2f")(value) + unit;
         }
+      },
+      zoom: {
+        //NOTE: All attributes below are optional
+        enabled: true,
+        scaleExtent: [1, 10],
+        useFixedDomain: false,
+        useNiceScale: false,
+        horizontalOff: false,
+        verticalOff: true,
+        unzoomEventType: "dblclick.zoom"
       }
     }
   };
