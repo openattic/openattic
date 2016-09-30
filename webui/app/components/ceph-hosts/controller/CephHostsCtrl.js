ea77a402a689952b2da8a7608bd881a0e9815680
Sebastian Krah <sebastian@openattic.org>
2016-09-30 10:11:22 +0200

    initial commit
    Signed-off-by: Sebastian Krah <sebastian@openattic.org>

---
+++ webui/app/components/ceph-hosts/controller/CephHostsCtrl.js
@@ -0,0 +1,70 @@
+/**
+ *
+ * @source: http://bitbucket.org/openattic/openattic
+ *
+ * @licstart  The following is the entire license notice for the
+ *  JavaScript code in this page.
+ *
+ * Copyright (C) 2011-2016, it-novum GmbH <community@openattic.org>
+ *
+ *
+ * The JavaScript code in this page is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software
+ * Foundation; version 2.
+ *
+ * This package is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * As additional permission under GNU GPL version 2 section 3, you
+ * may distribute non-source (e.g., minimized or compacted) forms of
+ * that code without the copy of the GNU GPL normally required by
+ * section 1, provided you include this license notice and a URL
+ * through which recipients can access the Corresponding Source.
+ *
+ * @licend  The above is the entire license notice
+ * for the JavaScript code in this page.
+ *
+ */
+"use strict";
+
+var app = angular.module("openattic.cephHosts");
+app.controller("CephHostsCtrl", function ($scope, cephHostsService) {
+  $scope.data = {};
+
+  $scope.filterConfig = {
+    page: 0,
+    entries: null,
+    search: "",
+    sortfield: null,
+    sortorder: null
+  };
+
+  $scope.selection = {};
+
+  $scope.loadHosts = function () {
+    cephHostsService.filter({
+          page: $scope.filterConfig.page + 1,
+          pageSize: $scope.filterConfig.entries,
+          search: $scope.filterConfig.search,
+          ordering: ($scope.filterConfig.sortorder === "ASC" ? "" : "-") + $scope.filterConfig.sortfield
+        })
+        .$promise
+        .then(function (res) {
+          $scope.data = res;
+        })
+        .catch(function (error) {
+          throw error
+        });
+  };
+
+  // Watcher
+  $scope.$watch("filterConfig", function (newVal) {
+    if (newVal.entries === null) {
+      return;
+    }
+    $scope.loadHosts();
+  }, true);
+});
