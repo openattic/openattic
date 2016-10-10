a76e58aa5f405b1acc2b448590929c610233b683
Sebastian Wagner <sebastian.wagner@it-novum.com>
2016-10-10 12:14:02 +0200

    Removed ceph(_deployment).__init__.has_executable()
    removed has_librados(). No need to define a function here.
    
    Signed-off-by: Sebastian Wagner <sebastian.wagner@it-novum.com>

--- backend/ceph_deployment/__init__.py
+++ backend/ceph_deployment/__init__.py
@@ -1,4 +1,4 @@
-import os
+from distutils.spawn import find_executable
 
 try:
     import ceph
@@ -6,15 +6,6 @@ except ImportError:
     print 'Cannot import app "ceph", disabling app "ceph_deployment"'
     raise ImportError()
 
-def has_executable(name):
-    """Look in PATH if there's a file `ceph` which is callable."""
-    for path in os.environ['PATH'].split(':'):
-        file_path = os.path.join(path, name)
-        if os.access(file_path, os.X_OK) and os.path.isfile(file_path):
-            return True
-
-    return False
-
-if not has_executable('salt'):
+if not find_executable('salt'):
     print '"salt" executable not found, disabling app "ceph_deployment"'
     raise ImportError()
