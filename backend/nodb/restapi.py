5f57c65246bf11d09cfdf135280711c8697e253d
Sebastian Wagner <sebastian.wagner@it-novum.com>
2016-10-10 13:06:26 +0200

    Added missing import
    Signed-off-by: Sebastian Wagner <sebastian.wagner@it-novum.com>

--- backend/nodb/restapi.py
+++ backend/nodb/restapi.py
@@ -11,6 +11,7 @@
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
 """
+from django.core import validators
 from rest_framework import serializers, viewsets
 
 from rest.utilities import ToNativeToRepresentationMixin, drf_version
