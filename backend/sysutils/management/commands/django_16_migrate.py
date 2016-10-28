5a5ff0724ab2565442bd0b552560680d734332c3
Sebastian Wagner <sebastian.wagner@it-novum.com>
2016-10-28 18:16:50 +0200

    Extended our migration framework to be compatible to Django 1.7+ migrations.
    We will now create the db table django_migrations and insert all applied
    migrations.
    
    Signed-off-by: Sebastian Wagner <sebastian.wagner@it-novum.com>

--- backend/sysutils/management/commands/django_16_migrate.py
+++ backend/sysutils/management/commands/django_16_migrate.py
@@ -15,6 +15,7 @@ import logging
 from contextlib import closing
 
 import django
+from django.conf import settings
 from django.core.management import BaseCommand
 from django.db import ProgrammingError
 from django.db import connection
@@ -25,9 +26,9 @@ except ImportError:
 
 logger = logging.getLogger(__name__)
 
-# Look, as Django doesn't support migrations, we have either to use the outdated and deprecated
+# Look, as Django doesn't support migrations, we either have to use the outdated and deprecated
 # South migrations, or delete the DB on every update, or build our own migration framework.
-# This is our own migration framework. Backward Migrations will not work.
+# This is our own migration framework. Backward migrations will not work.
 #
 # To add a new migration run ./manage.py sqlmigrate <your app> <your migration> with a Django >1.6
 # and paste the SQL output here. Also, build a test function that returns true, if the migration
@@ -35,6 +36,18 @@ logger = logging.getLogger(__name__)
 # does not.
 #
 # Let's hope Django 1.6 will be unsupported, before we have more than 10 migrations.
+#
+# Unfortunately, this got a bit more complicated than anticipated, becuase these migrations need
+# to be detected by future Django versions.
+#
+# Here are some important rules:
+# * If you add a new app here, make sure to include fake the initial migration.
+# * The result must be compatible to Django 1.7 migrations.
+# * Don't add initial migrations, until you need a second one.
+# * Include all migrations, even migrations without SQL statements.
+# * The order of migrations must be valid, according to migration dependencies.
+# * We still need to add a test function, in order to cope with db tables created by syncdb, which
+#   are newer than the initial table schema. This a main reason for the this complexity here.
 
 
 def test_ifconfig_0002_auto_20160329_1248(cursor):
@@ -48,25 +61,52 @@ def test_ifconfig_0003_host_is_oa_host(cursor):
     stmt = "select column_name from INFORMATION_SCHEMA.COLUMNS where table_name = 'ifconfig_host'"
     return "is_oa_host" not in [d['column_name'] for d in execute_and_fetch(cursor, stmt)]
 
-
-migrations = [
+# (app, name, test function, sql statement)
+# * If app and name is None, this migration will always be executed, if test function returns True.
+# * If test function and sql stmt is None, the migratio will only be added to the django_migrations
+#   db table.
+_migrations = [
     (
+        None, None,
+        lambda cursor: not _table_exists('django_migrations', cursor),
+        """
+        CREATE TABLE "django_migrations"
+        (
+            "id" serial NOT NULL PRIMARY KEY,
+            "app" varchar(255) NOT NULL,
+            "name" varchar(255) NOT NULL,
+            "applied" timestamp with time zone NOT NULL
+        );
+        """
+    ),
+    (
+        'ifconfig', u'0001_initial', None, None
+    ),
+    (
+        'ifconfig', u'0002_auto_20160329_1248',
+        test_ifconfig_0002_auto_20160329_1248,
         """
         BEGIN;
         ALTER TABLE "ifconfig_netdevice" ALTER COLUMN "devname" TYPE varchar(15);
         COMMIT;
-        """,
-        test_ifconfig_0002_auto_20160329_1248
+        """
     ),
     (
+        'ifconfig', u'0003_host_is_oa_host',
+        test_ifconfig_0003_host_is_oa_host,
         """
         BEGIN;
         ALTER TABLE "ifconfig_host" ADD COLUMN "is_oa_host" boolean NULL;
         ALTER TABLE "ifconfig_host" ALTER COLUMN "is_oa_host" DROP DEFAULT;
         COMMIT;
-        """,
-        test_ifconfig_0003_host_is_oa_host
-    )
+        """
+    ),
+    (
+        'ceph', u'0001_initial', None, None
+    ),
+    (
+        'ceph', u'0002_auto_20161007_1921', None, None
+    ),
 ]
 
 
@@ -75,26 +115,79 @@ class Command(BaseCommand):
 
     def handle(self, **options):
         if django.VERSION >= (1, 7):
-            raise ValueError('Django 1.7+ is not supported.')
+            raise ValueError('Django 1.7 (or newer) is not supported.')
         migrate_all()
+
+
+def get_migrations(cursor):
+    django_migrations_exists = _table_exists('django_migrations', cursor)
+
+    def should_run(migration):
+
+        app, name, test, stmt = migration
+        if app is None:
+            return True
+        if name is not None and (django_migrations_exists
+                                 and django_migration_already_inserted(app, name, cursor)):
+            logger.info('Migration already applied: {}.{}'.format(app, name))
+            return False
+        apps = settings.INSTALLED_APPS
+        if app not in apps:
+            logger.info('App not installed: {}.{}'.format(app, name))
+            return False
+        return True
+
+    return filter(should_run, _migrations)
 
 
 def migrate_all():
     with closing(connection.cursor()) as cursor:  # type: CursorWrapper
-        for migration, test in migrations:
-            if test(cursor):
-                logger.info('Running Migration {}.'.format(test.__name__[5:]))
-                cursor.execute(migration)
-            else:
-                logger.info('Skipping Migration {}.'.format(test.__name__[5:]))
+        migrations = get_migrations(cursor)
+        for migration in migrations:
+            migrate_one(migration, cursor)
 
-        if any([test(cursor) for _, test in migrations]):
+        if any([test is not None and test(cursor) for _, _, test, _ in migrations]):
             raise ProgrammingError('After applying all migrations, all test '
-                                   'function must return false.')
+                                   'functions must return false.')
 
 
-def execute_and_fetch(cursor, stmt):
-    cursor.execute(stmt)
+def migrate_one(migration, cursor):
+    app, name, test, stmt = migration
+    if (test is None) != (stmt is None):
+        raise ProgrammingError('{} != {}'.format(test is None, stmt is None))
+
+    if test is not None and test(cursor) and stmt is not None:
+        logger.info('Running migration {}.{}'.format(app, name))
+        cursor.execute(stmt)
+    elif name is None and test is not None and not test(cursor):
+        logger.info('Skipping migration {}.{}'.format(app, name))
+    elif test is not None and not test(cursor):
+        logger.info('Skipping and inserting migration {}.{}'.format(app, name))
+    elif test is None:
+        logger.info('Faking migration {}.{}'.format(app, name))
+
+    if app is not None and name is not None:
+        insert_into_django_migrations(app, unicode(name), cursor)
+
+
+def insert_into_django_migrations(app, name, cursor):
+    stmt = """
+        INSERT INTO "django_migrations" ("app", "name", "applied")
+        VALUES (%s, %s, now())
+        """
+    cursor.execute(stmt, [app, name])
+
+
+def django_migration_already_inserted(app, name, cursor):
+    stmt = """
+    SELECT * FROM "django_migrations"
+    WHERE app = %s AND name = %s
+    """
+    return len(execute_and_fetch(cursor, stmt, [app, name])) == 1
+
+
+def execute_and_fetch(cursor, stmt, args=None):
+    cursor.execute(stmt, args)
     return dictfetchall(cursor)
 
 
@@ -108,3 +201,10 @@ def dictfetchall(cursor):
         dict(zip(columns, row))
         for row in cursor.fetchall()
     ]
+
+
+def _table_exists(table_name, cursor):
+    stmt = """select table_name from INFORMATION_SCHEMA.COLUMNS
+              where table_name = %s;"""
+    res = execute_and_fetch(cursor, stmt, [table_name])
+    return len(res) > 0 and res[0]['table_name'] == table_name
