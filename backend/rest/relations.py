76a7aa7b13efbc68ee363027f160f5e54e7dcb4c
Sebastian Wagner <sebastian.wagner@it-novum.com>
2016-11-07 10:55:04 +0100

    OP-1679: Made JSON represenatation of HyperlinkedRelatedField compatible to DRF 2
    We actually modify the output of `HyperlinkedRelatedField` to be non-standard.
    This should have been done in a different class, and not directly in
    `HyperlinkedRelatedField`.
    
    I've copied the code from the DRF 2 version above.
    
    Signed-off-by: Sebastian Wagner <sebastian.wagner@it-novum.com>

--- backend/rest/relations.py
+++ backend/rest/relations.py
@@ -102,5 +102,31 @@ else:
         def use_pk_only_optimization(self):
             return False
 
+        def to_representation(self, obj):
+            """
+            We actually modify the output of `HyperlinkedRelatedField` to be non-standard.
+            This should have been done in a different class, and not directly here.
+            """
+            url = super(HyperlinkedRelatedField, self).to_representation(obj)
+            return {
+                'id':    obj.pk,
+                'url':   url,
+                'title': unicode(obj)
+            }
+
+        def from_representation(self, value):
+            """
+            We actually modify the output of `HyperlinkedRelatedField` to be non-standard.
+            This should have been done in a different class, and not directly here.
+            """
+            if type(value) != dict:
+                raise TypeError("value needs to be a dictionary")
+            if "id" in value:
+                return self.queryset.get(id=value["id"])
+            if "url" in value:
+                return super(HyperlinkedRelatedField, self).from_representation(value["url"])
+            raise KeyError("need id or url field (id preferred)")
+
+
 
     HyperlinkedIdentityField = RestFramework_HyperlinkedIdentityField
