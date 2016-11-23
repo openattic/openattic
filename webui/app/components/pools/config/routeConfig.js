fe2958399f7481b4d2a6bffe5ae1882cde8b154b
Sebastian Krah <skrah@suse.com>
2016-11-23 09:04:57 +0100

    Move pools route config to routeConfig of module pools
    Signed-off-by: Sebastian Krah <skrah@suse.com>

---
+++ webui/app/components/pools/config/routeConfig.js
@@ -0,0 +1,100 @@
+/**
+ *
+ * @source: http://bitbucket.org/openattic/openattic
+ *
+ * @licstart  The following is the entire license notice for the
+ *  JavaScript code in this page.
+ *
+ * Copyright (C) 2011-2016, it-novum GmbH <community@openattic.org>
+ *
+ *
+ * The JavaScript code in this page is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software
+ * Foundation; version 2.
+ *
+ * This package is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * As additional permission under GNU GPL version 2 section 3, you
+ * may distribute non-source (e.g., minimized or compacted) forms of
+ * that code without the copy of the GNU GPL normally required by
+ * section 1, provided you include this license notice and a URL
+ * through which recipients can access the Corresponding Source.
+ *
+ * @licend  The above is the entire license notice
+ * for the JavaScript code in this page.
+ *
+ */
+"use strict";
+
+var app = angular.module("openattic.pools");
+app.config(function ($stateProvider) {
+  $stateProvider
+      .state("pools", {
+        url: "/pools",
+        views: {
+          "main": {
+            templateUrl: "templates/pools.html",
+            controller : "PoolCtrl"
+          }
+        },
+        ncyBreadcrumb: {
+          label: "Pools"
+        }
+      })
+      .state("pools-add", {
+        url: "/pools/add/:diskId",
+        views: {
+          "main": {
+            templateUrl: "templates/pools/pool-form.html",
+            controller : "PoolFormCtrl"
+          }
+        },
+        ncyBreadcrumb: {
+          parent: "pools",
+          label: "Add"
+        }
+      })
+      .state("pools.detail", {
+        url: "/:pool",
+        views: {
+          "tab": {templateUrl: "templates/pools/tab.html"}
+        },
+        ncyBreadcrumb: {
+          skip: true
+        }
+      })
+      .state("pools.detail.status", {
+        url: "/status",
+        views: {
+          "tab-content": {templateUrl: "templates/pools/status.html"}
+        },
+        ncyBreadcrumb: {
+          label: "{{selection.item.name}} Status"
+        }
+      })
+      .state("pools.detail.storage", {
+        url: "/storage",
+        views: {
+          "tab-content": {
+            templateUrl: "templates/pools/storage.html",
+            controller : "PoolStorageCtrl"
+          }
+        },
+        ncyBreadcrumb: {
+          label: "{{selection.item.name}} Storage"
+        }
+      })
+      .state("pools.detail.cephpool", {
+        url: "/cephpool",
+        views: {
+          "tab-content": {templateUrl: "templates/pools/cephpool.html"}
+        },
+        ncyBreadcrumb: {
+          label: "{{selection.item.name}} Cephpool"
+        }
+      });
+});
