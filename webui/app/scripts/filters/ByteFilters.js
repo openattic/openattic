650616b8967d321488f4017cef6b980bba1abe72
Volker Theile <vtheile@suse.com>
2016-11-25 12:30:42 +0100

    Introduce new parameter 'inUnit' to specify the unit of the input value.
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- webui/app/scripts/filters/ByteFilters.js
+++ webui/app/scripts/filters/ByteFilters.js
@@ -35,34 +35,51 @@ var app = angular.module("openattic");
 app.filter("bytes", function () {
   /**
    * Display the given size in the best matching unit.
-   * @param {number} value The value in MiB.
-   * @param {number} precision The number of digits after the decimal point.
+   * @param {number} value The value to be displayed.
+   * @param {number|string} inUnit The input unit, e.g. 1 or 'KiB'.
+   *   Default is 'B'.
+   * @param {number} outPrecision The number of digits after the decimal point.
    *   Default is 2.
-   * @param {number|string} unit The unit to output, e.g. 2 or 'MiB'.
+   * @param {number|string} outUnit The unit to output, e.g. 2 or 'MiB'.
    *   If not set, the best matching unit is calculated. Default is auto-calc.
    */
-  return function (value, precision, unit) {
+  return function (value, inUnit, outPrecision, outUnit) {
     var units = ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB"];
+    // Validate the input value.
     if (isNaN(parseFloat(value)) || !isFinite(value)) {
       return "-";
     }
     if (value === 0) {
       return value + " " + units[0];
     }
-    value = (value * Math.pow(1024, 2)).toFixed(0); // Convert to bytes
-    precision = precision || 2;
-    if (angular.isDefined(unit)) {
-      // Manually set the unit.
-      if (angular.isString(unit)) {
-        unit = units.indexOf(unit);
+    // Get the input unit.
+    if (angular.isDefined(inUnit)) {
+      if (angular.isString(inUnit)) {
+        inUnit = units.indexOf(inUnit);
       }
-      if (!unit || unit < 0 || unit > units.length) {
-        unit = 0;
+      if (!inUnit || inUnit < 0 || inUnit > units.length) {
+        inUnit = 0;
+      }
+    } else {
+      inUnit = 0;
+    }
+    // Convert the value to bytes if necessary.
+    if (0 < inUnit) {
+      value = (value * Math.pow(1024, inUnit)).toFixed(0);
+    }
+    // Set the output precision and unit.
+    outPrecision = outPrecision || 2;
+    if (angular.isDefined(outUnit)) {
+      if (angular.isString(outUnit)) {
+        outUnit = units.indexOf(outUnit);
+      }
+      if (!outUnit || outUnit < 0 || outUnit > units.length) {
+        outUnit = 0;
       }
     } else {
       // Calculate the best matching unit.
-      unit = Math.floor(Math.log(value) / Math.log(1024));
+      outUnit = Math.floor(Math.log(value) / Math.log(1024));
     }
-    return (value / Math.pow(1024, unit)).toFixed(precision) +  " " + units[unit];
+    return (value / Math.pow(1024, outUnit)).toFixed(outPrecision) +  " " + units[outUnit];
   };
 });
