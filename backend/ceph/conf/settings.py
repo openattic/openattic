5982fec4ee78549ac320227c9566a8ccc33824d1
Tatjana Dehler <tatjana@openattic.org>
2016-11-25 15:06:46 +0100

    Correct license header
    Signed-off-by: Tatjana Dehler <tatjana@openattic.org>

--- backend/ceph/conf/settings.py
+++ backend/ceph/conf/settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
- *  Copyright (C) 2011-2016, it-novum GmbH <community@openattic.org>
+ *  Copyright (c) 2016 SUSE LLC
  *
  *  openATTIC is free software; you can redistribute it and/or modify it
  *  under the terms of the GNU General Public License as published by
