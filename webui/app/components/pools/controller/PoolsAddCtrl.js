8f3fc86148081b3ee4261bd25a4d5ce4c467ebbe
Sebastian Krah <skrah@suse.com>
2016-11-25 12:25:57 +0100

    Rename file and controller
    Signed-off-by: Sebastian Krah <skrah@suse.com>

--- webui/app/components/pools/controller/PoolsAddCtrl.js
+++ webui/app/components/pools/controller/PoolsAddCtrl.js
@@ -31,7 +31,7 @@
 "use strict";
 
 var app = angular.module("openattic");
-app.controller("PoolFormCtrl", function ($scope, $state, $stateParams, PoolService) {
+app.controller("PoolsAddCtrl", function ($scope, $state, $stateParams, PoolService) {
   $scope.pool = {
     options: { type: "lvm" },
     name: "",
