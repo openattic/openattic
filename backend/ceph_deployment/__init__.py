23ee5d0080eb22176af0993e1a1e65d96616f4b2
Volker Theile <vtheile@suse.com>
2016-11-29 16:16:59 +0100

    - Initialize Django to get rid of the "Models aren't loaded yet" error - Redirect the messages from ceph_deployment/__init__.py to stderr to do not break scripts that parse stdout
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- backend/ceph_deployment/__init__.py
+++ backend/ceph_deployment/__init__.py
@@ -1,11 +1,12 @@
+import sys
 from distutils.spawn import find_executable
 
 try:
     import ceph
 except ImportError:
-    print 'Cannot import app "ceph", disabling app "ceph_deployment"'
+    print >>sys.stderr, 'Cannot import app "ceph", disabling app "ceph_deployment"'
     raise ImportError()
 
 if not find_executable('salt'):
-    print '"salt" executable not found, disabling app "ceph_deployment"'
+    print >>sys.stderr, '"salt" executable not found, disabling app "ceph_deployment"'
     raise ImportError()
