9bdacf18362f9c0b56965c97ea41c2a6dc8a21a3
Volker Theile <vtheile@suse.com>
2016-12-02 10:38:39 +0100

    Redirect output to stderr now with Python2 syntax :-)
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- backend/ceph/__init__.py
+++ backend/ceph/__init__.py
@@ -2,13 +2,12 @@ import sys
 from distutils.spawn import find_executable
 
 if not find_executable('ceph'):
-    print('ERROR:ceph:Ceph executable couldn\'t be found. ' +
-        'The `ceph` package provided by your distribution is probably not installed.',
-        file=sys.stderr)
+    print >> sys.stderr, 'ERROR:ceph:Ceph executable couldn\'t be found. ' \
+        'The `ceph` package provided by your distribution is probably not installed.'
     raise ImportError()
 
 try:
     import rados
 except ImportError:
-    print('ERROR:ceph:`rados` library couldn\'t be found.', file=sys.stderr)
+    print >> sys.stderr, 'ERROR:ceph:`rados` library couldn\'t be found.'
     raise
--- backend/ceph_deployment/__init__.py
+++ backend/ceph_deployment/__init__.py
@@ -4,11 +4,9 @@ from distutils.spawn import find_executable
 try:
     import ceph
 except ImportError:
-    print('Cannot import app "ceph", disabling app "ceph_deployment"',
-        file=sys.stderr)
+    print >>sys.stderr, 'Cannot import app "ceph", disabling app "ceph_deployment"'
     raise ImportError()
 
 if not find_executable('salt'):
-    print('"salt" executable not found, disabling app "ceph_deployment"',
-        file=sys.stderr)
+    print >>sys.stderr, '"salt" executable not found, disabling app "ceph_deployment"'
     raise ImportError()
