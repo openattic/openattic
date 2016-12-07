e8dc752b7ff318f986c023d5ae4e82bd7b1ddc86
Tatjana Dehler <tdehler@suse.com>
2016-12-07 08:39:08 +0100

    Replace the part that does not work by a simple invoke call
    Signed-off-by: Tatjana Dehler <tdehler@suse.com>

--- backend/systemd/procutils.py
+++ backend/systemd/procutils.py
@@ -29,6 +29,8 @@ from django.conf import settings
 
 from cmdlog.models import LogEntry
 from ifconfig.models import Host
+from utilities import is_executable_installed
+
 
 def invoke(args, close_fds=True, return_out_err=False, log=True, stdin=None, fail_on_err=True):
     """ Invoke a subprocess with the given args and log the output.
@@ -133,31 +135,11 @@ def service_command(service, command="reload"):
         raise ValueError("invalid command %s" % command)
 
     if settings.USE_SYSTEMD_IF_AVAIL:
-        try:
-            bus = dbus.SystemBus(private=True)
-            obj = bus.get_object("org.freedesktop.systemd1", "/org/freedesktop/systemd1")
-            systemd = dbus.Interface(obj, "org.freedesktop.systemd1.Manager")
-        except dbus.DBusException:
-            import traceback
-            logging.warn("service_command(%s): systemd(1) not available, falling back to invoke()\n%s" % (service, traceback.format_exc()))
-        else:
-            logging.info("service_command(%s): calling systemd(1)" % service)
-            def passfn(*args):
-                pass
-            try:
-                if command == "reload":
-                    systemd.ReloadOrRestartUnit("%s.service" % service, "replace", reply_handler=passfn, error_handler=passfn)
-                elif command == "restart":
-                    systemd.RestartUnit("%s.service" % service, "replace", reply_handler=passfn, error_handler=passfn)
-                elif command == "start":
-                    systemd.StartUnit("%s.service" % service, "replace", reply_handler=passfn, error_handler=passfn)
-                elif command == "stop":
-                    systemd.StopUnit("%s.service" % service, "replace", reply_handler=passfn, error_handler=passfn)
-            except dbus.DBusException, err:
-                import traceback
-                logging.error("service_command(%s): caught exception, falling back to invoke():\n%s" % (service, traceback.format_exc()))
-            else:
-                return
+        if is_executable_installed('systemctl'):
+            logging.info('service_command({}): invoking `systemctl {} {}`'.format(service, command,
+                                                                                  service))
+            invoke(['systemctl', command, service])
+            return
 
     if os.path.exists("/usr/sbin/service"):
         logging.info("service_command(%s): invoking `service %s %s`" % (service, service, command))
