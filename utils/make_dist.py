9d1200aff32b3e57469e9fa937266e28d88f51cb
Patrick Nawracay <patrick.nawracay@it-novum.com>
2017-01-06 16:29:05 +0100

    Refactoring
    - Improve documentation of --push-changes switch
    - Refactor rmtree and copytree to wrapped methods that log their calls by themselves
    - Exit on system calls which do not return a zero exit status
    
    Signed-off-by: Patrick Nawracay <pnawracay@suse.com>

--- utils/make_dist.py
+++ utils/make_dist.py
@@ -76,17 +76,21 @@ Options:
 
     --push-changes
 
-        Pushes the changes made to the temporary repository which is used to
-        create the tarball. This switch is ment to be used on a release to push
-        changes back to the repository.  Those changes include the adaption of
-        the `debian/changelog` as well as any created Mercurial tags.  This
-        switch will be ignored if the argument to --source is a local path
+        Pushes the changes that have been made in the temporary repository
+        which is used to create the tarball. This switch is ment to be used on
+        a release to push changes back to the configured remote repository.
+        Those changes include the adaption of the `debian/changelog` as well as
+        any Mercurial tags created.
+
+        This switch will be ignored if the argument to --source is a local path
         which contains uncommitted changes. If there aren't uncommited changes
-        to the repository, the switch won't be ignored. Be aware of that every
-        commited but not yet pushed change to the local repository is going to
-        be pushed! If the push would create a new head on the remote
-        repository, the changes won't get pushed and the execution of the
-        script will be aborted.
+        to the repository, the switch won't be ignored. Be aware that every
+        committed but unpushed change in the local source repository is going
+        to be pushed when using this switch!
+
+        If the push would create a new head on the remote repository, the
+        changes won't be pushed and the execution of the script will be
+        aborted.
 
     --tag
 
@@ -210,12 +214,12 @@ class Process(object):
 
             if exit_on_error:
                 # Print message on exit for the sake of debugging.
-                print('Error occurred, exiting')
+                sys.stderr.write('Error occurred, exiting\n')
                 sys.exit(result.returncode)
 
         return result
 
-    def system(self, args, cwd):
+    def system(self, args, cwd, exit_on_error=None):
         """Make a system call.
 
         The args aren't escaped automatically. Either don't pass user input to this function or
@@ -227,9 +231,14 @@ class Process(object):
         :type cwd: str
         :return:
         """
+        exit_on_error = self.exit_on_error is True and exit_on_error is not False
         self.log_command(args, cwd, self.use_bold)
         os.chdir(cwd)
-        return os.system(' '.join(args))
+        exit_code = os.system(' '.join(args))
+        if exit_code > 0 and exit_on_error:
+            sys.stderr.write('Error occurred, exiting\n')
+            sys.exit(exit_code)
+        return exit_code
 
     def log_command(self, args, cwd='', use_bold=True):
         """Log a command call.
@@ -308,6 +317,15 @@ class DistBuilder(object):
 
     def _command_exists(self, command):
         return self._process.run(['which', command], exit_on_error=False).success()
+
+    def _rmtree(self, path, **kwargs):
+        force = '' if 'ignore_errors' not in kwargs.keys() else 'f'
+        self._process.log_command(['rm', '-r{}'.format(force), path])
+        rmtree(path, **kwargs)
+
+    def _copytree(self, *args, **kwargs):
+        self._process.log_command(['cp', '-r'] + list(args))
+        copytree(*args, **kwargs)
 
     def _check_dependencies(self, commands):
         """Check the existence of the given commands.
@@ -484,7 +502,7 @@ class DistBuilder(object):
             return
 
         if self._source_is_path:
-            copytree(source, destination_dir, symlinks=True)
+            self._copytree(source, destination_dir, symlinks=True)
         else:
             self._process.run(['hg', 'clone', source, destination_dir])
 
@@ -544,8 +562,7 @@ class DistBuilder(object):
 
         # Clean up previous versions.
         if isdir(tmp_abs_build_dir):
-            self._process.log_command(['rm', '-r', tmp_abs_build_dir])
-            rmtree(tmp_abs_build_dir)
+            self._rmtree(tmp_abs_build_dir)
         if isfile(abs_tarball_dest_file):
             self._process.log_command(['rm', abs_tarball_dest_file])
             os.remove(abs_tarball_dest_file)
@@ -581,20 +598,18 @@ class DistBuilder(object):
                 log_msg = 'No cache found for {}'
                 self._log(log_msg.format(os.path.basename(cache_entry['checksum_file'])))
                 self._process.run(cache_entry['command'], cwd=webui_dir)
-                copytree(cache_entry['source_dir'], cache_dir)  # Update cache dir.
+                self._copytree(cache_entry['source_dir'], cache_dir)  # Update cache dir.
             else:
                 log_msg = 'Cache found for {}. Copying files...'
                 self._log(log_msg.format(os.path.basename(cache_entry['checksum_file'])))
-                copytree(cache_dir, cache_entry['source_dir'])  # Use cache dir.
+                self._copytree(cache_dir, cache_entry['source_dir'])  # Use cache dir.
 
         if cache_used:  # Build the frontend files.
             self._process.run(['grunt', 'build'], cwd=webui_dir)
 
             # Remove no longer required dirs.
-            self._process.log_command(['rm', '-r', bower_components_dir])
-            rmtree(bower_components_dir)
-            self._process.log_command(['rm', '-r', node_modules_dir])
-            rmtree(node_modules_dir)
+            self._rmtree(bower_components_dir)
+            self._rmtree(node_modules_dir)
 
         # Update version.txt.
         data = {
@@ -619,7 +634,7 @@ class DistBuilder(object):
         self._process.run(['tar', options, abs_tarball_dest_file, build_basename],
                           cwd=self._tmp_dir)
 
-        rmtree(tmp_abs_build_dir)  # Remove no longer required temporary folder.
+        self._rmtree(tmp_abs_build_dir)  # Remove no longer required temporary folder.
 
         return abs_tarball_dest_file
 
@@ -668,8 +683,7 @@ class DistBuilder(object):
         self._process.run(['hg', 'pull', '--update'], cwd=self._fe_cache_dir)
 
         if self._source_is_path and isdir(self._tmp_oa_clone_dir):
-            self._process.log_command(['rm', '-r', self._tmp_oa_clone_dir])
-            rmtree(self._tmp_oa_clone_dir)
+            self._rmtree(self._tmp_oa_clone_dir)
         self._retrieve_source(self._source, self._tmp_oa_clone_dir, skip_if_exists=True)
 
         channel = self._get_release_channel()
@@ -746,7 +760,7 @@ class DistBuilder(object):
 
         abs_tarball_file_path = self._create_source_tarball(build_basename)
 
-        rmtree(self._tmp_oa_clone_dir)
+        self._rmtree(self._tmp_oa_clone_dir)
         self._remove_npmrc_prefix()
 
         return abs_tarball_file_path
