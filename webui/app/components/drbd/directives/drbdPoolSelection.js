8983c0f17dcbef237c4628982bd2086491ffab1a
Volker Theile <vtheile@suse.com>
2017-01-24 10:10:57 +0100

    Introduce drbd-pool-selection directive.
    Signed-off-by: Volker Theile <vtheile@suse.com>

---
+++ webui/app/components/drbd/directives/drbdPoolSelection.js
@@ -0,0 +1,66 @@
+/**
+ *
+ * @source: http://bitbucket.org/openattic/openattic
+ *
+ * @licstart  The following is the entire license notice for the
+ *  JavaScript code in this page.
+ *
+ * Copyright (c) 2016 SUSE LLC
+ *
+ *
+ * The JavaScript code in this page is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software
+ * Foundation; version 2.
+ *
+ * This package is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * As additional permission under GNU GPL version 2 section 3, you
+ * may distribute non-source (e.g., minimized or compacted) forms of
+ * that code without the copy of the GNU GPL normally required by
+ * section 1, provided you include this license notice and a URL
+ * through which recipients can access the Corresponding Source.
+ *
+ * @licend  The above is the entire license notice
+ * for the JavaScript code in this page.
+ *
+ */
+"use strict";
+
+var app = angular.module("openattic");
+app.directive("drbdPoolSelection", function () {
+  return {
+    restrict: "E",
+    scope: {
+      pool: "=",
+      validation: "=",
+      wizard: "="
+    },
+    templateUrl: "components/drbd/templates/pool-selection-drbd.html",
+    controller: function ($scope, PoolService, toasty) {
+      $scope.getPoolList = function (options) {
+        $scope.waitingMsg = "Retrieving pool list...";
+        PoolService.query(options)
+          .$promise
+          .then(function (res) {
+            $scope.pools = res;
+            $scope.waitingMsg = "-- Select a pool --";
+          }, function (error) {
+            console.log("An error occurred", error);
+            toasty.error({
+              title: "Pool list couldn't be loaded",
+              msg: "Server failure."
+            });
+            $scope.waitingMsg = "Error: List couldn't be loaded!";
+            $scope.validation.$setValidity("loading", false);
+          })
+      }
+
+      $scope.waitingMsg = "Retrieving pool list...";
+      $scope.getPoolList();
+    }
+  };
+});
