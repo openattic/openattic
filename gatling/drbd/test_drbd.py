76e7de69956660f33c33e657a1cbf98795cf32ed
Volker Theile <vtheile@suse.com>
2017-02-17 11:31:10 +0100

    Add another test case.
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- gatling/drbd/test_drbd.py
+++ gatling/drbd/test_drbd.py
@@ -45,6 +45,32 @@ class DrbdTests(object):
         endpoints_res = self.send_request("GET", "volumes", search_param=upper_id)
         self.assertEqual(endpoints_res["count"], 2, msg="number of endpoints is not equal 2")
 
+    def test_create_get_delete_via_volumes(self):
+        """
+        Create a Connection via /api/volumes and check that its Endpoints are
+        created correctly.
+        """
+        vol_data = {
+                "megs": self.volumesize,
+                "name": "gatling_drbd_vol1",
+                "source_pool": {
+                    "id": source_pool_id
+                },
+                "remote_pool": self._get_remote_pool(),
+                "is_mirrored": True,
+                "syncer_rate": "25M",
+                "protocol": "C"
+            }
+        mirror_res = self.send_request("POST", "volumes", data=vol_data)
+        time.sleep(self.sleeptime)
+        self.addCleanup(requests.request, "DELETE", mirror_res["cleanup_url"],
+                        headers=mirror_res["headers"])
+
+        # Check if there are to endpoints (volumes) related to this drbd mirror
+        upper_id = "upper__id=%s" % mirror_res["response"]["volume"]["id"]
+        endpoints_res = self.send_request("GET", "volumes", search_param=upper_id)
+        self.assertEqual(endpoints_res["count"], 2, msg="number of endpoints is not equal 2")
+
     def test_remote_create_get_delete(self):
         """ Create a Connection for a volume located on the remote host. """
         # Create a volume on the remote host
