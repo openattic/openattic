915bada9282c4b0e9578a79511f1cf79f207f926
Sebastian Krah <skrah@suse.com>
2017-03-06 15:41:06 +0100

    activate and specs
    Signed-off-by: Sebastian Krah <skrah@suse.com>

--- e2e/base/users/user.e2e.js
+++ e2e/base/users/user.e2e.js
@@ -111,22 +111,22 @@ describe('Should add an user', function(){
     element(by.id('bot2-Msg1')).click();
   });
 
-  //it('should not show the "protractor_test_user" anymore', function(){
-  //  expect(user.isPresent()).toBe(false);
-  //  //expect that we are still on the users panel
-  //  expect(browser.getCurrentUrl()).toContain('/#/users');
-  //});
+  it('should not show the "protractor_test_user" anymore', function(){
+    expect(user.isPresent()).toBe(false);
+    //expect that we are still on the users panel
+    expect(browser.getCurrentUrl()).toContain('/#/users');
+  });
 
   //to make sure that the user is deleted, try to login again
-  //it('should make sure that the user really does not exist anymore', function(){
-  //  logout.click();
-  //  expect(browser.getCurrentUrl()).toContain('/#/login');
-  //  element.all(by.model('username')).sendKeys(testUser.username);
-  //  element.all(by.model('password')).sendKeys(testUser.userpasswd);
-  //  element.all(by.css('input[type="submit"]')).click();
-  //  expect(correctInput.isDisplayed()).toBe(true);
-  //  expect(correctInput.getText()).toBe('The given credentials are not correct.');
-  //});
+  it('should make sure that the user really does not exist anymore', function(){
+    logout.click();
+    expect(browser.getCurrentUrl()).toContain('/#/login');
+    element.all(by.model('username')).sendKeys(testUser.username);
+    element.all(by.model('password')).sendKeys(testUser.userpasswd);
+    element.all(by.css('input[type="submit"]')).click();
+    expect(correctInput.isDisplayed()).toBe(true);
+    expect(correctInput.getText()).toBe('The given credentials are not correct.');
+  });
 
   afterAll(function(){
     console.log('users -> user.e2e.js');
