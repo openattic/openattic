ffae9ef820a23720cc9577ce6c1ac1d4a92b3379
Sebastian Wagner <sebastian.wagner@suse.com>
2017-04-12 14:22:42 +0200

    Moved the crushmap to NoDB
    * Removed old traditional volumes based Ceph REST API
    * Moved the crushmap to a detail route of a cluster.
    
    The crushmap location changed from
    
    * `curl localhost/api/cephcluster | jq .results[0].crushmap.crushmap` to `curl localhost/api/ceph/:fsid/crushmap | jq .crushmap`
    * `curl localhost/api/cephcluster/:fsid | jq .crushmap.crushmap` to `curl localhost/api/ceph/:fsid/crushmap | jq .crushmap`
    
    Signed-off-by: Sebastian Wagner <sebastian.wagner@suse.com>

--- backend/ceph/migrations/0001_initial.py
+++ backend/ceph/migrations/0001_initial.py
@@ -9,7 +9,6 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('volumes', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('ifconfig', '0002_auto_20160329_1248'),
     ]
