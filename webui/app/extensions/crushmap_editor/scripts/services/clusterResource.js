143e918d921d00491bc480e506254b2b584cc613
Stephan Müller <smueller@suse.com>
2017-05-02 16:46:17 +0200

    Amends cluster error handling
    * Use of Angular's JSON function.
    
    * Reply fallback objects if cluster can't be reached.
    
    * Removed "Sorry folks" messages.
    
    * Using smarter error conditions.
    
    * Amended changelog.
    
    Signed-off-by: Stephan Müller <smueller@suse.com>

--- webui/app/extensions/crushmap_editor/scripts/services/clusterResource.js
+++ webui/app/extensions/crushmap_editor/scripts/services/clusterResource.js
@@ -40,7 +40,8 @@ app.factory("ClusterResource", function ($resource) {
       method: "GET",
       isArray: true,
       transformResponse: function (data) {
-        return JSON.parse(data).results;
+        var res = angular.fromJson(data);
+        return res && res.results || [];
       }
     }
   });
