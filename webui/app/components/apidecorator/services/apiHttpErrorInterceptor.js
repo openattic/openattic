openattic/openattic

commit 4722d3881aced3e0d591bccc53662a6500437652

Fixes typo in variable
* Fixed typo in variable.

Signed-off-by: Stephan Müller <smueller@suse.com>

webui/app/components/apidecorator/services/apiHttpErrorInterceptor.js
@@ -37,7 +37,7 @@ app.factory("ApiErrorDecoratorService", function ($q, $log, Notification) {
     decorate: function (error) {
       var simpleMsg;
       var notification;
-      var detalMsg = "";
+      var detailMsg = "";
 
       if (error) {
         $log.error(error);
@@ -45,9 +45,9 @@ app.factory("ApiErrorDecoratorService", function ($q, $log, Notification) {
           "[" + error.config.method + ": " + error.config.url + "] => " + error.status;
         angular.forEach(error.data, function (val, key) {
           if (key === "detail") {
-            detalMsg = val + detalMsg;
+            detailMsg = val + detailMsg;
           } else {
-            detalMsg += "<br>" + key + ": " + val;
+            detailMsg += "<br>" + key + ": " + val;
           }
         });
         notification = new Notification({

commit bdf14d3db907a6b97f5a3e7e4680b15daea5f6e2

fix regression on master
commit 4722d38 introduced a regression, by not changing all the
variables names when fixing a typo.

Signed-off-by: Tiago Melo <tspmelo@gmail.com>

webui/app/components/apidecorator/services/apiHttpErrorInterceptor.js
@@ -52,7 +52,7 @@ app.factory("ApiErrorDecoratorService", function ($q, $log, Notification) {
         });
         notification = new Notification({
             title: error.status + " - " + error.statusText,
-            msg: detalMsg || simpleMsg
+            msg: detailMsg || simpleMsg
           }, error)
           .show();
 