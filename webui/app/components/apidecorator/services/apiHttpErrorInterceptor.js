d9a173fe41ed1ca6b6b26b8b7493b037975af88f
Stephan Müller <smueller@suse.com>
2017-05-04 17:00:29 +0200

    Add an API timeout to the UI
    * Created new HTTP interceptor, which takes a default timeout value
      or the global value, if available.
    
    * Customized the error thrown by a timeout, it will show an button that routes the
      user to command logs.
    
    * Timeout toasty has no timeout. You have to click on it to disable it.
    
    * Timeout for a request is reached after 30 seconds.
    
    * Amends changelog
    
    Signed-off-by: Stephan Müller <smueller@suse.com>

--- webui/app/components/apidecorator/services/apiHttpErrorInterceptor.js
+++ webui/app/components/apidecorator/services/apiHttpErrorInterceptor.js
@@ -41,8 +41,21 @@ app.factory("ApiErrorDecoratorService", function ($q, $log, Notification) {
 
       if (error) {
         $log.error(error);
+        var notificationConfig = {
+          title: error.status === -1 ? "No API response" : error.status + " - " + error.statusText
+        };
         simpleMsg = error && error.config && error.config.method && error.config.url &&
           "[" + error.config.method + ": " + error.config.url + "] => " + error.status;
+        if (error.status === -1) {  // Rejected because of a timeout.
+          simpleMsg = [
+            "The openATTIC REST API didn't respond in time.",
+            "Please see the openattic log file for possible errors."
+          ].join("<br>");
+          angular.extend(notificationConfig, {
+            timeout: false,
+            clickToClose: true
+          });
+        }
         angular.forEach(error.data, function (val, key) {
           if (key === "detail") {
             detailMsg = val + detailMsg;
@@ -50,10 +63,8 @@ app.factory("ApiErrorDecoratorService", function ($q, $log, Notification) {
             detailMsg += "<br>" + key + ": " + val;
           }
         });
-        notification = new Notification({
-            title: error.status + " - " + error.statusText,
-            msg: detailMsg || simpleMsg
-          }, error)
+        notificationConfig.msg = detailMsg || simpleMsg;
+        notification = new Notification(notificationConfig, error)
           .show();
 
         /**
