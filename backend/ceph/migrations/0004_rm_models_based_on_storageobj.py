08c31900c0c56d386ea654ca2557817530972669
Sebastian Wagner <sebastian.wagner@suse.com>
2017-05-10 01:06:24 +0200

    OP-2159 Added Django migrations for removing StorageObject based Ceph models
    * Added Django 1.7+ migration
    * Added Django 1.6 migration
    * Added `--noinpu` to `manage.py migrate` to prevent a question regarding stale content types.
    
    Signed-off-by: Sebastian Wagner <sebastian.wagner@suse.com>

---
+++ backend/ceph/migrations/0004_rm_models_based_on_storageobj.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+from nodb.models import AlterNoDBField, JsonField
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('volumes', '0001_initial'),
+        ('ceph', '0003_allow_blanks_in_cephpool'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='cluster',
+            name='storageobject_ptr',
+        ),
+        migrations.RemoveField(
+            model_name='entity',
+            name='cluster',
+        ),
+        migrations.RemoveField(
+            model_name='image',
+            name='blockvolume_ptr',
+        ),
+        migrations.RemoveField(
+            model_name='image',
+            name='rbd_pool',
+        ),
+        migrations.RemoveField(
+            model_name='mds',
+            name='cluster',
+        ),
+        migrations.RemoveField(
+            model_name='mds',
+            name='host',
+        ),
+        migrations.RemoveField(
+            model_name='mon',
+            name='cluster',
+        ),
+        migrations.RemoveField(
+            model_name='mon',
+            name='host',
+        ),
+        migrations.AlterUniqueTogether(
+            name='osd',
+            unique_together=set([]),
+        ),
+        migrations.RemoveField(
+            model_name='osd',
+            name='cluster',
+        ),
+        migrations.RemoveField(
+            model_name='osd',
+            name='journal',
+        ),
+        migrations.RemoveField(
+            model_name='osd',
+            name='volume',
+        ),
+        migrations.AlterUniqueTogether(
+            name='pool',
+            unique_together=set([]),
+        ),
+        migrations.RemoveField(
+            model_name='pool',
+            name='cluster',
+        ),
+        migrations.RemoveField(
+            model_name='pool',
+            name='volumepool_ptr',
+        ),
+        migrations.AlterModelOptions(
+            name='crushmapversion',
+            options={'managed': True},
+        ),
+        AlterNoDBField(
+            model_name='crushmapversion',
+            name='crushmap',
+            field=JsonField(base_type=dict),
+        ),
+        migrations.AlterUniqueTogether(
+            name='crushmapversion',
+            unique_together=set([]),
+        ),
+        migrations.DeleteModel(
+            name='Entity',
+        ),
+        migrations.DeleteModel(
+            name='Image',
+        ),
+        migrations.DeleteModel(
+            name='MDS',
+        ),
+        migrations.DeleteModel(
+            name='Mon',
+        ),
+        migrations.DeleteModel(
+            name='OSD',
+        ),
+        migrations.DeleteModel(
+            name='Pool',
+        ),
+        migrations.RemoveField(
+            model_name='crushmapversion',
+            name='author',
+        ),
+        migrations.RemoveField(
+            model_name='crushmapversion',
+            name='cluster',
+        ),
+        migrations.RemoveField(
+            model_name='crushmapversion',
+            name='created_at',
+        ),
+        migrations.RemoveField(
+            model_name='crushmapversion',
+            name='edited_at',
+        ),
+        migrations.RemoveField(
+            model_name='crushmapversion',
+            name='epoch',
+        ),
+        migrations.DeleteModel(
+            name='Cluster',
+        ),
+    ]
