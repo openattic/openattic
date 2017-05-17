388ac7e8aadee82eec7b9391f65a4502bf62410e
Stephan Müller <smueller@suse.com>
2017-05-17 15:33:59 +0200

    Removes unnecessary sleeps
    * Removed unnecessary sleeps
    
    Signed-off-by: Stephan Müller <smueller@suse.com>

--- e2e/common.js
+++ e2e/common.js
@@ -19,21 +19,18 @@
       dropdown = dropdown || 0;
       element.all(by.css('.tc_menudropdown')).get(dropdown).click();
       element(by.css('.tc_deleteItem > a')).click();
-      browser.sleep(helper.configs.sleep);
       var enteredNameInput = 'input.enteredName';
       if (controllerName) {
         enteredNameInput = controllerName + '.' + enteredNameInput;
       }
       element(by.model(enteredNameInput)).sendKeys('yes');
       element(by.id('bot2-Msg1')).click();
-      browser.sleep(helper.configs.sleep);
     },
 
     search_for: function(query){
       var search = element.all(by.model('filterConfig.search')).first();
       search.clear();
       search.sendKeys(query);
-      browser.sleep(helper.configs.sleep);
     },
 
     login: function(){
@@ -41,8 +38,6 @@
       element.all(by.model('username')).sendKeys(configs.username);
       element.all(by.model('password')).sendKeys(configs.password);
       element.all(by.css('input[type="submit"]')).click();
-
-      browser.sleep(configs.sleep);
     },
 
     check_form: function(){
