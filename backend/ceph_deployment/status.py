0497fefecc21becd588c8e36c51f3771adf6b110
Ricardo Dias <rdias@suse.com>
2017-06-28 12:01:44 +0100

    Implemented ceph_deployment module status service (OP-2362)
    Signed-off-by: Ricardo Dias <rdias@suse.com>

---
+++ backend/ceph_deployment/status.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+ *   Copyright (c) 2017 SUSE LLC
+ *
+ *  openATTIC is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2.
+ *
+ *  This package is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+"""
+from module_status import check_deepsea_connection
+
+
+def status(params):
+    check_deepsea_connection()
