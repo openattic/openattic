6c992b03c9fa6c64ddfc75ab21bb0da1ccccea7a
Volker Theile <vtheile@suse.com>
2017-08-03 10:02:44 +0200

    Fix Vagrant script run-oa-ui.sh to be able to load backend/settings_local.conf.
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- vagrant/run-oa-systemd.sh
+++ vagrant/run-oa-systemd.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 . /home/vagrant/env/bin/activate
 which systemctl && sudo systemctl reload dbus || sudo service dbus reload
-sudo /home/vagrant/env/bin/python /home/vagrant/openattic/backend/manage.py runsystemd
+# Change to the directory, otherwise 'settings_local.conf' won't be loaded.
+cd /home/vagrant/openattic/backend
+sudo /home/vagrant/env/bin/python manage.py runsystemd
