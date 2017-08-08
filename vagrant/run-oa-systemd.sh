899241e07c8c05d2b60e4ba562baa52d97e645ae
Volker Theile <vtheile@suse.com>
2017-08-08 10:44:29 +0200

    Ensure that all child processes are killed. To do so, all processes with the process group ID of the helper script are killed.
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- vagrant/run-oa-systemd.sh
+++ vagrant/run-oa-systemd.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+cleanup() {
+	# Kill the child processes.
+	pkill -g $$
+}
 . /home/vagrant/env/bin/activate
 which systemctl && sudo systemctl reload dbus || sudo service dbus reload
 # Change to the directory, otherwise 'settings_local.conf' won't be loaded.
 cd /home/vagrant/openattic/backend
+# Start oA systemd.
+trap cleanup SIGINT
 sudo /home/vagrant/env/bin/python manage.py runsystemd
