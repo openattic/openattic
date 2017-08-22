5d66d7dcfb03e81f8c5139a1370b476266c0a54c
Stephan Müller <smueller@suse.com>
2017-08-22 18:57:00 +0200

    WebUI: Create pools with application tags (OP-2591)
    Luminous changed the behavior how pools are working, this breaking
    change leads to an unhealthy cluster state if any pool is not set to the
    use of an application.
    
    Signed-off-by: Stephan Müller <smueller@suse.com>

--- e2e/ceph/pools/ceph_pool_form.e2e.js
+++ e2e/ceph/pools/ceph_pool_form.e2e.js
@@ -5,6 +5,7 @@ var cephPoolCommon = require('./cephPoolCommon.js');
 
 describe('ceph pool creation form', function(){
   const cephPoolProperties = new cephPoolCommon();
+  const fe = cephPoolProperties.formElements;
 
   beforeAll(function(){
     helpers.login();
@@ -38,21 +39,24 @@ describe('ceph pool creation form', function(){
   };
 
   const showByDefaultHiddenElement = function(e){
-    if(e.displayedIf){
-      it('should display the form element "' + e.name + '" if "' + e.displayedIf + '"', function(){
-        cephPoolProperties.selectNeededSelection(e);
+    if(!e.displayedIf){
+      return;
+    }
+    e.displayedIf.forEach((displayedIf) => {
+      it('should display the form element "' + e.name + '" if "' + displayedIf + '"', function(){
+        cephPoolProperties.selectNeededSelection(displayedIf);
         expect(cephPoolProperties.getFormElement(e).isDisplayed()).toBe(true);
       });
       verifySubitems(e);
-    }
+    });
   };
 
-  for(var formElement in cephPoolProperties.formElements){
-    isFormElementAvailableOnInit(cephPoolProperties.formElements[formElement]);
+  for(var formElement in fe){
+    isFormElementAvailableOnInit(fe[formElement]);
   }
 
-  for(var formElement in cephPoolProperties.formElements){
-    showByDefaultHiddenElement(cephPoolProperties.formElements[formElement]);
+  for(var formElement in fe){
+    showByDefaultHiddenElement(fe[formElement]);
   }
 
   Object.keys(cephPoolProperties.formLabels).forEach(function(name){
@@ -63,7 +67,7 @@ describe('ceph pool creation form', function(){
   });
 
   it('should show the typed in volume name in the header', function(){
-    cephPoolProperties.formElements.name.byModel.sendKeys('protractor_test');
+    fe.name.byModel.sendKeys('protractor_test');
     var header = cephPoolProperties.formLabels.header;
     expect(header.byClass.getText()).toEqual(header.text + ' protractor_test');
   });
@@ -74,12 +78,12 @@ describe('ceph pool creation form', function(){
 
   it('should check if the submit button is disabled when the required fields are empty', function(){
     browser.refresh();
-    expect(cephPoolProperties.formElements.createButton.byClass.isEnabled()).toBe(false);
+    expect(fe.createButton.byClass.isEnabled()).toBe(false);
     browser.refresh();
   });
 
-  Object.keys(cephPoolProperties.formElements).forEach(function(name){
-    var e = cephPoolProperties.formElements[name];
+  Object.keys(fe).forEach(function(name){
+    var e = fe[name];
     if(e.type === 'select'){
       it('should offer a list of ' + name + 's', function(){
         cephPoolProperties.isListInSelectBox(e);
@@ -87,14 +91,39 @@ describe('ceph pool creation form', function(){
     }
   });
 
+  fe.selectApplication.displayedIf.forEach((displayedIf) => {
+    it('should offer a list of 4 apps with ' + displayedIf, function(){
+      const e = cephPoolProperties.getFormElement(fe.selectApplication);
+      cephPoolProperties.selectNeededSelection(displayedIf);
+      e.click();
+      var listEntries = e.all(by.css('option'));
+      expect(listEntries.count()).toBe(5); // Includes the placeholder element
+    });
+  });
+
+  fe.selectApplication.displayedIf.forEach((displayedIf) => {
+    it('should be able to create a custom app with ' + displayedIf, function(){
+      cephPoolProperties.selectNeededSelection(displayedIf);
+      let firstCase = fe.firstUsedApp.byClass;
+      expect(firstCase.isPresent()).toBe(false);
+      cephPoolProperties.addApplication(4);
+      firstCase = fe.firstUsedApp.byClass;
+      expect(firstCase.isDisplayed()).toBe(true);
+      firstCase.sendKeys('tux');
+      fe.name.byModel.click();
+      fe.firstAppDeletionBtn.byClass.click();
+      expect(firstCase.isPresent()).toBe(false);
+    });
+  });
+
   it('should have a disabled crush rule set selection for replicated pools', function(){
-    cephPoolProperties.formElements.types.byModel.sendKeys('Replicated').click();
-    expect(cephPoolProperties.formElements.crushRules.byClass.getAttribute('disabled')).toBe('true');
+    fe.types.byModel.sendKeys('Replicated').click();
+    expect(fe.crushRules.byClass.getAttribute('disabled')).toBe('true');
   });
 
   it('should have a disabled crush rule set selection for ec pools', function(){
-    cephPoolProperties.formElements.types.byModel.sendKeys('Erasure');
-    expect(cephPoolProperties.formElements.crushRules.byClass.getAttribute('disabled')).toBe('true');
+    fe.types.byModel.sendKeys('Erasure');
+    expect(fe.crushRules.byClass.getAttribute('disabled')).toBe('true');
   });
 
   afterAll(function(){
