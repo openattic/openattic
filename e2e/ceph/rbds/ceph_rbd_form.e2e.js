b30f79d727794ebddf5e33fa04641ea1a9701d93
Ricardo Marques <rimarques@suse.com>
2017-09-12 16:19:17 +0100

    RBD striping feature preview (OP-2654)
    Signed-off-by: Ricardo Marques <rimarques@suse.com>

--- e2e/ceph/rbds/ceph_rbd_form.e2e.js
+++ e2e/ceph/rbds/ceph_rbd_form.e2e.js
@@ -2,10 +2,12 @@
 
 var helpers = require('../../common.js');
 var rbdCommons = require('./cephRbdCommon.js');
+var CephRBDStripingPreview = require('./CephRBDStripingPreview.js');
 
 describe('should test the ceph rbd creation form', function(){
   var rbdProperties = new rbdCommons();
   const fe = rbdProperties.formElements;
+  var stripingPreview = new CephRBDStripingPreview();
 
   var objSizeInput = [
     {input: '0', output: '4.00 KiB'},
@@ -197,6 +199,146 @@ describe('should test the ceph rbd creation form', function(){
     expect(element(by.css('label[for=dataPool] > span[uib-tooltip]')).getAttribute('uib-tooltip'))
       .toBe('Dedicated pool that stores the object-data of the RBD');
     rbdProperties.useDataPool.click();
+  });
+
+  it('should display the RBD striping preview with partial stripe unit', function(){
+    const size = rbdProperties.size;
+    const objSize = rbdProperties.objSize;
+    const unit = rbdProperties.stripingUnit;
+    const count = rbdProperties.stripingCount;
+    helpers.changeInput(size, '33 M');
+    helpers.changeInput(objSize, '4 M');
+    helpers.changeInput(unit, '2 M');
+    helpers.changeInput(count, '3');
+
+    stripingPreview.stripingPreviewLink.click();
+
+    expect(stripingPreview.stripes.get(0).getText()).toBe('Stripe 0');
+    expect(stripingPreview.stripes.get(1).getText()).toBe('Stripe 1');
+    expect(stripingPreview.stripes.get(2).getText()).toBe('Stripe 2');
+    expect(stripingPreview.stripes.get(3).getText()).toBe('Stripe 3');
+    expect(stripingPreview.stripes.get(4).getText()).toBe('Stripe 4');
+    expect(stripingPreview.stripes.get(5).getText()).toBe('Stripe 5');
+
+    expect(stripingPreview.objectSets.get(0).getText()).toBe('Object\nset\n0');
+    expect(stripingPreview.objectSets.get(1).getText()).toBe('Object\nset\n1');
+    expect(stripingPreview.objectSets.get(2).getText()).toBe('Object\nset\n2');
+
+    expect(stripingPreview.objects.get(0).getText()).toBe('Object\n0');
+    expect(stripingPreview.objects.get(1).getText()).toBe('Object\n1');
+    expect(stripingPreview.objects.get(2).getText()).toBe('Object\n2');
+    expect(stripingPreview.objects.get(3).getText()).toBe('Object\n3');
+    expect(stripingPreview.objects.get(4).getText()).toBe('Object\n4');
+    expect(stripingPreview.objects.get(5).getText()).toBe('Object\n5');
+    expect(stripingPreview.objects.get(6).getText()).toBe('Object\n6');
+    expect(stripingPreview.objects.get(7).getText()).toBe('Object\n7');
+    expect(stripingPreview.objects.get(8).getText()).toBe('Object\n8');
+
+    expect(stripingPreview.stripUnits.get(0).getText()).toBe('Stripe unit\n0');
+    expect(stripingPreview.stripUnits.get(1).getText()).toBe('Stripe unit\n1');
+    expect(stripingPreview.stripUnits.get(2).getText()).toBe('Stripe unit\n2');
+    expect(stripingPreview.stripUnits.get(3).getText()).toBe('Stripe unit\n3');
+    expect(stripingPreview.stripUnits.get(4).getText()).toBe('Stripe unit\n4');
+    expect(stripingPreview.stripUnits.get(5).getText()).toBe('Stripe unit\n5');
+    expect(stripingPreview.stripUnits.get(6).getText()).toBe('Stripe unit\n6');
+    expect(stripingPreview.stripUnits.get(7).getText()).toBe('Stripe unit\n7');
+    expect(stripingPreview.stripUnits.get(8).getText()).toBe('Stripe unit\n8');
+    expect(stripingPreview.stripUnits.get(9).getText()).toBe('Stripe unit\n9');
+    expect(stripingPreview.stripUnits.get(10).getText()).toBe('Stripe unit\n10');
+    expect(stripingPreview.stripUnits.get(11).getText()).toBe('Stripe unit\n11');
+    expect(stripingPreview.stripUnits.get(12).getText()).toBe('Stripe unit\n12');
+    expect(stripingPreview.stripUnits.get(13).getText()).toBe('Stripe unit\n13');
+    expect(stripingPreview.stripUnits.get(14).getText()).toBe('Stripe unit\n14');
+    expect(stripingPreview.stripUnits.get(15).getText()).toBe('Stripe unit\n15');
+    expect(stripingPreview.stripUnits.get(16).getText()).toBe('Stripe unit\n(partial)\n16');
+    expect(stripingPreview.stripUnits.get(17).getText()).toBe('');
+
+    stripingPreview.close.click();
+  });
+
+  it('should display the RBD striping preview with ellipsis', function(){
+    const size = rbdProperties.size;
+    const objSize = rbdProperties.objSize;
+    const unit = rbdProperties.stripingUnit;
+    const count = rbdProperties.stripingCount;
+    helpers.changeInput(size, '256 M');
+    helpers.changeInput(objSize, '8 M');
+    helpers.changeInput(unit, '2 M');
+    helpers.changeInput(count, '6');
+
+    stripingPreview.stripingPreviewLink.click();
+
+    expect(stripingPreview.stripes.get(0).getText()).toBe('Stripe 0');
+    expect(stripingPreview.stripes.get(1).getText()).toBe('Stripe 1');
+    expect(stripingPreview.stripes.get(2).getText()).toBe('Stripe 3');
+    expect(stripingPreview.stripes.get(3).getText()).toBe('Stripe 4');
+    expect(stripingPreview.stripes.get(4).getText()).toBe('Stripe 5');
+    expect(stripingPreview.stripes.get(5).getText()).toBe('Stripe 7');
+    expect(stripingPreview.stripes.get(6).getText()).toBe('Stripe 20');
+    expect(stripingPreview.stripes.get(7).getText()).toBe('Stripe 21');
+
+    expect(stripingPreview.objectSets.get(0).getText()).toBe('Object\nset\n0');
+    expect(stripingPreview.objectSets.get(1).getText()).toBe('Object\nset\n1');
+    expect(stripingPreview.objectSets.get(2).getText()).toBe('Object\nset\n5');
+
+    expect(stripingPreview.objects.get(0).getText()).toBe('Object\n0');
+    expect(stripingPreview.objects.get(1).getText()).toBe('Object\n1');
+    expect(stripingPreview.objects.get(2).getText()).toBe('Object\n2');
+    expect(stripingPreview.objects.get(3).getText()).toBe('Object\n3');
+    expect(stripingPreview.objects.get(4).getText()).toBe('Object\n5');
+    expect(stripingPreview.objects.get(5).getText()).toBe('Object\n6');
+    expect(stripingPreview.objects.get(6).getText()).toBe('Object\n7');
+    expect(stripingPreview.objects.get(7).getText()).toBe('Object\n8');
+    expect(stripingPreview.objects.get(8).getText()).toBe('Object\n9');
+    expect(stripingPreview.objects.get(9).getText()).toBe('Object\n11');
+    expect(stripingPreview.objects.get(10).getText()).toBe('Object\n30');
+    expect(stripingPreview.objects.get(11).getText()).toBe('Object\n31');
+    expect(stripingPreview.objects.get(12).getText()).toBe('Object\n32');
+    expect(stripingPreview.objects.get(13).getText()).toBe('Object\n33');
+    expect(stripingPreview.objects.get(14).getText()).toBe('Object\n35');
+
+    expect(stripingPreview.stripUnits.get(0).getText()).toBe('Stripe unit\n0');
+    expect(stripingPreview.stripUnits.get(1).getText()).toBe('Stripe unit\n1');
+    expect(stripingPreview.stripUnits.get(2).getText()).toBe('Stripe unit\n2');
+    expect(stripingPreview.stripUnits.get(3).getText()).toBe('Stripe unit\n3');
+    expect(stripingPreview.stripUnits.get(4).getText()).toBe('Stripe unit\n5');
+    expect(stripingPreview.stripUnits.get(5).getText()).toBe('Stripe unit\n6');
+    expect(stripingPreview.stripUnits.get(6).getText()).toBe('Stripe unit\n7');
+    expect(stripingPreview.stripUnits.get(7).getText()).toBe('Stripe unit\n8');
+    expect(stripingPreview.stripUnits.get(8).getText()).toBe('Stripe unit\n9');
+    expect(stripingPreview.stripUnits.get(9).getText()).toBe('Stripe unit\n11');
+    expect(stripingPreview.stripUnits.get(10).getText()).toBe('Stripe unit\n18');
+    expect(stripingPreview.stripUnits.get(11).getText()).toBe('Stripe unit\n19');
+    expect(stripingPreview.stripUnits.get(12).getText()).toBe('Stripe unit\n20');
+    expect(stripingPreview.stripUnits.get(13).getText()).toBe('Stripe unit\n21');
+    expect(stripingPreview.stripUnits.get(14).getText()).toBe('Stripe unit\n23');
+    expect(stripingPreview.stripUnits.get(15).getText()).toBe('Stripe unit\n24');
+    expect(stripingPreview.stripUnits.get(16).getText()).toBe('Stripe unit\n25');
+    expect(stripingPreview.stripUnits.get(17).getText()).toBe('Stripe unit\n26');
+    expect(stripingPreview.stripUnits.get(18).getText()).toBe('Stripe unit\n27');
+    expect(stripingPreview.stripUnits.get(19).getText()).toBe('Stripe unit\n29');
+    expect(stripingPreview.stripUnits.get(20).getText()).toBe('Stripe unit\n30');
+    expect(stripingPreview.stripUnits.get(21).getText()).toBe('Stripe unit\n31');
+    expect(stripingPreview.stripUnits.get(22).getText()).toBe('Stripe unit\n32');
+    expect(stripingPreview.stripUnits.get(23).getText()).toBe('Stripe unit\n33');
+    expect(stripingPreview.stripUnits.get(24).getText()).toBe('Stripe unit\n35');
+    expect(stripingPreview.stripUnits.get(25).getText()).toBe('Stripe unit\n42');
+    expect(stripingPreview.stripUnits.get(26).getText()).toBe('Stripe unit\n43');
+    expect(stripingPreview.stripUnits.get(27).getText()).toBe('Stripe unit\n44');
+    expect(stripingPreview.stripUnits.get(28).getText()).toBe('Stripe unit\n45');
+    expect(stripingPreview.stripUnits.get(29).getText()).toBe('Stripe unit\n47');
+    expect(stripingPreview.stripUnits.get(30).getText()).toBe('Stripe unit\n120');
+    expect(stripingPreview.stripUnits.get(31).getText()).toBe('Stripe unit\n121');
+    expect(stripingPreview.stripUnits.get(32).getText()).toBe('Stripe unit\n122');
+    expect(stripingPreview.stripUnits.get(33).getText()).toBe('Stripe unit\n123');
+    expect(stripingPreview.stripUnits.get(34).getText()).toBe('Stripe unit\n125');
+    expect(stripingPreview.stripUnits.get(35).getText()).toBe('Stripe unit\n126');
+    expect(stripingPreview.stripUnits.get(36).getText()).toBe('Stripe unit\n127');
+    expect(stripingPreview.stripUnits.get(37).getText()).toBe('');
+    expect(stripingPreview.stripUnits.get(38).getText()).toBe('');
+    expect(stripingPreview.stripUnits.get(39).getText()).toBe('');
+
+    stripingPreview.close.click();
   });
 
   afterAll(function(){
---
+++ webui/app/components/ceph-rbd/ceph-rbd-striping-modal/ceph-rbd-striping-object-set.component.js
@@ -0,0 +1,98 @@
+/**
+ *
+ * @source: http://bitbucket.org/openattic/openattic
+ *
+ * @licstart  The following is the entire license notice for the
+ *  JavaScript code in this page.
+ *
+ * Copyright (c) 2017 SUSE LLC
+ *
+ *
+ * The JavaScript code in this page is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software
+ * Foundation; version 2.
+ *
+ * This package is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * As additional permission under GNU GPL version 2 section 3, you
+ * may distribute non-source (e.g., minimized or compacted) forms of
+ * that code without the copy of the GNU GPL normally required by
+ * section 1, provided you include this license notice and a URL
+ * through which recipients can access the Corresponding Source.
+ *
+ * @licend  The above is the entire license notice
+ * for the JavaScript code in this page.
+ *
+ */
+"use strict";
+
+class CephRbdStripingObjectSet {
+
+  constructor (SizeParserService) {
+    this.SizeParserService = SizeParserService;
+  }
+
+  $onInit () {
+    this.size = this.SizeParserService.parseInt(this.sizeStr, "b");
+    this.objectSize = this.SizeParserService.parseInt(this.objectSizeStr, "b");
+    this.stripeUnit = this.SizeParserService.parseInt(this.stripeUnitStr, "b");
+    let objectSetSize = this.objectSize * this.stripeCount;
+    let stripeSize = this.stripeUnit * this.stripeCount;
+    this.numStripesObjectSet = objectSetSize / stripeSize;
+    this.numStripUnitsObjectSet = this.stripeCount * this.numStripesObjectSet;
+    this.maxStripeUnit = Math.ceil(this.size / this.stripeUnit) - 1;
+    let remainingSize = this.size - objectSetSize * this.objectSetIndex;
+    this.numStripesCurrentObjectSet = Math.min(Math.ceil(remainingSize / stripeSize), this.numStripesObjectSet);
+    this.isLastStripeUnitPartial = this.size % this.stripeUnit !== 0;
+  }
+
+  getStripUnitText (stripeUnitNumber) {
+    if (stripeUnitNumber === this.maxStripeUnit && this.isLastStripeUnitPartial) {
+      return `<small>Stripe unit<br>(partial)</small><br>${stripeUnitNumber}`;
+    }
+    if (stripeUnitNumber <= this.maxStripeUnit) {
+      return `<small>Stripe unit</small><br>${stripeUnitNumber}`;
+    }
+    return "";
+  }
+
+  getStripUnitStyle (stripeUnitNumber, stripeNumber) {
+    let classes = [];
+    if (stripeUnitNumber === this.maxStripeUnit && this.isLastStripeUnitPartial) {
+      classes.push("warning");
+    } else if (stripeUnitNumber <= this.maxStripeUnit) {
+      classes.push("info");
+    } else {
+      classes.push("danger");
+    }
+    let isBottom = (this.objectSetIndex * this.numStripesObjectSet + this.numStripesCurrentObjectSet - 1) === stripeNumber;
+    let isTop = (this.objectSetIndex * this.numStripesObjectSet) === stripeNumber;
+    if (isBottom) {
+      classes.push("rbd-striping-cell-bottom");
+    }
+    if (isTop) {
+      classes.push("rbd-striping-cell-top");
+    }
+    if ((!isBottom && !isTop) || (isBottom && !isTop)) {
+      classes.push("rbd-striping-cell-center");
+    }
+    return classes.join(" ");
+  }
+}
+
+var app = angular.module("openattic.cephRbd");
+app.component("cephRbdStripingObjectSet", {
+  templateUrl: "components/ceph-rbd/ceph-rbd-striping-modal/ceph-rbd-striping-object-set.component.html",
+  bindings: {
+    sizeStr: "<",
+    objectSizeStr: "<",
+    stripeUnitStr: "<",
+    stripeCount: "<",
+    objectSetIndex: "<"
+  },
+  controller: CephRbdStripingObjectSet
+});
