a9f75842b5b1d26a3a5331fe92e3e591c0144762
Ricardo Marques <rimarques@suse.com>
2017-09-13 16:13:11 +0100

    Fix typo on test description
    Signed-off-by: Ricardo Marques <rimarques@suse.com>

--- e2e/ceph/rbds/ceph_rbd_form.e2e.js
+++ e2e/ceph/rbds/ceph_rbd_form.e2e.js
@@ -135,7 +135,7 @@ describe('should test the ceph rbd creation form', function(){
     expect(element(by.className(fe.stripingCount.items.min)).isDisplayed()).toBe(false);
   });
 
-  it('should show an error on size if object set size icreases the setted size', () => {
+  it('should show an error on size if object set size increase the setted size', () => {
     const size = rbdProperties.size;
     helpers.changeInput(rbdProperties.stripingUnit, '4 M');
     helpers.changeInput(rbdProperties.stripingCount, '5');
