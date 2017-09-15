6462ca3646edcd19d446a5fb7d96c7c2efda61f4
Sebastian Wagner <sebastian.wagner@suse.com>
2017-09-15 18:00:36 +0200

    Cleanup systemd module (OP-2677)
    * Removed the locking
    * Removed transactions
    * Removed systemd queueing
    * Removed special logging parameters
    * Removed `invoke`
    * Removed `service_command`
    * Import taskqueue uncondiionally
    * Removed `LockutilsTestCase`
    * Removed Settings: `SYSD_PIDFILE`, `SYSD_LOGFILE`, `SYSD_LOGLEVEL` and `SYSD_OPTIONS`.
    
    Also:
    
    * removed `cmdlog.runshell`
    
    Signed-off-by: Sebastian Wagner <sebastian.wagner@suse.com>

--- backend/systemd/tests.py
+++ backend/systemd/tests.py
@@ -12,78 +12,12 @@
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
 """
+import doctest
 
-import os
-import os.path
-import mock
+from systemd import plugins  # test imports
+from systemd import helpers
 
-from time import time
 
-from django.test import TestCase
-
-from systemd.lockutils import acquire_lock, release_lock, AlreadyLocked, Lockfile
-import systemd.helpers  # test imports
-import systemd.plugins  # test imports
-import systemd.procutils  # test imports
-
-class LockutilsTestCase(TestCase):
-
-    def setUp(self):
-        if os.path.exists("/tmp/testlock"):
-            os.unlink("/tmp/testlock")
-
-    def test_acquire_nonexistant_lock(self):
-        self.assertFalse(os.path.exists("/tmp/testlock"), msg="Lock exists beforehand")
-
-        lock = acquire_lock("/tmp/testlock")
-        self.assertTrue(os.path.exists("/tmp/testlock"),  msg="Lock has not been created")
-        self.assertTrue(isinstance(lock, tuple))
-
-        release_lock(lock)
-        self.assertFalse(os.path.exists("/tmp/testlock"), msg="Lock has not been removed")
-
-    def test_acquire_existing_lock(self):
-        if not os.path.exists("/tmp/testlock"):
-            # create an empty testlock file
-            open("/tmp/testlock", "a").close()
-
-        self.assertTrue(os.path.exists("/tmp/testlock"),  msg="Lock has not been created (we need it to exist for this test)")
-
-        lock = acquire_lock("/tmp/testlock")
-        self.assertTrue(os.path.exists("/tmp/testlock"),  msg="Lock has been removed while acquiring")
-        self.assertIsInstance(lock, tuple)
-        release_lock(lock)
-        # Existing locks are not supposed to get unlink()ed
-        self.assertTrue(os.path.exists("/tmp/testlock"),  msg="Pre-existing lock has been removed")
-
-    def test_acquire_lock_fails_when_acquired(self):
-        self.assertFalse(os.path.exists("/tmp/testlock"), msg="Lock exists beforehand")
-        lock = acquire_lock("/tmp/testlock")
-
-        start = time()
-        with mock.patch("systemd.lockutils.logging") as mock_logging:
-            with self.assertRaises(AlreadyLocked):
-                acquire_lock("/tmp/testlock", 0)
-            self.assertTrue(mock_logging.error.called,    msg="Error has not been logged")
-
-        self.assertTrue( time() - start < 0.1,            msg="Acquire timeout has not been honoured" )
-
-        release_lock(lock)
-        self.assertFalse(os.path.exists("/tmp/testlock"), msg="Lock has not been removed")
-
-    def test_lockfile_noexception(self):
-        with mock.patch("systemd.lockutils.acquire_lock") as mock_acquire_lock, \
-             mock.patch("systemd.lockutils.release_lock") as mock_release_lock:
-            with Lockfile("/tmp/testlock"):
-                pass
-            self.assertTrue(mock_acquire_lock.called,     msg="Lock has not been acquired")
-            self.assertTrue(mock_release_lock.called,     msg="Lock has not been released")
-
-    def test_lockfile_exception(self):
-        with mock.patch("systemd.lockutils.acquire_lock") as mock_acquire_lock, \
-             mock.patch("systemd.lockutils.release_lock") as mock_release_lock:
-            with self.assertRaises(Exception):
-                with Lockfile("/tmp/testlock"):
-                    raise Exception()
-            self.assertTrue(mock_acquire_lock.called,     msg="Lock has not been acquired")
-            self.assertTrue(mock_release_lock.called,     msg="Lock has not been released")
+def load_tests(loader, tests, ignore):
+    tests.addTests(doctest.DocTestSuite(plugins))
+    return tests
