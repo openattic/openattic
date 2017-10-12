81b28b2475aa86dbb7063910832cbf248be7aa13
Volker Theile <vtheile@suse.com>
2017-10-12 15:00:07 +0200

    Now it is possible to delete a RGW user with buckets (OP-2790)
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- webui/e2e/ceph/rgw/ceph_rgw_buckets.e2e.js
+++ webui/e2e/ceph/rgw/ceph_rgw_buckets.e2e.js
@@ -28,13 +28,11 @@ describe("ceph rgw buckets", function () {
     helpers.login();
     helpers.setLocation("ceph/rgw/buckets");
     helpers.deleteAllIfExists("e2e_");
+    helpers.setLocation("ceph/rgw/users");
+    helpers.deleteAllIfExists("e2e_");
   });
 
-  beforeEach(function () {
-    helpers.setLocation("ceph/rgw/buckets");
-  });
-
-  it("should create test user", function () {
+  it("should create test user 'e2e_tuxdoe'", function () {
     helpers.setLocation("ceph/rgw/users");
     cephRgwCommons.addUser();
     helpers.checkLocation("ceph/rgw/users/add");
@@ -49,6 +47,7 @@ describe("ceph rgw buckets", function () {
   });
 
   it("should check the invalid bucket name error message", function () {
+    helpers.setLocation("ceph/rgw/buckets");
     addBucket();
     element(by.model("bucket.bucket")).sendKeys("xy");
     // Wait some time until the bucket name has been validated via REST API call.
@@ -97,6 +96,7 @@ describe("ceph rgw buckets", function () {
   it("should redirect from the user list to the bucket list", function () {
     helpers.setLocation("ceph/rgw/users");
     // Mark the user row and select the 'List buckets' menu.
+    // !!! Note, the data table sort field is set to 'Owner'. !!!
     helpers.get_list_element(testBucket.owner).click();
     element(by.css(".tc_menudropdown")).click();
     element(by.css(".tc_listBucketsItem > a")).click();
@@ -108,14 +108,32 @@ describe("ceph rgw buckets", function () {
   });
 
   it("should delete the test bucket", function () {
-    element(by.cssContainingText("tr", testBucket.name)).click();
-    helpers.delete_selection(0, "$ctrl");
+    let bucket = helpers.get_list_element(testBucket.name);
+    bucket.click();
+    helpers.delete_selection(undefined, "$ctrl");
+    expect(bucket.isPresent()).toBe(false);
   });
 
-  it("should delete the test user", function () {
+  it("should create test bucket (owner=e2e_tuxdoe)", function () {
+    addBucket();
+    element(by.model("bucket.bucket")).sendKeys(testBucket.name);
+    element(by.model("bucket.owner")).sendKeys(testUser.user_id);
+    cephRgwCommons.submitBtn.click();
+  });
+
+  it("should delete the test user 'e2e_tuxdoe'", function () {
+    // Note, the previously created bucket 'e2e_bucket' should be deleted, too.
     helpers.setLocation("ceph/rgw/users");
-    element(by.cssContainingText("tr", testUser.user_id)).click();
-    helpers.delete_selection(0, "$ctrl");
+    let user = helpers.get_list_element(testUser.user_id);
+    user.click();
+    helpers.delete_selection(undefined, "$ctrl");
+    expect(user.isPresent()).toBe(false);
+  });
+
+  it("should not display the bucket after deleting the owner user", function () {
+    helpers.setLocation("ceph/rgw/buckets");
+    let bucket = helpers.search_for_element(testBucket.name);
+    expect(bucket.isPresent()).toBe(false);
   });
 
   afterAll(function () {
