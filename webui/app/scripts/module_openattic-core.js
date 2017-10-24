a41ae46dfd8d1d9941770adfd24d7802f5318f3a
Tiago Melo <tspmelo@gmail.com>
2017-10-24 14:58:12 +0100

    Migration of User Info module into Angular (OP-2870)
    Signed-off-by: Tiago Melo <tmelo@suse.com>

--- webui/app/scripts/module_openattic-core.js
+++ webui/app/scripts/module_openattic-core.js
@@ -40,7 +40,7 @@ import "../components/notification/notification.module";
 import "../components/settings/settings.module";
 import "../components/shared/shared.module";
 import "../scripts/services/sizeParserService";
-import "../components/userinfo/module";
+import "../components/userinfo/userinfo.module";
 import "../components/taskQueue/module";
 import "../components/users/module";
 import "../components/feedback/feedback.module";
