b8166fb366d1bebd9b02a908d1c03ba513aadcd3
Stephan Müller <smueller@suse.com>
2019-09-02 16:55:00 +0200

    Backend: Create pools with rule (OP-3200)
    The problem was that the pools were first created without a rule, after
    creation the pool rule was changed. This wasn't a problem until
    a rule with the same name as the created pool was automatically created
    if a erasure coded profile was submitted.
    This cli convenience caused this bug, as it created rules with ec pool
    names on every creation of an ec pool that didn't used the 'default'
    ec profile.
    
    Signed-off-by: Stephan Müller <smueller@suse.com>

--- webui/e2e/ceph/pools/ceph_pool_creation.e2e.js
+++ webui/e2e/ceph/pools/ceph_pool_creation.e2e.js
@@ -32,9 +32,11 @@
 
 const helpers = require("../../common.js");
 const CephPoolCommon = require("./cephPoolCommon.js");
+const EcProfilePage = require("./ec_profile/ec_profile.page.js");
 
 describe("ceph pool creation", () => {
   const cephPoolProperties = new CephPoolCommon();
+  const ecHelper = new EcProfilePage();
 
   beforeAll(() => {
     helpers.login();
@@ -42,34 +44,80 @@ describe("ceph pool creation", () => {
     helpers.deleteAllIfExists("e2e_");
   });
 
-  it("should create replicated pool", () => {
-    cephPoolProperties.addButton.click();
-    cephPoolProperties.createPool("e2e_replicated_with_8_pgs", "replicated", 8, false, "cephfs", "replicated_rule");
+  describe("Replicated pool", () => {
+    it("should create replicated pool", () => {
+      cephPoolProperties.addButton.click();
+      cephPoolProperties.createPool("e2e_replicated_with_8_pgs", "replicated", 8, false, "cephfs", "replicated_rule");
+    });
+
+    it("should delete replicated pool", () => {
+      cephPoolProperties.deletePool("e2e_replicated_with_8_pgs");
+    });
   });
 
-  it("should delete replicated pool", () => {
-    cephPoolProperties.deletePool("e2e_replicated_with_8_pgs");
+  describe("Erasure code pool with overwrite", () => {
+    it("should create erasure code pool with overwrite enabled", () => {
+      cephPoolProperties.addButton.click();
+      cephPoolProperties.fillForm("e2e_erasure_overwrite", "erasure", 32, false, "cephfs", null, "default");
+      cephPoolProperties.checkCheckboxToBe(element(by.model("$ctrl.data.flags.ec_overwrites")));
+      cephPoolProperties.submitForm("e2e_erasure_overwrite", "erasure", 32);
+      expect(element(by.className("tc-flag-ec_overwrites")).isDisplayed()).toBe(true);
+    });
+
+    it("should delete the created erasure code pool with overwrite enabled", () => {
+      cephPoolProperties.deletePool("e2e_erasure_overwrite");
+    });
   });
 
-  it("should create erasure code pool with overwrite enabled", () => {
-    cephPoolProperties.addButton.click();
-    cephPoolProperties.fillForm("e2e_erasure_overwrite", "erasure", 32, false, "cephfs", null, "default");
-    cephPoolProperties.checkCheckboxToBe(element(by.model("$ctrl.data.flags.ec_overwrites")));
-    cephPoolProperties.submitForm("e2e_erasure_overwrite", "erasure", 32);
-    expect(element(by.className("tc-flag-ec_overwrites")).isDisplayed()).toBe(true);
+  describe("Erasure code pool with profile", () => {
+    const pool = "e2e_ec_profiled_pool";
+    const profile = "e2e_ec_profile_selection_tests";
+    const fe = cephPoolProperties.formElements;
+
+    it("should create erasure code pool with profile", () => {
+      cephPoolProperties.addButton.click();
+      cephPoolProperties.fillForm(pool, "erasure", 4, false, "rgw", null, "default");
+
+      // Create profile
+      fe.erasureProfiles.items.addProfile.click();
+      helpers.changeInput(ecHelper.name, profile);
+      helpers.changeInput(ecHelper.k, "2");
+      helpers.changeInput(ecHelper.m, "1");
+      ecHelper.submitBtn.click();
+
+      cephPoolProperties.submitForm(pool, "erasure", 4);
+    });
+
+    it("should delete pool", () => {
+      cephPoolProperties.deletePool(pool);
+    });
+
+    it("should delete the profile", () => {
+      cephPoolProperties.addButton.click();
+      ecHelper.deleteProfile(profile);
+      helpers.leaveForm(true);
+    });
+
+    it("should not have created a new rule during creation", () => {
+      cephPoolProperties.addButton.click();
+      helpers.selectOption(cephPoolProperties.getFormElement(fe.types), "Erasure");
+      expect(cephPoolProperties.getFormElement(fe.crushRules)
+        .all(by.cssContainingText("option", pool)).isPresent()).toBe(false);
+      helpers.leaveForm(true);
+    });
   });
 
-  it("should delete the created erasure code pool with overwrite enabled", () => {
-    cephPoolProperties.deletePool("e2e_erasure_overwrite");
-  });
+  describe("Pool with compression", () => {
+    it("should create erasure code pool with compression", () => {
+      cephPoolProperties.addButton.click();
+      cephPoolProperties.createPool(
+        "e2e_erasure_compression", "erasure", 16, true, "cephfs", "erasure-code", "default"
+      );
+    });
 
-  it("should create erasure code pool with compression", () => {
-    cephPoolProperties.addButton.click();
-    cephPoolProperties.createPool("e2e_erasure_compression", "erasure", 16, true, "cephfs", "erasure-code", "default");
-  });
-
-  it("should delete the created erasure code pool with compression", () => {
-    cephPoolProperties.deletePool("e2e_erasure_compression");
+    it("should delete the created erasure code pool with compression", () => {
+      cephPoolProperties.deletePool("e2e_erasure_compression");
+    });
   });
 
   afterAll(() => {
--- webui/e2e/ceph/pools/ec_profile/ec_profile.page.js
+++ webui/e2e/ceph/pools/ec_profile/ec_profile.page.js
@@ -30,14 +30,37 @@
  */
 "use strict";
 
+const helpers = require("../../../common.js");
+const CephPoolCommon = require("./../cephPoolCommon.js");
+
 class EcProfilePage {
   constructor () {
+    this.poolHelper = new CephPoolCommon();
+    this.fe = this.poolHelper.formElements.erasureProfiles;
+    this.selection = this.fe.byClass;
+    this.buttons = this.fe.items;
+
     this.name = element(by.model("$ctrl.erasureCodeProfile.name"));
     this.k = element(by.model("$ctrl.erasureCodeProfile.k"));
     this.m = element(by.model("$ctrl.erasureCodeProfile.m"));
     this.rulesetDomain = element(by.model("$ctrl.erasureCodeProfile.ruleset_failure_domain"));
     this.submitBtn = element(by.id("bot2-Msg1"));
     this.cancelBtn = element(by.id("bot1-Msg1"));
+  }
+
+  deleteProfile (name, expectation) {
+    helpers.selectOption(this.poolHelper.formElements.types.byModel, "Erasure");
+    helpers.selectOption(this.selection, name);
+    helpers.getOptionText(this.selection).then(text => {
+      if (expectation !== undefined) {
+        expect(text === name).toBe(expectation);
+      }
+      if (text !== name) {
+        return;
+      }
+      this.buttons.deleteProfile.click();
+      this.submitBtn.click();
+    });
   }
 }
 
